1304e336adbbe3618ed5062af02427eb5593d876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 14:03:27 +0000

    Add package pypipr/1.0.119/pypipr-1.0.119-py3-none-any.whl

---
+++ packages/pypipr/pypipr-1.0.119-py3-none-any.whl/pypipr/poetry_publish.py
@@ -0,0 +1,19 @@
+from .console_run import console_run
+
+
+def poetry_publish(token=None):
+    """
+    Publish project to pypi,org
+
+    ```py
+    poetry_publish()
+    ```
+    """
+    if token:
+        console_run("update token", f"poetry config pypi-token.pypi {token}")
+    console_run("Build", "poetry build")
+    console_run("Publish to PyPi.org", "poetry publish")
+
+
+if __name__ == "__main__":
+    poetry_publish()

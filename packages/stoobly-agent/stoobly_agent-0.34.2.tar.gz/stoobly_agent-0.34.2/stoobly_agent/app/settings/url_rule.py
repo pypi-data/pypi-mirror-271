1ebbc7912163b991a1498e67737204eb7ab6a86c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 14:03:31 +0000

    Add package stoobly-agent/0.34.2/stoobly_agent-0.34.2.tar.gz

---
+++ packages/stoobly-agent/stoobly_agent-0.34.2.tar.gz/stoobly_agent-0.34.2/stoobly_agent/app/settings/url_rule.py
@@ -0,0 +1,39 @@
+from typing import List
+
+from .types.proxy_settings import UrlRule as IUrlRule
+
+class UrlRule:
+
+  def __init__(self, url_rule: IUrlRule):
+    self.update(url_rule)
+
+  @property
+  def hostname(self):
+    return self.__hostname
+
+  @property 
+  def modes(self):
+    return self.__modes
+
+  @property
+  def port(self):
+    return self.__port
+
+  @property
+  def scheme(self):
+    return self.__scheme
+
+  def update(self, url_rule: IUrlRule):
+    self.__url_rule = url_rule
+    self.__hostname = self.__url_rule.get('hostname')
+    self.__modes = self.__url_rule.get('modes')
+    self.__port = self.__url_rule.get('port')
+    self.__scheme = self.__url_rule.get('scheme')
+
+  def to_dict(self):
+    return {
+      'hostname': self.__hostname,
+      'modes': self.__modes,
+      'port': self.__port,
+      'scheme': self.__scheme,
+    }

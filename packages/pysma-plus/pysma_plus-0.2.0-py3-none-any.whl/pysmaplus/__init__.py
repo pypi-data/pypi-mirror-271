406d79e82ab4a8f9b6a3d4e2a17fdad84399c01a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 15:06:41 +0000

    Add package pysma-plus/0.2.0/pysma_plus-0.2.0-py3-none-any.whl

---
+++ packages/pysma-plus/pysma_plus-0.2.0-py3-none-any.whl/pysmaplus/__init__.py
@@ -0,0 +1,37 @@
+"""SMA WebConnect library for Python.
+
+See: http://www.sma.de/en/products/monitoring-control/webconnect.html
+
+Source: http://www.github.com/kellerza/pysma
+"""
+import logging
+from typing import Optional
+
+from aiohttp import ClientSession
+from .device_webconnect import SMAwebconnect
+from .device_ennexos import SMAennexos
+from .device_em import SMAspeedwireEM
+from .device import Device
+_LOGGER = logging.getLogger(__name__)
+
+
+# Backward compatibility
+def SMA(session, url, password, group):
+    return SMAwebconnect(session, url, password=password, group=group)
+
+
+def getDevice(session: ClientSession,
+        url: str,
+        password: Optional[str] = None,
+        groupuser: str = "user",
+        accessmethod: str = "webconnect"
+    ) -> Device:
+        _LOGGER.debug(f"Device Called! Url: {url} User/Group: {groupuser} Accessmethod: {accessmethod}")
+        if (accessmethod == "webconnect"):
+            return SMAwebconnect(session, url, password=password, group=groupuser)
+        elif (accessmethod == "ennexos"):
+            return SMAennexos(session, url, password=password, group=groupuser)
+        elif (accessmethod == "speedwire"):
+              return SMAspeedwireEM()
+        else:
+             return None

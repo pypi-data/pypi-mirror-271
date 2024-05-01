efbde2e64aecd93fe708994781d112a43b3a6f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 15:06:26 +0000

    Add package upowwalletpy/1.0.2/upowwalletpy-1.0.2.tar.gz

---
+++ packages/upowwalletpy/upowwalletpy-1.0.2.tar.gz/upowwalletpy-1.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="upowwalletpy",
+    version="1.0.2",
+    author="upow",
+    author_email="contact@upow.ai",
+    description="A package for UPOW wallet transactions",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    py_modules=["upowwalletpy"],
+    install_requires=[
+        "asttokens==2.4.1",
+        "base58==0.2.5",
+        "certifi==2023.11.17",
+        "charset-normalizer==2.0.12",
+        "colorama==0.4.6",
+        "executing==2.0.1",
+        "fastecdsa==2.3.0",
+        "icecream==2.1.3",
+        "idna==3.6",
+        "pygments==2.17.2",
+        "requests==2.26.0",
+        "six==1.16.0",
+        "urllib3==1.26.18",
+        "pickleDB~=0.9.2",
+    ],
+    python_requires=">=3.11",
+)

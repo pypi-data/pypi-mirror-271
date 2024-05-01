357c69680e01eff306f2c34c1be2993d376176d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 15:06:18 +0000

    Add package nehushtan/0.4.31/nehushtan-0.4.31.tar.gz

---
+++ packages/nehushtan/nehushtan-0.4.31.tar.gz/nehushtan-0.4.31/tests/config.py
@@ -0,0 +1,23 @@
+test_log_store = '/Users/leqee/code/nehushtan/log'
+
+MYSQL_CONFIG = {
+    'host': '172.16.1.52',
+    'port': 3306,
+    'user': 'ljni',
+    'password': 'riIOTtIdBbT1',
+    'db': 'sinri',
+    'charset': 'utf8',
+    'auto_commit': True,
+}
+
+IMAP_CONFIG = {
+    # ...
+}
+
+SMTP_CONFIG = {
+    # ...
+}
+
+TWITTER_CONFIG = {
+    'bearer_token': 'AAAAAAAAAAAAAAAAAAAAAG87TAEAAAAAwj3DZhZ1qyDkEcxXchaIcV2IcUQ%3DcWw6QomnYWbxOnj9zZDXnOXEKYKJrB5r7UFElATQEh9n2BluDG',
+}

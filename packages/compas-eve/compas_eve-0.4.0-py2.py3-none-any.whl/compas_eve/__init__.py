f79e15c7642c4f12fc066012470beb28fdb4917e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 15:06:24 +0000

    Add package compas-eve/0.4.0/compas_eve-0.4.0-py2.py3-none-any.whl

---
+++ packages/compas-eve/compas_eve-0.4.0-py2.py3-none-any.whl/compas_eve/__init__.py
@@ -0,0 +1,58 @@
+"""
+********************************************************************************
+compas_eve
+********************************************************************************
+
+.. currentmodule:: compas_eve
+
+
+Classes
+=======
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    Message
+    Topic
+    Publisher
+    Subscriber
+    Transport
+    InMemoryTransport
+    get_default_transport
+    set_default_transport
+
+"""
+
+from __future__ import print_function
+
+import os
+
+
+__author__ = ["Gonzalo Casas"]
+__copyright__ = "Gramazio Kohler Research"
+__license__ = "MIT License"
+__email__ = "casas@arch.ethz.ch"
+__version__ = "0.4.0"
+
+from .event_emitter import EventEmitterMixin  # noqa: F401 needed here to avoid circular import on py2.7
+from .core import Message, Publisher, Subscriber, Transport, Topic, get_default_transport, set_default_transport
+from .memory import InMemoryTransport
+
+HERE = os.path.dirname(__file__)
+HOME = os.path.abspath(os.path.join(HERE, "../../"))
+
+set_default_transport(InMemoryTransport())
+
+__all__ = [
+    "HOME",
+    "Message",
+    "Publisher",
+    "Subscriber",
+    "Topic",
+    "Transport",
+    "get_default_transport",
+    "set_default_transport",
+    "InMemoryTransport",
+]
+__all_plugins__ = ["compas_eve.rhino.install"]

92b4ee1e13897ce9e7aaf4c800bb0ce66632a3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 14:03:54 +0000

    Add package economic-complexity/0.2.4/economic_complexity-0.2.4.tar.gz

---
+++ packages/economic-complexity/economic_complexity-0.2.4.tar.gz/economic_complexity-0.2.4/economic_complexity/__init__.py
@@ -0,0 +1,38 @@
+"""Economic Complexity
+
+This module contains functions to ease the calculation of Economic Complexity values.
+"""
+
+from .complexity import complexity
+from .cross_space import cross_proximity, cross_relatedness
+from .product_space import (
+    distance,
+    opportunity_gain,
+    peii,
+    pgi,
+    proximity,
+    relatedness,
+    relative_relatedness,
+    similarity,
+)
+from .rca import rca
+from .subnational import complexity_subnational
+
+__version_info__ = ("0", "2", "4")
+__version__ = ".".join(__version_info__)
+
+__all__ = (
+    "rca",
+    "complexity",
+    "distance",
+    "opportunity_gain",
+    "proximity",
+    "relatedness",
+    "cross_proximity",
+    "cross_relatedness",
+    "similarity",
+    "pgi",
+    "peii",
+    "complexity_subnational",
+    "relative_relatedness",
+)

3ede71be99d127abedd01a363d556dcebc857f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 14:03:30 +0000

    Add package limelightlib-python/0.9.6/limelightlib_python-0.9.6.tar.gz

---
+++ packages/limelightlib-python/limelightlib_python-0.9.6.tar.gz/limelightlib_python-0.9.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+setup(
+name='limelightlib-python',
+version='0.9.6',
+url='https://limelightvision.io',
+author='Brandon Hjelstrom',
+author_email='brandon@limelightvision.io',
+description='Built to interface with any Limelight Smart Camera',
+long_description=long_description,
+long_description_content_type="text/markdown",
+py_modules=["limelight", "limelightresults"],
+package_dir={"": "limelightlib-python"},
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+install_requires=[
+    'websocket-client>=1.5',
+    'requests',
+    'ifaddr',
+],
+python_requires='>=3.6',
+)

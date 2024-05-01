d7a39dd7ebb40d775e7741b23147c8eca1583883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 14:03:28 +0000

    Add package frontengine-dev/0.0.62/frontengine_dev-0.0.62-py3-none-any.whl

---
+++ packages/frontengine-dev/frontengine_dev-0.0.62-py3-none-any.whl/frontengine/utils/critical_exit/critical_exit.py
@@ -0,0 +1,20 @@
+import _thread
+import sys
+
+from PySide6.QtWidgets import QApplication
+from je_auto_control import CriticalExit
+from je_auto_control.wrapper.platform_wrapper import keyboard_check
+
+
+class FrontEngineCriticalExit(CriticalExit):
+
+    def run(self) -> None:
+        """
+        listener keycode _exit_check_key to interrupt
+        """
+        try:
+            while True:
+                if keyboard_check.check_key_is_press(self._exit_check_key):
+                    QApplication.exit(0)
+        except Exception as error:
+            print(repr(error), file=sys.stderr)

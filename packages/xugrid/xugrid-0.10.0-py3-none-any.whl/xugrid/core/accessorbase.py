98c66e77d9b7cb1d5736d93ead42363d933211ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 15:06:07 +0000

    Add package xugrid/0.10.0/xugrid-0.10.0-py3-none-any.whl

---
+++ packages/xugrid/xugrid-0.10.0-py3-none-any.whl/xugrid/core/accessorbase.py
@@ -0,0 +1,174 @@
+import abc
+from typing import Tuple, Union
+
+import numpy as np
+import xarray as xr
+
+import xugrid
+
+
+class AbstractUgridAccessor(abc.ABC):
+    @abc.abstractmethod
+    def to_dataset():
+        pass
+
+    @abc.abstractmethod
+    def assign_node_coords():
+        pass
+
+    @abc.abstractmethod
+    def set_node_coords():
+        pass
+
+    @abc.abstractproperty
+    def crs():
+        pass
+
+    @abc.abstractmethod
+    def set_crs():
+        pass
+
+    @abc.abstractmethod
+    def to_crs():
+        pass
+
+    @abc.abstractmethod
+    def sel():
+        pass
+
+    @abc.abstractmethod
+    def sel_points():
+        pass
+
+    @abc.abstractmethod
+    def intersect_line():
+        pass
+
+    @abc.abstractmethod
+    def intersect_linestring():
+        pass
+
+    @abc.abstractproperty
+    def bounds():
+        pass
+
+    @abc.abstractproperty
+    def total_bounds():
+        pass
+
+    @abc.abstractproperty
+    def name():
+        pass
+
+    @abc.abstractproperty
+    def names():
+        pass
+
+    @abc.abstractproperty
+    def topology():
+        pass
+
+    @staticmethod
+    def _raster_xy(bounds: Tuple[float, float, float, float], resolution: float):
+        xmin, ymin, xmax, ymax = bounds
+        d = abs(resolution)
+        xmin = np.floor(xmin / d) * d
+        xmax = np.ceil(xmax / d) * d
+        ymin = np.floor(ymin / d) * d
+        ymax = np.ceil(ymax / d) * d
+        x = np.arange(xmin + 0.5 * d, xmax, d)
+        y = np.arange(ymax - 0.5 * d, ymin, -d)
+        return x, y
+
+    def _raster(self, x, y, index) -> xr.DataArray:
+        index = index.ravel()
+        indexer = xr.DataArray(
+            data=index.reshape(y.size, x.size),
+            coords={"y": y, "x": x},
+            dims=["y", "x"],
+        )
+        out = self.obj.isel({self.grid.face_dimension: indexer}).where(indexer != -1)
+        return out
+
+    def clip_box(
+        self,
+        xmin: float,
+        ymin: float,
+        xmax: float,
+        ymax: float,
+    ):
+        """
+        Clip the DataArray or Dataset by a bounding box.
+
+        Parameters
+        ----------
+        xmin: float
+        ymin: float
+        xmax: float
+        ymax: float
+
+        -------
+        clipped:
+            xugrid.UgridDataArray or xugrid.UgridDataset
+        """
+        return self.sel(x=slice(xmin, xmax), y=slice(ymin, ymax))
+
+    def partition_by_label(
+        self, labels: np.ndarray
+    ) -> Union["xugrid.UgridDataArray", "xugrid.UgridDataset"]:
+        """
+        Partition a grid by labels.
+
+        Parameters
+        ----------
+        labels: np.ndarray of integers labeling each face.
+
+        Returns
+        -------
+        partitioned: list of partitions
+        """
+        from xugrid.ugrid import partitioning
+
+        return partitioning.partition_by_label(self.grid, self.obj, labels)
+
+    def partition(
+        self, n_part: int
+    ) -> Union["xugrid.UgridDataArray", "xugrid.UgridDataset"]:
+        """
+        Partition a grid into a given number of parts.
+
+        Parameters
+        ----------
+        n_part: integer
+            The number of parts to partition the mesh.
+
+        Returns
+        -------
+        partitioned: list of partitions
+        """
+        labels = self.grid.label_partitions(n_part)
+        return self.partition_by_label(labels)
+
+    def to_netcdf(self, *args, **kwargs):
+        """
+        Write dataset contents to a UGRID compliant netCDF file.
+
+        This function wraps :py:meth:`xr.Dataset.to_netcdf`; it adds the UGRID
+        variables and coordinates to a standard xarray Dataset, then writes the
+        result to a netCDF.
+
+        All arguments are forwarded to :py:meth:`xr.Dataset.to_netcdf`.
+        """
+        self.to_dataset().to_netcdf(*args, **kwargs)
+
+    def to_zarr(self, *args, **kwargs):
+        """
+        Write dataset contents to a UGRID compliant Zarr file.
+
+        This function wraps :py:meth:`xr.Dataset.to_zarr`; it adds the UGRID
+        variables and coordinates to a standard xarray Dataset, then writes the
+        result to a Zarr file.
+
+        All arguments are forwarded to :py:meth:`xr.Dataset.to_zarr`.
+        """
+        self.to_dataset().to_zarr(*args, **kwargs)

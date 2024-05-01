405cd8a5f94fcc3b3fe0037ab2fe4642ee35bf82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 15:06:55 +0000

    Add package fake-bpy-module/20240501/fake_bpy_module-20240501.tar.gz

---
+++ packages/fake-bpy-module/fake_bpy_module-20240501.tar.gz/fake_bpy_module-20240501/gpu_extras/__init__.pyi
@@ -0,0 +1,20 @@
+"""
+gpu_extras.batch.rst
+gpu_extras.presets.rst
+
+:maxdepth: 1
+:caption: Submodules
+
+gpu_extras.batch.rst
+gpu_extras.presets.rst
+
+:maxdepth: 1
+:caption: Submodules
+
+"""
+
+import typing
+from . import batch
+from . import presets
+
+GenericType = typing.TypeVar("GenericType")

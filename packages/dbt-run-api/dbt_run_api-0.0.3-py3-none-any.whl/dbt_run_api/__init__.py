a0e58b69afd61add1235ade38b0f44d9c5f1dbc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 15:06:16 +0000

    Add package dbt-run-api/0.0.3/dbt_run_api-0.0.3-py3-none-any.whl

---
+++ packages/dbt-run-api/dbt_run_api-0.0.3-py3-none-any.whl/dbt_run_api/__init__.py
@@ -0,0 +1,55 @@
+import uvicorn
+import os
+import yaml
+from fastapi import BackgroundTasks, FastAPI
+from dbt.cli.main import dbtRunner
+from pydantic import BaseModel
+import multiprocessing
+import subprocess
+
+app = FastAPI()
+
+def compile_command(run_config):
+    cmd_list = ["dbt"]
+    cmd_list.append(run_config.pop("cmd"))
+    if "vars" in run_config:
+        vars_yaml = yaml.dump(run_config.pop("vars"), default_flow_style=None) 
+        cmd_list.extend(["--vars", f"{vars_yaml.strip()}"])
+    for key, value in run_config.items():
+        cmd_list.append(f"--{key}")
+        if value != True:
+            cmd_list.append(str(value))
+    return cmd_list
+
+
+def dbt_task(cmd_list, return_dict):
+    try:
+        res = subprocess.run(cmd_list, check=True)
+        return_dict["result"] = "Success"
+    except subprocess.CalledProcessError as e:
+        return_dict["result"] = f"Error: {e}"        
+
+
+def run_dbt(run_config):
+    is_async  = run_config.pop("async", False)
+    cmd = compile_command(run_config)
+    print(" ".join(cmd))
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
+    proc = multiprocessing.Process(
+        target=dbt_task,
+        args=(cmd,return_dict))
+    proc.start()
+    if not is_async:
+       print(proc.join())
+    return {'pid': proc.pid, "res": return_dict["result"]}
+
+
+@app.get('/callback')
+def root():
+    return {'message': 'This endpoint works!'}
+
+
+@app.post("/dbt")
+async def run_dbt_command_endpoint(run_config: dict, background_tasks: BackgroundTasks):
+    return run_dbt(run_config)

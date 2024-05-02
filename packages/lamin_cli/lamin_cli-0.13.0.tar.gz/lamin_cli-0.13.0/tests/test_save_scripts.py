cb24ee80e51759e7f73b1631fcfe0bff56dd2621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 05:03:35 +0000

    Add package lamin_cli/0.13.0/lamin_cli-0.13.0.tar.gz

---
+++ packages/lamin_cli/lamin_cli-0.13.0.tar.gz/lamin_cli-0.13.0/tests/test_save_scripts.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+import subprocess
+import os
+import lamindb as ln
+from lamindb_setup import settings
+
+
+scripts_dir = Path(__file__).parent.resolve() / "scripts"
+
+
+def test_run_save_cache():
+    env = os.environ
+    env["LAMIN_TESTING"] = "true"
+
+    filepath = scripts_dir / "initialized.py"
+    # attempt to save the script without it yet being run
+    # lamin save sub/lamin-cli/tests/scripts/initialized.py
+    result = subprocess.run(
+        f"lamin save {filepath}",
+        shell=True,
+        capture_output=True,
+    )
+    print(result.stderr.decode())
+    assert result.returncode == 1
+    assert "Did you run ln.track()" in result.stdout.decode()
+
+    # run the script
+    result = subprocess.run(
+        f"python {filepath}",
+        shell=True,
+        capture_output=True,
+    )
+    print(result.stdout.decode())
+    print(result.stderr.decode())
+    assert result.returncode == 0
+    assert "saved: Transform" in result.stdout.decode()
+    assert "saved: Run" in result.stdout.decode()
+
+    transform = ln.Transform.get("m5uCHTTpJnjQ")
+    assert transform.source_code.hash == "-QN2dVdC8T3xWG8vBl-wew"
+    assert transform.latest_run.environment.path.exists()
+    assert transform.source_code.path.exists()
+
+    # you can rerun the same script
+    result = subprocess.run(
+        f"python {filepath}",
+        shell=True,
+        capture_output=True,
+        env=env,
+    )
+    print(result.stdout.decode())
+    print(result.stderr.decode())
+    assert result.returncode == 0
+
+    # try to get the the source code via command line
+    result = subprocess.run(
+        "lamin get"
+        f" https://lamin.ai/{settings.user.handle}/laminci-unit-tests/transform/m5uCHTTpJnjQ5zKv",  # noqa
+        shell=True,
+        capture_output=True,
+    )
+    print(result.stderr.decode())
+    assert result.returncode == 0

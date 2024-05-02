aba5c80083401d3f1b31688fb42233c2cd57dc91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 15:03:56 +0000

    Add package huanzhi-utils/0.1.0/huanzhi_utils-0.1.0-py3-none-any.whl

---
+++ packages/huanzhi-utils/huanzhi_utils-0.1.0-py3-none-any.whl/huanzhi_utils/gorilla.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+def load_file(file_path:str):
+    result = []
+    with open(file_path) as f:
+        file = f.readlines()
+        for line in file:
+            result.append(json.loads(line))
+    return result
+
+
+def write_list_of_dicts_to_file(filename: str, data: list, subdir=None, appendMode=False):
+    if subdir:
+        # Ensure the subdirectory exists
+        os.makedirs(subdir, exist_ok=True)
+
+        # Construct the full path to the file
+        filename = os.path.join(subdir, filename)
+
+    # Write the list of dictionaries to the file in JSON format
+    with open(filename, "a" if appendMode else "w") as f:
+        for i, entry in enumerate(data):
+            json_str = json.dumps(entry)
+            f.write(json_str)
+            if i < len(data) - 1:
+                f.write("\n")

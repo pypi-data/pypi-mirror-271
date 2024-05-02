55075d1a08e4e62403445bbe71978f3cabd840eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 16:04:11 +0000

    Add package gekkota/0.7.0/gekkota-0.7.0-py3-none-any.whl

---
+++ packages/gekkota/gekkota-0.7.0-py3-none-any.whl/gekkota/assignment.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+from typing import Sequence, Union
+
+from .utils import Utils
+from .constants import Config, StrGen
+from .core import Statement
+from .values import GetAttr, Indexing, Identifier
+
+
+AugAssignmentTarget = Union[
+    Identifier,
+    GetAttr["AugAssignmentTarget"],
+    Indexing["AugAssignmentTarget"],
+]
+
+AssignmentTarget = Union[
+    Identifier,
+    "SequenceExpr[AssignmentTarget]",
+    "StarArg[AssignmentTarget]",
+    Indexing["AssignmentTarget"],
+    GetAttr["AssignmentTarget"],
+]
+
+
+class Assignment(Statement):
+    def __init__(
+        self,
+        targets: Sequence[AssignmentTarget] | AnnotatedTarget,
+        value: Expression,
+    ):
+        self.targets = targets
+        self.value = value
+
+    def render(self, config: Config) -> StrGen:
+        if isinstance(self.targets, AnnotatedTarget):
+            yield from self.targets.render(config)
+        else:
+            yield from Utils.separated(" = ", self.targets, config)
+        yield " "
+        yield "="
+        yield " "
+        yield from self.value.render(config)
+
+
+class AnnotatedTarget(Statement):
+    def __init__(self, target: AssignmentTarget, annotation: Expression):
+        self.target = target
+        self.annotation = annotation
+
+    def render(self, config: Config) -> StrGen:
+        yield from self.target.render(config)
+        yield ": "
+        yield from self.annotation.render(config)
+
+
+class AugmentedAssignment(Statement):
+    def __init__(self, target: AugAssignmentTarget, op: str, expression: Expression):
+        self.target = target
+        self.op = op
+        self.expression = expression
+
+    def render(self, config: Config) -> StrGen:
+        yield from self.target.render(config)
+        yield " "
+        yield self.op
+        yield " "
+        yield from self.expression.render(config)
+
+
+from .sequences import SequenceExpr
+from .args import StarArg
+from .expression import Expression
---
+++ packages/gekkota/gekkota-0.7.0-py3-none-any.whl/gekkota/functions.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+from typing import Optional, Sequence
+
+from .annotations import TypeParam
+from .args import FuncArg
+from .block import BlockStmt
+from .constants import Config, StrGen
+from .core import Statement
+from .utils import Utils
+from .expression import Expression
+
+
+class LambDef(Expression):
+    def __init__(self, args: Sequence[FuncArg], body: Expression):
+        self.args = args
+        self.body = body
+
+    def render(self, config: Config) -> StrGen:
+        yield "lambda"
+        if self.args:
+            yield " "
+            yield from Utils.comma_separated(self.args, config)
+        yield ":"
+        yield " "
+        yield from self.body.render(config)
+
+
+class FuncDef(BlockStmt):
+    spacing = 1
+
+    def __init__(
+        self,
+        name: str,
+        args: Sequence[FuncArg],
+        body: Statement,
+        *,
+        rtype: Optional[Expression] = None,
+        is_async: bool = False,
+        type_params: Sequence[TypeParam] = (),
+    ):
+        self.name = name
+        self.body = body
+        self.args = args
+        self.rtype = rtype
+        self.is_async = is_async
+        self.type_params = type_params
+
+    def render_head(self, config: Config) -> StrGen:
+        if self.is_async:
+            yield "async"
+            yield " "
+
+        yield "def"
+        yield " "
+        yield self.name
+
+        if self.type_params:
+            yield "["
+            yield from Utils.comma_separated(self.type_params, config)
+            yield "]"
+
+        yield "("
+        yield from Utils.comma_separated(self.args, config)
+        yield ")"
+
+        if self.rtype:
+            yield " "
+            yield "->"
+            yield " "
+            yield from self.rtype.render(config)
+
+
+class Decorated(Statement):
+    def __init__(self, decorator: Expression, statement: ClassDef | FuncDef):
+        self.decorator = decorator
+        self.statement = statement
+
+    def render(self, config: Config) -> StrGen:
+        yield "@"
+        yield from self.decorator.render(config)
+        yield "\n"
+        yield from self.statement.render(config)
+
+
+from gekkota.classes import ClassDef

13ea172848dcc0a9f7d3d56d5e3f0ac7aab56002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 03:04:19 +0000

    Add package maplibre/0.1.4/maplibre-0.1.4.tar.gz

---
+++ packages/maplibre/maplibre-0.1.4.tar.gz/maplibre-0.1.4/maplibre/__init__.py
@@ -0,0 +1,8 @@
+from .controls import ControlPosition, ControlType
+
+# from .ipywidget import MapWidget
+from .layer import Layer, LayerType
+from .map import Map, MapOptions
+from .mapcontext import MapContext
+from .server import render_maplibregl
+from .ui import output_maplibregl

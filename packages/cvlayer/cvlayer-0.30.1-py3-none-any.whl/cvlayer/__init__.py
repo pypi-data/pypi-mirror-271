376510bb77116f12ae5df8273efb2857e82ed789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 15:03:35 +0000

    Add package cvlayer/0.30.1/cvlayer-0.30.1-py3-none-any.whl

---
+++ packages/cvlayer/cvlayer-0.30.1-py3-none-any.whl/cvlayer/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from cvlayer.cv import CvLayer
+from cvlayer.cvwindow import CvWindow
+from cvlayer.layer.manager.cvmanager import CvManager
+from cvlayer.layer.manager.mixins import CvMixin
+
+__version__ = "0.30.1"
+__all__ = [
+    "__version__",
+    "CvLayer",
+    "CvManager",
+    "CvMixin",
+    "CvWindow",
+]

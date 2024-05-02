31d8e5d53388dbc5ac45d915a80d297eb92ed055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:04:43 +0000

    Add package lyygui/1.5/lyygui-1.5.tar.gz

---
+++ packages/lyygui/lyygui-1.5.tar.gz/lyygui-1.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lyygui',
+    version='1.5',
+    author='lyy',
+    author_email='',
+    description='yyjdgui for lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+        "win10toast ",
+
+    ],
+)

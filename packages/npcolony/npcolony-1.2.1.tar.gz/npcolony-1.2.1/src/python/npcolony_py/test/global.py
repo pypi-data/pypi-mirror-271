1c4ea6aefd2f5ebc795329d593d4f21eb3562e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 03:04:29 +0000

    Add package npcolony/1.2.1/npcolony-1.2.1.tar.gz

---
+++ packages/npcolony/npcolony-1.2.1.tar.gz/npcolony-1.2.1/src/python/npcolony_py/test/global.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import npcolony
+
+class GlobalTest(unittest.TestCase):
+
+    def test_basic(self):
+        self.assertEqual(type(npcolony.VERSION), str)
+        self.assertEqual(npcolony.VERSION, "1.2.1")
+
+        self.assertEqual(type(npcolony.get_devices()), list)

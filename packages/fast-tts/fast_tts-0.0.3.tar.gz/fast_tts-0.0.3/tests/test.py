67366b22c153ec59bb3543c004743a8c9377a5d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 05:03:24 +0000

    Add package fast-tts/0.0.3/fast_tts-0.0.3.tar.gz

---
+++ packages/fast-tts/fast_tts-0.0.3.tar.gz/fast_tts-0.0.3/tests/test.py
@@ -0,0 +1,7 @@
+from fast_tts.fast_tts import FastTTS
+
+
+
+
+
+FastTTS.fast_tts('Hello World! This is the ultimate test of the Fast TTS! I hope it works! Thank you!', 'C:\\backend_server\\ServerFiles\\Speakers\\Drinker', 'C:\\Users\\mathi\\OneDrive\\Bureau')

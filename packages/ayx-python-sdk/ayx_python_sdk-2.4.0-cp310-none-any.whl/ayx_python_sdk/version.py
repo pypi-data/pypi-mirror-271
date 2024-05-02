1dbaa75246f77e472a41c8d1cfe119130c56e88b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 06:03:36 +0000

    Add package ayx-python-sdk/2.4.0/ayx_python_sdk-2.4.0-cp310-none-any.whl

---
+++ packages/ayx-python-sdk/ayx_python_sdk-2.4.0-cp310-none-any.whl/ayx_python_sdk/version.py
@@ -0,0 +1,9 @@
+"""FILE GENERATED FROM SETUP.PY."""
+short_version = "2.4.0"
+version = "2.4.0"
+full_version = "2.4.0"
+git_revision = "1e94d378fe212bc3538d28c8d8195747e82de144"
+release = True
+
+if not release:
+    version = full_version

4c71c71e5e156612cc74ef3cefe514b09e4a914b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 15:04:07 +0000

    Add package mylocale/0.15.0/mylocale-0.15.0-py3-none-any.whl

---
+++ packages/mylocale/mylocale-0.15.0-py3-none-any.whl/mylocale/create.py
@@ -0,0 +1,10 @@
+import click
+
+@click.command()
+def create():
+    string = "stringname,en_EN\nHELLOWORLD,Hello World"
+    with open(file="localisation.csv", mode="w") as f:
+        f.write(string)
+
+
+create()

81bd0335eacfde7cc530de479cb8c12585287be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:03:15 +0000

    Add package stylepy/0.5/stylepy-0.5.tar.gz

---
+++ packages/stylepy/stylepy-0.5.tar.gz/stylepy-0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stylepy',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[],
+    author='Venkatraman.R',
+    author_email='ramsunvtech@gmail.com',
+    description='Format CLI output like HTML.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/web-slate/stylepy',
+    license='MIT',
+    setup_requires=['wheel'],
+    python_requires=">=3.3",
+)

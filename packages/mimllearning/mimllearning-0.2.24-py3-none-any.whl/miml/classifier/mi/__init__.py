eba9a227075d501ebbf0963ea4d0b86ac17846db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:03:46 +0000

    Add package mimllearning/0.2.24/mimllearning-0.2.24-py3-none-any.whl

---
+++ packages/mimllearning/mimllearning-0.2.24-py3-none-any.whl/miml/classifier/mi/__init__.py
@@ -0,0 +1,4 @@
+from .all_positive_apr_classifier import AllPositiveAPRClassifier
+from .apr_classifier import APRClassifier
+from .iterated_discrim_apr_classifier import IteratedDiscrimAPRClassifier
+from .miles_classifier import MILESClassifier
---
+++ packages/mimllearning/mimllearning-0.2.24-py3-none-any.whl/miml/transformation/mimlTOml/miml_to_ml_transformation.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from ...data import Bag
+from ...data import MIMLDataset
+
+
+class MIMLtoMLTransformation(ABC):
+
+    def __init__(self) -> None:
+        self.dataset = None
+
+    @abstractmethod
+    def transform_dataset(self, dataset: MIMLDataset):
+        pass
+
+    @abstractmethod
+    def transform_bag(self, bag: Bag):
+        pass

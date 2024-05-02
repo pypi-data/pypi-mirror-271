e5d0b2daf77cc1af90ff429ece17287c935827ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 16:04:03 +0000

    Add package mani-skill/3.0.0b0/mani_skill-3.0.0b0-py3-none-any.whl

---
+++ packages/mani-skill/mani_skill-3.0.0b0-py3-none-any.whl/mani_skill/agents/robots/__init__.py
@@ -0,0 +1,13 @@
+from .allegro_hand import AllegroHandLeft, AllegroHandRight, AllegroHandRightTouch
+from .anymal import ANYmalC
+from .dclaw import DClaw
+from .fetch import Fetch
+from .humanoid import Humanoid
+from .panda import *
+from .stompy import Stompy
+from .trifingerpro import TriFingerPro
+from .unitree_go import *
+from .unitree_h1 import UnitreeH1, UnitreeH1Simplified
+from .ur_e import UR10e
+from .xarm import XArm7Ability
+from .xmate3 import Xmate3Robotiq

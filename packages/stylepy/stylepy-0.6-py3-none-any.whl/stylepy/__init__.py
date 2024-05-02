fe852354dca6b0a2ae90f4b46a258f133386898a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:03:22 +0000

    Add package stylepy/0.6/stylepy-0.6-py3-none-any.whl

---
+++ packages/stylepy/stylepy-0.6-py3-none-any.whl/stylepy/__init__.py
@@ -0,0 +1,18 @@
+"""
+stylepy
+-------------------------------------------
+Format CLI output like HTML.
+"""
+
+__author__ = "Venkatraman.R (ramsunvtech@gmail.com) @ramsunvtech"
+__version__ = "0.6.0"
+__license__ = "MIT"
+
+from blockquote import blockquote
+from header import h1, h2, h3, h4, h5, h6
+from inline import span
+from json import pretty_json
+from list import bullet_list, ordered_list
+from space_complexity import spaceComplexity
+from time_complexity import timeComplexity
+from table import tabular_list

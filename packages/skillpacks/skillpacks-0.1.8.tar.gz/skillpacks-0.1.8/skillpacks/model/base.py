4699503d6c49ab843f560007019f5a28193b5559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 06:03:45 +0000

    Add package skillpacks/0.1.8/skillpacks-0.1.8.tar.gz

---
+++ packages/skillpacks/skillpacks-0.1.8.tar.gz/skillpacks-0.1.8/skillpacks/model/base.py
@@ -0,0 +1,16 @@
+# type: ignore
+from typing import List
+
+from skillpacks.history import Histories
+from skillpacks.select import Selector
+
+
+class ActionModel:
+
+    def tune(histories: Histories) -> str:
+        """ """
+        pass
+
+    def selector(self) -> Selector:
+        """ """
+        pass

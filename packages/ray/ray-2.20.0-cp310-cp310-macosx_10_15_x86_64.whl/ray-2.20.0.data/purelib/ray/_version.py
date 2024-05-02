0d86e643727f5f55c2dff869672fe26b52b4c32e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 05:05:03 +0000

    Add package ray/2.20.0/ray-2.20.0-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/ray/ray-2.20.0-cp310-cp310-macosx_10_15_x86_64.whl/ray-2.20.0.data/purelib/ray/_version.py
@@ -0,0 +1,6 @@
+# Replaced with the current commit when building the wheels.
+commit = "5708e75978413e46c703e44f43fd89769f3c148b"
+version = "2.20.0"
+
+if __name__ == "__main__":
+    print("%s %s" % (version, commit))

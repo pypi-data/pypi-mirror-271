6d9f463715f3a40510dbccb60abd90e15495ff1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 06:03:38 +0000

    Add package sentient-switchblade/0.3.7/sentient_switchblade-0.3.7-py3-none-any.whl

---
+++ packages/sentient-switchblade/sentient_switchblade-0.3.7-py3-none-any.whl/switchbladecli/cli/lint.py
@@ -0,0 +1,23 @@
+import click
+
+from operator import itemgetter
+from switchbladecli.modes.tool_runner import get_tool_runner
+
+
+@click.command()
+@click.argument("linter", required=False)
+@click.pass_context
+def lint(ctx, linter: str):
+    """Run linter(s) on project.
+    
+    LINTER: Linter to run. If not specified, all linters will be run.
+    """
+    project_dir, config, verbose = itemgetter("project_dir", "config", "verbose")(ctx.obj)
+    result = cmd_lint(verbose, project_dir, config, linter)
+    if not result:
+        ctx.exit(1)
+
+
+def cmd_lint(verbose: bool, project_dir: str, config: dict, linter_tool: str = "all"):
+    tool_runner = get_tool_runner(config)(config, verbose)
+    return tool_runner.lint(linter_tool)

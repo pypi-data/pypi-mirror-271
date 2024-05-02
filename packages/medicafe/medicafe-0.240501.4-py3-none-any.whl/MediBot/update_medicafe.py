666d6f18e1fbfed1813445ec1fb30ec65f2613bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 15:03:54 +0000

    Add package medicafe/0.240501.4/medicafe-0.240501.4-py3-none-any.whl

---
+++ packages/medicafe/medicafe-0.240501.4-py3-none-any.whl/MediBot/update_medicafe.py
@@ -0,0 +1,20 @@
+import subprocess
+import sys
+from tqdm import tqdm
+
+# BUG This keeps failing
+
+def upgrade_medicafe(package):
+    try:
+        # Use tqdm to create a progress bar
+        with tqdm(total=100, desc="Upgrading %s" % package, unit="%") as progress_bar:
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', package, '--no-deps', '--disable-pip-version-check'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            # Update progress bar to 100% upon completion
+            progress_bar.update(100 - progress_bar.n)
+    except subprocess.CalledProcessError:
+        print("Update failed. Please check your internet connection and try again later.")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    medicafe_package = "medicafe"
+    upgrade_medicafe(medicafe_package)

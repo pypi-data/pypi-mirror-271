3bb611b31558fb6584bb9759c931c75487c68c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 03:05:01 +0000

    Add package jarm/0.1.0/jarm-0.1.0.tar.gz

---
+++ packages/jarm/jarm-0.1.0.tar.gz/jarm-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(name='jarm',
+      version='0.1.0',
+      description='The smarter, faster pip.',
+      author='rayan haddad',
+      author_email='rayan.m.haddad@proton.me',
+      scripts=['jarm.py'],
+      license='MIT',
+      platforms='any',
+      classifiers=['Development Status :: 4 - Beta',
+                   "Operating System :: OS Independent",
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
+                   ],
+      )

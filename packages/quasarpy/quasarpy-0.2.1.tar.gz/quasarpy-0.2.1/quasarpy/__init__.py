4aa22f3f90d1adda53d78a72ca717df1c2c0acea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 15:04:07 +0000

    Add package quasarpy/0.2.1/quasarpy-0.2.1.tar.gz

---
+++ packages/quasarpy/quasarpy-0.2.1.tar.gz/quasarpy-0.2.1/quasarpy/__init__.py
@@ -0,0 +1,26 @@
+'''This module contains the main() function, which is the entry point for the
+command line interface.'''
+from dotenv import load_dotenv
+from quasarpy.cli import cli
+from quasarpy.utils import logger
+from quasarpy._version import _version_ as _version
+
+
+load_dotenv()  # Load environment variables from .env file
+
+
+def main():
+    '''Entry point for the command line interface.'''
+    logger.info(f"Quasar {_version} started")
+    try: 
+        cli()
+    except NotImplementedError as e:
+        logger.error(e)
+    except ValueError as e:
+        logger.error(e)
+    finally:
+        logger.info(f"Quasar {_version} stopped")
+    
+
+if __name__ == "__main__":
+    main()

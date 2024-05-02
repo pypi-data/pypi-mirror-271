41a38d4dc96bf69daa693dc4efc553a5f2152569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 05:05:45 +0000

    Add package ablog/0.11.9/ablog-0.11.9.tar.gz

---
+++ packages/ablog/ablog-0.11.9.tar.gz/ablog-0.11.9/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+# We keep this around as of time of writing, I do not know how to avoid
+# having this if we want to run extract_messages  for the translations
+setup()

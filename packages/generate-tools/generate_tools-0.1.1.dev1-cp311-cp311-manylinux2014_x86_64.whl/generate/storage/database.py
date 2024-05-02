928637cc45cde3a4da8dd3135a98116a4a34ed69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 15:03:51 +0000

    Add package generate-tools/0.1.1.dev1/generate_tools-0.1.1.dev1-cp311-cp311-manylinux2014_x86_64.whl

---
+++ packages/generate-tools/generate_tools-0.1.1.dev1-cp311-cp311-manylinux2014_x86_64.whl/generate/storage/database.py
@@ -0,0 +1,17 @@
+import typing
+
+from exception import InvalidCollectionName
+
+from .base import Base
+from .collection import Collection
+
+
+class Database(Base):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+    def collection(self, collectionName: str) -> "Collection":
+        if not collectionName:
+            raise InvalidCollectionName()
+        collect = Collection(collectionName)
+        return collect

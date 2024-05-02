9b7de92b6c63f18d539246c69477817df89535be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 16:03:54 +0000

    Add package pygorpho/1.0.1/pygorpho-1.0.1.zip

---
+++ packages/pygorpho/pygorpho-1.0.1.zip/setup.py
@@ -0,0 +1,28 @@
+from skbuild import setup
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name='pygorpho',
+    version='1.0.1',
+    description='Python bindings for gorpho',
+    url='https://pygorpho.readthedocs.io/',
+    project_urls={
+        'Documentation':
+            'https://pygorpho.readthedocs.io/en/latest/api-doc.html',
+        'PyPI': 'https://pypi.org/project/pygorpho/',
+        'Source': 'https://github.com/patmjen/pygorpho',
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author='Patrick M. Jensen',
+    author_email='patmjen@gmail.com',
+    package_dir={'': 'src'},
+    packages=["pygorpho"],
+    setup_requires=['numpy', 'scikit-build>=0.7.0'],
+    install_requires=['numpy', 'scikit-build>=0.7.0'],
+    cmake_languages=('CUDA',),
+    cmake_minimum_required_version='3.10',
+)

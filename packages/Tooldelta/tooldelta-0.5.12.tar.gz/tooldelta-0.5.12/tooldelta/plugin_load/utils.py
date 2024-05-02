07b0731356cea311781f09851e8ed165eba0bc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 03:04:19 +0000

    Add package Tooldelta/0.5.12/tooldelta-0.5.12.tar.gz

---
+++ packages/Tooldelta/tooldelta-0.5.12.tar.gz/tooldelta-0.5.12/tooldelta/plugin_load/utils.py
@@ -0,0 +1,18 @@
+"简易函数"
+import zipfile
+from ..color_print import Print
+
+
+def unzip_plugin(zip_dir: str, exp_dir: str) -> None:
+    """解压插件ZIP包
+
+    Args:
+        zip_dir (str): 压缩文件路径
+        exp_dir (str): 解压目录
+    """
+    try:
+        f = zipfile.ZipFile(zip_dir, "r")
+        f.extractall(exp_dir)
+    except Exception as err:
+        Print.print_err(f"zipfile: 解压失败: {err}")
+        raise EOFError("解压失败") from err

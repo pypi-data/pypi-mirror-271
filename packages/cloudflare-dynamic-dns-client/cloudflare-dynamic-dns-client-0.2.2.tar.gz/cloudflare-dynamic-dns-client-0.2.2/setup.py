12b8fc1708698c9accd04df4573988b82351f953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 06:03:35 +0000

    Add package cloudflare-dynamic-dns-client/0.2.2/cloudflare-dynamic-dns-client-0.2.2.tar.gz

---
+++ packages/cloudflare-dynamic-dns-client/cloudflare-dynamic-dns-client-0.2.2.tar.gz/cloudflare-dynamic-dns-client-0.2.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("readme.md","r") as f:
+	description = f.read()
+
+setup(
+	name='cloudflare-dynamic-dns-client',
+	description='Allows selfhosting on cloudflare with a dynamic ip address',
+	long_description=description,
+	long_description_content_type='text/markdown',
+	version='0.2.2',
+	license='MIT',
+	url='https://github.com/Littlewhinging/cloudflare-dynamic-dns',
+	packages=['cloudflare_dynamic_dns'],
+	author="Littlewhinging",
+	install_requires=[
+		"cloudflare==2.11.1",
+		"requests==2.31.0"
+	],
+	entry_points={
+		'console_scripts': [
+			'cloudflare-dynamic-dns = cloudflare_dynamic_dns.__main__:main'
+		],
+	}
+)

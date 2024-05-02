656721f6f96057fa4a5166017df660e321732f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 16:04:06 +0000

    Add package RPICommLink/1.0.2/RPICommLink-1.0.2.tar.gz

---
+++ packages/RPICommLink/RPICommLink-1.0.2.tar.gz/RPICommLink-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='RPICommLink',
+    version='1.0.2',
+    author='adixu',
+    author_email='adixu7@gmail.com',
+    description='A library for communication on Raspberry Pi',
+    packages=['RPICommLink'],
+    install_requires=[],
+)

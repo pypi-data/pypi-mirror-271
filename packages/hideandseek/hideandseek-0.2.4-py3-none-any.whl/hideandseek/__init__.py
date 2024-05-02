44fd73ed25965733ca4e8b9dbbf78833bdb50038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:03:33 +0000

    Add package hideandseek/0.2.4/hideandseek-0.2.4-py3-none-any.whl

---
+++ packages/hideandseek/hideandseek-0.2.4-py3-none-any.whl/hideandseek/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.4" # Starting 0.2.0, somewhat functional
+from . import evaluation as E
+from .trainer import Trainer
+# from . import validation as V
+from . import utils as U
+from . import dataset as D
+from . import fv
+from .dataset import Dataset
+
+# TODO: Make scripts for projects to fork from
+# TODO: Refine tutorial

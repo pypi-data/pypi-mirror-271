e5d16536e59e5aee0c6c843e479e1060df458add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:03:54 +0000

    Add package cs3560-grading-lib/0.1.1a2/cs3560_grading_lib-0.1.1a2.tar.gz

---
+++ packages/cs3560-grading-lib/cs3560_grading_lib-0.1.1a2.tar.gz/cs3560_grading_lib-0.1.1a2/tests/test_common.py
@@ -0,0 +1,53 @@
+import os
+import tempfile
+import time
+from pathlib import Path
+
+import pytest
+
+from grading_lib import is_debug_mode
+from grading_lib.common import (
+    get_mtime_as_datetime,
+    get_seed_from_env,
+    has_file_changed,
+)
+
+
+def test_is_debug_mode():
+    vals_for_true = ["1", "True", "T", "On", "ON", "t", "true"]
+    for val in vals_for_true:
+        os.environ["test_DEBUG"] = val
+        assert is_debug_mode("test_DEBUG")
+
+    vals_for_false = ["", None, "False", "false", "lorem", "0"]
+    for val in vals_for_false:
+        if val is None:
+            del os.environ["test_DEBUG"]
+        else:
+            os.environ["test_DEBUG"] = val
+        assert is_debug_mode("test_DEBUG") is False
+
+
+def test_get_seed_from_env():
+    val = 123
+    os.environ["test_SEED"] = str(val)
+    assert get_seed_from_env("test_SEED") == val
+
+
+@pytest.fixture
+def a_temp_file():
+    f = tempfile.NamedTemporaryFile()
+    f.seek(0)
+    yield f
+    f.close()
+
+
+def test_has_file_changed(a_temp_file):
+    path = Path(a_temp_file.name)
+
+    last_known_mtime = get_mtime_as_datetime(path)
+    assert not has_file_changed(last_known_mtime, path)
+
+    time.sleep(2)
+    path.touch()
+    assert has_file_changed(last_known_mtime, path)

2ecb704faf6446e5a9170fd393bf0fb8c7341c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:03:22 +0000

    Add package istr-python/0.2.0.post0/istr_python-0.2.0.post0.tar.gz

---
+++ packages/istr-python/istr_python-0.2.0.post0.tar.gz/istr_python-0.2.0.post0/README.md
@@ -0,0 +1,473 @@
+<img src="https://www.salabim.org/istr_logo.png" width=500>
+
+## Introduction
+
+The istr module has exactly one class: istr.
+
+With this it is possible to interpret strings as if they were integers.
+
+This can be very handy for solving puzzles, but also for other purposes.</br>
+For instance the
+famous send more money puzzle
+```
+  S E N D
+  M O R E
+--------- +
+M O N E Y
+```
+can be nicely, albeit not very efficient, coded as:
+```
+import itertools
+from istr import istr
+
+for s, e, n, d, m, o, r, y in istr(itertools.permutations(range(10), 8)):
+    if m and ((s|e|n|d) + (m|o|r|e) == (m|o|n|e|y)):
+        print(f' {s|e|n|d}')
+        print(f' {m|o|r|e}')
+        print('-----')
+        print(f'{m|o|n|e|y}')
+```
+
+And it is a nice demonstration of extending a class (str) with extra and changed functionality.
+
+## Installation
+Installing istr with pip is easy.
+```
+$ pip install istr-python
+```
+or when you want to upgrade,
+```
+$ pip install istr-python --upgrade
+```
+Alternatively, istr.py can be just copied into you current work directory from GitHub (https://github.com/salabim/istr).
+
+No dependencies!
+
+## Usage
+Just start with
+
+```
+from istr import istr
+```
+
+Now we can define some istrs:
+```
+four = istr('4')
+five = istr('5')
+```
+Then we can do
+```
+x= four * five
+```
+, after which x is `istr('20')`
+
+And now we can do
+```
+print(x == 20)
+print(x == '20')
+```
+resulting in two times `True`. That's because istrs instances are treated as int, although they are strings.
+
+That means that we can also say
+```
+print(x < 30)
+print(x >= '10')
+```
+again resulting in two times `True`.
+
+In contrast to an ordinary string
+```
+print(four + five)
+```
+prints `9`, as istr are treated as ints.
+
+Please note that `four` and `five` could have also be initialized with
+```
+four = istr(4)
+five = istr(5)
+```
+or even
+```
+four, five = istr(4, 5)
+```
+
+But how can we concatenate istrs? Just use the or operator (|): 
+```
+print(four | five)
+```
+will output `45`.
+
+And the result is again an istr.
+
+That means that
+```
+(four | five) / 3
+```
+is `istr('9')`.
+
+In order to repeat a string in the usual sense, you cannot use `3 * four`, as that woud be `12`. 
+
+We use the matrix multiplication operator (@) for this. So `3 @ four` is `444`. As is `four @ 3`.
+
+Also allowed are
+```
+abs(four)
+-four 
+```
+
+The bool operator works on the integer value of an istr. So
+`bool(istr('0'))` ==> `False`
+`bool(istr('1'))` ==> `True`
+The code
+```
+if istr('0'):
+    print('True')
+else:
+    print('False')
+```
+will print `False`
+
+For the in operator, an istr is treated as an ordinary string, although it is possible to use ints as well:
+```
+'34' in istr(1234)
+34 in istr(1234)
+```
+On the left hand side an istr is always treated as a string:
+```
+istr(1234) in '01234566890ABCDEF'
+```
+
+Note that all calculations are strictly integer calculations. That means that if a float variale is ever produced it will be converted to an int.
+Also divisions are always floor divisions!
+
+There's a special case for `istr('')`. This is a proper empty string, but also represents the value of 0.
+That is to allow for `istr('').join(i for i in '01234)'`, resulting in `istr('01234')`.
+
+Sorting a list of istrs is based on the integer value, not the string. So
+
+`' '.join(sorted('1 3 2 4 5 6 11 7 9 8 10 12 0'.split()))`
+
+is
+
+`'0 1 10 11 2 3 4 5 6 7 8 9'`
+
+,whereas
+
+`' '.join(sorted(istr('1 3 2 4 5 6 11 7 9 8 10 12 0'.split())))`
+
+is 
+
+`'0 1 2 3 4 5 6 7 8 9 10 11'`
+
+## Using other values for istr than numeric value or str
+Apart from with simple numeric (to be interpreted as an int) or str, istr can be initialized with
+several other types:
+
+- if a dict (or subtype of dict), the same type dict will be returned with all values istr'ed
+
+    `istr({0: 0, 1: 1, 2: 4})` ==> `{0: istr('0'), 1: istr('1'), 2: istr('4')}`
+
+- if an iterator, the iterator will be mapped with istr
+
+    `istr(i * i for i in range(3))` ==> `<map object>`
+
+    `list(istr(i * i for i in range(3)))` ==> `[istr('0'), istr('1'), istr('4')]`
+
+- if an iterable, the same type will be returned with all elements istr'ed
+
+    `istr([0, 1, 4])` ==> `[istr('0'), istr('1'), istr('4')]`
+
+    `istr((0, 1, 4))` ==> `(istr('0'), istr('1'), istr('4'))`
+
+    `istr({0, 1, 4})` ==> `{istr('4'), istr('0'), istr('1')} ## or similar`
+
+- if a range, an istr.range instance will be returned
+    
+    `istr(range(3))` ==> `istr.range(3)`
+
+    `list(istr(range(3)))` ==> `[istr('0'), istr('1'), istr('2')]`
+
+    `len(istr(range(3)))` ==> `3`
+
+- if an istr.range instance, the same istr.range will be returned
+
+## More than one parameter for istr
+It is possible to give more than one parameter, in which case a tuple
+of the istrs of the parameters will be returned, which can be handy
+to unpack multiple values, e.g.
+
+`a, b, c = istr(5, 6, 7)` ==> `a=istr('5') , b=istr('6'), c=istr('7')`
+
+## test for even/odd
+It is possible to test for even/odd with the
+
+`is_even` and `is_odd` method, e.g.
+
+```
+print(istr(4).is_even())
+print(istr(5).is_odd())
+```
+This will print `True` twice.
+
+## reverse an istr
+The method `istr.reversed()` will return the an istr with the reversed content:
+```
+print(repr(istr(456).reversed()))
+print(repr(istr('0456').reversed()))
+```
+result:
+```
+istr('654')
+istr('6540')
+```
+The same can -of course- be achieved with
+```
+print(repr(istr(456)[::-1]))
+print(repr(istr('0456')[::-1]))
+```
+Note that is impossible to reverse a negative istr.
+
+## enumerate with istrs
+
+The `istr.enumerate` method can be used just as the builtin enumerate function.
+The iteration counter however is an istr rather than an int. E.g. 
+```
+    for i,c in istr.enumerate('abc'):
+        print(f'{repr(i)} {c}')
+```
+prints
+```
+istr('0') a
+istr('1') b
+istr('2') c
+```
+
+## concatenate an iterable
+
+The `istr.concat` method can be useful to map all items of an iterable
+to `istr` and then concatenate these.
+
+`list(istr.concat(((1,2),(3,4)))` ==> `istr([12,34])`
+
+`list(istr.concat(itertools.permutations(range(3),2)))` ==> `[istr('01'), istr('02'), istr('10'), istr('12'), istr('20'), istr('21')]` 
+
+## generate istr with digits
+
+The class method `digits` can be used to return an istr of digits according to a given specification.
+The method takes either no or a number of arguments.
+
+If no arguments are given, the result will be istr('0123456789').
+
+The given argument(s) result in a range of digits.
+
+- `<n>` ==> n
+- `<n-m> ==> n, n+1, ..., m
+- `-n>` ==> 0, 1, ... n
+- `n->` ==> n, n+1, ..., 9
+- `''` ==> 0, 1, ..., 9
+
+(n and m must be digit between 0 and 9)
+
+The final result is an istr composed of the given range(s).
+
+Here are some examples:
+
+- `istr.digits()` ==> `istr('0123456789')`
+- `istr.digits('')` ==> `istr('0123456789')`
+- `istr.digits('1')` ==> `istr('1')`
+- `istr.digits('3-')` ==> `istr('3456789')`
+- `istr.digits('-3')` ==> `istr('0123')`
+- `istr('1-4', '6', '8-9')` ==> `istr('1234689')`
+- `istr('1', '1-2', '1-3')` ==> `istr('11213')`
+
+
+## Subclassing istr
+When a class is derived from istr, all methods will return that newly derived class. 
+
+E.g.
+```
+class jstr(istr):
+    ...
+    
+print(repr(jstr(4) * jstr(5)))
+```
+will print `jstr('20')`
+
+## Changing the way repr works
+
+It is possible to control the way an `istr` instance will be repr'ed.
+
+By default, the `istr('5')` is represented as `istr('5')`.
+
+With the istr.repr_mode() context manager, that can be changed:
+```
+with istr.repr_mode('str'):
+    five = istr('5')
+    print(repr(five))
+with istr.repr_mode('int'):
+    five = istr('5')
+    print(repr(five))
+with istr.repr_mode('istr'):
+    five = istr('5')
+    print(repr(five))
+```
+This will print
+```
+'5'
+5
+istr('5')
+```
+Note that the way an `istr` is represented is determined at initialization.
+
+It is also possible to set the repr mode without a context manager:
+
+```
+istr.repr_mode('str')
+five = istr('5')
+print(repr(five))
+```
+This will print
+```
+'5'
+```
+Finally, the current repr mode can be queried with `istr.repr_mode()`. So upon start:
+```
+print(repr(istr.repr_mode()))
+```
+will output `istr`.
+
+## Changing the base system
+
+By default, `istr` works in base 10. However it is possible to change the base system with the `istr.base()` context manager / method.
+
+Any base between 2 and 36 may be used.
+
+Note that the integer is always stored in base 10 mode, but the string
+representation will reflect the chosen base (at time of initialization).
+
+Some examples:
+```
+with istr.base(16):
+    a = istr('7fff')
+    print(int(a))
+
+    b = istr(15)
+    print(repr(b))
+```
+This will result in
+```
+32767
+istr('F')
+```
+All calculations are done in the decimal 10 system.
+
+Note that the way an `istr` is interpreted is determined at initialization.
+
+It is also possible to set the repr mode without a context manager:
+```
+istr.base(16)
+print(int(istr('7fff')))
+```
+This will print
+```
+32767
+```
+Finally, the current base can be queried with `istr.base()`, so upon start:
+```
+print(istr.base())
+```
+will result in `10`.
+
+## Changing the format of the string
+
+By default, `istr` does not change the way an istr is stored when a str is to initialize:
+
+`repr('4'))` ==> `istr('4')`
+
+`repr(' 4'))` ==> `istr(' 4')`
+
+`repr('4  '))` ==> `istr('4  ')`
+
+For initializing with an int (or other numeric) value, the string is simply the str representation
+
+`repr(4))` ==> `istr('4')`
+
+With the `istr.format()` context manager this behavior can be changed.
+If the format specifier is a number, most likely a single digit, that
+will be the minimum number of characters in the string:
+```
+with istr.format('3'):
+    print(repr(istr(1)))
+    print(repr(istr(12)))
+    print(repr(istr(123)))
+    print(repr(istr(1234)))
+```
+will print
+```
+istr('  1')
+istr(' 12')
+istr('123')
+istr('1234')
+```
+If the string starts with a `0`, the string will be zero filled:
+```
+with istr.format('03'):
+    print(repr(istr(1)))
+    print(repr(istr(12)))
+    print(repr(istr(123)))
+    print(repr(istr(1234)))
+```
+will print
+```
+istr('001')
+istr('012')
+istr('123')
+istr('1234')
+```
+Note that if a format other than the default `''` is used, the string will reformatted even if the `istr` is specified with a string:
+```
+with istr.format('03'):
+    print(repr(istr('  12 ')))
+```
+will result in `istr('0012')`
+
+Remark: For bases other than 10, the string will never be reformatted!
+
+## Operations
+----------------------------
+```
+The table below shows whether the string or the int version of istr is applied.
+
+operator/function   int  str  Example
+----------------------------
++                    x        istr(20) + 3 ==> istr('23')
+_                    x        istr(20) - 3 ==> istr('17')
+*                    x        istr(20) * 3 ==> istr('60')
+/                    x        istr(20) / 3 ==> istr('6')
+//                   x        istr(20) // 3 ==> istr('6')
+%                    x        istr(20) % 3 ==> istr('2')
+divmod               x        divmod(istr(20), 3) ==> (istr('6'), istr('2'))
+**                   x        istr(2) ** 3 ==> istr('8')
+@                         x   istr(20) @ 3 ==> istr('202020')
+==                   x    x   istr(20) == 20 ==> True | istr(20) == '20' ==> True
+|                         x   istr(20) | 5 ==> istr('205')
+abs                  x        abs(istr(-20)) ==> istr('20')
+bool                 x        bool(istr(' 0 ')) ==> False
+<=, <, >, >=         x        istr('100') > istr('2') ==> True
+slicing              x        istr(12345)[1:3] ==> istr('23')
+iterate                   x   [x for x in istr(20)] ==> [istr('2'), istr('0')
+len                       x   len(istr(' 20 ')) ==> 4
+count                     x   istr(100),count('0') ==> 2
+index                     x   istr(' 100 ').index('0') ==> 2
+----------------------------
+```
+## Test script
+There's an extensive pytest script in the `\tests` directory.
+
+This script also shows clearly the ways istr can be used.
+
+## Badges
+![PyPI](https://img.shields.io/pypi/v/istr-python) ![PyPI - Python Version](https://img.shields.io/pypi/pyversions/istr-python) ![PyPI - Implementation](https://img.shields.io/pypi/implementation/istr-python)
+
+![PyPI - License](https://img.shields.io/pypi/l/istr-python) ![Black](https://img.shields.io/badge/code%20style-black-000000.svg) 
+ ![GitHub last commit](https://img.shields.io/github/last-commit/salabim/istr)
---
+++ packages/istr-python/istr_python-0.2.0.post0.tar.gz/istr_python-0.2.0.post0/tests/test_istr.py
@@ -0,0 +1,497 @@
+import math
+import itertools
+
+if __name__ == "__main__":  # to make the tests run without the pytest cli
+    import sys
+
+    sys.path = ["../istr"] + sys.path
+
+import pytest
+
+from istr import istr
+
+istr.equals = lambda self, other: type(self) == type(other) and (str(self) == str(other))
+# this method tests whether self and other are exactly the same
+
+for i, name in enumerate("minus_one zero one two three four five six seven eight nine ten eleven twelve thirteen".split(), -1):
+    globals()[name] = istr(i)
+one_to_twelve = istr.range(1, thirteen)
+
+
+def test_arithmetic():
+    assert two + three == "5"
+    assert two + three == five
+    assert 2 + three == five
+    assert two + 3 == five
+
+    assert two - three == "-1"
+    assert two - three == minus_one
+    assert 2 - three == minus_one
+    assert two - 3 == minus_one
+
+    assert two * three == "6"
+    assert two * three == six
+    assert 2 * three == six
+    assert two * 3 == six
+
+    assert twelve // three == "4"
+    assert twelve // three == four
+    assert 12 // three == four
+    assert twelve // 3 == four
+
+    assert twelve / three == "4"
+    assert twelve / three == four
+    assert 12 / three == four
+    assert twelve / 3 == four
+
+    assert thirteen / three == "4"
+    assert thirteen / three == four
+    assert 13 / three == four
+    assert thirteen / 3 == four
+
+    assert twelve % five == 2
+    assert twelve % 5 == 2
+    assert 12 % five == 2
+
+    assert twelve % five == "2"
+    assert twelve % 5 == "2"
+    assert 12 % five == "2"
+
+    assert two**three == "8"
+    assert two**3 == "8"
+    assert 2**three == "8"
+
+
+def test_lt():
+    assert two < three
+    assert 2 < three
+    assert two < 3
+
+    assert not three < two
+    assert not 3 < two
+    assert not three < 2
+
+    assert not two < two
+    assert not 2 < two
+    assert not two < 2
+
+
+def test_le():
+    assert two <= three
+    assert 2 <= three
+    assert two <= 3
+
+    assert two <= two
+    assert 2 <= two
+    assert two <= two
+
+
+def test_gt():
+    assert three > two
+    assert 3 > two
+    assert three > 2
+
+    assert not two > three
+    assert not 2 > three
+    assert not two > 3
+
+    assert not three > three
+    assert not 3 > three
+    assert not three > 3
+
+
+def test_ge():
+    assert three >= two
+    assert 3 >= two
+    assert three >= 2
+
+    assert three >= three
+    assert 3 >= three
+    assert three >= three
+
+
+def test_eq():
+    assert two == istr("2")
+    assert two == 2
+    assert two == "2"
+    assert 2 == two
+    assert "2" == two
+
+    assert not two == "ab"
+    assert not two == istr
+
+
+def test_ne():
+    assert not two != istr("2")
+    assert not two != 2
+    assert not two != "2"
+    assert not 2 != two
+    assert not "2" != two
+
+    assert two != "ab"
+    assert two != istr
+
+
+def test_order():
+    assert " ".join(sorted(istr.range(1, 13))) == "1 2 3 4 5 6 7 8 9 10 11 12"
+    assert " ".join(sorted(map(istr, range(1, 13)))) == "1 2 3 4 5 6 7 8 9 10 11 12"
+
+def test_concat():
+    c = list(istr.concat(((1,2),(3,4))))
+    assert c == istr(['12','34'])
+    c=list(istr.concat(itertools.permutations(range(3),2)))
+    assert c == [istr('01'), istr('02'), istr('10'), istr('12'), istr('20'), istr('21')]
+    
+def test_range():
+    assert one_to_twelve == istr.range("1", "13")
+    assert one_to_twelve == istr.range(one, thirteen, one)
+    assert one_to_twelve == istr(range(1, 13))
+
+    assert len(one_to_twelve) == 12
+    assert 2 in one_to_twelve
+    assert "2" in one_to_twelve
+    assert two in one_to_twelve
+
+    assert 13 not in one_to_twelve
+    assert "13" not in one_to_twelve
+    assert thirteen not in one_to_twelve
+
+    assert one_to_twelve[2] == 3
+    assert one_to_twelve[2] == "3"
+    assert one_to_twelve[2] == three
+
+    assert one_to_twelve[2:4] == istr.range(3, 5)
+
+    with pytest.raises(IndexError):
+        a = one_to_twelve[12]
+
+
+def test_misc():
+    assert istr("") == ""
+    assert istr("") == 0
+    with pytest.raises(ValueError):
+        istr(" ")
+    assert istr(istr(6)) == "6"
+    assert istr(" 12 ") == " 12 "
+    with istr.format("03"):
+        assert istr("   12  ") == "012"
+        assert istr("") == ""
+
+
+def test_divmod():
+    assert divmod(eleven, three) == (istr(3), istr(2))
+    assert divmod(11, three) == (istr(3), istr(2))
+    assert divmod(eleven, 3) == (istr(3), istr(2))
+    assert divmod(11, 3) == (3, 2)
+
+
+def test_iter():
+    assert [x for x in istr.range(3)] == [istr(0), istr(1), istr(2)]
+
+
+def test_reversed():
+    assert [x for x in reversed(istr.range(3))] == [istr(2), istr(1), istr(0)]
+
+
+def test_lazy():
+    for x in istr.range(10000000000000000000000000):
+        if x == four:
+            break
+    assert x == 4
+
+    for x in istr(range(10000000000000000000)):
+        if x == four:
+            break
+    assert x == 4
+
+
+def test_str_repr():
+    assert one.equals(istr("1"))
+    assert str(one) == "1"
+    assert f"{one}" == "1"
+    assert repr(one_to_twelve) == "istr.range(1, 13)"
+    assert repr(istr.range(10)) == "istr.range(0, 10)"
+    assert repr(istr.range(one, two, three)) == "istr.range(1, 2, 3)"
+
+    assert str(one_to_twelve) == "istr.range(1, 13)"
+    assert str(istr.range(10)) == "istr.range(0, 10)"
+    assert str(istr.range(one, two, three)) == "istr.range(1, 2, 3)"
+
+
+def test_index():
+    assert (one_to_twelve.index(2)) == 1
+    assert (one_to_twelve.index(two)) == 1
+    assert (one_to_twelve.index("2")) == 1
+    with pytest.raises(ValueError):
+        one_to_twelve.index(13)
+    with pytest.raises(ValueError):
+        one_to_twelve.index(thirteen)
+    with pytest.raises(ValueError):
+        one_to_twelve.index("13")
+
+
+def test_count():
+    assert one_to_twelve.count(2) == 1
+    assert one_to_twelve.count(two) == 1
+    assert one_to_twelve.count("2") == 1
+    assert one_to_twelve.count(13) == 0
+    assert one_to_twelve.count(thirteen) == 0
+    assert one_to_twelve.count("13") == 0
+
+
+def test_hash():
+    assert hash(istr.range(1, 13)) == hash(one_to_twelve)
+    assert hash(istr.range(1, 12)) != hash(one_to_twelve)
+
+
+def test_format():
+    assert istr(" 1 ") == " 1 "
+    with istr.format("0"):
+        assert istr(" 1 ") == "1"
+    with istr.format("03"):
+        assert istr(1) == "001"
+        assert istr(1234) == "1234"
+    with istr.format("3"):
+        assert istr(1) == "  1"
+        assert istr(1234) == "1234"
+    with istr.format(""):
+        assert istr(1234) == "1234"
+    with istr.format("003"):
+        assert istr(1) == "001"
+    with pytest.raises(ValueError):
+        with istr.format(" 1"):
+            ...
+    with pytest.raises(ValueError):
+        with istr.format("1 "):
+            ...
+    with pytest.raises(ValueError):
+        with istr.format("a"):
+            ...
+    with pytest.raises(ValueError):
+        with istr.format(1):
+            ...
+    with istr.format("0"):
+        assert istr(" 3 ") == "3"
+    assert istr.format() == ""
+    istr.format("03")
+    assert istr.format() == "03"
+    assert istr("  8 ") == "008"
+    istr.format("")
+    assert istr(" 8 ") == " 8 "
+
+
+def test_range_format():
+    r = istr.range(11)
+    assert repr(r) == "istr.range(0, 11)"
+    assert " ".join(r) == "0 1 2 3 4 5 6 7 8 9 10"
+    r = istr.range(11)
+    with istr.format("02"):
+        assert " ".join(r) == "00 01 02 03 04 05 06 07 08 09 10"
+
+
+def test_even_odd():
+    assert istr(1).is_odd()
+    assert not istr(1).is_even()
+
+    assert istr(12345678).is_even()
+    assert not istr(12345678).is_odd()
+
+
+def test_join():
+    s = "".join(istr(("4", "5", "6")))
+    assert s == "456"
+    assert type(s) == str
+
+    s = istr("").join(("4", "5", "6"))
+    assert s == "456"
+    assert s == 456
+    assert type(s) == istr
+
+    s = istr("").join(istr(("4", "5", "6")))
+    assert s == "456"
+    assert s == 456
+    assert type(s) == istr
+
+    s = istr("").join(istr(("", "", "6")))
+    assert s == "6"
+    assert s == 6
+    assert type(s) == istr
+
+
+def test_matmul():
+    assert (five @ 3).equals(istr("555"))
+    assert (3 @ five).equals(istr("555"))
+
+    with pytest.raises(TypeError):
+        three @ five
+    with pytest.raises(TypeError):
+        three @ "5"
+    with pytest.raises(TypeError):
+        "3" @ five
+
+
+def test_str():
+    assert repr(str(five)) == "'5'"
+
+
+def test_trunc_and_friends():
+    assert math.trunc(one).equals(istr("1"))
+    assert math.ceil(one).equals(istr("1"))
+    assert math.floor(one).equals(istr("1"))
+    assert round(one).equals(istr("1"))
+
+
+def test_data_structures():
+    assert repr(istr(list(range(1, 4)))) == "[istr('1'), istr('2'), istr('3')]"
+    assert repr(istr(tuple(range(1, 4)))) == "(istr('1'), istr('2'), istr('3'))"
+    assert istr(set(range(1, 4))) == {istr(1), istr(2), istr(3)}
+
+    assert repr(list(istr(range(1, 4)))) == "[istr('1'), istr('2'), istr('3')]"
+
+    assert repr(list(istr.enumerate("abc"))) == "[(istr('0'), 'a'), (istr('1'), 'b'), (istr('2'), 'c')]"
+    assert repr(list(istr.enumerate("abc", 1))) == "[(istr('1'), 'a'), (istr('2'), 'b'), (istr('3'), 'c')]"
+
+    assert repr(istr(dict(zero=0, one=1, two=4))) == "{'zero': istr('0'), 'one': istr('1'), 'two': istr('4')}"
+
+
+def test_indexing():
+    a = istr(12345)
+    assert a[0].equals(istr(1))
+    assert a[:2].equals(istr(12))
+    assert a[::-1].equals(istr(54321))
+    assert a[-2:].equals(istr(45))
+
+
+def test_reverse():
+    a = istr(12345)
+    assert a.reversed(), same(istr(54321))
+
+
+def test_edge_cases():
+    with pytest.raises(ValueError):
+        istr("ab")
+    with pytest.raises(ValueError):
+        istr(istr)
+    assert istr(istr(one)).equals(istr("1"))
+    with pytest.raises(TypeError):
+        istr()
+    rng=istr.range(5)
+    assert rng is istr(rng)
+
+def test_unpacking():
+    a = istr("123")
+    x, y, z = istr(*a)
+    assert x.equals(istr(1))
+    assert y.equals(istr(2))
+    assert z.equals(istr(3))
+
+
+def test_repr_mode():
+    hundred = istr(100)
+    assert repr(hundred) == "istr('100')"
+
+    with istr.repr_mode("istr"):
+        hundred = istr(100)
+    assert repr(hundred) == "istr('100')"
+
+    with istr.repr_mode("int"):
+        hundred = istr(100)
+    assert repr(hundred) == "100"
+    with istr.repr_mode("str"):
+        hundred = istr(100)
+    assert repr(hundred) == "'100'"
+    hundred = istr(100)
+    assert repr(hundred) == "istr('100')"
+
+    assert istr.repr_mode() == "istr"
+
+    with pytest.raises(TypeError):
+        istr.repr_mode("no")
+
+def test_str_methods():
+    a=istr("   123123 ")
+    b=istr("123123")
+    assert a.capitalize().equals(a)
+    assert a.casefold().equals(a)
+    assert a.center(20).equals(istr('        123123      '))
+    assert a.expandtabs(4).equals(a)
+    assert istr("").join(("0","1","2")).equals(istr("012"))
+    assert a.ljust(20).equals(istr('   123123           '))
+    assert a.lower().equals(a)
+    assert a.lstrip().equals(istr('123123 '))
+    assert a.partition("3")==(istr('   12'), istr('3'), istr('123 '))
+    assert a.removeprefix("   12").equals(istr("3123 "))
+    assert a.removesuffix("23 ").equals(istr("   1231"))
+    assert a.replace("1","9").equals(istr("   923923 "))
+    assert a.rjust(20).equals(istr('             123123 '))
+    assert b.partition("2")==(istr('1'), istr('2'), istr('3123'))
+    assert b.rpartition("2")==(istr('1231'), istr('2'), istr('3'))
+    assert b.rsplit("1")==[istr(''), istr('23'), istr('23')]
+    assert a.rstrip().equals(istr('   123123'))
+    assert b.split("1")==[istr(''), istr('23'), istr('23')]
+    assert a.strip().equals(istr('123123'))
+    assert a.swapcase().equals(a)
+    assert a.title().equals(a)
+    assert a.translate({49:  52}).equals(istr('   423423 '))
+    assert a.upper().equals(a)
+    assert b.zfill(10).equals(istr('0000123123'))
+
+def test_base():
+    assert istr.base() == 10
+    with istr.base(16):
+        a = istr("7fff")
+        assert a == 32767
+        assert a == "7fff"
+        b = istr("3fff")
+        c = a - b
+        assert c == istr("4000")
+        assert c == 2**14
+    assert istr.base() == 10
+    with istr.base(36):
+        a = istr("PA7")
+        assert a == 32767
+
+    with istr.base(2):
+        a = istr("111")
+        assert a == 7
+        b = a - istr("110")
+        assert repr(b) == "istr('1')"
+
+    with pytest.raises(ValueError):
+        istr.base(1)
+    with pytest.raises(ValueError):
+        istr.base(37)
+
+    with pytest.raises(ValueError):
+        with istr.base(16):
+            istr(-1)
+
+    with istr.base(16):
+        a = istr(15)
+    with istr.base(10):
+        assert a * a == 225
+    with pytest.raises(ValueError):
+        assert a | a # FF can't be converted to base 10
+        
+    
+def test_digits():
+    assert istr.digits().equals(istr('0123456789'))
+    assert istr.digits('').equals(istr('0123456789'))
+    assert istr.digits('1').equals(istr('1'))
+    assert istr.digits('3-').equals(istr('3456789'))
+    assert istr.digits('-3').equals(istr('0123'))
+    assert istr.digits('1-4', '6', '8-9').equals(istr('1234689'))
+    assert istr.digits('1', '1-2', '1-3').equals(istr('112123'))
+
+
+def test_subclassing():
+    class jstr(istr):
+        ...
+
+    assert jstr(5).equals(jstr(5))
+    assert repr(jstr(*range(3))) == "(jstr('0'), jstr('1'), jstr('2'))"
+
+
+if __name__ == "__main__":
+    pytest.main(["-vv", "-s", "-x", __file__])
+

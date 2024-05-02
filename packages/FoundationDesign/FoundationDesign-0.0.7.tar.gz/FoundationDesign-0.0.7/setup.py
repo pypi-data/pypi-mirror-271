0a827296d4127fa8d9304e769c3a5f703b4e0aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 05:04:11 +0000

    Add package FoundationDesign/0.0.7/FoundationDesign-0.0.7.tar.gz

---
+++ packages/FoundationDesign/FoundationDesign-0.0.7.tar.gz/FoundationDesign-0.0.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+
+# store readme.md files
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+# read the requirements
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setup(
+    name="FoundationDesign",
+    packages=find_packages("FoundationDesign"),
+    version="0.0.7",
+    author="Kunle Yusuf",
+    author_email="kunleyusuf858@gmail.com",
+    description="A python module for structural analysis and design of different foundation types in accordance to the Eurocodes",
+    url = 'https://github.com/kunle009/FoundationDesign',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=requirements,
+)

2eb24b06007db85b7b579d6ebfb7783d8f24cedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 05:03:31 +0000

    Add package UniswapPy/1.4.0/uniswappy-1.4.0.tar.gz

---
+++ packages/UniswapPy/uniswappy-1.4.0.tar.gz/uniswappy-1.4.0/python/prod/cpt/index/RebaseIndexToken.py
@@ -0,0 +1,74 @@
+# Copyright [2023] [Ian Moore]
+# Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+# Email: defipy.devs@gmail.com
+
+from ...erc import ERC20
+from ...process.swap import WithdrawSwap
+from ...utils.data import UniswapExchangeData
+
+class RebaseIndexToken():
+    
+    """ 
+        Determine rebase amount of index token given a certain amount of liquidity from LP (inverse of SettlementLPToken)  
+    """      
+
+    
+    def __init__(self):
+        self.fac = ERC20("DAI", "0x09")
+     
+    def apply(self, lp, tkn, liq_amt): 
+       
+        """ apply
+
+            Apply rebase token calculation settlement; given liquidity amount, what is the reserve amount
+                
+            Parameters
+            -----------------
+            lp : UniswapExchange
+                Uniswap LP    
+            tkn: ERC20
+                Token asset from CPT pair       
+            liq_amt: float
+                Liquidity amount to be priced in in terms of reserve token                 
+
+            Returns
+            -----------------
+            rebase_amount: float
+                Reserve token amount, given liquidity amount
+                   
+        """        
+        
+        return self.calc_tkn_settlement(lp, tkn, liq_amt)
+    
+    def calc_tkn_settlement(self, lp, token_in, dL):
+            
+        (x, y) = self.get_reserves(lp, token_in)
+        L = lp.get_liquidity()
+        a0 = dL*x/L
+        a1 = dL*y/L
+        gamma = 997/1000
+
+        dy1 = a1
+        dy2 = gamma*a0*(y - a1)/(x - a0 + gamma*a0)
+        itkn_amt = dy1 + dy2
+
+        return itkn_amt if itkn_amt > 0 else 0  
+
+    def get_reserves(self, lp, token_in):
+        tokens = lp.factory.token_from_exchange[lp.name]
+        if(lp.version == UniswapExchangeData.VERSION_V2):
+            if(token_in.token_name == lp.token1):
+                x = lp.get_reserve(tokens[lp.token0])
+                y = lp.get_reserve(tokens[lp.token1])
+            else: 
+                x = lp.get_reserve(tokens[lp.token1])
+                y = lp.get_reserve(tokens[lp.token0])
+        elif(lp.version == UniswapExchangeData.VERSION_V3):   
+            if(token_in.token_name == lp.token1):
+                x = lp.get_virtual_reserve(tokens[lp.token0])
+                y = lp.get_virtual_reserve(tokens[lp.token1])
+            else: 
+                x = lp.get_virtual_reserve(tokens[lp.token1])
+                y = lp.get_virtual_reserve(tokens[lp.token0]) 
+        return (x, y)          
+            
---
+++ packages/UniswapPy/uniswappy-1.4.0.tar.gz/uniswappy-1.4.0/python/prod/cpt/index/SettlementLPToken.py
@@ -0,0 +1,75 @@
+# Copyright [2023] [Ian Moore]
+# Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+# Email: defipy.devs@gmail.com
+
+import numpy as np
+from ...utils.data import UniswapExchangeData
+
+class SettlementLPToken():
+    
+    """ Determine settlement amount of LP token given a certain amount of token    
+    """  
+    
+    def __init__(self):
+        pass
+     
+    def apply(self, lp, tkn, itkn_amt): 
+        
+        """ apply
+
+            Apply LP calculation settlement; given token amount, what is the liquidity amount
+                
+            Parameters
+            -----------------
+            lp : UniswapExchange
+                Uniswap LP    
+            tkn: ERC20
+                Token asset from CPT pair       
+            itkn_amt: float
+                Token reserve amount to be priced in terms of liquidity                 
+
+            Returns
+            -----------------
+            lp_amount: float
+                Liquidity amount given reserve token amount
+                   
+        """          
+        
+        return self.calc_lp_settlement(lp, tkn, itkn_amt)
+    
+    def calc_lp_settlement(self, lp, token_in, itkn_amt):
+
+        (x, y) = self.get_reserves(lp, token_in)
+        L = lp.get_liquidity()
+        if(L == 0): 
+            return 0
+        else:
+            gamma = 997
+
+            a1 = x*y/L
+            a2 = L
+            a = a1/a2
+            b = (1000*itkn_amt*x - itkn_amt*gamma*x + 1000*x*y + x*y*gamma)/(1000*L);
+            c = itkn_amt*x;
+
+            dL = (b*a2 - a2*np.sqrt(b*b - 4*a1*c/a2)) / (2*a1);
+            return dL  
+
+    def get_reserves(self, lp, token_in):
+        tokens = lp.factory.token_from_exchange[lp.name]
+        if(lp.version == UniswapExchangeData.VERSION_V2):
+            if(token_in.token_name == lp.token1):
+                x = lp.get_reserve(tokens[lp.token0])
+                y = lp.get_reserve(tokens[lp.token1])
+            else: 
+                x = lp.get_reserve(tokens[lp.token1])
+                y = lp.get_reserve(tokens[lp.token0])
+        elif(lp.version == UniswapExchangeData.VERSION_V3):   
+            if(token_in.token_name == lp.token1):
+                x = lp.get_virtual_reserve(tokens[lp.token0])
+                y = lp.get_virtual_reserve(tokens[lp.token1])
+            else: 
+                x = lp.get_virtual_reserve(tokens[lp.token1])
+                y = lp.get_virtual_reserve(tokens[lp.token0]) 
+        return (x, y)  
+            

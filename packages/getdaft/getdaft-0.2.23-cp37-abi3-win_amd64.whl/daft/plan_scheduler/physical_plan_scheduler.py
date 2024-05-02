3b5326b3dde379a0f5d8f5b78256de056e163a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 06:03:37 +0000

    Add package getdaft/0.2.23/getdaft-0.2.23-cp37-abi3-win_amd64.whl

---
+++ packages/getdaft/getdaft-0.2.23-cp37-abi3-win_amd64.whl/daft/plan_scheduler/physical_plan_scheduler.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from daft.daft import PhysicalPlanScheduler as _PhysicalPlanScheduler
+from daft.execution import physical_plan
+from daft.runners.partitioning import PartitionT
+
+
+class PhysicalPlanScheduler:
+    """
+    Generates executable tasks for an underlying physical plan.
+    """
+
+    def __init__(self, scheduler: _PhysicalPlanScheduler):
+        self._scheduler = scheduler
+
+    def num_partitions(self) -> int:
+        return self._scheduler.num_partitions()
+
+    def pretty_print(self, simple: bool = False) -> str:
+        """
+        Pretty prints the current underlying physical plan.
+        """
+        if simple:
+            return self._scheduler.repr_ascii(simple=True)
+        else:
+            return repr(self)
+
+    def __repr__(self) -> str:
+        return self._scheduler.repr_ascii(simple=False)
+
+    def to_partition_tasks(self, psets: dict[str, list[PartitionT]]) -> physical_plan.MaterializedPhysicalPlan:
+        return physical_plan.materialize(self._scheduler.to_partition_tasks(psets))

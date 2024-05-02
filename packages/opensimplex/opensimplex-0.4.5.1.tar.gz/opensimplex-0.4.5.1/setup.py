e900cccee7b9b157fe4712c58f3af504cf395789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:04:16 +0000

    Add package opensimplex/0.4.5.1/opensimplex-0.4.5.1.tar.gz

---
+++ packages/opensimplex/opensimplex-0.4.5.1.tar.gz/opensimplex-0.4.5.1/setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup, find_packages
+
+from opensimplex import __version__, __author__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='opensimplex',
+    version=__version__,
+    author=__author__,
+    author_email='opensimplex@larus.se',
+    description='OpenSimplex is a noise generation function like Perlin or Simplex noise, but better.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='opensimplex simplex noise 2D 3D 4D',
+    url='https://code.larus.se/lmas/opensimplex',
+    download_url='https://code.larus.se/lmas/opensimplex/releases',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy>=1.22',
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+    python_requires='>=3.8',
+)

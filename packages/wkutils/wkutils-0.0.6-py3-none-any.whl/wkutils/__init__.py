6360e1388da86946354a5a3cfd29c2a0c21c34b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 16:04:06 +0000

    Add package wkutils/0.0.6/wkutils-0.0.6-py3-none-any.whl

---
+++ packages/wkutils/wkutils-0.0.6-py3-none-any.whl/wkutils/__init__.py
@@ -0,0 +1,22 @@
+import numpy_financial as npf
+
+def twoDecimalPlaces(answer):
+    '''
+        This simply formats the input as two decimal places.
+    '''
+    return("%.2f" % answer)
+
+def gross_payment(rate, nper, pv):
+    '''
+        
+    '''
+    return float(twoDecimalPlaces(npf.pmt(rate/12, nper*12, pv)))
+
+def net_payment(grosspayment):
+    return float(twoDecimalPlaces(grosspayment * (1-0.3693)))
+
+def calculate_loan_payments(rate, nper, pv):
+    gp = gross_payment(rate, nper, pv)
+    np = net_payment(gp)
+    print('Gross payment:', gp)
+    print('Net payment:', np)

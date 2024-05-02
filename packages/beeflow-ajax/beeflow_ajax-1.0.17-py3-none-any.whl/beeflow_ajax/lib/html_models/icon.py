ae867592e07ee8afbafb455f796b0aef217644d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 05:03:17 +0000

    Add package beeflow-ajax/1.0.17/beeflow_ajax-1.0.17-py3-none-any.whl

---
+++ packages/beeflow-ajax/beeflow_ajax-1.0.17-py3-none-any.whl/beeflow_ajax/lib/html_models/icon.py
@@ -0,0 +1,9 @@
+"""copyright (c) 2014 - 2024 Beeflow Ltd.
+
+Author Rafal Przetakowski <rafal.p@beeflow.co.uk>"""
+
+from beeflow_ajax.lib.html_models.base_html_model import BaseHTMLModel
+
+
+class Icon(BaseHTMLModel):
+    element_name: str = "i"

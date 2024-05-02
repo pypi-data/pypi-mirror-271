202ea07c654323515481be2dac9a564918a7a21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 03:03:45 +0000

    Add package runit-cli/0.4.1/runit_cli-0.4.1.tar.gz

---
+++ packages/runit-cli/runit_cli-0.4.1.tar.gz/runit_cli-0.4.1/runit/tools/javascript/runner.js
@@ -0,0 +1,30 @@
+// const dotenv = require('dotenv')
+const path = require('node:path')
+
+const args = process.argv
+
+let functionArguments;
+
+try {
+    if (args.length >= 3) {
+        const filename = args[2]
+        // const filepath = path.dirname(filename)
+        // dotenv.config({
+        //     path: path.join(filepath, '.env')
+        // })
+        const functionname = args[3]
+        
+        const method = require(filename)[functionname]
+
+        if (args.length > 4) functionArguments = args[4]
+    
+        if (functionArguments !== undefined) {
+            console.log(method(functionArguments))
+        } else {
+            console.log(method())
+        }
+    
+    }
+} catch (error) {
+    console.log(error)
+}

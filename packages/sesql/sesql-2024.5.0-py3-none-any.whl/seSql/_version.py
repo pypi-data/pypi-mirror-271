49bf01a1fc8c6c73c4be9f93b6cf86c40d40051d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 15:03:52 +0000

    Add package sesql/2024.5.0/sesql-2024.5.0-py3-none-any.whl

---
+++ packages/sesql/sesql-2024.5.0-py3-none-any.whl/seSql/_version.py
@@ -0,0 +1,10 @@
+"""
+Provides `seSql` version information.
+"""
+# This file is auto-generated! Do not edit!
+# Use `seVersion seSql` to change this file.
+
+from seVersion import Version
+__version__ = Version("seSql", 2024, 5, 0, 75)
+__setuptools__ = __version__.public()
+__all__ = ["__version__", "__setuptools__"]

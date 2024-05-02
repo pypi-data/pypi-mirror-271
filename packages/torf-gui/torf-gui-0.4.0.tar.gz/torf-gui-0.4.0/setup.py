fa785d978d1333a4faaa5e1a8772b697e64a6293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 14:04:11 +0000

    Add package torf-gui/0.4.0/torf-gui-0.4.0.tar.gz

---
+++ packages/torf-gui/torf-gui-0.4.0.tar.gz/torf-gui-0.4.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+
+with open("torf_gui/version.py") as f:
+    exec(f.read())
+
+setup(
+    name="torf-gui",
+    version=__version__,  # noqa: F821
+    packages=find_packages(),
+    entry_points={"gui_scripts": ["torf-gui = torf_gui.gui:main"]},
+    install_requires=[
+        "torf>=4.2.4",
+        "humanfriendly>=10.0",
+        "PyQt5>=5.15.9",
+        "pyqtdarktheme>=2.1.0",
+    ],
+    # metadata for upload to PyPI
+    author="Oliver Sayers",
+    author_email="talk@savagecore.uk",
+    description=(
+        "An advanced GUI torrent file creator with batch functionality, "
+        "powered by PyQt and torf."
+    ),
+    long_description=open("README.md").read(),
+    keywords="bittorrent torrent",
+    url="https://github.com/SavageCore/torf-gui",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+)

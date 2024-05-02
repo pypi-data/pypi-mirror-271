11a14fb79418d29d4ab8dda23cb8dee56b585283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 15:03:55 +0000

    Add package ldc-google/0.0.1/ldc-google-0.0.1.tar.gz

---
+++ packages/ldc-google/ldc-google-0.0.1.tar.gz/ldc-google-0.0.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_namespace_packages
+
+
+def _read(f):
+    """
+    Reads in the content of the file.
+    :param f: the file to read
+    :type f: str
+    :return: the content
+    :rtype: str
+    """
+    return open(f, 'rb').read()
+
+
+setup(
+    name="ldc-google",
+    description="Python3 library integrating Google services into the llm-dataset-converter library.",
+    long_description=(
+            _read('DESCRIPTION.rst') + b'\n' +
+            _read('CHANGES.rst')).decode('utf-8'),
+    url="https://github.com/waikato-llm/ldc-google",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Programming Language :: Python :: 3',
+    ],
+    license='MIT License',
+    package_dir={
+        '': 'src'
+    },
+    packages=find_namespace_packages(where='src'),
+    install_requires=[
+        "llm-dataset-converter>=0.2.1",
+        "google-cloud-translate",
+    ],
+    version="0.0.1",
+    author='Peter Reutemann',
+    author_email='fracpete@waikato.ac.nz',
+    entry_points={
+        "class_lister": [
+            "ldc_google=ldc_google.class_lister",
+        ],
+    },
+)

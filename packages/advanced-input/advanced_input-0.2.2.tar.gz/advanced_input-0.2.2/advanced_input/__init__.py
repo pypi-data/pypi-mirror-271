846ac4db5759691ec4a40e587b1493e6cb655833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:03:35 +0000

    Add package advanced-input/0.2.2/advanced_input-0.2.2.tar.gz

---
+++ packages/advanced-input/advanced_input-0.2.2.tar.gz/advanced_input-0.2.2/advanced_input/__init__.py
@@ -0,0 +1,15 @@
+from .input import AdvancedInput, is_input_ready, get_character_from_input
+from .constants import PriorityOrder, EventType, EventWithType
+from platform import system
+import atexit
+current_input_extender: AdvancedInput = None
+
+def get_advanced_input(encoding: str = None):
+    global current_input_extender
+    if encoding is None:
+        encoding = "cp850" if system() == "Windows" else "utf-8"
+    if current_input_extender is None:
+        current_input_extender = AdvancedInput(encoding)
+        current_input_extender.start()
+        atexit.register(current_input_extender.stop)
+    return current_input_extender

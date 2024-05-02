9875985acadd068331938bf269dcbea693489e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 05:03:38 +0000

    Add package LanusStats/1.3.1/LanusStats-1.3.1.tar.gz

---
+++ packages/LanusStats/LanusStats-1.3.1.tar.gz/LanusStats-1.3.1/LanusStats/sofascore.py
@@ -0,0 +1,12 @@
+import requests
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+from mplsoccer import Pitch
+from datetime import datetime
+import time
+from .functions import get_possible_leagues_for_page
+from .exceptions import InvalidStrType
+
+class SofaScore:
+    print('Not implemented yet')

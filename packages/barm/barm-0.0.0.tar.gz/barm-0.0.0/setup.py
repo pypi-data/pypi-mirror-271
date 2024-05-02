69ca658b728fe4f683f18568f0eb682a021da486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 03:04:42 +0000

    Add package barm/0.0.0/barm-0.0.0.tar.gz

---
+++ packages/barm/barm-0.0.0.tar.gz/barm-0.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(name='barm',
+      description='The smarter, faster pip.',
+      author='rayan haddad',
+      author_email='rayan.m.haddad@proton.me',
+      scripts=['barm.py'],
+      license='MIT',
+      platforms='any',
+      classifiers=['Development Status :: 4 - Beta',
+                   "Operating System :: OS Independent",
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
+                   ],
+      )

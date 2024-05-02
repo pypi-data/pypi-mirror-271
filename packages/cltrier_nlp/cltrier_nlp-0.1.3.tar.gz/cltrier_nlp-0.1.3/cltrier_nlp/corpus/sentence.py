3bd67d643e201a4732bc7595df3c0a454c0705b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 15:04:05 +0000

    Add package cltrier_nlp/0.1.3/cltrier_nlp-0.1.3.tar.gz

---
+++ packages/cltrier_nlp/cltrier_nlp-0.1.3.tar.gz/cltrier_nlp-0.1.3/cltrier_nlp/corpus/sentence.py
@@ -0,0 +1,49 @@
+import typing
+
+import pandas
+import pydantic
+
+from .. import functional
+
+
+class Sentence(pydantic.BaseModel):
+    raw: str
+
+    language: str = None
+    tokens: typing.List[str] = pydantic.Field(default_factory=lambda: [])
+
+    def model_post_init(self, __context) -> None:
+
+        self.raw = self.raw.replace("\n", " ").strip()
+
+        if not self.language:
+            self.language = functional.text.detect_language(self.raw)
+
+        if not self.tokens:
+            self.tokens = functional.text.tokenize(self.raw)
+
+    @pydantic.computed_field
+    @property
+    def bigrams(self) -> typing.List[typing.Tuple[str, ...]]:
+        return functional.text.ngrams(self.tokens, 2)
+
+    @pydantic.computed_field
+    @property
+    def trigrams(self) -> typing.List[typing.Tuple[str, ...]]:
+        return functional.text.ngrams(self.tokens, 3)
+
+    @pydantic.computed_field
+    @property
+    def tetragram(self) -> typing.List[typing.Tuple[str, ...]]:
+        return functional.text.ngrams(self.tokens, 4)
+
+    @pydantic.computed_field
+    @property
+    def pentagram(self) -> typing.List[typing.Tuple[str, ...]]:
+        return functional.text.ngrams(self.tokens, 5)
+
+    def to_row(self) -> pandas.Series:
+        return pandas.Series(self.model_dump())
+
+    def __len__(self) -> int:
+        return len(self.tokens)

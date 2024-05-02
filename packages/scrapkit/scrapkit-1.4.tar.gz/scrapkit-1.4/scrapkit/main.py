0d6b2097c40b50226e4b0c101f4ad285f1571286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:04:27 +0000

    Add package scrapkit/1.4/scrapkit-1.4.tar.gz

---
+++ packages/scrapkit/scrapkit-1.4.tar.gz/scrapkit-1.4/scrapkit/main.py
@@ -0,0 +1,61 @@
+# webscraper.py
+
+import requests
+from bs4 import BeautifulSoup
+
+def getTitle(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        title = soup.title.string
+        return title
+    else:
+        print("Failed to fetch title. Status code:", response.status_code)
+        return None
+
+def getHTML(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        print("Failed to fetch HTML content. Status code:", response.status_code)
+        return None
+
+def getText(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        text = soup.get_text()
+        return text
+    else:
+        print("Failed to fetch text content. Status code:", response.status_code)
+        return None
+
+def saveHTML(url, filename="output.html"):
+    html_content = getHTML(url)
+    if html_content:
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(html_content)
+        print(f"HTML content saved to {filename}")
+    else:
+        print("Failed to save HTML content.")
+
+def getLinks(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        links = [link.get('href') for link in soup.find_all('a')]
+        return links
+    else:
+        print("Failed to fetch links. Status code:", response.status_code)
+        return None
+
+# def getImageUrls(url):
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         soup = BeautifulSoup(response.text, 'html.parser')
+#         img_urls = [img.get('src') for img in soup.find_all('img')]
+#         return img_urls
+#     else:
+#         print("Failed to fetch image URLs. Status code:", response.status_code)
+#         return None

4cd0aacdebfc0b81d2d54f52a52c4953e20b09ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 16:04:07 +0000

    Add package timeseriesflattener/2.2.1/timeseriesflattener-2.2.1-py3-none-any.whl

---
+++ packages/timeseriesflattener/timeseriesflattener-2.2.1-py3-none-any.whl/timeseriesflattener/frame_utilities/_horisontally_concat.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import polars as pl
+from iterpy.iter import Iter
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+
+def horizontally_concatenate_dfs(
+    dfs: Sequence[pl.LazyFrame], pred_time_uuid_col_name: str
+) -> pl.LazyFrame:
+    dfs_without_identifiers = Iter(dfs).map(lambda df: df.drop([pred_time_uuid_col_name])).to_list()
+
+    return pl.concat([dfs[0], *dfs_without_identifiers[1:]], how="horizontal")

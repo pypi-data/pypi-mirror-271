8dc4db875617ac3c2e025b5831f95cc7e55956e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 05:04:02 +0000

    Add package dwdhandler/0.2.2/dwdhandler-0.2.2-py3-none-any.whl

---
+++ packages/dwdhandler/dwdhandler-0.2.2-py3-none-any.whl/dwdhandler/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.2.2'
+
+from .dow_handler import dow_handler
+from .plotting import plotconstr
+from .calculation import calc_stats
+from .calculation import return_period
+from .calculation import geo

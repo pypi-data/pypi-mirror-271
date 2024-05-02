f5b967a15aa529e892527b06c0b81887bab69fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 03:04:14 +0000

    Add package pyranges/0.1.0/pyranges-0.1.0.tar.gz

---
+++ packages/pyranges/pyranges-0.1.0.tar.gz/pyranges-0.1.0/pyranges/methods/merge.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sorted_nearest import find_clusters  # type: ignore[import]
+
+from pyranges.core.names import END_COL, START_COL
+
+
+def _merge(df: pd.DataFrame, **kwargs) -> pd.DataFrame:
+    if df.empty:
+        return df
+
+    slack = kwargs.get("slack", 0)
+    by = kwargs["by"]
+
+    cdf = df.sort_values(START_COL)
+
+    starts, ends, number = find_clusters(cdf.Start.values, cdf.End.values, slack)
+
+    by_values = df.head(1).squeeze()[by].to_dict()
+
+    cluster_df = pd.DataFrame(
+        {
+            START_COL: starts,
+            END_COL: ends,
+        }
+        | by_values,
+    )
+    # Sort columns in the original order of the dataframe.
+    cluster_df = cluster_df[[c for c in cdf.columns if c in cluster_df]]
+
+    if kwargs["count_col"]:
+        cluster_df.insert(cluster_df.shape[1], kwargs["count_col"], number)
+
+    return cluster_df

e31c4e84668870c4c232c46b76b2f840f1ce92a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:04:37 +0000

    Add package daxa/0.0.5/daxa-0.0.5-py3-none-any.whl

---
+++ packages/daxa/daxa-0.0.5-py3-none-any.whl/daxa/__init__.py
@@ -0,0 +1,5 @@
+#  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
+#  Last modified by David J Turner (turne540@msu.edu) 15/04/2024, 14:49. Copyright (c) The Contributors
+
+from .config import daxa_conf, OUTPUT, NUM_CORES, sb_rate
+from .mission.xmm import *

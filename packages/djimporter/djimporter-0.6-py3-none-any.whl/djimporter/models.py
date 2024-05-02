0e7ae8f9431fcd2b4fd44346458ccfe06f4ab938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 16:04:22 +0000

    Add package djimporter/0.6/djimporter-0.6-py3-none-any.whl

---
+++ packages/djimporter/djimporter-0.6-py3-none-any.whl/djimporter/models.py
@@ -0,0 +1,35 @@
+import json
+
+from django.db import models
+
+
+class AbstractBaseLog(models.Model):
+    CREATED = 'created'
+    RUNNING = 'running'
+    FAILED = 'failed'
+    COMPLETED = 'completed'
+    STATUS_CHOICES = (
+        (CREATED, 'CREATED'),
+        (RUNNING, 'RUNNING'),
+        (FAILED, 'FAILED'),
+        (COMPLETED, 'COMPLETED'),
+    )
+
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.CharField(max_length=512)
+
+    errors = models.TextField(blank=True)
+    num_rows = models.IntegerField(null=True)
+    input_file = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
+
+    def list_errors(self):
+        return json.loads(self.errors or "[]")
+
+
+class ImportLog(AbstractBaseLog):
+    pass

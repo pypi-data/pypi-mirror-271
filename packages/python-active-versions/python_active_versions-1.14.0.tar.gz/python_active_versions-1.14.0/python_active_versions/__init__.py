3319d525e79e403de5f61d379ccda5cc1fe465a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 06:03:34 +0000

    Add package python-active-versions/1.14.0/python_active_versions-1.14.0.tar.gz

---
+++ packages/python-active-versions/python_active_versions-1.14.0.tar.gz/python_active_versions-1.14.0/python_active_versions/__init__.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2023 - 2024 Gabriele Pongelli
+#
+# SPDX-License-Identifier: MIT
+
+"""Top-level package for python active versions."""
+import logging
+
+from python_active_versions.bundle import get_bundle_dir
+
+try:
+    from icecream import ic, install
+
+    # installing icecream
+    install()
+    ic.configureOutput(outputFunction=logging.debug, includeContext=True)
+except ImportError:  # Graceful fallback if IceCream isn't installed.
+    ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa  # pylint: disable=C3001
+
+__author__ = "Gabriele Pongelli"
+__email__ = "gabriele.pongelli@gmail.com"
+__version__ = "1.14.0"
+
+__description__ = "Gather active python versions."
+__project_name__ = "python-active-versions"

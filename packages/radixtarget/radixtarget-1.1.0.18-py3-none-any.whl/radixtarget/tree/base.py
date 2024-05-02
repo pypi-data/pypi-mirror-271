71a8b42dbe3699bb001ce692cc6a4b65f4836fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 06:03:47 +0000

    Add package radixtarget/1.1.0.18/radixtarget-1.1.0.18-py3-none-any.whl

---
+++ packages/radixtarget/radixtarget-1.1.0.18-py3-none-any.whl/radixtarget/tree/base.py
@@ -0,0 +1,12 @@
+class RadixTreeNode:
+    __slots__ = ("children", "host", "data")
+
+    def __init__(self):
+        self.children = {}
+        self.host = None
+        self.data = None
+
+
+class BaseRadixTree:
+    def __init__(self):
+        self.root = RadixTreeNode()

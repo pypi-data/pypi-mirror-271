14a6c590c31afb0a5a737e095449dfb4e950f40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:03:15 +0000

    Add package pyfujitsugeneral/2.0.19/pyfujitsugeneral-2.0.19.tar.gz

---
+++ packages/pyfujitsugeneral/pyfujitsugeneral-2.0.19.tar.gz/pyfujitsugeneral-2.0.19/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+__version__ = "2.0.19"
+
+setup(
+    name="pyfujitsugeneral",
+    py_modules=["pyfujitsugeneral"],
+    version=__version__,
+    description="Python Library for interacting with Fujitsu General split air conditioners API",
+    long_description="Python Library for interacting with Fujitsu General split air conditioners API",
+    author="Fabio Mauro, Mehdi Modarressi",
+    author_email="bigmoby.pymeianlike@gmail.com",
+    url="https://github.com/bigmoby/pyfujitsugeneral",
+    download_url="https://github.com/bigmoby/pyfujitsugeneral",
+    license="MIT License",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["Fujitsu General air conditioners"],
+    packages=["pyfujitsugeneral"],
+    include_package_data=True,
+    install_requires=["requests", "certifi", "chardet", "idna", "urllib3", "numpy"],
+    python_requires=">=3.11",
+)

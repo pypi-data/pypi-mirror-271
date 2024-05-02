bda685fd9bb960f6b466b2acb5f4e39b82ad29a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 14:03:24 +0000

    Add package pyrit/0.2.1/pyrit-0.2.1.tar.gz

---
+++ packages/pyrit/pyrit-0.2.1.tar.gz/pyrit-0.2.1/pyrit/common/path.py
@@ -0,0 +1,20 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+import pathlib
+
+PYRIT_PATH = pathlib.Path(__file__, "..", "..").resolve()
+# Path to where all the results files will be stores
+RESULTS_PATH = pathlib.Path(PYRIT_PATH, "..", "results").resolve()
+RESULTS_PATH.mkdir(parents=True, exist_ok=True)
+# Path to where the logs are located
+LOG_PATH = pathlib.Path(PYRIT_PATH, "..", "results", "logs.txt").resolve()
+LOG_PATH.touch(exist_ok=True)
+
+DATASETS_PATH = pathlib.Path(PYRIT_PATH, "datasets").resolve()
+CONTENT_CLASSIFIERS_PATH = pathlib.Path(DATASETS_PATH, "score", "content_classifiers").resolve()
+LIKERT_SCALES_PATH = pathlib.Path(DATASETS_PATH, "score", "likert_scales").resolve()
+SCORING_INSTRUCTIONS_PATH = pathlib.Path(DATASETS_PATH, "score", "scoring_instructions").resolve()
+
+# Points to the root of the project
+HOME_PATH = pathlib.Path(PYRIT_PATH, "..").resolve()

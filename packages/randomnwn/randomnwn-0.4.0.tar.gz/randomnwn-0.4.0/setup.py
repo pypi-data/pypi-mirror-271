a0d2431c0f571488f6efec33ab4b0b95d685901b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 06:03:44 +0000

    Add package randomnwn/0.4.0/randomnwn-0.4.0.tar.gz

---
+++ packages/randomnwn/randomnwn-0.4.0.tar.gz/randomnwn-0.4.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+namespace = {}
+with open("randomnwn/version.py", "r") as f:
+    exec(f.read(), namespace)
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='randomnwn',
+    version=namespace["__version__"],    
+    description='Modelling and analyzing random nanowire networks in Python.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/marcus-k/Random-NWNs',
+    author='Marcus Kasdorf',
+    author_email='marcus.kasdorf@ucalgary.ca',
+    license='MIT License',
+    packages=['randomnwn'],
+    python_requires='>=3.10'
+)

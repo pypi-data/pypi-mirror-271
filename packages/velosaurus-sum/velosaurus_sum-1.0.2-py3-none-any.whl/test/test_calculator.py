510d53ba2a569058ccf5a141d0076fbbfadc0a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:04:33 +0000

    Add package velosaurus-sum/1.0.2/velosaurus_sum-1.0.2-py3-none-any.whl

---
+++ packages/velosaurus-sum/velosaurus_sum-1.0.2-py3-none-any.whl/test/test_calculator.py
@@ -0,0 +1,15 @@
+import unittest
+
+from src.calculator import sum
+
+
+class TestCalculator(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(sum(2, 3), 5)
+        self.assertEqual(sum(-1, 1), 0)
+        self.assertEqual(sum(0, 0), 0)
+        self.assertEqual(sum(10, -5), 5)
+
+
+if __name__ == "__main__":
+    unittest.main()

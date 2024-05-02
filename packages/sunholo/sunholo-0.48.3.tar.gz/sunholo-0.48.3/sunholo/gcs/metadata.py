a6dcf5d25a0065287ec13c88235e77009f2ab85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 03:04:49 +0000

    Add package sunholo/0.48.3/sunholo-0.48.3.tar.gz

---
+++ packages/sunholo/sunholo-0.48.3.tar.gz/sunholo-0.48.3/sunholo/gcs/metadata.py
@@ -0,0 +1,30 @@
+from google.cloud import storage
+from ..logging import log
+from ..utils.gcp import is_running_on_gcp
+
+
+def get_object_metadata(bucket_name, object_name):
+
+    if not is_running_on_gcp():
+        return None
+
+    if bucket_name is None or object_name is None:
+        log.warning("Got invalid bucket name and object name")
+        return None
+    try:
+        storage_client = storage.Client()
+    except Exception as e:
+        log.warning(f"Could not connect to Google Cloud Storage for metadata: {str(e)}")
+        return None
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(object_name)
+
+    # Fetch the blob's metadata
+    blob.reload()  # Make sure to reload the blob to get the most up-to-date metadata
+
+    # Access custom metadata
+    custom_metadata = blob.metadata
+
+    log.info(f"Custom Metadata for {object_name}: {custom_metadata}")
+    return custom_metadata

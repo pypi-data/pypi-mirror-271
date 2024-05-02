9b8eccad92b52ce5c4e90c02e0671e3430288adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:03:09 +0000

    Add package financepy/0.360/financepy-0.360.tar.gz

---
+++ packages/financepy/financepy-0.360.tar.gz/financepy-0.360/tests/test_FinEquityForward.py
@@ -0,0 +1,43 @@
+###############################################################################
+# Copyright (C) 2018, 2019, 2020 Dominic O'Kane
+###############################################################################
+
+from financepy.market.curves.discount_curve_flat import DiscountCurveFlat
+from financepy.utils.global_types import FinLongShort
+from financepy.utils.date import Date
+from financepy.products.equity.equity_forward import EquityForward
+
+
+def test_equity_forward():
+
+    value_dt = Date(13, 2, 2018)
+    expiry_dt = value_dt.add_months(12)
+
+    stock_price = 130.0
+    forward_price = 125.0  # Locked
+    discount_rate = 0.05
+    dividend_rate = 0.02
+
+    expiry_dt = value_dt.add_months(12)
+    notional = 100.0
+
+    discount_curve = DiscountCurveFlat(value_dt, discount_rate)
+    dividend_curve = DiscountCurveFlat(value_dt, dividend_rate)
+
+    equityForward = EquityForward(expiry_dt,
+                                  forward_price,
+                                  notional,
+                                  FinLongShort.LONG)
+
+    fwdPrice = equityForward.forward(value_dt,
+                                     stock_price,
+                                     discount_curve,
+                                     dividend_curve)
+
+    fwdValue = equityForward.value(value_dt,
+                                   stock_price,
+                                   discount_curve,
+                                   dividend_curve)
+
+    assert round(fwdPrice, 4) == 133.9591
+    assert round(fwdValue, 4) == 852.2149

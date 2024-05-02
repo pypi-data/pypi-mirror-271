b907ec289112d3127ede5856877b366746b128f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 04:03:11 +0000

    Add package nodespecs/0.0.8/nodespecs-0.0.8.tar.gz

---
+++ packages/nodespecs/nodespecs-0.0.8.tar.gz/nodespecs-0.0.8/src/specs/__init__.py
@@ -0,0 +1,8 @@
+from .hardware import *
+from . import cpuinfo
+from .benchmark import (bench_cpu,info_plat)
+from .communicate.tcpsocket import (server,client)
+# Version of the node-specs package
+__version__ = "0.0.8"
+
+

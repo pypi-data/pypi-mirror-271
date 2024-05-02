ee497e9e8494e83d4fb9b1f9c1c082395deeeadc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 16:03:54 +0000

    Add package flask-labtest/1.0.1/flask_labtest-1.0.1.tar.gz

---
+++ packages/flask-labtest/flask_labtest-1.0.1.tar.gz/flask_labtest-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask_labtest',
+    version='1.0.1',
+    description='A simple package that prints "Hello, World!"',
+    author='Abhisht Chouhan',
+    author_email='abhishtchouhan@gmail.com',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)

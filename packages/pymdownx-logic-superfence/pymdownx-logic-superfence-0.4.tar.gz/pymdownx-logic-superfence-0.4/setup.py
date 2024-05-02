6fcaef98611306d14b00004094b22d47aeb3f6bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-02 16:04:24 +0000

    Add package pymdownx-logic-superfence/0.4/pymdownx-logic-superfence-0.4.tar.gz

---
+++ packages/pymdownx-logic-superfence/pymdownx-logic-superfence-0.4.tar.gz/pymdownx-logic-superfence-0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pymdownx-logic-superfence',
+    version='0.4',
+    packages=find_packages(),
+    author='Davy Cottet',
+    description='A superfence pymarkdown extension for logic circuit simulator',
+    url='https://gitlab.com/davy39/logic-superfence',
+    license='GNU General Public License v3.0',
+    platforms=['Any'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    ],
+)
+
+

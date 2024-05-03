fead0323fec18f14d2bf6e6fe27ed146ed41fd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 04:04:29 +0000

    Add package quantumz/0.0.2/quantumz-0.0.2.tar.gz

---
+++ packages/quantumz/quantumz-0.0.2.tar.gz/quantumz-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(name="quantumz",
+	version="0.0.2",
+	author="Sergei Zuev",
+	author_email="shoukhov@mail.ru",
+	description="A module for quantum computing emulations",
+	packages=setuptools.find_packages(),
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	classifiers=[
+		"Programming Language :: Python :: 3.10",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
+

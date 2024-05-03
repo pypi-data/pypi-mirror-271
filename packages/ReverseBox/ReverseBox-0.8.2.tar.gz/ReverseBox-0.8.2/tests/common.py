1d426bdc42ea9734415b16623af02fc125233bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 03:04:08 +0000

    Add package ReverseBox/0.8.2/ReverseBox-0.8.2.tar.gz

---
+++ packages/ReverseBox/ReverseBox-0.8.2.tar.gz/ReverseBox-0.8.2/tests/common.py
@@ -0,0 +1,55 @@
+"""
+Copyright © 2022  Bartłomiej Duda
+License: GPL-3.0 License
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class CRCTestEntry:
+    test_data: bytes
+    expected_int: int
+    expected_str: str
+
+
+@dataclass
+class XORTestEntry:
+    test_data: bytes
+    key: bytes
+    expected_result: bytes
+
+
+@dataclass
+class XORGuesserEntry:
+    encrypted_data: bytes
+    decrypted_data: bytes
+    max_key_length: int
+    expected_xor_key: bytes
+
+
+@dataclass
+class ROT13TestEntry:
+    test_data: bytes
+    key: bytes
+    expected_result: bytes
+
+
+@dataclass
+class XORRetro64ECOTestEntry:
+    test_data: bytes
+    key: int
+    expected_result: bytes
+
+
+@dataclass
+class HashTestEntry:
+    test_data: bytes
+    expected_result: bytes
+
+
+@dataclass
+class PaddingTestEntry:
+    test_offset: int
+    test_div: int
+    expected_padding: int

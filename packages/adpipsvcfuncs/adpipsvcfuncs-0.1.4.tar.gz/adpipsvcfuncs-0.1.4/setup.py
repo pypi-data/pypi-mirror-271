185b8b6027f6504c2eeebf389dc9213650b98df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 05:03:59 +0000

    Add package adpipsvcfuncs/0.1.4/adpipsvcfuncs-0.1.4.tar.gz

---
+++ packages/adpipsvcfuncs/adpipsvcfuncs-0.1.4.tar.gz/adpipsvcfuncs-0.1.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="adpipsvcfuncs",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=[],
+    author="Oleg D",
+    author_email="olegd@gmail.com",
+    description="Service functions for the Adaptive Pipeline Workflow project.",
+    license="MIT",
+    keywords="GenAI, GCP",
+    url="https://github.com/odegay/adaptive-pipeline-service-functions",            
+)

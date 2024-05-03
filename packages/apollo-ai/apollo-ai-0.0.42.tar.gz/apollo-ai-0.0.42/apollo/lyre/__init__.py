8e224a419792bf78c233ab96a1608012c835b144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 04:04:07 +0000

    Add package apollo-ai/0.0.42/apollo-ai-0.0.42.tar.gz

---
+++ packages/apollo-ai/apollo-ai-0.0.42.tar.gz/apollo-ai-0.0.42/apollo/lyre/__init__.py
@@ -0,0 +1,12 @@
+from .lyre import (
+    record_voice,
+    record_ontime,
+    save_to_file,
+    convert_to_audio,
+    full_signal,
+    freq_analysis,
+    mel_freq_cepstral,
+    chroma_features,
+    spectral_features,
+    onset_detection,
+)

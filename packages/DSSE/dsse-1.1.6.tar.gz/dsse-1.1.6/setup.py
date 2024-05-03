dcde9f00d220f8e0f6a8c26c01016064da0c9f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 05:03:31 +0000

    Add package DSSE/1.1.6/dsse-1.1.6.tar.gz

---
+++ packages/DSSE/dsse-1.1.6.tar.gz/dsse-1.1.6/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+download_url = f"https://github.com/pfeinsper/drone-swarm-search/archive/refs/tags/v1.1.6.tar.gz"
+setup(
+    version="1.1.6",
+    download_url=download_url,
+)

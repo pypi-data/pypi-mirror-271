f8474fb52482734cd5298fd59c18f0b0a470d57d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 04:03:29 +0000

    Add package hypll/0.1.1/hypll-0.1.1-py3-none-any.whl

---
+++ packages/hypll/hypll-0.1.1-py3-none-any.whl/hypll/nn/__init__.py
@@ -0,0 +1,9 @@
+from .modules.activation import HReLU
+from .modules.batchnorm import HBatchNorm, HBatchNorm2d
+from .modules.change_manifold import ChangeManifold
+from .modules.container import TangentSequential
+from .modules.convolution import HConvolution2d
+from .modules.embedding import HEmbedding
+from .modules.flatten import HFlatten
+from .modules.linear import HLinear
+from .modules.pooling import HAvgPool2d, HMaxPool2d

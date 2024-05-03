18dc21b5cb240a95592515af331190c112d2b757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 03:03:58 +0000

    Add package aitelegrambot/0.7.0/aitelegrambot-0.7.0-py2.py3-none-any.whl

---
+++ packages/aitelegrambot/aitelegrambot-0.7.0-py2.py3-none-any.whl/aitelegrambot/constants.py
@@ -0,0 +1,20 @@
+from importlib.metadata import version
+
+WELCOME_MESSAGE: str = f"""
+Hello! I am running aitelegrambot v{version('aitelegrambot')}.
+
+To inference something please run, /infer.
+
+I am licensed under the GPLv3. For more information, Please find the source code at https://github.com/tusharhero/aitelegrambot.git/
+"""
+HELP_MESSAGE: str = """
+*Normal commands*:
+- /start
+- /infer <query>
+- /help
+*Administration commands*:
+- /list\_models
+- /change\_model <model\_name>
+- /pull\_model <model\_name>
+- /delete\_model <model\_name>
+"""

8e29bfd002cb13fc0dfb5e431173682851afe9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 03:04:04 +0000

    Add package logic-superfence/0.2/logic_superfence-0.2.tar.gz

---
+++ packages/logic-superfence/logic_superfence-0.2.tar.gz/logic_superfence-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='logic_superfence',
+    version='0.2',
+    packages = find_packages(),
+    author='Davy Cottet',
+    description='A superfence pymarkdown extension for logic circuit simulator',
+    url='https://nsi.forge.apps.education.fr/mkdocs-logic',
+    license='GNU General Public License v3.0',
+    platforms=['Any'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    ],
+)
+
+

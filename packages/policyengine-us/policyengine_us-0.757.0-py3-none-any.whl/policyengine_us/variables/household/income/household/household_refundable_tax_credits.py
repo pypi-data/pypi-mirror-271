9fa581a71d23fe10560dfe9d6554b37bed77f908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 05:04:01 +0000

    Add package policyengine-us/0.757.0/policyengine_us-0.757.0-py3-none-any.whl

---
+++ packages/policyengine-us/policyengine_us-0.757.0-py3-none-any.whl/policyengine_us/variables/household/income/household/household_refundable_tax_credits.py
@@ -0,0 +1,11 @@
+from policyengine_us.model_api import *
+
+
+class household_refundable_tax_credits(Variable):
+    value_type = float
+    entity = Household
+    label = "refundable tax credits"
+    definition_period = YEAR
+    unit = USD
+
+    adds = "gov.household_refundable_credits"

1371ecfb32dff7bdc0fcf2fda4aa509e874e1ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 04:03:30 +0000

    Add package traveltimepy/3.9.7/traveltimepy-3.9.7.tar.gz

---
+++ packages/traveltimepy/traveltimepy-3.9.7.tar.gz/traveltimepy-3.9.7/tests/routes_test.py
@@ -0,0 +1,70 @@
+from typing import List
+
+import pytest
+from datetime import datetime
+
+from traveltimepy import PublicTransport, Driving, Location, Coordinates
+from traveltimepy.dto.common import SnapPenalty
+from traveltimepy.sdk import TravelTimeSdk
+
+
+@pytest.mark.asyncio
+async def test_departures(sdk: TravelTimeSdk, locations):
+    results = await sdk.routes_async(
+        locations=locations,
+        search_ids={
+            "London center": ["Hyde Park", "ZSL London Zoo"],
+            "ZSL London Zoo": ["Hyde Park", "London center"],
+        },
+        transportation=PublicTransport(),
+        departure_time=datetime.now(),
+    )
+    assert len(results) == 2
+
+
+@pytest.mark.asyncio
+async def test_arrivals(sdk: TravelTimeSdk, locations):
+    results = await sdk.routes_async(
+        locations=locations,
+        search_ids={
+            "London center": ["Hyde Park", "ZSL London Zoo"],
+            "ZSL London Zoo": ["Hyde Park", "London center"],
+        },
+        transportation=PublicTransport(),
+        departure_time=datetime.now(),
+    )
+    assert len(results) == 2
+
+
+@pytest.mark.asyncio
+async def test_snap_penalty(sdk: TravelTimeSdk):
+    locations: List[Location] = [
+        Location(id="A", coords=Coordinates(lat=53.806479, lng=-2.615711)),
+        Location(id="B", coords=Coordinates(lat=53.810129, lng=-2.601099)),
+    ]
+    result_with_penalty = await sdk.routes_async(
+        locations=locations,
+        search_ids={
+            "A": ["B"],
+        },
+        transportation=Driving(),
+        departure_time=datetime.now(),
+    )
+    traveltime_with_penalty = (
+        result_with_penalty[0].locations[0].properties[0].travel_time
+    )
+    result_without_penalty = await sdk.routes_async(
+        locations=locations,
+        search_ids={
+            "A": ["B"],
+        },
+        transportation=Driving(),
+        departure_time=datetime.now(),
+        snap_penalty=SnapPenalty.DISABLED,
+    )
+    traveltime_without_penalty = (
+        result_without_penalty[0].locations[0].properties[0].travel_time
+    )
+    assert traveltime_with_penalty is not None
+    assert traveltime_without_penalty is not None
+    assert traveltime_with_penalty > traveltime_without_penalty

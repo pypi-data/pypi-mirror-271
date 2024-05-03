d3e655da5805f923a7a610cd2b355e5e24153828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 16:04:36 +0000

    Add package my-data-quality-library/0.1/my_data_quality_library-0.1.tar.gz

---
+++ packages/my-data-quality-library/my_data_quality_library-0.1.tar.gz/my_data_quality_library-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_data_quality_library',
+    version='0.1',
+    packages=find_packages(),
+    description='A library for data quality checks in Databricks notebooks',
+    author='Harun Raseed Basheer',
+    author_email='harun.raseed093@gmail.com',
+    url='https://github.com/your_username/my_data_quality_library',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)

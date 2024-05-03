6499238355e28d952305c985db27be15fdae2087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 14:04:43 +0000

    Add package seesoink/1.9/seesoink-1.9.tar.gz

---
+++ packages/seesoink/seesoink-1.9.tar.gz/seesoink-1.9/seesoink/__init__.py
@@ -0,0 +1,3 @@
+from .seesoink import seesoink1, seesoink2, seesoink3, seesoink4, seesoink5
+
+__all__ = ['seesoink1', 'seesoink2', 'seesoink3', 'seesoink4', 'seesoink5']
---
+++ packages/seesoink/seesoink-1.9.tar.gz/seesoink-1.9/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name='seesoink',
+    version='1.9',
+    author='Programmer Golden ',
+    description='Module By @rrrrrf In telegram ',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)

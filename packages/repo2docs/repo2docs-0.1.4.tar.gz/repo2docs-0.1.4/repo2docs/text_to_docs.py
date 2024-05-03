45a4c6bc9eae4d35039ecd6f5b046aaee06be0f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 15:04:53 +0000

    Add package repo2docs/0.1.4/repo2docs-0.1.4.tar.gz

---
+++ packages/repo2docs/repo2docs-0.1.4.tar.gz/repo2docs-0.1.4/repo2docs/text_to_docs.py
@@ -0,0 +1,59 @@
+import os
+from dotenv import load_dotenv
+from repo2docs.llm import OpenAIClient, AnthropicClient
+from repo2docs.prompts import documentation_prompt, diagram_prompt, mobile_prompt, database_prompt
+
+load_dotenv()
+
+
+class TextToDocs:
+    """A class to convert text to documentation using a language model API."""
+
+    def __init__(self, api_choice):
+        if api_choice == "anthropic":
+            api_key = os.getenv("ANTHROPIC_API_KEY")
+            self.client = AnthropicClient(api_key=api_key)
+        elif api_choice == "openai":
+            api_key = os.getenv("OPENAI_API_KEY")
+            self.client = OpenAIClient(api_key=api_key)
+        else:
+            raise ValueError(
+                "Invalid API choice. Please choose either 'anthropic' or 'openai'."
+            )
+
+    def generate_docs(self, repo_txt):
+        """Generate documentation from the text."""
+        response = self.client.generate_response(
+            prompt=documentation_prompt,
+            messages=[{"role": "user", "content": repo_txt}],
+        )
+        return response
+
+    def generate_diagram(self, repo_txt):
+        """Generate a diagram from the text."""
+        response = self.client.generate_response(
+            prompt=diagram_prompt, messages=[{"role": "user", "content": repo_txt}]
+        )
+        return response
+
+    def generate_mobile(self, repo_txt):
+        """Generate a mobile application documentation from the text."""
+        response = self.client.generate_response(
+            prompt=mobile_prompt, messages=[{"role": "user", "content": repo_txt}]
+        )
+        return response
+
+    def generate_database(self, repo_txt):
+        """Generate a database ERD from the text."""
+        response = self.client.generate_response(
+            prompt=database_prompt, messages=[{"role": "user", "content": repo_txt}]
+        )
+        return response
+
+    def generate_custom(self, repo_txt, prompt):
+        """Generate a custom documentation from the text."""
+        response = self.client.generate_response(
+            prompt=prompt, messages=[{"role": "user", "content": repo_txt}]
+        )
+        return response
+

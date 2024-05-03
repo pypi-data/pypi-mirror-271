5764ee66e377f7aa8e0e5ec51d2119349b1d2db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 04:04:28 +0000

    Add package easyknn/0.4.0/easyknn-0.4.0.tar.gz

---
+++ packages/easyknn/easyknn-0.4.0.tar.gz/easyknn-0.4.0/easyknn/__init__.py
@@ -0,0 +1,10 @@
+from easyknn.knn import AnnoyBackend, EasyKNN, EmbeddingsIndexBuilder, SKLearnKNNBackend
+
+__version__ = "0.3.1"
+
+__all__ = [
+    "EmbeddingsIndexBuilder",
+    "EasyKNN",
+    "AnnoyBackend",
+    "SKLearnKNNBackend",
+]

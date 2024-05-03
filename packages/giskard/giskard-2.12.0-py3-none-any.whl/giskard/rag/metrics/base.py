2e47506ef5467eca68f36c72d5031d93a33647ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 16:04:43 +0000

    Add package giskard/2.12.0/giskard-2.12.0-py3-none-any.whl

---
+++ packages/giskard/giskard-2.12.0-py3-none-any.whl/giskard/rag/metrics/base.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+from ...llm.client.base import LLMClient
+
+
+class Metric(ABC):
+    """
+    Metric base class. All metrics should inherit from this class and implement the __call__ method.
+    The instances of this class can be passed to the evaluate method.
+    """
+
+    def __init__(self, name: str, llm_client: LLMClient = None) -> None:
+        self.name = name
+        self._llm_client = llm_client
+
+    @abstractmethod
+    def __call__(self, question_sample: dict, answer: str):
+        """
+        Compute the metric on a single question and its associated answer.
+
+        Parameters
+        ----------
+        question_sample : dict
+            A question sample from a QATestset.
+        answer : Sequence[str]
+            The agent answer on that question.
+
+        Returns
+        -------
+        dict
+            The result of the metric. The keys should be the names of the metrics computed.
+        """
+        pass

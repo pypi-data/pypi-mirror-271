273442e23846ee261d3e5484e41bac42f0866ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 05:04:09 +0000

    Add package mx-platform-python/0.24.0/mx_platform_python-0.24.0.tar.gz

---
+++ packages/mx-platform-python/mx_platform_python-0.24.0.tar.gz/mx_platform_python-0.24.0/mx_platform_python/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from mx_platform_python.api.mx_platform_api import MxPlatformApi
+from mx_platform_python.api.spending_plan_api import SpendingPlanApi
+

b14186be57587d817049ccd9cec2d9ff91225e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 03:03:51 +0000

    Add package datasetron/0.0.1/datasetron-0.0.1.tar.gz

---
+++ packages/datasetron/datasetron-0.0.1.tar.gz/datasetron-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='datasetron',
+    version='0.0.1',
+    packages=find_packages(),
+    description='This package name is reserved',
+    long_description='This package name is reserved for internal use. If you want to claim this name for legitimate purposes, please contact us at security@toloka.ai',
+    url='https://www.example.com',
+    author='solesensei',
+    author_email='solergfd@gmail.com',
+    classifiers=['Development Status :: 1 - Planning'],
+)

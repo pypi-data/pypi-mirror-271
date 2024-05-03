a0a28bf46b670ea3652b09fa8dc6f6b0a2271cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 03:04:14 +0000

    Add package pupil-labs-realtime-api/1.2.1/pupil_labs_realtime_api-1.2.1-py3-none-any.whl

---
+++ packages/pupil-labs-realtime-api/pupil_labs_realtime_api-1.2.1-py3-none-any.whl/pupil_labs/realtime_api/device.py
@@ -0,0 +1,239 @@
+import asyncio
+import inspect
+import json
+import logging
+import types
+import typing as T
+
+import aiohttp
+import numpy as np
+import websockets
+
+import pupil_labs  # noqa: F401
+
+from .base import DeviceBase
+from .models import (
+    APIPath,
+    Component,
+    Event,
+    Status,
+    UnknownComponentError,
+    parse_component,
+)
+
+logger = logging.getLogger(__name__)
+
+UpdateCallbackSync = T.Callable[["pupil_labs.realtime_api.models.Component"], None]
+"""Type annotation for synchronous update callbacks"""
+
+UpdateCallbackAsync = T.Callable[
+    ["pupil_labs.realtime_api.models.Component"], T.Awaitable[None]
+]
+"""Type annotation for asynchronous update callbacks"""
+
+UpdateCallback = T.Union[UpdateCallbackSync, UpdateCallbackAsync]
+"""Type annotation for synchronous and asynchronous callbacks"""
+
+
+class DeviceError(Exception):
+    pass
+
+
+class Device(DeviceBase):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self._create_client_session()
+
+    async def get_status(self) -> Status:
+        """
+        :raises pupil_labs.realtime_api.device.DeviceError: if the request fails
+        """
+        async with self.session.get(self.api_url(APIPath.STATUS)) as response:
+            confirmation = await response.json()
+            if response.status != 200:
+                raise DeviceError(response.status, confirmation["message"])
+            result = confirmation["result"]
+            logger.debug(f"[{self}.get_status] Received status: {result}")
+            return Status.from_dict(result)
+
+    async def status_updates(self) -> T.AsyncIterator[Component]:
+        # Auto-reconnect, see
+        # https://websockets.readthedocs.io/en/stable/reference/client.html#websockets.client.connect
+        websocket_status_endpoint = self.api_url(APIPath.STATUS, protocol="ws")
+        async for websocket in websockets.connect(websocket_status_endpoint):
+            try:
+                async for message_raw in websocket:
+                    message_json = json.loads(message_raw)
+                    try:
+                        component = parse_component(message_json)
+                    except UnknownComponentError:
+                        logger.warning(f"Dropping unknown component: {component}")
+                        continue
+                    yield component
+            except websockets.ConnectionClosed:
+                logger.debug("Websocket connection closed. Reconnecting...")
+                continue
+            except asyncio.CancelledError:
+                logger.debug("status_updates() cancelled")
+                break
+
+    async def recording_start(self) -> str:
+        """
+        :raises pupil_labs.realtime_api.device.DeviceError:
+            if the recording could not be started. Possible reasons include
+            - Recording already running
+            - Template has required fields
+            - Low battery
+            - Low storage
+            - No wearer selected
+            - No workspace selected
+            - Setup bottom sheets not completed
+        """
+        async with self.session.post(self.api_url(APIPath.RECORDING_START)) as response:
+            confirmation = await response.json()
+            logger.debug(f"[{self}.start_recording] Received response: {confirmation}")
+            if response.status != 200:
+                raise DeviceError(response.status, confirmation["message"])
+            return confirmation["result"]["id"]
+
+    async def recording_stop_and_save(self):
+        """
+        :raises pupil_labs.realtime_api.device.DeviceError:
+            if the recording could not be started
+            Possible reasons include
+            - Recording not running
+            - template has required fields
+        """
+        async with self.session.post(
+            self.api_url(APIPath.RECORDING_STOP_AND_SAVE)
+        ) as response:
+            confirmation = await response.json()
+            logger.debug(f"[{self}.stop_recording] Received response: {confirmation}")
+            if response.status != 200:
+                raise DeviceError(response.status, confirmation["message"])
+
+    async def recording_cancel(self):
+        """
+        :raises pupil_labs.realtime_api.device.DeviceError:
+            if the recording could not be started
+            Possible reasons include
+            - Recording not running
+        """
+        async with self.session.post(
+            self.api_url(APIPath.RECORDING_CANCEL)
+        ) as response:
+            confirmation = await response.json()
+            logger.debug(f"[{self}.stop_recording] Received response: {confirmation}")
+            if response.status != 200:
+                raise DeviceError(response.status, confirmation["message"])
+
+    async def send_event(
+        self, event_name: str, event_timestamp_unix_ns: T.Optional[int] = None
+    ) -> Event:
+        """
+        :raises pupil_labs.realtime_api.device.DeviceError: if sending the event fails
+        """
+        event: T.Dict[str, T.Any] = {"name": event_name}
+        if event_timestamp_unix_ns is not None:
+            event["timestamp"] = event_timestamp_unix_ns
+
+        async with self.session.post(
+            self.api_url(APIPath.EVENT), json=event
+        ) as response:
+            confirmation = await response.json()
+            logger.debug(f"[{self}.send_event] Received response: {confirmation}")
+            if response.status != 200:
+                raise DeviceError(response.status, confirmation["message"])
+            return Event.from_dict(confirmation["result"])
+
+    async def close(self):
+        await self.session.close()
+        self.session = None
+
+    async def __aenter__(self) -> "Device":
+        if self.session is None:
+            self._create_client_session()
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: T.Optional[T.Type[BaseException]],
+        exc_val: T.Optional[BaseException],
+        exc_tb: T.Optional[types.TracebackType],
+    ) -> None:
+        await self.close()
+
+    def _create_client_session(self):
+        self.session = aiohttp.ClientSession()
+
+    async def get_calibration(self) -> np.ndarray:
+        """
+        :raises pupil_labs.realtime_api.device.DeviceError: if the request fails
+        """
+        async with self.session.get(self.api_url(APIPath.CALIBRATION)) as response:
+            if response.status != 200:
+                raise DeviceError(response.status, "Failed to fetch calibration")
+
+            raw_data = await response.read()
+            return np.frombuffer(
+                raw_data,
+                np.dtype(
+                    [
+                        ("version", "u1"),
+                        ("serial", "6a"),
+                        ("scene_camera_matrix", "(3,3)d"),
+                        ("scene_distortion_coefficients", "8d"),
+                        ("scene_extrinsics_affine_matrix", "(4,4)d"),
+                        ("right_camera_matrix", "(3,3)d"),
+                        ("right_distortion_coefficients", "8d"),
+                        ("right_extrinsics_affine_matrix", "(4,4)d"),
+                        ("left_camera_matrix", "(3,3)d"),
+                        ("left_distortion_coefficients", "8d"),
+                        ("left_extrinsics_affine_matrix", "(4,4)d"),
+                        ("crc", "u4"),
+                    ]
+                ),
+            )
+
+
+class StatusUpdateNotifier:
+    def __init__(self, device: Device, callbacks: T.List[UpdateCallback]) -> None:
+        self._auto_update_task: T.Optional[asyncio.Task] = None
+        self._device = device
+        self._callbacks = callbacks
+
+    async def receive_updates_start(self) -> None:
+        if self._auto_update_task is not None:
+            logger.debug("Auto-update already started!")
+            return
+        self._auto_update_task = asyncio.create_task(self._auto_update())
+
+    async def receive_updates_stop(self):
+        if self._auto_update_task is None:
+            logger.debug("Auto-update is not running!")
+            return
+        self._auto_update_task.cancel()
+        try:
+            # wait for the task to be cancelled
+            await self._auto_update_task
+        except asyncio.CancelledError:
+            pass  # task has been successfully cancelled
+        self._auto_update_task = None
+
+    async def __aenter__(self):
+        await self.receive_updates_start()
+
+    async def __aexit__(
+        self,
+        exc_type: T.Optional[T.Type[BaseException]],
+        exc_val: T.Optional[BaseException],
+        exc_tb: T.Optional[types.TracebackType],
+    ):
+        await self.receive_updates_stop()
+
+    async def _auto_update(self) -> None:
+        async for changed in self._device.status_updates():
+            for callback in self._callbacks:
+                result = callback(changed)
+                if inspect.isawaitable(result):
+                    await result
---
+++ packages/pupil-labs-realtime-api/pupil_labs_realtime_api-1.2.1-py3-none-any.whl/pupil_labs/realtime_api/models.py
@@ -0,0 +1,281 @@
+import dataclasses
+import datetime
+import enum
+import logging
+import typing as T
+
+try:
+    from typing import Literal
+except ImportError:
+    # FIXME: Remove when dropping py3.7 support
+    from typing_extensions import Literal
+
+logger = logging.getLogger(__name__)
+
+
+class APIPath(enum.Enum):
+    STATUS = "/status"
+    RECORDING_START = "/recording:start"
+    RECORDING_STOP_AND_SAVE = "/recording:stop_and_save"
+    RECORDING_CANCEL = "/recording:cancel"
+    EVENT = "/event"
+    CALIBRATION = "/../calibration.bin"
+
+    def full_address(
+        self, address: str, port: int, protocol: str = "http", prefix: str = "/api"
+    ) -> str:
+        return f"{protocol}://{address}:{port}" + prefix + self.value
+
+
+class DiscoveredDeviceInfo(T.NamedTuple):
+    name: str
+    """Full mDNS service name.
+    Follows ``'PI monitor:<phone name>:<hardware id>._http._tcp.local.'`` naming pattern
+    """
+    server: str
+    "e.g. ``'pi.local.'``"
+    port: int
+    "e.g. `8080`"
+    addresses: T.List[str]
+    "e.g. ``['192.168.0.2']``"
+
+
+class Event(T.NamedTuple):
+    name: T.Optional[str]
+    recording_id: T.Optional[str]
+    timestamp: int  # unix epoch, in nanoseconds
+
+    @classmethod
+    def from_dict(cls, dct: T.Dict[str, T.Any]) -> "Event":
+        return cls(
+            name=dct.get("name"),
+            recording_id=dct.get("recording_id"),
+            timestamp=dct["timestamp"],
+        )
+
+    @property
+    def datetime(self) -> datetime.datetime:
+        return datetime.datetime.fromtimestamp(self.timestamp / 1e9)
+
+    def __repr__(self) -> str:
+        return (
+            f"Event(name={self.name} "
+            f"recording_id={self.recording_id} "
+            f"timestamp_unix_ns={self.timestamp} "
+            f"datetime={self.datetime})"
+        )
+
+
+class Phone(T.NamedTuple):
+    battery_level: int
+    battery_state: Literal["OK", "LOW", "CRITICAL"]
+    device_id: str
+    device_name: str
+    ip: str
+    memory: int
+    memory_state: Literal["OK", "LOW", "CRITICAL"]
+    time_echo_port: T.Optional[int] = None
+
+
+class Hardware(T.NamedTuple):
+    version: str = "unknown"
+    glasses_serial: str = "unknown"
+    world_camera_serial: str = "unknown"
+    module_serial: str = "unknown"
+
+
+class NetworkDevice(T.NamedTuple):
+    """Information about devices discovered by the host device, not the client.
+
+    .. note::
+        This class represents device information made available via the websocket update
+        connection by the host device (exposed via
+        :py:meth:`pupil_labs.realtime_api.device.Device.status_updates`). Devices
+        discovered directly by this library are represented as
+        :py:class:`.DiscoveredDeviceInfo` and returned by
+        :py:func:`pupil_labs.realtime_api.discovery.discover_devices` and
+        :py:class:`pupil_labs.realtime_api.discovery.Network`.
+    """
+
+    ip: str
+    device_id: str
+    device_name: str
+    connected: bool
+
+
+class Sensor(T.NamedTuple):
+    sensor: str
+    conn_type: str
+    connected: bool = False
+    ip: T.Optional[str] = None
+    params: T.Optional[str] = None
+    port: T.Optional[int] = None
+    protocol: str = "rtsp"
+
+    @property
+    def url(self) -> T.Optional[str]:
+        if self.connected:
+            return f"{self.protocol}://{self.ip}:{self.port}/?{self.params}"
+        return None
+
+    class Name(enum.Enum):
+        ANY = None
+        GAZE = "gaze"
+        WORLD = "world"
+        IMU = "imu"
+        EYES = "eyes"
+
+    class Connection(enum.Enum):
+        ANY = None
+        WEBSOCKET = "WEBSOCKET"
+        DIRECT = "DIRECT"
+
+
+class Recording(T.NamedTuple):
+    action: str
+    id: str
+    message: str
+    rec_duration_ns: int
+
+    @property
+    def rec_duration_seconds(self) -> float:
+        return self.rec_duration_ns / 1e9
+
+
+Component = T.Union[Phone, Hardware, Sensor, Recording, NetworkDevice]
+"""Type annotation for :py:class:`Status <.Status>` components."""
+
+ComponentRaw = T.Dict[str, T.Any]
+"""Type annotation for json-parsed responses from the REST and Websocket API."""
+
+_model_class_map: T.Dict[str, T.Type[Component]] = {
+    "Phone": Phone,
+    "Hardware": Hardware,
+    "Sensor": Sensor,
+    "Recording": Recording,
+    "Event": Event,
+    "NetworkDevice": NetworkDevice,
+}
+
+
+def _init_cls_with_annotated_fields_only(cls, d: T.Dict[str, T.Any]):
+    return cls(**{attr: d.get(attr, None) for attr in cls.__annotations__})
+
+
+class UnknownComponentError(ValueError):
+    pass
+
+
+def parse_component(raw: ComponentRaw) -> Component:
+    """Initialize an explicitly modelled representation
+    (:py:obj:`pupil_labs.realtime_api.models.Component`) from the json-parsed dictionary
+    (:py:obj:`pupil_labs.realtime_api.models.ComponentRaw`) received from the API.
+
+    :raises UnknownComponentError: if the component name cannot be mapped to an
+        explicitly modelled class or the contained data does not fit the modelled
+        fields.
+    """
+    model_name = raw["model"]
+    data = raw["data"]
+    try:
+        model_class = _model_class_map[model_name]
+        return _init_cls_with_annotated_fields_only(model_class, data)
+    except KeyError as err:
+        raise UnknownComponentError(
+            f"Could not generate component for {model_name} from {data}"
+        ) from err
+
+
+@dataclasses.dataclass
+class Status:
+    "Represents the Companion's full status"
+    phone: Phone
+    hardware: Hardware
+    sensors: T.List[Sensor]
+    recording: T.Optional[Recording]
+
+    @classmethod
+    def from_dict(cls, status_json_result: T.List[ComponentRaw]) -> "Status":
+        phone = None  # always present
+        recording = None  # might not be present
+        hardware = Hardware()  # won't be present if glasses are not connected
+        sensors = []
+        for dct in status_json_result:
+            try:
+                component = parse_component(dct)
+            except UnknownComponentError:
+                logger.warning(f"Dropping unknown component: {dct}")
+                continue
+            if isinstance(component, Phone):
+                phone = component
+            elif isinstance(component, Hardware):
+                hardware = component
+            elif isinstance(component, Sensor):
+                sensors.append(component)
+            elif isinstance(component, Recording):
+                recording = component
+            elif isinstance(component, NetworkDevice):
+                pass  # no need to handle NetworkDevice updates here
+            else:
+                logger.warning(f"Unknown model class: {type(component).__name__}")
+        sensors.sort(key=lambda s: (not s.connected, s.conn_type, s.sensor))
+        return cls(phone, hardware, sensors, recording)
+
+    def update(self, component: Component) -> None:
+        if isinstance(component, Phone):
+            self.phone = component
+        elif isinstance(component, Hardware):
+            self.hardware = component
+        elif isinstance(component, Recording):
+            self.recording = component
+        elif isinstance(component, Sensor):
+            for idx, sensor in enumerate(self.sensors):
+                if (
+                    sensor.sensor == component.sensor
+                    and sensor.conn_type == component.conn_type
+                ):
+                    self.sensors[idx] = component
+                    break
+
+    def matching_sensors(self, name: Sensor.Name, connection: Sensor.Connection):
+        for sensor in self.sensors:
+            if name is not Sensor.Name.ANY and sensor.sensor != name.value:
+                continue
+            if (
+                connection is not Sensor.Connection.ANY
+                and sensor.conn_type != connection.value
+            ):
+                continue
+            yield sensor
+
+    def direct_world_sensor(self) -> T.Optional[Sensor]:
+        return next(
+            self.matching_sensors(Sensor.Name.WORLD, Sensor.Connection.DIRECT),
+            Sensor(
+                sensor=Sensor.Name.WORLD.value, conn_type=Sensor.Connection.DIRECT.value
+            ),
+        )
+
+    def direct_gaze_sensor(self) -> T.Optional[Sensor]:
+        return next(
+            self.matching_sensors(Sensor.Name.GAZE, Sensor.Connection.DIRECT),
+            Sensor(
+                sensor=Sensor.Name.GAZE.value, conn_type=Sensor.Connection.DIRECT.value
+            ),
+        )
+
+    def direct_imu_sensor(self) -> T.Optional[Sensor]:
+        return next(
+            self.matching_sensors(Sensor.Name.IMU, Sensor.Connection.DIRECT),
+            Sensor(
+                sensor=Sensor.Name.IMU.value, conn_type=Sensor.Connection.DIRECT.value
+            ),
+        )
+
+    def direct_eyes_sensor(self) -> T.Optional[Sensor]:
+        return next(
+            self.matching_sensors(Sensor.Name.EYES, Sensor.Connection.DIRECT),
+            Sensor(
+                sensor=Sensor.Name.EYES.value, conn_type=Sensor.Connection.DIRECT.value
+            ),
+        )

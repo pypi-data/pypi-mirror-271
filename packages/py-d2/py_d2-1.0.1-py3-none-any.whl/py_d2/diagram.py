097b4f0f8cdda8cd3ba98bde24fb5de069ade681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 15:04:59 +0000

    Add package py-d2/1.0.1/py_d2-1.0.1-py3-none-any.whl

---
+++ packages/py-d2/py_d2-1.0.1-py3-none-any.whl/py_d2/diagram.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from typing import List
+from typing import Optional
+
+from py_d2.connection import D2Connection
+from py_d2.shape import D2Shape
+
+
+class D2Diagram:
+    def __init__(
+        self,
+        shapes: Optional[List[D2Shape]] = None,
+        connections: Optional[List[D2Connection]] = None,
+    ):
+        self.shapes = shapes or []
+        self.connections = connections or []
+
+    def add_shape(self, shape: D2Shape):
+        self.shapes.append(shape)
+
+    def add_connection(self, connection: D2Connection):
+        self.connections.append(connection)
+
+    def __repr__(self) -> str:
+        shapes = [str(shape) for shape in self.shapes]
+        connections = [str(connection) for connection in self.connections]
+
+        return "\n".join(shapes + connections)

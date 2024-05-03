52beb0d2f0af67a90659187e60df0d49b4dc380f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 05:04:32 +0000

    Add package django-chromepdf/1.7.3/django-chromepdf-1.7.3.tar.gz

---
+++ packages/django-chromepdf/django-chromepdf-1.7.3.tar.gz/django-chromepdf-1.7.3/chromepdf/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '1.7.3'
+
+from chromepdf.exceptions import ChromePdfException
+from chromepdf.maker import ChromePdfMaker
+from chromepdf.shortcuts import generate_pdf, generate_pdf_url
+
+
+__all__ = ['__version__',
+           'generate_pdf', 'generate_pdf_url', 'ChromePdfException', 'ChromePdfMaker']

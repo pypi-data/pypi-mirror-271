abc46b0e7cab74bf7db49fcf21725c4fa56d20d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 05:04:05 +0000

    Add package motivationalqpy/0.1/motivationalqpy-0.1.tar.gz

---
+++ packages/motivationalqpy/motivationalqpy-0.1.tar.gz/motivationalqpy-0.1/README.md
@@ -0,0 +1,10 @@
+# motivationalqpy
+
+A simple Python package to get motivational quotes.
+
+## Installation
+
+Run the following to install:
+
+```bash
+pip install motivationalqpy
---
+++ packages/motivationalqpy/motivationalqpy-0.1.tar.gz/motivationalqpy-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='motivationalqpy',
+    version='0.1',
+    packages=find_packages(),
+    description='A minimal package for motivational quotes',
+    author='darkbit',
+    author_email='darkbit131@gmail.com',
+    keywords=['motivation', 'quotes', 'inspiration'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)

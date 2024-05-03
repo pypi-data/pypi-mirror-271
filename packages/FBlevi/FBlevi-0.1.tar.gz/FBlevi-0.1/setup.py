83564c434e9e31f62b7d204a66ac9368cccfedd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 06:03:53 +0000

    Add package FBlevi/0.1/FBlevi-0.1.tar.gz

---
+++ packages/FBlevi/FBlevi-0.1.tar.gz/FBlevi-0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools 
+setuptools.setup(
+
+name= 'FBlevi',
+
+version= '0.1',
+
+author= 'levi tools',
+
+description= 'face tools',
+
+packages=setuptools.find_packages(),
+
+classifiers=[
+
+"Programming Language :: Python :: 3",
+
+"Operating System :: OS Independent",
+
+"License :: OSI Approved :: MIT License"
+
+
+]
+
+)

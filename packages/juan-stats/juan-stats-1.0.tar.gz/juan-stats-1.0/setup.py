028d542e48925075052aa196ebd1b87f33e39758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 15:03:50 +0000

    Add package juan-stats/1.0/juan-stats-1.0.tar.gz

---
+++ packages/juan-stats/juan-stats-1.0.tar.gz/juan-stats-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='juan-stats',
+      version='1.0',
+      description='Probability Density Function  calculator for Binmonial and Gaussian distributions',
+      packages=['juan-stats'],
+      author='Juan P',
+      zip_safe=False)

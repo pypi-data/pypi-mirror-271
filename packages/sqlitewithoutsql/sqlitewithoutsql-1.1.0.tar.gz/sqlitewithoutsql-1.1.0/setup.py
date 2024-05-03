746a20760e5369356bdb07aa2ac737a3bb49ad3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 05:04:19 +0000

    Add package sqlitewithoutsql/1.1.0/sqlitewithoutsql-1.1.0.tar.gz

---
+++ packages/sqlitewithoutsql/sqlitewithoutsql-1.1.0.tar.gz/sqlitewithoutsql-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='sqlitewithoutsql',
+  version='1.1.0',
+  author='evryz4',
+  author_email='evryhvh@gmail.com',
+  description='My first module for using sqlite without sql',
+  url='https://github.com/evryz4/sqlitewithoutsql',
+  packages=find_packages(),
+)

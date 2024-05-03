0676bf3bd9ca3378695067f948c1eebcb6e0f4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 05:03:43 +0000

    Add package ssm-svg/0.0.4/ssm_svg-0.0.4.tar.gz

---
+++ packages/ssm-svg/ssm_svg-0.0.4.tar.gz/ssm_svg-0.0.4/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+ROOT_DIR = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (ROOT_DIR / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="ssm-svg",
+    version="0.0.4",
+    description="SVG spritesheet maker",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/obeezzy/ssm",
+    author="Chronic Coder",
+    author_email="efeoghene.obebeduo@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=["lxml"],
+    entry_points={
+        "console_scripts": ["ssm=ssm.ssm:main"]
+    },
+)

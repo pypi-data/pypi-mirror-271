79710001a1316b16b85a41b151ed68d1c9926505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 03:04:14 +0000

    Add package DataExtractorOEG/0.4.1/dataextractoroeg-0.4.1.tar.gz

---
+++ packages/DataExtractorOEG/dataextractoroeg-0.4.1.tar.gz/dataextractoroeg-0.4.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="DataExtractorOEG",
+    version="0.4.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "beautifulsoup4",
+        "selenium",
+        "requests",
+        "pandas"
+    ],
+    entry_points={
+        "console_scripts": [
+            "DataExtractorOEG = doiExtractor:cli"
+        ]
+    },
+    long_description= long_description,
+    long_description_content_type="text/markdown"
+)

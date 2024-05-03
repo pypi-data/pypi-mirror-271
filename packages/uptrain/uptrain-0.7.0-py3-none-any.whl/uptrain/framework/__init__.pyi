60fc29e8787fe44ffac96ec1c589d7ef7b65e7ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 15:05:00 +0000

    Add package uptrain/0.7.0/uptrain-0.7.0-py3-none-any.whl

---
+++ packages/uptrain/uptrain-0.7.0-py3-none-any.whl/uptrain/framework/__init__.pyi
@@ -0,0 +1,37 @@
+__all__ = [
+    "builtins",
+    "Check",
+    "CheckSet",
+    "ExperimentArgs",
+    "EvalLLM",
+    "OperatorDAG",
+    "Settings",
+    "Signal",
+    "APIClient",
+    "DataSchema",
+    "Evals",
+    "CritiqueTone",
+    "GuidelineAdherence",
+    "ResponseMatching",
+    "ConversationSatisfaction",
+    "CustomPromptEval",
+    "RcaTemplate",
+    "JailbreakDetection",
+]
+
+from .base import OperatorDAG, Settings
+from .checks import CheckSet, Check, ExperimentArgs
+from .signal import Signal
+from .remote import APIClient, DataSchema
+from .builtins import builtins
+from .evals import (
+    Evals,
+    CritiqueTone,
+    GuidelineAdherence,
+    ResponseMatching,
+    ConversationSatisfaction,
+    CustomPromptEval,
+    JailbreakDetection,
+)
+from .rca_templates import RcaTemplate
+from .evalllm import EvalLLM

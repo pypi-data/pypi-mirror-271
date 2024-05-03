2f3669860bf6fc043c0659c1422eab69b22cb8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 03:03:26 +0000

    Add package sqlvana/0.1.3/sqlvana-0.1.3.tar.gz

---
+++ packages/sqlvana/sqlvana-0.1.3.tar.gz/sqlvana-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sqlvana',
+    version='0.1.3',
+    package_dir={'': 'src'}, 
+    packages=find_packages(where='src'),
+    install_requires=[
+         "requests", "tabulate", "plotly", "pandas", "sqlparse", "kaleido", "flask", "sqlalchemy"
+    ],
+    author='fomo3d',
+    author_email='fomo3d.wiki@gmail.com',
+    description='SQL自然语言可视化查询',
+    license='MIT',
+    keywords='SQL 自然语言 可视化 查询',
+    url='https://github.com/chuangyeshuo/sqlvana'
+)

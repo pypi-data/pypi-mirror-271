219b71d9a2d17f8bb098b6898ea336aa7208d9d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 03:03:27 +0000

    Add package ember-mivia/0.0.5/ember-mivia-0.0.5.tar.gz

---
+++ packages/ember-mivia/ember-mivia-0.0.5.tar.gz/ember-mivia-0.0.5/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="UTF-8") as f:
+    readme = f.read()
+
+version = "0.0.5"
+package_data = {}
+setup(
+    name="ember-mivia",
+    version=version,
+    url = "https://github.com/gparrella12/ember",
+    long_description_content_type = "text/markdown",
+    description="Endgame Malware BEnchmark for Research",
+    long_description=readme,
+    packages=["ember"],
+    package_data=package_data,
+    author_email="proth@endgame.com")

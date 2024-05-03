6f2727b948f7844ca7c9b4cc82f5eb184806e338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 16:04:27 +0000

    Add package drf-firebase-auth-fjcg/1.0.1/drf_firebase_auth_fjcg-1.0.1-py3-none-any.whl

---
+++ packages/drf-firebase-auth-fjcg/drf_firebase_auth_fjcg-1.0.1-py3-none-any.whl/drf_firebase_auth/__init__.py
@@ -0,0 +1,15 @@
+"""Metadata for drf-firebase-auth package."""
+__title__ = 'drf-firebase-auth-fjcg'
+__version__ = '1.0.1'
+__description__ = (
+    'Custom Django Rest Framework authentication backend for '
+    'parsing Firebase uid tokens and storing as local users.'
+)
+__url__ = 'https://github.com/romulus85/drf-firebase-auth.git'
+__author__ = 'Fran Castaño'
+__author_email__ = 'djlargo50@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 Fran Castaño'
+
+# Version synonym
+VERSION = __version__

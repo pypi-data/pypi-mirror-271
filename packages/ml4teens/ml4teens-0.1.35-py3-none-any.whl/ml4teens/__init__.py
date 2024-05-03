5e8b48f06ed5aa56cad9b4fc5c4a9ab2ed7edba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 16:04:10 +0000

    Add package ml4teens/0.1.35/ml4teens-0.1.35-py3-none-any.whl

---
+++ packages/ml4teens/ml4teens-0.1.35-py3-none-any.whl/ml4teens/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import torch;
+Tensor=torch.Tensor;
+
+import pandas;
+DataFrame=pandas.DataFrame;
+
+import numpy;
+NDArray=numpy.ndarray;
+
+from . import core;
+from . import blocks;
+from . import tools;

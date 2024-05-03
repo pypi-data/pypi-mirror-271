4aba9520d5cbfdf8fb61f11878c03a9a622e25f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 15:03:53 +0000

    Add package kitsunekko-tools/24.5.3.0/kitsunekko_tools-24.5.3.0-py3-none-any.whl

---
+++ packages/kitsunekko-tools/kitsunekko_tools-24.5.3.0-py3-none-any.whl/kitsunekko_tools/consts.py
@@ -0,0 +1,10 @@
+# Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+
+PROG_NAME = "kitsunekko-tools"
+SETTINGS_FILE_NAME = f"{PROG_NAME}.toml"
+KITSUNEKKO_DOMAIN_URL = "https://kitsunekko.net"
+IGNORE_FILENAME = ".kitsuignore"
+
+__all__ = [name for name in globals() if name.isupper()]

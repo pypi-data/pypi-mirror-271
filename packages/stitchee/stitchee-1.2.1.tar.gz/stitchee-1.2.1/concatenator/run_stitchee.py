ebbea1d29b5593e7eb0ce7e0be292a9d3854c09a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 04:03:25 +0000

    Add package stitchee/1.2.1/stitchee-1.2.1.tar.gz

---
+++ packages/stitchee/stitchee-1.2.1.tar.gz/stitchee-1.2.1/concatenator/run_stitchee.py
@@ -0,0 +1,270 @@
+"""A simple CLI wrapper around the main concatenation process."""
+
+import json
+import logging
+import os
+import shutil
+import sys
+import uuid
+from argparse import ArgumentParser
+from pathlib import Path
+
+import netCDF4 as nc
+
+from concatenator.attribute_handling import construct_history, retrieve_history
+from concatenator.file_ops import add_label_to_path
+from concatenator.stitchee import stitchee
+
+
+def parse_args(args: list) -> tuple[list[str], str, str, bool, str | None, str, dict]:
+    """
+    Parse args for this script.
+
+    Returns
+    -------
+    tuple
+    """
+    parser = ArgumentParser(
+        prog="stitchee", description="Run the along-existing-dimension concatenator."
+    )
+
+    # Required arguments
+    req_grp = parser.add_argument_group(title="Required")
+    req_grp.add_argument(
+        "input",
+        metavar="path/directory or path list",
+        nargs="+",
+        help="Files to be concatenated, specified via a "
+        "(1) single directory containing the files to be concatenated, "
+        "(2) single text file containing linebreak-separated paths of the files to be concatenated, "
+        "or (3) multiple filepaths of the files to be concatenated.",
+    )
+    req_grp.add_argument(
+        "-o",
+        "--output_path",
+        required=True,
+        help="The output filename for the merged output.",
+    )
+
+    # Optional arguments
+    parser.add_argument(
+        "--no_input_file_copies",
+        action="store_true",
+        help="By default, input files are copied into a temporary directory to avoid modification "
+        "of input files. This is useful for testing, but uses more disk space.  "
+        "By specifying this argument, no copying is performed.",
+    )
+    parser.add_argument(
+        "--keep_tmp_files",
+        action="store_true",
+        help="Prevents removal, after successful execution, of "
+        "(1) the flattened concatenated file and "
+        "(2) the input directory copy  if created by '--make_dir_copy'.",
+    )
+    parser.add_argument(
+        "--concat_method",
+        choices=["xarray-concat", "xarray-combine"],
+        default="xarray-concat",
+        help="Whether to use the xarray concat method or the combine-by-coords method.",
+    )
+    parser.add_argument(
+        "--concat_dim",
+        help="Dimension to concatenate along, if possible.  "
+        "This is required if using the 'xarray-concat' method",
+    )
+    parser.add_argument(
+        "--xarray_arg_compat",
+        help="'compat' argument passed to xarray.concat() or xarray.combine_by_coords().",
+    )
+    parser.add_argument(
+        "--xarray_arg_combine_attrs",
+        help="'combine_attrs' argument passed to xarray.concat() or xarray.combine_by_coords().",
+    )
+    parser.add_argument(
+        "--xarray_arg_join",
+        help="'join' argument passed to xarray.concat() or xarray.combine_by_coords().",
+    )
+    parser.add_argument(
+        "-O", "--overwrite", action="store_true", help="Overwrite output file if it already exists."
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="Enable verbose output to stdout; useful for debugging",
+        action="store_true",
+    )
+
+    parsed = parser.parse_args(args)
+
+    if parsed.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+
+    # Validate the input and output paths
+    output_path = _validate_output_path(parsed)
+    input_files = _validate_input_path(parsed)
+
+    print(f"CONCAT METHOD === {parsed.concat_method}")
+    print(f"CONCAT DIM === {parsed.concat_dim}")
+    if parsed.concat_method == "xarray-concat":
+        if not parsed.concat_dim:
+            raise ValueError(
+                "If using the xarray-concat method, then 'concat_dim' must be specified."
+            )
+    elif parsed.concat_method == "xarray-combine":
+        if parsed.concat_dim:
+            raise ValueError(
+                "If using the xarray-combine method, then 'concat_dim' cannot be specified."
+            )
+
+    # Gather the concatenation arguments that will be passed to xarray.
+    concat_kwargs = {}
+    if parsed.xarray_arg_compat:
+        concat_kwargs["compat"] = parsed.xarray_arg_compat
+    if parsed.xarray_arg_combine_attrs:
+        concat_kwargs["combine_attrs"] = parsed.xarray_arg_combine_attrs
+    if parsed.xarray_arg_join:
+        concat_kwargs["join"] = parsed.xarray_arg_join
+
+    # If requested, make a temporary directory with new copies of the original input files
+    temporary_dir_to_remove = None
+    if not parsed.no_input_file_copies:
+        input_files, temporary_dir_to_remove = _make_temp_dir_with_input_file_copies(
+            input_files, output_path
+        )
+
+    return (
+        input_files,
+        str(output_path),
+        parsed.concat_dim,
+        bool(parsed.keep_tmp_files),
+        temporary_dir_to_remove,
+        parsed.concat_method,
+        concat_kwargs,
+    )
+
+
+def _make_temp_dir_with_input_file_copies(input_files, output_path):
+    new_data_dir = Path(
+        add_label_to_path(str(output_path.parent / "temp_copy"), label=str(uuid.uuid4()))
+    ).resolve()
+    os.makedirs(new_data_dir, exist_ok=True)
+    print("Created temporary directory: %s", str(new_data_dir))
+
+    new_input_files = []
+    for file in input_files:
+        new_path = new_data_dir / Path(file).name
+        shutil.copyfile(file, new_path)
+        new_input_files.append(str(new_path))
+
+    input_files = new_input_files
+    print("Copied files to temporary directory: %s", new_data_dir)
+    temporary_dir_to_remove = str(new_data_dir)
+
+    return input_files, temporary_dir_to_remove
+
+
+def _validate_output_path(parsed):
+    # The output file path is validated.
+    output_path = Path(parsed.output_path).resolve()
+    if output_path.is_file():  # the file already exists
+        if parsed.overwrite:
+            os.remove(output_path)
+        else:
+            raise FileExistsError(
+                f"File already exists at <{output_path}>. Run again with option '-O' to overwrite."
+            )
+    if output_path.is_dir():  # the specified path is an existing directory
+        raise TypeError("Output path cannot be a directory. Please specify a new filepath.")
+    return output_path
+
+
+def _validate_input_path(parsed):
+    # The input directory or file is validated.
+    print(f"parsed_input === {parsed.input}")
+    if len(parsed.input) > 1:
+        input_files = parsed.input
+    elif len(parsed.input) == 1:
+        directory_or_path = Path(parsed.input[0]).resolve()
+        if directory_or_path.is_dir():
+            input_files = _get_list_of_filepaths_from_dir(directory_or_path)
+        elif directory_or_path.is_file():
+            input_files = _get_list_of_filepaths_from_file(directory_or_path)
+        else:
+            raise TypeError(
+                "If one path is provided for 'data_dir_or_file_or_filepaths', "
+                "then it must be an existing directory or file."
+            )
+    else:
+        raise TypeError("input argument must be one path/directory or a list of paths.")
+    return input_files
+
+
+def _get_list_of_filepaths_from_file(file_with_paths: Path):
+    # Each path listed in the specified file is resolved using pathlib for validation.
+    paths_list = []
+    with open(file_with_paths, encoding="utf-8") as file:
+        while line := file.readline():
+            paths_list.append(str(Path(line.rstrip()).resolve()))
+
+    return paths_list
+
+
+def _get_list_of_filepaths_from_dir(data_dir: Path):
+    # Get a list of files (ignoring hidden files) in directory.
+    input_files = [str(f) for f in data_dir.iterdir() if not f.name.startswith(".")]
+    return input_files
+
+
+def run_stitchee(args: list) -> None:
+    """
+    Parse arguments and run subsetter on the specified input file
+    """
+    (
+        input_files,
+        output_path,
+        concat_dim,
+        keep_tmp_files,
+        temporary_dir_to_remove,
+        concat_method,
+        concat_kwargs,
+    ) = parse_args(args)
+    num_inputs = len(input_files)
+
+    history_json: list[dict] = []
+    for file_count, file in enumerate(input_files):
+        with nc.Dataset(file, "r") as dataset:
+            history_json.extend(retrieve_history(dataset))
+
+    history_json.append(construct_history(input_files, input_files))
+
+    new_history_json = json.dumps(history_json, default=str)
+
+    logging.info("Executing stitchee concatenation on %d files...", num_inputs)
+    stitchee(
+        input_files,
+        output_path,
+        write_tmp_flat_concatenated=keep_tmp_files,
+        keep_tmp_files=keep_tmp_files,
+        concat_method=concat_method,
+        concat_dim=concat_dim,
+        concat_kwargs=concat_kwargs,
+        history_to_append=new_history_json,
+    )
+    logging.info("STITCHEE complete. Result in %s", output_path)
+
+    if not keep_tmp_files and temporary_dir_to_remove:
+        shutil.rmtree(temporary_dir_to_remove)
+
+
+def main() -> None:
+    """Entry point to the script"""
+    logging.basicConfig(
+        stream=sys.stdout,
+        format="[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s",
+        level=logging.DEBUG,
+    )
+    run_stitchee(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/stitchee/stitchee-1.2.1.tar.gz/stitchee-1.2.1/concatenator/stitchee.py
@@ -0,0 +1,199 @@
+"""Concatenation service that appends data along an existing dimension, using netCDF4 and xarray."""
+
+from __future__ import annotations
+
+import logging
+import os
+import time
+from contextlib import ExitStack
+from logging import Logger
+from warnings import warn
+
+import netCDF4 as nc
+import xarray as xr
+
+from concatenator import GROUP_DELIM
+from concatenator.dataset_and_group_handling import (
+    flatten_grouped_dataset,
+    regroup_flattened_dataset,
+    validate_workable_files,
+)
+from concatenator.dimension_cleanup import remove_duplicate_dims
+from concatenator.file_ops import add_label_to_path
+
+default_logger = logging.getLogger(__name__)
+
+# class netcdfExitStack(ExitStack):
+#     """A context manager that handles netCDF.Dataset.close exceptions."""
+#     def __exit__(self, *args, logger=default_logger, **kwargs):
+#         try:
+#             super().__exit__(*args, **kwargs)
+#         except RuntimeError as err:
+#             if str(err) == "NetCDF: Not a valid ID":
+#                 logger.warning("Tried closing an already closed netCDF.")
+
+
+def stitchee(
+    files_to_concat: list[str],
+    output_file: str,
+    write_tmp_flat_concatenated: bool = False,
+    keep_tmp_files: bool = True,
+    concat_method: str = "xarray-concat",
+    concat_dim: str = "",
+    concat_kwargs: dict | None = None,
+    history_to_append: str | None = None,
+    logger: Logger = default_logger,
+) -> str:
+    """Concatenate netCDF data files along an existing dimension.
+
+    Parameters
+    ----------
+    files_to_concat : list[str]
+    output_file : str
+    write_tmp_flat_concatenated
+    keep_tmp_files : bool
+    concat_method
+    concat_dim : str, optional
+    concat_kwargs
+    history_to_append
+    logger : logging.Logger
+
+    Returns
+    -------
+    str
+    """
+    intermediate_flat_filepaths: list[str] = []
+    benchmark_log = {"flattening": 0.0, "concatenating": 0.0, "reconstructing_groups": 0.0}
+
+    # Proceed to concatenate only files that are workable (can be opened and are not empty).
+    input_files, num_input_files = validate_workable_files(files_to_concat, logger)
+
+    # Exit cleanly if no workable netCDF files found.
+    if num_input_files < 1:
+        logger.info("No non-empty netCDF files found. Exiting.")
+        return ""
+
+    if concat_dim and (concat_method == "xarray-combine"):
+        warn(
+            "'concat_dim' was specified, but will not be used because xarray-combine method was "
+            "selected."
+        )
+
+    try:
+        # Instead of "with nc.Dataset() as" inside the loop, we use a context manager stack.
+        # This way all files are cleanly closed outside the loop.
+        with ExitStack() as context_stack:
+
+            logger.info("Flattening all input files...")
+            xrdataset_list = []
+            concat_dim_order = []
+            for i, filepath in enumerate(input_files):
+                # The group structure is flattened.
+                start_time = time.time()
+                logger.info("    ..file %03d/%03d <%s>..", i + 1, num_input_files, filepath)
+
+                ncfile = context_stack.enter_context(nc.Dataset(filepath, "r+"))
+
+                flat_dataset, coord_vars, _ = flatten_grouped_dataset(
+                    ncfile, ensure_all_dims_are_coords=True
+                )
+
+                logger.info("Removing duplicate dimensions")
+                flat_dataset = remove_duplicate_dims(flat_dataset)
+
+                logger.info("Opening flattened file with xarray.")
+                xrds = xr.open_dataset(
+                    xr.backends.NetCDF4DataStore(flat_dataset),
+                    decode_times=False,
+                    decode_coords=False,
+                    drop_variables=coord_vars,
+                )
+                first_value = xrds[GROUP_DELIM + concat_dim].values.flatten()[0]
+                concat_dim_order.append(first_value)
+
+                benchmark_log["flattening"] = time.time() - start_time
+
+                # The flattened file is written to disk.
+                # flat_file_path = add_label_to_path(filepath, label="_flat_intermediate")
+                # xrds.to_netcdf(flat_file_path, encoding={v_name: {'dtype': 'str'} for v_name in string_vars})
+                # intermediate_flat_filepaths.append(flat_file_path)
+                # xrdataset_list.append(xr.open_dataset(flat_file_path))
+                xrdataset_list.append(xrds)
+
+            # Reorder the xarray datasets according to the concat dim values.
+            xrdataset_list = [
+                dataset
+                for _, dataset in sorted(zip(concat_dim_order, xrdataset_list), key=lambda x: x[0])
+            ]
+
+            # Flattened files are concatenated together (Using XARRAY).
+            start_time = time.time()
+            logger.info("Concatenating flattened files...")
+            # combined_ds = xr.open_mfdataset(intermediate_flat_filepaths,
+            #                                 decode_times=False,
+            #                                 decode_coords=False,
+            #                                 data_vars='minimal',
+            #                                 coords='minimal',
+            #                                 compat='override')
+
+            if concat_kwargs is None:
+                concat_kwargs = {}
+
+            if concat_method == "xarray-concat":
+                combined_ds = xr.concat(
+                    xrdataset_list,
+                    dim=GROUP_DELIM + concat_dim,
+                    data_vars="minimal",
+                    coords="minimal",
+                    **concat_kwargs,
+                )
+            elif concat_method == "xarray-combine":
+                combined_ds = xr.combine_by_coords(
+                    xrdataset_list,
+                    data_vars="minimal",
+                    coords="minimal",
+                    **concat_kwargs,
+                )
+            else:
+                raise ValueError(f"Unexpected concatenation method, <{concat_method}>.")
+
+            benchmark_log["concatenating"] = time.time() - start_time
+
+            if write_tmp_flat_concatenated:
+                logger.info("Writing concatenated flattened temporary file to disk...")
+                # Concatenated, yet still flat, file is written to disk for debugging.
+                tmp_flat_concatenated_path = add_label_to_path(
+                    output_file, label="_flat_intermediate"
+                )
+                combined_ds.to_netcdf(tmp_flat_concatenated_path, format="NETCDF4")
+            else:
+                tmp_flat_concatenated_path = None
+
+            # new_global_attributes = create_new_attributes(combined_ds, request_parameters=dict())
+
+            # The group hierarchy of the concatenated file is reconstructed (using XARRAY).
+            start_time = time.time()
+            logger.info("Reconstructing groups within concatenated file...")
+            regroup_flattened_dataset(combined_ds, output_file, history_to_append)
+            benchmark_log["reconstructing_groups"] = time.time() - start_time
+
+            logger.info("--- Benchmark results ---")
+            total_time = 0.0
+            for k, v in benchmark_log.items():
+                logger.info("%s: %f", k, v)
+                total_time += v
+            logger.info("-- total processing time: %f", total_time)
+
+            # If requested, remove temporary intermediate files.
+            if not keep_tmp_files:
+                for file in intermediate_flat_filepaths:
+                    os.remove(file)
+                if tmp_flat_concatenated_path:
+                    os.remove(tmp_flat_concatenated_path)
+
+    except Exception as err:
+        logger.info("Stitchee encountered an error!")
+        logger.error(err)
+        raise err
+
+    return output_file

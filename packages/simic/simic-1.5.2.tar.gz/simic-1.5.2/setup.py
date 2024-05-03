1ae663541dd632b8079fc3734d56ecf9ffdf4497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 05:04:00 +0000

    Add package simic/1.5.2/simic-1.5.2.tar.gz

---
+++ packages/simic/simic-1.5.2.tar.gz/simic-1.5.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='simic',
+    version='1.5.2',
+    packages=['simic'],
+    install_requires=[
+        'selenium',
+        
+    ]
+)
---
+++ packages/simic/simic-1.5.2.tar.gz/simic-1.5.2/simic/__init__.py
@@ -0,0 +1,8 @@
+from .xpath import *
+from concurrent.futures import ThreadPoolExecutor
+import pandas as pd
+import random
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium import webdriver

270a20637a82c01f3a664d652e9eeddff6f2a309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 14:04:29 +0000

    Add package easy-llama/0.1.9/easy_llama-0.1.9-py3-none-any.whl

---
+++ packages/easy-llama/easy_llama-0.1.9-py3-none-any.whl/easy_llama/__init__.py
@@ -0,0 +1,13 @@
+# easy_llama.py
+# https://github.com/ddh0/easy-llama/
+
+"""Text generation in Python, made easy"""
+
+__version__ = '0.1.9'
+
+from . import formats
+from . import samplers
+from . import utils
+
+from .model  import Model
+from .thread import Thread

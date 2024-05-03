e4d0db1de4d175361d23eef3ab1c39289b12907b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 05:03:43 +0000

    Add package ml4xcube/0.0.3/ml4xcube-0.0.3.tar.gz

---
+++ packages/ml4xcube/ml4xcube-0.0.3.tar.gz/ml4xcube-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ml4xcube',
+    version='0.0.3',
+    package_dir={'': 'mltools'},
+    packages=find_packages(where='mltools'),
+    install_requires=[
+        'bokeh >=2.4.3',
+        'dask >=2023.2.0',
+        'jinja2 ==3.1.3',
+        'mypy_extensions ==1.0.0',
+        'numpy >=1.24',
+        'pandas >=2.2',
+        'scikit-learn >1.3.1',
+        'xarray >2023.8.0',
+        'zarr >2.11',
+        'rechunker >=0.5.1',
+        'sentinelhub',
+        'xcube'
+    ],
+)

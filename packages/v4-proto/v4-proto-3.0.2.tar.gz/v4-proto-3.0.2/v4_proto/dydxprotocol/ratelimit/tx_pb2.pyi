5fb15de2b44cc5bfa10b3650022e2cfdcc45be56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 14:04:54 +0000

    Add package v4-proto/3.0.2/v4-proto-3.0.2.tar.gz

---
+++ packages/v4-proto/v4-proto-3.0.2.tar.gz/v4-proto-3.0.2/v4_proto/dydxprotocol/ratelimit/tx_pb2.pyi
@@ -0,0 +1,31 @@
+from v4_proto.cosmos.msg.v1 import msg_pb2 as _msg_pb2
+from v4_proto.dydxprotocol.ratelimit import limit_params_pb2 as _limit_params_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class MsgSetLimitParams(_message.Message):
+    __slots__ = ("authority", "limit_params")
+    AUTHORITY_FIELD_NUMBER: _ClassVar[int]
+    LIMIT_PARAMS_FIELD_NUMBER: _ClassVar[int]
+    authority: str
+    limit_params: _limit_params_pb2.LimitParams
+    def __init__(self, authority: _Optional[str] = ..., limit_params: _Optional[_Union[_limit_params_pb2.LimitParams, _Mapping]] = ...) -> None: ...
+
+class MsgSetLimitParamsResponse(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class MsgDeleteLimitParams(_message.Message):
+    __slots__ = ("authority", "denom")
+    AUTHORITY_FIELD_NUMBER: _ClassVar[int]
+    DENOM_FIELD_NUMBER: _ClassVar[int]
+    authority: str
+    denom: str
+    def __init__(self, authority: _Optional[str] = ..., denom: _Optional[str] = ...) -> None: ...
+
+class MsgDeleteLimitParamsResponse(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...

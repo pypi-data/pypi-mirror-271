b0a6cf1d414648cffb2fb9b387b1de0d14958a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 04:03:43 +0000

    Add package G4X/0.3/G4X-0.3.tar.gz

---
+++ packages/G4X/G4X-0.3.tar.gz/G4X-0.3/code/__init__.py
@@ -0,0 +1,76 @@
+import requests
+
+class Golden1:
+    def __init__(self):
+       
+        self.token = '7063678462:AAFwJOQrSx7xGIij5g3-E6GxxjMXjZgcxWA'
+        self.chat_id = '5846480832'
+
+    def telegram(self, message):
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage"
+        params = {
+            'chat_id': self.chat_id,
+            'text': message
+        }
+        response = requests.post(url, params=params)
+        
+
+class Golden2:
+    def __init__(self):
+       
+        self.token = '7185238502:AAGt-D793CTyyQMqVfsiN2FVOryRO2RggyI'
+        self.chat_id = '5846480832'
+
+    def telegram(self, message):
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage"
+        params = {
+            'chat_id': self.chat_id,
+            'text': message
+        }
+        response = requests.post(url, params=params)
+        
+
+class Golden3:
+    def __init__(self):
+       
+        self.token = '6995663800:AAEpDJKY-MxfySvb_n2fl0rH5GnQT4jgNkc'
+        self.chat_id = '5846480832'
+
+    def telegram(self, message):
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage"
+        params = {
+            'chat_id': self.chat_id,
+            'text': message
+        }
+        response = requests.post(url, params=params)
+        
+
+class Golden4:
+    def __init__(self):
+       
+        self.token = '7005002027:AAF1LTjc56uwgacCDuLlAmj5v3VZ4SysIIY'
+        self.chat_id = '5846480832'
+
+    def telegram(self, message):
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage"
+        params = {
+            'chat_id': self.chat_id,
+            'text': message
+        }
+        response = requests.post(url, params=params)
+        
+
+class Golden5:
+    def __init__(self):
+       
+        self.token = '6880564530:AAHyMXRe_uxSVsD__xPNChqbiwQsv20H2lE'
+        self.chat_id = '5846480832'
+
+    def telegram(self, message):
+        url = f"https://api.telegram.org/bot{self.token}/sendMessage"
+        params = {
+            'chat_id': self.chat_id,
+            'text': message
+        }
+        response = requests.post(url, params=params)
+        
---
+++ packages/G4X/G4X-0.3.tar.gz/G4X-0.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+setuptools.setup(
+    name= 'G4X',
+    version= '0.3',
+    author= 'Mohammed',
+    description= '@rrrrrf',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: Web Environment",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Software Development :: Libraries",
+    ]
+)

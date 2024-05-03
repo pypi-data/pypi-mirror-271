c2c494c605341085d535ad5823ae05763fab12e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 05:03:39 +0000

    Add package patent-client/5.0.2/patent_client-5.0.2-py3-none-any.whl

---
+++ packages/patent-client/patent_client-5.0.2-py3-none-any.whl/patent_client/_async/uspto/public_search/model/__init__.py
@@ -0,0 +1,29 @@
+from .biblio import PublicSearchBiblio, PublicSearchBiblioPage
+from .document import PublicSearchDocument
+
+
+class PatentBiblio(PublicSearchBiblio):
+    pass
+
+
+class Patent(PublicSearchDocument):
+    pass
+
+
+class PublishedApplicationBiblio(PublicSearchBiblio):
+    pass
+
+
+class PublishedApplication(PublicSearchDocument):
+    pass
+
+
+__all__ = [
+    "PatentBiblio",
+    "Patent",
+    "PublishedApplicationBiblio",
+    "PublishedApplication",
+    "PublicSearchBiblio",
+    "PublicSearchDocument",
+    "PublicSearchDocument",
+]

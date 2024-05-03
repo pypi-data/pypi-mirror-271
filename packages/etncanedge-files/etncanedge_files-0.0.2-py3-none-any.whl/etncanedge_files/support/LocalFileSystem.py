a5b7c527923868d8cfc7c8ab6678432a9af9e545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 04:04:30 +0000

    Add package etncanedge-files/0.0.2/etncanedge_files-0.0.2-py3-none-any.whl

---
+++ packages/etncanedge-files/etncanedge_files-0.0.2-py3-none-any.whl/etncanedge_files/support/LocalFileSystem.py
@@ -0,0 +1,15 @@
+from etncanedge_files.support.RelativeFileSystem import RelativeFileSystem
+
+
+class LocalFileSystem(RelativeFileSystem):
+    """LocalFileSystem implements a subset of the AbstractFileSystem from fsspec, such that all calls are forwarded to
+    the implementation from fsspec, but with another root dir.
+    
+    """
+    def __init__(self, base_path, *args, **storage_options):
+        storage_options["paths_with_leading_slash"] = True
+        super().__init__("file", base_path, *args, **storage_options)
+        
+        return
+    
+    pass

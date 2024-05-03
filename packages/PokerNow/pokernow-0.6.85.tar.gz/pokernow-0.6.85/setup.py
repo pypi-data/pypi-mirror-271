0da3378642b125a98765cbe894b6c3135d444616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 14:04:36 +0000

    Add package PokerNow/0.6.85/pokernow-0.6.85.tar.gz

---
+++ packages/PokerNow/pokernow-0.6.85.tar.gz/pokernow-0.6.85/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PokerNow',
+    version='0.6.85',
+    author='Zehm',
+    author_email='mrtentacleshasallthetalent@gmail.com',
+    description='A Python client for interacting with PokerNow games via the web.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Zehmosu/PokerNow/',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'selenium',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Games/Entertainment',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)

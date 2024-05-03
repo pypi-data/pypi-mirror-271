a65429cdc4052c2c1968917b98e04f93570aa304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 15:04:46 +0000

    Add package odoo14-addon-ssi-bill-of-service-work-log/14.0.1.0.0/odoo14_addon_ssi_bill_of_service_work_log-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-bill-of-service-work-log/odoo14_addon_ssi_bill_of_service_work_log-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_bill_of_service_work_log/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Bill of Service - Work Log Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_bill_of_service",
+        "ssi_work_log_mixin",
+    ],
+    "data": [],
+    "demo": [],
+    "images": [],
+}
---
+++ packages/odoo14-addon-ssi-bill-of-service-work-log/odoo14_addon_ssi_bill_of_service_work_log-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_bill_of_service_work_log/models/bill_of_service_pricelist.py
@@ -0,0 +1,15 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo import models
+
+
+class BillOfServicePricelist(models.Model):
+    _name = "bill_of_service_pricelist"
+    _inherit = [
+        "bill_of_service_pricelist",
+        "mixin.work_object",
+    ]
+
+    _work_log_create_page = True

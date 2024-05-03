ceab2289e94cfd957c5845272612cfcf9ee9a36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 15:04:54 +0000

    Add package nl2flow/0.0.0/nl2flow-0.0.0.tar.gz

---
+++ packages/nl2flow/nl2flow-0.0.0.tar.gz/nl2flow-0.0.0/profiler/data_types/pddl_generator_datatypes.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from typing import List
+from nl2flow.plan.schemas import ClassicalPlan, PlannerResponse
+from profiler.data_types.generator_data_type import AgentInfoGeneratorInput
+
+
+class PddlGeneratorOutput(BaseModel):
+    description: str  # the description of generated PDDL domain and problem
+    pddl_domain: str  # PDDL domain
+    pddl_problem: str  # PDDL problem
+    list_of_plans: List[ClassicalPlan] = []  # Response from a planner
+    prettified_plans: str  # prettified plans
+    sample_hash: str  # hash for PDDL domain and problem
+    agent_info_generator_input: AgentInfoGeneratorInput  # input to generator
+    compiler_planner_lag_millisecond: float  # lag in millisecond
+    planner_response: PlannerResponse  # planner response

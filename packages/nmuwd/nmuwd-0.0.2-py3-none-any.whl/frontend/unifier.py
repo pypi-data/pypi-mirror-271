a7772bba21d70d02ebed257375ca33e36003f478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 15:04:02 +0000

    Add package nmuwd/0.0.2/nmuwd-0.0.2-py3-none-any.whl

---
+++ packages/nmuwd/nmuwd-0.0.2-py3-none-any.whl/frontend/unifier.py
@@ -0,0 +1,55 @@
+# ===============================================================================
+# Copyright 2024 Jake Ross
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+from backend.connectors.ampapi.source import AMPAPISiteSource
+from backend.connectors.isc_seven_rivers.source import ISCSevenRiversSiteSource
+from backend.connectors.usgs.source import USGSSiteSource
+from backend.persister import CSVPersister, GeoJSONPersister
+
+
+def unify_sites(config):
+    print("unifying")
+
+    persister_klass = CSVPersister
+    if config.use_csv:
+        persister_klass = CSVPersister
+    elif config.use_geojson:
+        persister_klass = GeoJSONPersister
+
+    persister = persister_klass()
+
+    if config.use_source_ampapi:
+        s = AMPAPISiteSource()
+        persister.load(s.read(config))
+
+    if config.use_source_isc_seven_rivers:
+        isc = ISCSevenRiversSiteSource()
+        persister.load(isc.read(config))
+
+    if config.use_source_nwis:
+        nwis = USGSSiteSource()
+        persister.load(nwis.read(config))
+
+    persister.save(config.output_path)
+
+
+def unify_waterlevels(config):
+    pass
+
+
+if __name__ == "__main__":
+    unify_sites()
+
+# ============= EOF =============================================

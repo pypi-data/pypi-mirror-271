56ffbd9b9312f6c2af31f11c0adfa57996f05bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 16:03:57 +0000

    Add package intersphinx_registry/0.0.1/intersphinx_registry-0.0.1.tar.gz

---
+++ packages/intersphinx_registry/intersphinx_registry-0.0.1.tar.gz/intersphinx_registry-0.0.1/intersphinx_registry/__init__.py
@@ -0,0 +1,23 @@
+"""
+This package provides convenient utilities and data to write a sphinx config file.
+"""
+from __future__ import annotations
+
+from pathlib import Path
+import json
+from typing import Dict, Tuple
+
+__version__ = "0.0.1"
+
+registry_file = Path(__file__).parent / "registry.json"
+
+
+def get_intersphinx_mapping() -> Dict[str, Tuple[str, str]]:
+    """
+    Return values of intersphinx_mapping for sphinx configuration.
+
+    For conveneience, the return dict is a copy so should be ok to mutate
+    """
+    return json.loads(registry_file.read_bytes())
+
+

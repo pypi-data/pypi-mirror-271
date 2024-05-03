0063d4fb8bdf7d28cbf6f6d80c9f97fbe4a05244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 14:04:29 +0000

    Add package python-redux/0.14.4/python_redux-0.14.4-py3-none-any.whl

---
+++ packages/python-redux/python_redux-0.14.4-py3-none-any.whl/redux/basic_types.py
@@ -0,0 +1,226 @@
+# ruff: noqa: A003, D100, D101, D102, D103, D104, D105, D107
+from __future__ import annotations
+
+from dataclasses import field
+from types import NoneType
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Coroutine,
+    Generic,
+    Protocol,
+    Sequence,
+    TypeAlias,
+    TypeGuard,
+)
+
+from immutable import Immutable
+from typing_extensions import TypeVar
+
+if TYPE_CHECKING:
+    from asyncio import Task
+
+
+class BaseAction(Immutable): ...
+
+
+class BaseEvent(Immutable): ...
+
+
+# Type variables
+State = TypeVar('State', bound=Immutable, infer_variance=True)
+Action = TypeVar('Action', bound=BaseAction, infer_variance=True)
+Event = TypeVar('Event', bound=BaseEvent, infer_variance=True)
+Event2 = TypeVar('Event2', bound=BaseEvent, infer_variance=True)
+SelectorOutput = TypeVar('SelectorOutput', infer_variance=True)
+ComparatorOutput = TypeVar('ComparatorOutput', infer_variance=True)
+AutorunOriginalReturnType = TypeVar('AutorunOriginalReturnType', infer_variance=True)
+Comparator = Callable[[State], ComparatorOutput]
+EventHandler = Callable[[Event], Any] | Callable[[], Any]
+
+
+class CompleteReducerResult(Immutable, Generic[State, Action, Event]):
+    state: State
+    actions: Sequence[Action] | None = None
+    events: Sequence[Event] | None = None
+
+
+ReducerResult = CompleteReducerResult[State, Action, Event] | State
+ReducerType = Callable[[State | None, Action], ReducerResult[State, Action, Event]]
+
+
+class InitializationActionError(Exception):
+    def __init__(self: InitializationActionError, action: BaseAction) -> None:
+        super().__init__(
+            f"""The only accepted action type when state is None is "InitAction", \
+action "{action}" is not allowed.""",
+        )
+
+
+class InitAction(BaseAction): ...
+
+
+class FinishAction(BaseAction): ...
+
+
+class FinishEvent(BaseEvent): ...
+
+
+def is_complete_reducer_result(
+    result: ReducerResult[State, Action, Event],
+) -> TypeGuard[CompleteReducerResult[State, Action, Event]]:
+    return isinstance(result, CompleteReducerResult)
+
+
+def is_state_reducer_result(
+    result: ReducerResult[State, Action, Event],
+) -> TypeGuard[State]:
+    return not isinstance(result, CompleteReducerResult)
+
+
+class Scheduler(Protocol):
+    def __call__(self: Scheduler, callback: Callable, *, interval: bool) -> None: ...
+
+
+class TaskCreatorCallback(Protocol):
+    def __call__(self: TaskCreatorCallback, task: Task) -> None: ...
+
+
+class TaskCreator(Protocol):
+    def __call__(
+        self: TaskCreator,
+        coro: Coroutine,
+        *,
+        callback: TaskCreatorCallback | None = None,
+    ) -> None: ...
+
+
+class ActionMiddleware(Protocol, Generic[Action]):
+    def __call__(self: ActionMiddleware, action: Action) -> Action | None: ...
+
+
+class EventMiddleware(Protocol, Generic[Event]):
+    def __call__(self: EventMiddleware, event: Event) -> Event | None: ...
+
+
+class CreateStoreOptions(Immutable, Generic[Action, Event]):
+    auto_init: bool = False
+    threads: int = 5
+    scheduler: Scheduler | None = None
+    action_middlewares: Sequence[ActionMiddleware[Action]] = field(default_factory=list)
+    event_middlewares: Sequence[EventMiddleware[Event]] = field(default_factory=list)
+    task_creator: TaskCreator | None = None
+    on_finish: Callable[[], Any] | None = None
+    grace_time_in_seconds: float = 1
+
+
+class AutorunOptions(Immutable, Generic[AutorunOriginalReturnType]):
+    default_value: AutorunOriginalReturnType | None = None
+    initial_run: bool = True
+    keep_ref: bool = True
+    subscribers_initial_run: bool = True
+    subscribers_keep_ref: bool = True
+
+
+class AutorunType(Protocol, Generic[State]):
+    def __call__(
+        self: AutorunType,
+        selector: Callable[[State], SelectorOutput],
+        comparator: Callable[[State], Any] | None = None,
+        *,
+        options: AutorunOptions[AutorunOriginalReturnType] | None = None,
+    ) -> AutorunDecorator[
+        State,
+        SelectorOutput,
+        AutorunOriginalReturnType,
+    ]: ...
+
+
+class AutorunDecorator(
+    Protocol,
+    Generic[
+        State,
+        SelectorOutput,
+        AutorunOriginalReturnType,
+    ],
+):
+    def __call__(
+        self: AutorunDecorator,
+        func: Callable[[SelectorOutput], AutorunOriginalReturnType]
+        | Callable[[SelectorOutput, SelectorOutput], AutorunOriginalReturnType],
+    ) -> AutorunReturnType[AutorunOriginalReturnType]: ...
+
+
+class AutorunReturnType(
+    Protocol,
+    Generic[AutorunOriginalReturnType],
+):
+    def __call__(self: AutorunReturnType) -> AutorunOriginalReturnType: ...
+
+    @property
+    def value(self: AutorunReturnType) -> AutorunOriginalReturnType: ...
+
+    def subscribe(
+        self: AutorunReturnType,
+        callback: Callable[[AutorunOriginalReturnType], Any],
+        *,
+        initial_run: bool | None = None,
+        keep_ref: bool | None = None,
+    ) -> Callable[[], None]: ...
+
+    def unsubscribe(self: AutorunReturnType) -> None: ...
+
+
+class EventSubscriber(Protocol):
+    def __call__(
+        self: EventSubscriber,
+        event_type: type[Event],
+        handler: EventHandler[Event],
+        *,
+        keep_ref: bool = True,
+    ) -> Callable[[], None]: ...
+
+
+DispatchParameters: TypeAlias = Action | Event | list[Action | Event]
+
+
+class Dispatch(Protocol, Generic[State, Action, Event]):
+    def __call__(
+        self: Dispatch,
+        *items: Action | Event | list[Action | Event],
+        with_state: Callable[[State | None], Action | Event | list[Action | Event]]
+        | None = None,
+    ) -> None: ...
+
+
+class BaseCombineReducerState(Immutable):
+    _id: str
+
+
+class CombineReducerAction(BaseAction):
+    _id: str
+
+
+class CombineReducerInitAction(CombineReducerAction, InitAction):
+    key: str
+
+
+class CombineReducerRegisterAction(CombineReducerAction):
+    key: str
+    reducer: ReducerType
+
+
+class CombineReducerUnregisterAction(CombineReducerAction):
+    key: str
+
+
+SnapshotAtom = (
+    int
+    | float
+    | str
+    | bool
+    | NoneType
+    | dict[str, 'SnapshotAtom']
+    | list['SnapshotAtom']
+)

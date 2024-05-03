0a5fcdda378c989fdf0debb457725da2dfde4a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 16:04:14 +0000

    Add package sdgclassification-benchmark/0.7.0/sdgclassification_benchmark-0.7.0-py3-none-any.whl

---
+++ packages/sdgclassification-benchmark/sdgclassification_benchmark-0.7.0-py3-none-any.whl/sdgclassification/benchmark/__init__.py
@@ -0,0 +1,9 @@
+from .load_benchmark_df import load_benchmark_df
+from .Benchmark import Benchmark
+from .Metrics import Metrics
+from .Result import Result
+from .ResultSet import ResultSet
+from .Stats import Stats
+
+
+__all__ = ["load_benchmark_df", "Benchmark", "Metrics", "Result", "ResultSet", "Stats"]

7bd1e70e47a58204f1437194dd9def8abdd0ff3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 15:04:57 +0000

    Add package conversion_utils/0.1.0/conversion_utils-0.1.0-py3-none-any.whl

---
+++ packages/conversion_utils/conversion_utils-0.1.0-py3-none-any.whl/conversion_utils/conversion.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+class conversion:
+
+    # def __init__(self) -> None:
+    #     return
+
+    def mask_to_bb(self, path):
+    
+        ### Read the image in gray mode
+        mask_img = cv2.imread(path, 0)
+
+        if mask_img is not None:
+
+            mask_img_array = np.array(mask_img)
+
+            ### Extract color
+            background, mask = np.unique(mask_img_array)
+
+            segmentation = np.where(mask_img_array == mask)
+
+            bounding_box = 0, 0, 0, 0
+
+            if len(segmentation) != 0:
+                x_min = int(np.min(segmentation[1]))
+                x_max = int(np.max(segmentation[1]))
+                y_min = int(np.min(segmentation[0]))
+                y_max = int(np.max(segmentation[0]))
+
+                bounding_box = x_min, y_min, x_max, y_max
+                
+        return bounding_box

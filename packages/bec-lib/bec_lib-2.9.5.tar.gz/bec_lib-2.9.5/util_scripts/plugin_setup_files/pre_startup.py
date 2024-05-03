39e4c1a29ccefeadc9f282d167aee3e47efac4ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 03:04:22 +0000

    Add package bec-lib/2.9.5/bec_lib-2.9.5.tar.gz

---
+++ packages/bec-lib/bec_lib-2.9.5.tar.gz/bec_lib-2.9.5/util_scripts/plugin_setup_files/pre_startup.py
@@ -0,0 +1,14 @@
+"""
+Pre-startup script for BEC client. This script is executed before the BEC client
+is started. It can be used to add additional command line arguments. 
+"""
+
+
+def extend_command_line_args(parser):
+    """
+    Extend the command line arguments of the BEC client.
+    """
+
+    # parser.add_argument("--session", help="Session name", type=str, default="cSAXS")
+
+    return parser

a6299609db0f33b485b8d60da617b3890c6783a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 14:04:25 +0000

    Add package geoglows/1.5.0/geoglows-1.5.0-py3-none-any.whl

---
+++ packages/geoglows/geoglows-1.5.0-py3-none-any.whl/geoglows/__init__.py
@@ -0,0 +1,17 @@
+import geoglows.bias
+import geoglows._plots as plots
+import geoglows.data
+import geoglows.analyze
+import geoglows.streams
+import geoglows.tables
+import geoglows.streamflow
+
+from ._constants import METADATA_TABLE_PATH
+
+__all__ = [
+    'bias', 'plots', 'data', 'analyze', 'streams', 'tables', 'streamflow',
+    'METADATA_TABLE_PATH'
+]
+__version__ = '1.5.0'
+__author__ = 'Riley Hales'
+__license__ = 'BSD 3-Clause Clear License'

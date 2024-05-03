764769b7d16cb4a584417e3c59e6cadc8ff96b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 05:03:53 +0000

    Add package ansible-creator/24.4.7/ansible_creator-24.4.7-py3-none-any.whl

---
+++ packages/ansible-creator/ansible_creator-24.4.7-py3-none-any.whl/ansible_creator/constants.py
@@ -0,0 +1,6 @@
+"""Definition of constants for this package."""
+
+GLOBAL_TEMPLATE_VARS = {
+    "DEV_CONTAINER_IMAGE": "ghcr.io/ansible/community-ansible-dev-tools:latest",
+    "DEV_FILE_IMAGE": "ghcr.io/ansible/ansible-workspace-env-reference:latest",
+}

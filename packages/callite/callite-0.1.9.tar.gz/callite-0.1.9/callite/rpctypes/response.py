bb418ebe9e00bab28ebb4aa24347140351dd6706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 04:04:14 +0000

    Add package callite/0.1.9/callite-0.1.9.tar.gz

---
+++ packages/callite/callite-0.1.9.tar.gz/callite-0.1.9/callite/rpctypes/response.py
@@ -0,0 +1,18 @@
+import json
+
+from callite.rpctypes.message_base import MessageBase
+
+
+class Response(MessageBase):
+    def __init__(self, method: str, message_id = None):
+        super(Response, self).__init__(method, message_id)
+        self.data = None
+
+
+    def response_json(self):
+        # get the json string of the current instance
+        response_data = json.dumps(self).encode('utf-8')
+        return response_data
+
+    def __str__(self):
+        return "Response: message_id: %s, response_data: %s" % (self.message_id, self.data)

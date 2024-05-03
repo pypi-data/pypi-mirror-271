f0f38e99c91604293794e6017605b51d77459268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 04:04:04 +0000

    Add package gr1336-toolbox/0.0.1/gr1336_toolbox-0.0.1.tar.gz

---
+++ packages/gr1336-toolbox/gr1336_toolbox-0.0.1.tar.gz/gr1336_toolbox-0.0.1/gr1336_toolbox/misc_tools.py
@@ -0,0 +1,77 @@
+from .types_check import _array, _dict, _numpy
+from typing import Any, TypeAlias, Literal, Callable
+import re
+from datetime import datetime
+import pyperclip
+
+
+def np_list(content):
+    if _numpy(content):
+        return content.flatten().tolist()
+
+
+def current_time():
+    return f"{datetime.now().strftime('%Y-%m-%d-%H%M%S')}"
+
+
+def clipboard(text: str):
+    """
+    Set the clipboard to the given text.
+    """
+    pyperclip.copy(str(text))
+
+
+def flatten_list(entry):
+    """
+    Example:
+    ```py
+    from grtoolbox.type_tools import flatten_list
+
+    sample = ["test", [[[1]], [2]], 3, [{"last":4}]]
+    results = flatten_list(sample)
+    # results = ["test", 1, 2, 3, {"last": 4}]
+    ```"""
+    if _array(entry):
+        return [item for sublist in entry for item in flatten_list(sublist)]
+    return [entry] if entry is not None else []
+
+
+def filter_list(entry: list | tuple, types: TypeAlias) -> list:
+    if not _array(entry, allow_empty=False):
+        return []
+    return [x for x in entry if isinstance(x, types)]
+
+
+def dict_to_list(
+    entry: dict[str, Any],
+    return_item: Literal["key", "content"] = "content",
+) -> list:
+    res = []
+    assert _dict(entry), "the entry provided is not a valid dictionary. Received: {}".format(entry)
+    if return_item == "content":
+        return list(entry.values())
+    return list(entry.keys())
+
+
+def try_call(comp: Callable, verbose_exception: bool = False, **kwargs):
+    try: return comp(**kwargs)
+    except Exception as e:
+        if verbose_exception: 
+            print(e)
+
+
+def recursive_replacer(text: str, dic: dict) -> str:
+    for i, j in dic.items():
+        text = text.replace(i, j)
+    return text
+
+
+def atoi(text: str):
+    # From oobabooga
+    text = text.strip()
+    return int(text) if text.isdigit() else text.lower()
+
+
+def natural_keys(text):
+    # From oobabooga
+    return [atoi(c) for c in re.split(r"(\d+)", text)]
---
+++ packages/gr1336-toolbox/gr1336_toolbox-0.0.1.tar.gz/gr1336_toolbox-0.0.1/gr1336_toolbox/text_tools.py
@@ -0,0 +1,41 @@
+import re
+from .types_check import _str
+from textblob import TextBlob
+
+
+def unescape(elem: str) -> str:
+    assert _str(elem, True), "The input should be a valid string."
+    return elem.encode().decode("unicode-escape", "ignore")
+
+
+def simple_quotes(txt):
+    """Idea from kobold"""
+    replacements = {
+        "“": '"',
+        "”": '"',
+        "’": "'",
+        "‘": "'",
+        "`": "'",
+    }
+    for key, value in replacements.items():
+        txt = txt.replace(key, value)
+    return txt
+
+
+def blob_split(text: str) -> list[str]:
+    return [x for x in TextBlob(text).raw_sentences]
+
+
+def trimincompletesentence(txt: str) -> str:
+    """Idea from KoboldAI"""
+    # Cache length of text
+    ln = len(txt)
+    # Find last instance of punctuation (Borrowed from Clover-Edition by cloveranon)
+    lastpunc = max(txt.rfind("."), txt.rfind("!"), txt.rfind("?"))
+    # Is this the end of a quote?
+    if lastpunc < ln - 1:
+        if txt[lastpunc + 1] == '"':
+            lastpunc = lastpunc + 1
+    if lastpunc >= 0:
+        txt = txt[: lastpunc + 1]
+    return txt

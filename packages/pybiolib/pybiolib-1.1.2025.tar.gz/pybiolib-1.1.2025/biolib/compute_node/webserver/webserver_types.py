a26f53b36f0761815f500bb04802ddc100e144e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 03:03:35 +0000

    Add package pybiolib/1.1.2025/pybiolib-1.1.2025.tar.gz

---
+++ packages/pybiolib/pybiolib-1.1.2025.tar.gz/pybiolib-1.1.2025/biolib/compute_node/webserver/webserver_types.py
@@ -0,0 +1,20 @@
+from biolib.typing_utils import TypedDict
+
+
+class ComputeNodeInfo(TypedDict):
+    auth_token: str
+    ip_address: str
+    public_id: str
+    pybiolib_version: str
+
+
+class ShutdownTimes(TypedDict):
+    auto_shutdown_time_in_seconds: int
+
+
+class WebserverConfig(TypedDict):
+    base_url: str
+    compute_node_info: ComputeNodeInfo
+    is_dev: bool
+    s3_general_storage_bucket_name: str
+    shutdown_times: ShutdownTimes

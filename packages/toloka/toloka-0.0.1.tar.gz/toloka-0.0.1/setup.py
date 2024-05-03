d6375d2a504486333efc080d18bf8132fb2a1050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 03:04:22 +0000

    Add package toloka/0.0.1/toloka-0.0.1.tar.gz

---
+++ packages/toloka/toloka-0.0.1.tar.gz/toloka-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='toloka',
+    version='0.0.1',
+    packages=find_packages(),
+    description='This package name is reserved',
+    long_description='See https://pypi.org/project/toloka-kit/ for the real package',
+    url='https://www.example.com',
+    author='solesensei',
+    author_email='solergfd@gmail.com',
+    classifiers=['Development Status :: 1 - Planning'],
+)

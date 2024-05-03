3e95d9d9c6fc7968690436b384a2d201b1ad8b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 04:04:35 +0000

    Add package cloudzero-uca-tools/0.7.9/cloudzero_uca_tools-0.7.9.tar.gz

---
+++ packages/cloudzero-uca-tools/cloudzero_uca_tools-0.7.9.tar.gz/cloudzero_uca_tools-0.7.9/uca/features/transform.py
@@ -0,0 +1,55 @@
+#  Copyright (c) 2021-2023 CloudZero, Inc. or its affiliates. All Rights Reserved.
+#  SPDX-License-Identifier: Apache-2.0
+#  Direct all questions to support@cloudzero.com
+
+import os
+import sys
+
+import jq
+import simplejson as json
+
+from uca.common.cli import eprint
+
+
+def compile_transform(jq_script):
+    return jq.compile(jq_script)
+
+
+def apply_transform(jq_program, input_data):
+    if jq_program:
+        return jq_program.input(input_data).all()
+    else:
+        return input_data
+
+
+def transform_data(input_data, transform_script):
+    data_to_send = []
+    transformed_count = 0
+    filtered_count = 0
+
+    if not transform_script:
+        return input_data, transformed_count, filtered_count
+
+    jq_program = compile_transform(transform_script)
+    for line in input_data:
+        transformed_line = apply_transform(jq_program, json.loads(line))
+        if transformed_line:
+            data_to_send.append(transformed_line)
+            transformed_count += 1
+        else:
+            filtered_count += 1
+    return data_to_send, transformed_count, filtered_count
+
+
+def load_transform_script(transform):
+    transform_script = None
+    if transform:
+        transform_file_path = os.path.expanduser(transform)
+        if os.path.isfile(transform_file_path):
+            print(f" - Applying {transform} transform script")
+            with open(transform_file_path, 'r') as file:
+                transform_script = file.read()
+        else:
+            eprint(f"Could not read {transform_file_path}, please check file and try again")
+            sys.exit(1)
+    return transform_script
---
+++ packages/cloudzero-uca-tools/cloudzero_uca_tools-0.7.9.tar.gz/cloudzero_uca_tools-0.7.9/uca/features/transmit.py
@@ -0,0 +1,24 @@
+#  Copyright (c) 2021-2023 CloudZero, Inc. or its affiliates. All Rights Reserved.
+#  SPDX-License-Identifier: Apache-2.0
+#  Direct all questions to support@cloudzero.com
+
+import sys
+
+from uca.common.files import write_to_file
+from uca.interfaces.uca_api import send_uca_events
+
+
+def transmit(stream_name, stream_type, transmit_type, uca_to_send, output_file, api_key, dry_run):
+    # order maters here, we want dry run to come first, if not that, output to file, if not that api key
+    if dry_run:
+        print('\nDry run, exiting')
+        sys.exit()
+    elif output_file:
+        write_to_file(uca_to_send, output_file)
+        print(f'\nWrote results to {output_file}')
+    elif api_key:
+        print('\nSending to API')
+        send_uca_events(stream_name, stream_type, transmit_type, api_key, uca_to_send)
+
+    else:
+        print('\nFinished, nothing to do')

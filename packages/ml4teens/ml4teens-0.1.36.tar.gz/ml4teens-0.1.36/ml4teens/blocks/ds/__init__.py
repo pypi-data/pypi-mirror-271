17cd05254cf8be4a7b84a07b41bb3e37fa46c576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 16:04:34 +0000

    Add package ml4teens/0.1.36/ml4teens-0.1.36.tar.gz

---
+++ packages/ml4teens/ml4teens-0.1.36.tar.gz/ml4teens-0.1.36/ml4teens/blocks/ds/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from .dataLabelizer  import DataLabelizer;
+from .dataWarehouse  import DataWarehouse;
+from .splitColumns   import SplitColumns;
+from .clustering     import Clustering;
+from .plot2D         import Plot2D;
+

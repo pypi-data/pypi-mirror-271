21f4962ac892f4f457446f548406ef9fd1355548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-03 04:03:29 +0000

    Add package kvm-db/0.1.3/kvm_db-0.1.3.tar.gz

---
+++ packages/kvm-db/kvm_db-0.1.3.tar.gz/kvm_db-0.1.3/src/kvm_db/backends/base.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+
+class FastDatabaseBackend(ABC):
+    @abstractmethod
+    def _create_table(self, name: str) -> None:
+        pass
+
+    @abstractmethod
+    def _get_tables(self) -> list[str]:
+        pass
+
+    @abstractmethod
+    def _insert_datum(self, table: str, key: str, value: str) -> None:
+        pass
+
+    @abstractmethod
+    def _delete_datum(self, table: str, key: str) -> None:
+        pass
+
+    @abstractmethod
+    def _get_datum(self, table: str, key: str) -> str | None:
+        pass
+
+    @abstractmethod
+    def _get_all_data(self, table: str) -> list[tuple[str, str]]:
+        pass
+
+    @abstractmethod
+    def _update_datum(self, table: str, key: str, value: str) -> None:
+        pass

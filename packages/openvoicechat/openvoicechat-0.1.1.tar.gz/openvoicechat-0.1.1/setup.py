bf23563f5039943d3d04de975b89d05c0892d587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 05:03:48 +0000

    Add package openvoicechat/0.1.1/openvoicechat-0.1.1.tar.gz

---
+++ packages/openvoicechat/openvoicechat-0.1.1.tar.gz/openvoicechat-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    url='http://www.fakhirali.pk/OpenVoiceChat/',
+    name='openvoicechat',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'sounddevice',
+        'torch',
+        'torchaudio',
+        'pygame',
+        'pyaudio',
+        'librosa',
+        'pydub',
+        'python-dotenv',
+        'phonemizer',
+        'websockets'
+    ],
+    extras_require={
+        'transformers' : ['transformers'],
+        'piper': ['piper-tts','piper-phonemize'],
+        'vosk': ['vosk'],
+        'llama': ['llama-cpp-python'],
+        'open_ai': ['openai'],
+        'tortoise': ['tortoise-tts'],
+        'xtts': ['TTS'],
+    },
+    dependency_links=[],
+)

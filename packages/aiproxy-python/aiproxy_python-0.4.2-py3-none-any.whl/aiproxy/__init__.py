fdb17b4e5462d9e7e216c1143fb7901389ee9fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 14:05:56 +0000

    Add package aiproxy-python/0.4.2/aiproxy_python-0.4.2-py3-none-any.whl

---
+++ packages/aiproxy-python/aiproxy_python-0.4.2-py3-none-any.whl/aiproxy/__init__.py
@@ -0,0 +1,26 @@
+__version__ = "0.4.2"
+
+import logging
+
+logger = logging.getLogger("aiproxy")
+logger.setLevel(logging.INFO)
+log_format = logging.Formatter("[%(levelname)s] %(asctime)s : %(message)s")
+streamHandler = logging.StreamHandler()
+streamHandler.setFormatter(log_format)
+logger.addHandler(streamHandler)
+
+
+from .proxy import (
+    RequestFilterBase,
+    ResponseFilterBase,
+    RequestFilterException,
+    ResponseFilterException
+)
+
+from .accesslog import (
+    AccessLogBase,
+    AccessLog,
+    AccessLogWorker
+)
+
+from .chatgpt import ChatGPTProxy

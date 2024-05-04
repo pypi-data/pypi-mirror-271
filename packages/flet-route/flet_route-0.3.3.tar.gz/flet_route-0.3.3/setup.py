edc90795e2db824b225cac85aa6c20ea1add1d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 03:06:43 +0000

    Add package flet-route/0.3.3/flet_route-0.3.3.tar.gz

---
+++ packages/flet-route/flet_route-0.3.3.tar.gz/flet_route-0.3.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name = "flet_route",
+    version = "0.3.3",
+    author="Saurabh Wadekar [ INDIA ]",
+    packages=["flet_route","cli"],
+    license="MIT",
+    maintainer="Saurabh Wadekar",
+    maintainer_email="saurabhwadekar420@gmail.com",
+    keywords=["flet","routing","flet_route","routes","flet app","flet-route","flet simple routing"],
+    description="This makes it easy to manage multiple views with dynamic routing.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/saurabhwadekar/flet_route",
+    include_package_data=True,
+    install_requires=[
+        'click==8.1.3',
+        "repath",
+        "flet",
+    ],
+    entry_points= {
+        'console_scripts': 
+        ['flet-route=cli:make_app']
+    }, 
+)

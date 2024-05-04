31b5df478a7cdadf63af47a91ae745e1ed5df93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 03:06:05 +0000

    Add package survival-predictor-model/0.1.0/survival_predictor_model-0.1.0.tar.gz

---
+++ packages/survival-predictor-model/survival_predictor_model-0.1.0.tar.gz/survival_predictor_model-0.1.0/tests/test_prediction.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from sklearn.metrics import accuracy_score
+from model.predict import make_prediction
+
+
+def test_prediction_against_truth_value(sample_input_data):
+    # When
+    estimated_results = make_prediction(input_data=sample_input_data)
+
+    # Then
+    predictions = estimated_results.get("predictions")
+    assert isinstance(predictions, np.ndarray)
+    assert isinstance(predictions[0], np.int64)
+    assert estimated_results.get("errors") is None
+    _predictions = list(predictions)
+    y_true = sample_input_data["survived"]
+    accuracy = accuracy_score(y_true, _predictions)
+    assert accuracy > 0.7

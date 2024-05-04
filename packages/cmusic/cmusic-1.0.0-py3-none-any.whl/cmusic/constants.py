3b5753a138a7ea88ec25436e5293058e9733ac0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 15:04:01 +0000

    Add package cmusic/1.0.0/cmusic-1.0.0-py3-none-any.whl

---
+++ packages/cmusic/cmusic-1.0.0-py3-none-any.whl/cmusic/constants.py
@@ -0,0 +1,42 @@
+"""cmusic constants"""
+
+import os
+import json
+import objlog
+
+from objlog.LogMessages import Info, Debug
+
+# the path to the config file
+
+CMUSIC_DIR = os.path.join(os.path.expanduser("~"), ".cmusic")
+
+CONFIG_FILE = os.path.join(CMUSIC_DIR, "config.json")
+
+LOG_FILE = os.path.join(CMUSIC_DIR, "cmusic.log")
+MAIN = objlog.LogNode("CMUSIC", log_file=LOG_FILE)
+
+DEFAULT_CONFIG = {
+    "library": os.path.join(os.path.expanduser("~"), "cMusic Library"),
+    "volume": 100,
+}
+
+# check if the cmusic directory exists
+if not os.path.exists(CMUSIC_DIR):
+    MAIN.log(Info("Creating cmusic directory"))
+    os.makedirs(CMUSIC_DIR)
+
+if not os.path.exists(CONFIG_FILE):
+    with open(CONFIG_FILE, "w") as f:
+        f.write(json.dumps(DEFAULT_CONFIG, indent=4))
+    MAIN.log(Info("Created config file"))
+
+# create crashes directory
+if not os.path.exists(os.path.join(CMUSIC_DIR, "crashes")):
+    os.makedirs(os.path.join(CMUSIC_DIR, "crashes"))
+    MAIN.log(Info("Created crashes directory"))
+
+config = json.load(open(CONFIG_FILE))
+LIBRARY = config["library"]
+CRASH_FOLDER = os.path.join(CMUSIC_DIR, "crashes")
+
+MAIN.log(Info("Constants OK"))

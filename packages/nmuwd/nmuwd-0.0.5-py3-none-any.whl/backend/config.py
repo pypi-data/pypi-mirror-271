59b571663b2c5e7760723dadeb2932df82806fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 04:04:30 +0000

    Add package nmuwd/0.0.5/nmuwd-0.0.5-py3-none-any.whl

---
+++ packages/nmuwd/nmuwd-0.0.5-py3-none-any.whl/backend/config.py
@@ -0,0 +1,48 @@
+# ===============================================================================
+# Copyright 2024 Jake Ross
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+
+
+class Config:
+    bbox = None
+    output_path = "output"
+    use_csv = True
+    use_geojson = False
+    use_source_ampapi = True
+
+    # use_source_isc_seven_rivers = True
+    # use_source_nwis = True
+
+    use_source_isc_seven_rivers = False
+    use_source_nwis = False
+
+    def bounding_points(self):
+        p1, p2 = self.bbox.split(",")
+        x1, y1 = [float(a) for a in p1.strip().split(" ")]
+        x2, y2 = [float(a) for a in p2.strip().split(" ")]
+
+        if x1 > x2:
+            x1, x2 = x2, x1
+        if y1 > y2:
+            y1, y2 = y2, y1
+
+        return x1, y1, x2, y2
+
+    def bounding_wkt(self):
+        x1, y1, x2, y2 = self.bounding_points()
+        return f"POLYGON(({x1} {y1},{x1} {y2},{x2} {y2},{x2} {y1},{x1} {y1}))"
+
+
+# ============= EOF =============================================
---
+++ packages/nmuwd/nmuwd-0.0.5-py3-none-any.whl/frontend/cli.py
@@ -0,0 +1,65 @@
+# ===============================================================================
+# Copyright 2024 Jake Ross
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+import click
+
+from backend.config import Config
+from frontend.unifier import unify_sites, unify_waterlevels
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option(
+    "--bbox",
+    default="",
+    help="Bounding box in the form 'x1 y1, x2 y2'",
+)
+def wells(bbox):
+    """
+    Get locations
+    """
+
+    click.echo(f"Getting locations for bounding box {bbox}")
+
+    config = Config()
+    # bbox = -105.396826 36.219290, -106.024162 35.384307
+    config.bbox = bbox
+
+    unify_sites(config)
+
+
+@cli.command()
+@click.option(
+    "--bbox",
+    default="",
+    help="Bounding box in the form 'x1 y1, x2 y2'",
+)
+def waterlevels(bbox):
+    click.echo(f"Getting waterlevels for bounding box {bbox}")
+
+    config = Config()
+    # bbox = -107.468262,33.979809,-107.053528,34.191358
+    # bbox = -105.396826 36.219290, -106.024162 35.384307
+    # bbox = -107.266538 34.098781,-107.233107 34.114967
+    config.bbox = bbox
+
+    unify_waterlevels(config)
+
+
+# ============= EOF =============================================

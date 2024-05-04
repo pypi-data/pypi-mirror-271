4ffebe61254a126cdc0b021c619167b1d76fe5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 14:06:11 +0000

    Add package SMClib.py/1.0.3/SMClib.py-1.0.3.tar.gz

---
+++ packages/SMClib.py/SMClib.py-1.0.3.tar.gz/SMClib.py-1.0.3/SMCLIB/__init__.py
@@ -0,0 +1,2 @@
+def test():
+    print("MARIO BROS BRO")
---
+++ packages/SMClib.py/SMClib.py-1.0.3.tar.gz/SMClib.py-1.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='SMClib.py',
+  version='1.0.3',
+  description='biblioteka ułatwiająca prace przy discord botach',
+  long_description=open('README.rst').read(),
+  url='',  
+  author='ESKARABATOS',
+  author_email='bartek.torba@interia.pl',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='SMC Discord',
+  packages=find_packages(),
+  install_requires=[] 
+)

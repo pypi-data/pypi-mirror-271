36eb338d9f89f95168afdddbd223097d6794274a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 05:04:01 +0000

    Add package dstool/0.1.0.dev4/dstool-0.1.0.dev4.tar.gz

---
+++ packages/dstool/dstool-0.1.0.dev4.tar.gz/dstool-0.1.0.dev4/dstool/plot/__init__.py
@@ -0,0 +1,9 @@
+from .parser import parse_annotations
+from .style import color_palette, set_font, set_plotly_theme
+
+__all__ = [
+    "set_font",
+    "set_plotly_theme",
+    "color_palette",
+    "parse_annotations",
+]

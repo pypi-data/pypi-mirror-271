c05ab15f30baab987d43274416c1118ed71f4d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 04:04:57 +0000

    Add package toolfuse/0.1.20/toolfuse-0.1.20.tar.gz

---
+++ packages/toolfuse/toolfuse-0.1.20.tar.gz/toolfuse-0.1.20/toolfuse/__init__.py
@@ -0,0 +1,23 @@
+from .base import (
+    Tool,
+    Action,
+    Observation,
+    action,
+    observation,
+    tool,
+    tool_from_cls,
+    tool_from_function,
+    tool_from_object,
+)
+
+__all__ = [
+    "Tool",
+    "Action",
+    "Observation",
+    "action",
+    "observation",
+    "tool",
+    "tool_from_function",
+    "tool_from_cls",
+    "tool_from_object",
+]

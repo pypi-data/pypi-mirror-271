726e9b22d134a8b0b891e760ac8f35c23034c130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 04:04:51 +0000

    Add package poly-crop/0.2/poly_crop-0.2.tar.gz

---
+++ packages/poly-crop/poly_crop-0.2.tar.gz/poly_crop-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='poly_crop',
+    version='0.2',
+    packages=find_packages(),
+    author='Elias Jensen',
+    description='Python library for cropping polygons in images',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'Pillow',
+    ],
+)

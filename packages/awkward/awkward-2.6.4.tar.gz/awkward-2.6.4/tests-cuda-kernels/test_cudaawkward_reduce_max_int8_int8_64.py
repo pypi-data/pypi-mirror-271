6306f8dd83032d2b4ba4f05dceed190408cb4b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 14:05:35 +0000

    Add package awkward/2.6.4/awkward-2.6.4.tar.gz

---
+++ packages/awkward/awkward-2.6.4.tar.gz/awkward-2.6.4/tests-cuda-kernels/test_cudaawkward_reduce_max_int8_int8_64.py
@@ -0,0 +1,22 @@
+# AUTO GENERATED ON 2024-05-03 AT 20:26:59
+# DO NOT EDIT BY HAND!
+#
+# To regenerate file, run
+#
+#     python dev/generate-tests.py
+#
+
+# fmt: off
+
+import cupy
+import pytest
+
+import awkward as ak
+import awkward._connect.cuda as ak_cu
+from awkward._backends.cupy import CupyBackend
+
+cupy_backend = CupyBackend.instance()
+
+@pytest.mark.skip(reason='Unable to generate any tests for kernel')
+def test_cudaawkward_reduce_max_int8_int8_64_1():
+    raise NotImplementedError('Unable to generate any tests for kernel')

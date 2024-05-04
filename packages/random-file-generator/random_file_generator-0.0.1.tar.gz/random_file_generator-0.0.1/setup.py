d8a48f6b80eb1a2abf3fa5747b64385daac8678b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 03:05:11 +0000

    Add package random-file-generator/0.0.1/random_file_generator-0.0.1.tar.gz

---
+++ packages/random-file-generator/random_file_generator-0.0.1.tar.gz/random_file_generator-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+  
+setuptools.setup( 
+    name="random-file-generator", 
+    version="0.0.1", 
+    author="NishithRaveeshKashyap", 
+    author_email="nishithkp175@gmail.com", 
+    packages=["random-file-generator"],
+    description='Generate random text files',
+    url="https://github.com/NishithKashyap/Generate-Random-Files", 
+    license='MIT', 
+    python_requires='>=3.8',
+    install_requires=[],
+)

f34d1bc6ed3d5fb7e8ed6e3fd46a8729d69cfb0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 04:04:25 +0000

    Add package dbt-synapse/1.7.4/dbt_synapse-1.7.4-py3-none-any.whl

---
+++ packages/dbt-synapse/dbt_synapse-1.7.4-py3-none-any.whl/dbt/include/synapse/macros/materializations/models/table/create_table_as.sql
@@ -0,0 +1,44 @@
+{% macro synapse__create_table_as(temporary, relation, sql) -%}
+   {%- set index = config.get('index', default="CLUSTERED COLUMNSTORE INDEX") -%}
+   {%- set dist = config.get('dist', default="ROUND_ROBIN") -%}
+   {% set tmp_relation = relation.incorporate(
+   path={"identifier": relation.identifier.replace("#", "") ~ '_temp_view'},
+   type='view')-%}
+   {%- set temp_view_sql = sql.replace("'", "''") -%}
+
+   {{ synapse__drop_relation_script(tmp_relation) }}
+
+   {{ synapse__drop_relation_script(relation) }}
+
+   {{ synapse__create_view_as(tmp_relation, sql) }}
+
+   {% set contract_config = config.get('contract') %}
+
+   {% if contract_config.enforced %}
+
+        {{exceptions.warn("Model contracts cannot be enforced by <adapter>!")}}
+
+        CREATE TABLE [{{relation.schema}}].[{{relation.identifier}}]
+        {{ synapse__build_columns_constraints(tmp_relation) }}
+        WITH(
+          DISTRIBUTION = {{dist}},
+          {{index}}
+        )
+        {{ get_assert_columns_equivalent(sql)  }}
+
+        {% set listColumns %}
+            {% for column in model['columns'] %}
+                {{ "["~column~"]" }}{{ ", " if not loop.last }}
+            {% endfor %}
+        {%endset%}
+
+        INSERT INTO [{{relation.schema}}].[{{relation.identifier}}]
+        ({{listColumns}}) SELECT {{listColumns}} FROM [{{tmp_relation.schema}}].[{{tmp_relation.identifier}}]
+
+    {%- else %}
+        EXEC('CREATE TABLE [{{relation.database}}].[{{relation.schema}}].[{{relation.identifier}}]WITH(DISTRIBUTION = {{dist}},{{index}}) AS (SELECT * FROM [{{tmp_relation.database}}].[{{tmp_relation.schema}}].[{{tmp_relation.identifier}}]);');
+    {% endif %}
+
+   {{ synapse__drop_relation_script(tmp_relation) }}
+
+{% endmacro %}

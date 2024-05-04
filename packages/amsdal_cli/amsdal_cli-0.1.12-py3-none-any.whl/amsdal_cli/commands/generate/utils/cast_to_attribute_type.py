94bbf8aafb3f1ef3ef449bb4cd2dc1df4c12bb51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 04:05:06 +0000

    Add package amsdal_cli/0.1.12/amsdal_cli-0.1.12-py3-none-any.whl

---
+++ packages/amsdal_cli/amsdal_cli-0.1.12-py3-none-any.whl/amsdal_cli/commands/generate/utils/cast_to_attribute_type.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+from amsdal_cli.commands.generate.enums import AttributeType
+
+
+def cast_to_attribute_type(
+    attr_type: AttributeType,
+    value: str,
+) -> Any:
+    if value.lower() == 'null':
+        return None
+
+    match attr_type:
+        case AttributeType.NUMBER:
+            return float(value)
+        case AttributeType.BOOLEAN:
+            return value.lower() == 'true'
+        case _:
+            return value

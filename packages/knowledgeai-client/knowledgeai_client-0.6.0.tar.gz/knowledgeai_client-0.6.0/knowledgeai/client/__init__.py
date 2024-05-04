d2760a55a0e1106253a61b43c614fc6ce88517d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 03:06:19 +0000

    Add package knowledgeai-client/0.6.0/knowledgeai_client-0.6.0.tar.gz

---
+++ packages/knowledgeai-client/knowledgeai_client-0.6.0.tar.gz/knowledgeai_client-0.6.0/knowledgeai/client/__init__.py
@@ -0,0 +1,19 @@
+# isort:skip_file
+from knowledgeai.client.schema import (
+    LLM,
+    AskResponse,
+    HttpMethod,
+    IsoLanguage,
+    Plan,
+    Project,
+    ProjectConfigKey,
+    ProjectDocument,
+    RetrievedDocument,
+    Retriever,
+    SourceDocument,
+    User,
+)
+from knowledgeai.client.admin import KnowledgeAIAdminClient
+from knowledgeai.client.client import KnowledgeAIClient
+
+__version__ = "0.6.0"

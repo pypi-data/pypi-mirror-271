2a6b1d04989d928ce32011190ea3b79a12057c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 03:05:04 +0000

    Add package core-commands/0.4.1/core_commands-0.4.1-py3-none-any.whl

---
+++ packages/core-commands/core_commands-0.4.1-py3-none-any.whl/core_commands/cmd/echo.py
@@ -0,0 +1,14 @@
+from .command_cmd import command_cmd
+
+def echo(text = False):
+    """
+    Display messages on screen, turn command-echoing on or off.
+
+    arguments: ON | OFF | /?
+    """
+    if (text):
+        return command_cmd(f'echo {text}')
+    return command_cmd("echo")
+
+echo("HOLIS")
+echo()

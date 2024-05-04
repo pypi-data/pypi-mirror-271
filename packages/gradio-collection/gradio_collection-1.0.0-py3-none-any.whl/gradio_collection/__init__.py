94f7e0ed40653dcd526f94f8b24d330cb952686c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 14:06:07 +0000

    Add package gradio-collection/1.0.0/gradio_collection-1.0.0-py3-none-any.whl

---
+++ packages/gradio-collection/gradio_collection-1.0.0-py3-none-any.whl/gradio_collection/__init__.py
@@ -0,0 +1,26 @@
+"""Dependencies available as part of gradio_collection
+"""
+
+# Packages by Daniel Ialcin Misser Westergaard (dwancin)
+from gradio_toggle import Toggle as Toggle
+from gradio_grid import Toggle as Toggle
+
+# Packages by Freddy Boulton (freddyaboulton)
+from gradio_calendar import Calendar as Calendar
+from gradio_pdf import PDF as PDF
+from gradio_folium import Folium as Folium
+
+# Packages by Ali Abdalla (aliabd)
+from gradio_imagefeed import ImageFeed as ImageFeed
+
+# Packages by Abubakar Abid (abidlabs)
+from gradio_highlightedcode import HighlightedCode as HighlightedCode
+
+# Packages by Ali Abid (aliabid94)
+from gradio_modal import Modal as Modal
+
+# Packages by Pete (pngwn)
+from gradio_imageslider import ImageSlider as ImageSlider
+
+# Packages by LastMile AI (lastmileai)
+from gradio_notebook import GradioNotebook as Notebook

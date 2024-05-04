ad6f8fa1f07b42ba4440d12a1adba2cfeb541c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 06:04:06 +0000

    Add package awkward-cpp/33/awkward-cpp-33.tar.gz

---
+++ packages/awkward-cpp/awkward-cpp-33.tar.gz/awkward-cpp-33/tests-spec/__init__.py
@@ -0,0 +1,10 @@
+# AUTO GENERATED ON 2024-05-03 AT 20:26:59
+# DO NOT EDIT BY HAND!
+#
+# To regenerate file, run
+#
+#     python dev/generate-tests.py
+#
+
+# fmt: off
+
---
+++ packages/awkward-cpp/awkward-cpp-33.tar.gz/awkward-cpp-33/tests-spec/test_pyawkward_RegularArray_reduce_local_nextparents_64.py
@@ -0,0 +1,16 @@
+# AUTO GENERATED ON 2024-05-03 AT 20:26:59
+# DO NOT EDIT BY HAND!
+#
+# To regenerate file, run
+#
+#     python dev/generate-tests.py
+#
+
+# fmt: off
+
+import pytest
+import kernels
+
+@pytest.mark.skip(reason='Unable to generate any tests for kernel')
+def test_pyawkward_RegularArray_reduce_local_nextparents_64_1():
+    raise NotImplementedError('Unable to generate any tests for kernel')

a319a0d794519f7410a3b821644c0e827683abcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 15:04:04 +0000

    Add package music-assistant-frontend/2.5.0/music_assistant_frontend-2.5.0-py3-none-any.whl

---
+++ packages/music-assistant-frontend/music_assistant_frontend-2.5.0-py3-none-any.whl/music_assistant_frontend/assets/VCardText-b1a0de89.js
@@ -0,0 +1,1 @@
+import{h as t}from"./VMenu-f26d3a19.js";const e=t("v-card-title"),r=t("v-card-text");export{r as V,e as a};
---
+++ packages/music-assistant-frontend/music_assistant_frontend-2.5.0-py3-none-any.whl/music_assistant_frontend/assets/logo-61d7c0d2.js
@@ -0,0 +1,1 @@
+const o=""+new URL("logo-9391b78c.svg",import.meta.url).href;export{o as _};

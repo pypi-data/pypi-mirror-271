c6d40957c970d8adce953b87078a08dffd6f0a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 03:06:35 +0000

    Add package netbox-docker-plugin/1.7.0/netbox_docker_plugin-1.7.0.tar.gz

---
+++ packages/netbox-docker-plugin/netbox_docker_plugin-1.7.0.tar.gz/netbox_docker_plugin-1.7.0/netbox_docker_plugin/__init__.py
@@ -0,0 +1,25 @@
+"""Netbox Plugin Configuration"""
+
+from extras.plugins import PluginConfig
+from django.db.models.signals import post_migrate
+from .utilities import create_webhook
+
+class NetBoxDockerConfig(PluginConfig):
+    """Plugin Config Class"""
+
+    name = "netbox_docker_plugin"
+    verbose_name = " NetBox Docker Plugin"
+    description = "Manage Docker"
+    version = "1.7.0"
+    base_url = "docker"
+    author= "Vincent Simonin"
+    author_email= "vincent@saashup.com"
+
+    def ready(self):
+        post_migrate.connect(create_webhook)
+
+        super().ready()
+
+
+# pylint: disable=C0103
+config = NetBoxDockerConfig

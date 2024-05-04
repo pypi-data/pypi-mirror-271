e67cc7286d1ad5036d507562e4e93d9ea09a99d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 05:03:49 +0000

    Add package asyncrun/0.3.0/asyncrun-0.3.0.tar.gz

---
+++ packages/asyncrun/asyncrun-0.3.0.tar.gz/asyncrun-0.3.0/README.md
@@ -0,0 +1,9 @@
+# basically Promise.all but python.
+
+```python
+import asyncrun
+
+# Your function and arguments here
+result = asyncrun.all(sample_function, args=[(1,), (2,)])
+print(result)
+```
---
+++ packages/asyncrun/asyncrun-0.3.0.tar.gz/asyncrun-0.3.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='asyncrun',
+    version='0.3.0',
+    author='Jack English',
+    author_email='jackjenglish@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'tqdm' 
+    ],
+    python_requires='>=3.6',
+    description='A simple utility for running functions in parallel.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jackjenglish/asyncrun'
+)

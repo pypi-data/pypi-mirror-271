3275a99483ac374d4f0ed85b0791703bbc2ab16d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 03:05:57 +0000

    Add package hello-world-lib/0.1.0/hello_world_lib-0.1.0.tar.gz

---
+++ packages/hello-world-lib/hello_world_lib-0.1.0.tar.gz/hello_world_lib-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hello_world_lib',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A simple library to display "Hello, World!"',
+    author='Ema',
+    author_email='',
+    url='https://github.com/MissCoco01/testhello',
+)

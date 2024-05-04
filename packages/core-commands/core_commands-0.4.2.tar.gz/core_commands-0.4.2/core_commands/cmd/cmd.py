11a51ba51c7a19fab2027c5fa026aefc967ac3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 03:05:17 +0000

    Add package core-commands/0.4.2/core_commands-0.4.2.tar.gz

---
+++ packages/core-commands/core_commands-0.4.2.tar.gz/core_commands-0.4.2/core_commands/cmd/cmd.py
@@ -0,0 +1,34 @@
+from .command_cmd import command_cmd
+from .attrib import attrib
+from .call import call
+from .cd import cd
+from .chdir import chdir
+from .cls import cls
+from .color import color
+from .copy import copy
+from .curl import curl
+from .date import date
+from .DEL import DEL
+from .dir import dir
+from .echo import echo
+from .hostname import hostname
+from .mkdir import mkdir
+from .ver import ver
+from .verify import verify
+from .w32tm import w32tm
+from .waitfor import waitfor
+from .wbadmin import wbadmin
+from .wecutil import wecutil
+from .wevtutil import wevtutil
+from .where import where
+from .whoami import whoami
+from .windiff import windiff
+from .winget import winget
+from .wpr import wpr
+from .wt import wt
+from .xcopy import xcopy
+from .mkdir import mkdir
+
+def cmd():
+    # https://ss64.com/nt/cmd.html
+    pass

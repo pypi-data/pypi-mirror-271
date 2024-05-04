6dadbd73abb68ec5f681a554c1bd74835085d754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 03:05:37 +0000

    Add package salientsdk/0.1.6/salientsdk-0.1.6-py3-none-any.whl

---
+++ packages/salientsdk/salientsdk-0.1.6-py3-none-any.whl/salientsdk/cli.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""Command line interface for the Salient SDK.
+
+Usage:
+```
+salientsdk examples
+salientsdk version
+```
+
+Copyright Salient Predictions 2024
+"""
+
+import sys
+
+from salientsdk.__main__ import main as main_logic
+
+
+def main():
+    """Dispatch to __main__.py."""
+    main_logic()
+
+
+if __name__ == "__main__":
+    main()

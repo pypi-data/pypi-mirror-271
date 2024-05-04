bf3943363bd470747ec51bbb58c24d2c855af94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 15:04:16 +0000

    Add package oktool/0.0.0.1/oktool-0.0.0.1.tar.gz

---
+++ packages/oktool/oktool-0.0.0.1.tar.gz/oktool-0.0.0.1/README.md
@@ -0,0 +1,6 @@
+# oktool
+# 字符串工具类
+```python
+ #字符串是否为空白
+ str_util.is_blank('dd')
+``` 
---
+++ packages/oktool/oktool-0.0.0.1.tar.gz/oktool-0.0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+
+    name='oktool',
+    version="0.0.0.1",
+    author="yinlong",
+    install_requires=[
+        # 在这里列出你的库所需的其他Python包
+    ],
+
+    author_email="your.email@example.com",
+    description="A short description of your awesome package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    # url="https://github.com/yourusername/my-awesome-package",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)

22ec62ea3b66c06ad8190a4d99d2c177d6e1d1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 04:06:14 +0000

    Add package dexscreener/1.0/dexscreener-1.0-py3-none-any.whl

---
+++ packages/dexscreener/dexscreener-1.0-py3-none-any.whl/dexscreener/client.py
@@ -0,0 +1,90 @@
+from .models import TokenPair
+from .http_client import HttpClient
+from typing import Optional
+
+class DexscreenerClient:
+    def __init__(self):
+        self._client: HttpClient = HttpClient(100, 60)
+
+    def get_token_pair(self, chain: str, address: str) -> Optional[TokenPair]:
+        """
+        Fetch a pair on the provided chain id
+
+        https://api.dexscreener.io/latest/dex/pairs/bsc/0x7213a321F1855CF1779f42c0CD85d3D95291D34C
+
+        :param chain: Chain id
+        :param address: Token address
+        :return:
+            Response as TokenPair model
+        """
+        resp = self._client.request("GET", f"dex/pairs/{chain}/{address}")
+        if resp["pair"]:
+            return TokenPair(**resp["pair"])
+        else:
+            pass #returns None for now but owner might want to raise Error
+
+    async def get_token_pair_async(self, chain: str, address: str) -> Optional[TokenPair]:
+        """
+        Async version of `get_token_pair`
+        """
+        resp = await self._client.request_async("GET", f"dex/pairs/{chain}/{address}")
+        if resp["pair"]:
+            return TokenPair(**resp["pair"])
+        else:
+            pass
+
+    def get_token_pairs(self, address: str) -> list[TokenPair]:
+        """
+        Get pairs matching base token address
+
+        https://api.dexscreener.io/latest/dex/tokens/0x2170Ed0880ac9A755fd29B2688956BD959F933F8
+
+        :param address: Token address
+        :return:
+            Response as list of TokenPair model
+        """
+        resp = self._client.request("GET",  f"dex/tokens/{address}")
+
+        if resp["pairs"]:
+            return [TokenPair(**pair) for pair in resp["pairs"]]
+        else:
+            return []
+
+    async def get_token_pairs_async(self, address: str) -> list[TokenPair]:
+        """
+        Async version of `get_token_pairs`
+        """
+        resp = await self._client.request_async("GET", f"dex/tokens/{address}")
+        
+        if resp["pairs"]:        
+            return [TokenPair(**pair) for pair in resp["pairs"]]
+        else:
+            return []
+
+    def search_pairs(self, search_query: str) -> list[TokenPair]:
+        """
+        Search for pairs matching query
+
+        https://api.dexscreener.io/latest/dex/tokens/0x2170Ed0880ac9A755fd29B2688956BD959F933F8
+
+        :param search_query: query (e.g.: WBTC or WBTC/USDC)
+        :return:
+            Response as list of TokenPair model
+        """
+        resp = self._client.request("GET", f"dex/search/?q={search_query}")
+        
+        if resp["pairs"]:
+            return [TokenPair(**pair) for pair in resp["pairs"]]
+        else:
+            return []
+
+    async def search_pairs_async(self, search_query: str) -> list[TokenPair]:
+        """
+        Async version of `search_pairs`
+        """
+        resp = await self._client.request_async("GET", f"dex/search/?q={search_query}")
+        
+        if resp["pairs"]:
+            return [TokenPair(**pair) for pair in resp["pairs"]]
+        else:
+            return []

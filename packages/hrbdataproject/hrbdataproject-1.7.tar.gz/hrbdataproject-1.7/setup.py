28ac174311488850b2ae13d30176cec4b47a0a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 03:06:24 +0000

    Add package hrbdataproject/1.7/hrbdataproject-1.7.tar.gz

---
+++ packages/hrbdataproject/hrbdataproject-1.7.tar.gz/hrbdataproject-1.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hrbdataproject',
+    version='1.7',
+    packages=find_packages(),
+    #py_modules=['data_quality_checks'],  # Specify the individual Python file here
+    description='A library for data quality checks in Databricks notebooks',
+    author='Harun Raseed Basheer',
+    author_email='harun.raseed093@gmail.com',
+    url='https://github.com/harun_raseed_basheer/my_data_quality_library',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)

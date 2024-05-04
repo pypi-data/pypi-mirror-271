1043fec2ff0911f2013db63d30b69682b00a4219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 15:04:51 +0000

    Add package queuelib/1.7.0/queuelib-1.7.0.tar.gz

---
+++ packages/queuelib/queuelib-1.7.0.tar.gz/queuelib-1.7.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="queuelib",
+    version="1.7.0",
+    license="BSD",
+    description="Collection of persistent (disk-based) and non-persistent (memory-based) queues",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst",
+    author="Scrapy project",
+    author_email="info@scrapy.org",
+    url="https://github.com/scrapy/queuelib",
+    packages=find_packages(),
+    package_data={
+        "queuelib": ["py.typed"],
+    },
+    platforms=["Any"],
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)

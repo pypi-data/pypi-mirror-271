6dda6b648a707fdfe9b85b19c5505826e188fb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 14:06:07 +0000

    Add package SMClib.py/1.0.0/SMClib.py-1.0.0.tar.gz

---
+++ packages/SMClib.py/SMClib.py-1.0.0.tar.gz/SMClib.py-1.0.0/SMCLIB/__init__.py
@@ -0,0 +1,10 @@
+class SMC():
+    def __init__(self):
+        self.x = 2
+        self.y = 4
+    
+    def login(self):
+        print("niger", self.x + self.y)
+
+def test():
+    return("test")

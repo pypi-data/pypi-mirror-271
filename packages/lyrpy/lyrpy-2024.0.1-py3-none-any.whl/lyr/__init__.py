083c2b32c4dc2596b26f34773a4b5a76ac59d1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 03:06:01 +0000

    Add package lyrpy/2024.0.1/lyrpy-2024.0.1-py3-none-any.whl

---
+++ packages/lyrpy/lyrpy-2024.0.1-py3-none-any.whl/lyr/__init__.py
@@ -0,0 +1,21 @@
+import os
+
+from typing import IO, TYPE_CHECKING, Any, Callable, Optional, Union
+
+#-------------------------------------------------------------------------------
+#
+#-------------------------------------------------------------------------------
+def main ():
+#beginfunction
+    print("Hello, **World**")
+#endfunction
+
+#------------------------------------------
+#
+#------------------------------------------
+#beginmodule
+if __name__ == "__main__":
+    main()
+#endif
+
+#endmodule

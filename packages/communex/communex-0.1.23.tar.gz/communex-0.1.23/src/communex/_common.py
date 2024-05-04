54d1044a0cab9aacc9c449fdb13931c350d22092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 14:05:52 +0000

    Add package communex/0.1.23/communex-0.1.23.tar.gz

---
+++ packages/communex/communex-0.1.23.tar.gz/communex-0.1.23/src/communex/_common.py
@@ -0,0 +1,64 @@
+import random
+from enum import Enum
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from communex.balance import from_nano
+
+
+class ComxSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="COMX_")
+    # TODO: improve node lists
+    NODE_URLS: list[str] = [
+        "wss://commune-api-node-1.communeai.net",
+        "wss://commune-node-2.agicommies.org",
+        "wss://commune-archive-node-1.agicommies.org",
+        "wss://commune-archive-node-2.agicommies.org",
+    ]
+    TESTNET_NODE_URLS: list[str] = ["wss://testnet-commune-api-node-0.communeai.net"]
+
+
+def get_node_url(
+    comx_settings: ComxSettings | None = None, *, use_testnet: bool = False
+) -> str:
+    comx_settings = comx_settings or ComxSettings()
+    match use_testnet:
+        case True:
+            node_url = random.choice(comx_settings.TESTNET_NODE_URLS)
+        case False:
+            node_url = random.choice(comx_settings.NODE_URLS)
+    return node_url
+
+
+def get_available_nodes(
+    comx_settings: ComxSettings | None = None, *, use_testnet: bool = False
+) -> list[str]:
+    comx_settings = comx_settings or ComxSettings()
+
+    match use_testnet:
+        case True:
+            node_urls = comx_settings.TESTNET_NODE_URLS
+        case False:
+            node_urls = comx_settings.NODE_URLS
+    return node_urls
+
+
+class BalanceUnit(str, Enum):
+    joule = "joule"
+    j = "j"
+    nano = "nano"
+    n = "n"
+
+
+def format_balance(balance: int, unit: BalanceUnit = BalanceUnit.nano) -> str:
+    """
+    Formats a balance.
+    """
+
+    match unit:
+        case BalanceUnit.nano | BalanceUnit.n:
+            return f"{balance}"
+        case BalanceUnit.joule | BalanceUnit.j:
+            in_joules = from_nano(balance)
+            round_joules = round(in_joules, 4)
+            return f"{round_joules:,} J"

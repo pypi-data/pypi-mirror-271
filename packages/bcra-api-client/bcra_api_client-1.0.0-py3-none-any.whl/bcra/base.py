e0427448f168e33f0d97649be8d4d9190c50345b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 05:03:40 +0000

    Add package bcra-api-client/1.0.0/bcra_api_client-1.0.0-py3-none-any.whl

---
+++ packages/bcra-api-client/bcra_api_client-1.0.0-py3-none-any.whl/bcra/base.py
@@ -0,0 +1,56 @@
+import pkg_resources
+import urllib3
+import json
+
+version = "unknown"
+
+try:
+   version = pkg_resources.require("bcra-api-client")[0].version
+except:
+    pass
+
+class BaseClient:
+
+    def __init__(self,
+                 api_key: str,
+                 connect_timeout: float,
+                 base : str
+                 ) -> None:
+        
+        self.BASE = base
+        
+        self.headers = {
+            "Accept-Encoding": "gzip",
+            "User-Agent": f"paivae/BCRA BCRA_Python_Client/{version}",
+        }
+
+        self.client = urllib3.PoolManager(
+            headers = self.headers
+        )
+
+
+    def _get(self,
+             path: str,
+             params : str
+             ):
+        
+        response = self.client.request(
+            "GET",
+            self.BASE + path,
+            fields = params,
+            headers = self.headers
+        )
+        
+
+        if response.status != 200 :
+            raise "Mverga"
+        
+        try:
+            obj = self._decoded(response)
+        except Exception as e:
+            print(f"Error json response: {e.message}")
+        
+
+    def _decoded(self, response):
+        return json.loads(response.data.decode("utf-8"))
+    
---
+++ packages/bcra-api-client/bcra_api_client-1.0.0-py3-none-any.whl/bcra/statistics/variables.py
@@ -0,0 +1,8 @@
+
+
+class Variables:
+    
+    def __init__(self) -> None:
+        pass
+
+    

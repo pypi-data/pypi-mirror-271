b98f9a9bece245a2f24e68cdd4defd6796ae755c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 03:06:41 +0000

    Add package metaphor-connectors/0.13.187/metaphor_connectors-0.13.187.tar.gz

---
+++ packages/metaphor-connectors/metaphor_connectors-0.13.187.tar.gz/metaphor_connectors-0.13.187/metaphor/unity_catalog/config.py
@@ -0,0 +1,43 @@
+from dataclasses import field
+from typing import Optional, Set
+
+from pydantic.dataclasses import dataclass
+
+from metaphor.common.base_config import BaseConfig
+from metaphor.common.dataclass import ConnectorConfig
+from metaphor.common.filter import DatasetFilter
+
+
+@dataclass(config=ConnectorConfig)
+class UnityCatalogQueryLogConfig:
+    # Number of days back of query logs to fetch, if 0, don't fetch query logs
+    lookback_days: int = 1
+
+    # Query log filter to exclude certain usernames
+    excluded_usernames: Set[str] = field(default_factory=lambda: set())
+
+    # Limit the number of results returned in one page. The default is 100.
+    max_results: Optional[int] = 100
+
+
+@dataclass(config=ConnectorConfig)
+class UnityCatalogRunConfig(BaseConfig):
+    host: str
+    token: str
+
+    # Override the URL for each dataset
+    source_url: Optional[str] = None
+
+    # Include or exclude specific databases/schemas/tables
+    filter: DatasetFilter = field(default_factory=lambda: DatasetFilter())
+
+    # The id of warehouse which will run the sql
+    warehouse_id: Optional[str] = None
+
+    # cluster http path
+    cluster_path: Optional[str] = None
+
+    # configs for fetching query logs
+    query_log: UnityCatalogQueryLogConfig = field(
+        default_factory=lambda: UnityCatalogQueryLogConfig()
+    )

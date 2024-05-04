dfd643faeb439f5cae0c5923fe5d6c9403f07010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 05:03:34 +0000

    Add package apparata/0.1.1/apparata-0.1.1.tar.gz

---
+++ packages/apparata/apparata-0.1.1.tar.gz/apparata-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='apparata',
+    version='0.1.1',
+    description='A collection of useful functions for generating random data.',
+    author='Nishant Gerald',
+    author_email='nishantgerald@gmail.com',
+    packages=['apparata'],
+    install_requires=['faker', 'pandas'],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)

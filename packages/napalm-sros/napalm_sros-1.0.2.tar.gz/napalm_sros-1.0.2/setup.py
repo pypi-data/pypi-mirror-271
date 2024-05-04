21b513dc50831eb7bb17e3e09b1d9a5b0b725c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 15:04:15 +0000

    Add package napalm-sros/1.0.2/napalm_sros-1.0.2.tar.gz

---
+++ packages/napalm-sros/napalm_sros-1.0.2.tar.gz/napalm_sros-1.0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as fs:
+    reqs = [r for r in fs.read().splitlines() if (len(r) > 0 and not r.startswith("#"))]
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="napalm-sros",
+    version="1.0.2",
+    packages=find_packages(),
+    package_data={"napalm_sros.templates": ["*.js"], "napalm_sros.utils.textfsm_templates": ["*.tpl"]},
+    author="Nokia",
+    author_email="",
+    description="Network Automation and Programmability Abstraction Layer with Multivendor support",
+    classifiers=[
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Natural Language :: English",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    url="https://github.com/napalm-automation/napalm-sros",
+    include_package_data=True,
+    install_requires=reqs,
+    python_requires=">=3.7",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)

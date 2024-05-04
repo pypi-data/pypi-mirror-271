02066e4bd992d7c7b3972473cefbfb665ac42a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 15:04:12 +0000

    Add package evnex/0.4.0a1/evnex-0.4.0a1.tar.gz

---
+++ packages/evnex/evnex-0.4.0a1.tar.gz/evnex-0.4.0a1/evnex/schema/org.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from typing import Any
+
+from pydantic import BaseModel
+
+from evnex.schema.cost import EvnexCost
+
+
+class EvnexOrgBrief(BaseModel):
+    id: str
+    isDefault: bool
+    role: int
+    createdDate: datetime
+    name: str
+    slug: str
+    tier: int
+    tierDetails: Any = None
+    updatedDate: datetime
+
+
+class EvnexOrgInsightEntry(BaseModel):
+    carbonOffset: float
+    costs: list[EvnexCost]
+    duration: int
+    powerUsage: float
+    startDate: datetime
+    sessions: int
+
+
+class EvnexGetOrgInsights(BaseModel):
+    items: list[EvnexOrgInsightEntry]
+
+
+class EvnexGetOrgInsightResponse(BaseModel):
+    data: EvnexGetOrgInsights

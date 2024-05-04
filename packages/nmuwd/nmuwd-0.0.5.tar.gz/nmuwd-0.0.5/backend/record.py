62814de61e5bd4ac5a21367f911e30e78d3dd866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-04 04:04:30 +0000

    Add package nmuwd/0.0.5/nmuwd-0.0.5.tar.gz

---
+++ packages/nmuwd/nmuwd-0.0.5.tar.gz/nmuwd-0.0.5/backend/record.py
@@ -0,0 +1,85 @@
+# ===============================================================================
+# Copyright 2024 Jake Ross
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+
+
+class BaseRecord:
+    def to_csv(self):
+        raise NotImplementedError
+
+    def __init__(self, payload):
+        self._payload = payload
+
+    def to_row(self):
+
+        def get(attr):
+            v = self._payload.get(attr)
+            if attr == "elevation" and v is not None:
+                v = round(v, 2)
+
+            if v is None:
+                v = self.defaults.get(attr)
+            return v
+
+        return [get(k) for k in self.keys]
+
+    def __getattr__(self, k):
+        return self._payload.get(k)
+
+
+class WaterLevelRecord(BaseRecord):
+    keys = (
+        "source",
+        "id",
+        "depth_to_water_below_ground_surface_ft",
+        "date_measured",
+        "time_measured",
+    )
+    defaults = {}
+
+
+class SiteRecord(BaseRecord):
+    keys = (
+        "source",
+        "id",
+        "name",
+        "latitude",
+        "longitude",
+        "elevation",
+        "elevation_units",
+        "horizontal_datum",
+        "vertical_datum",
+        "usgs_site_id",
+        "alternate_site_id",
+        "formation",
+    )
+
+    defaults = {
+        "source": None,
+        "id": None,
+        "name": "",
+        "latitude": None,
+        "longitude": None,
+        "elevation": None,
+        "elevation_units": "feet",
+        "horizontal_datum": "",
+        "vertical_datum": "",
+        "usgs_site_id": "",
+        "alternate_site_id": "",
+        "formation": "",
+    }
+
+
+# ============= EOF =============================================

7a86bb0247d8ffea3d178a9b7e6b71e5272f850a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 14:04:30 +0000

    Add package tmticket/0.1.2/tmticket-0.1.2.tar.gz

---
+++ packages/tmticket/tmticket-0.1.2.tar.gz/tmticket-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import os
+#import tmticket
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='tmticket',
+    version='0.1.2',
+    author='hokiebrian',
+    author_email='hokiebrian@gmail.com',
+    description="Python wrapper for the Ticketmaster Discovery API",
+#    long_description=read('README.rst'),
+    license='MIT',
+    keywords='Ticketmaster',
+    url='https://github.com/hokiebrian/pytmtickets',
+    packages=['pytmtickets'],
+    install_requires=['requests']
+)

8c8cc12c3ab952be8bd49f5aca24c53465218a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 15:04:39 +0000

    Add package bw-interface-schemas/0.0.1/bw_interface_schemas-0.0.1.tar.gz

---
+++ packages/bw-interface-schemas/bw_interface_schemas-0.0.1.tar.gz/bw_interface_schemas-0.0.1/bw_interface_schemas/__init__.py
@@ -0,0 +1,8 @@
+"""bw_interface_schemas."""
+
+__all__ = (
+    "__version__",
+    # Add functions and variables you want exposed in `bw_interface_schemas.` namespace here
+)
+
+__version__ = "0.0.1"

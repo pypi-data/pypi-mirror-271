ffea0d9f6ea5445b78f06f6543b5c82ab23690be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 04:04:46 +0000

    Add package hfutils/0.2.4/hfutils-0.2.4.tar.gz

---
+++ packages/hfutils/hfutils-0.2.4.tar.gz/hfutils-0.2.4/hfutils/entry/cli.py
@@ -0,0 +1,16 @@
+from .dispatch import hfutilcli
+from .download import _add_download_subcommand
+from .ls import _add_ls_subcommand
+from .upload import _add_upload_subcommand
+from .whoami import _add_whoami_subcommand
+
+_DECORATORS = [
+    _add_download_subcommand,
+    _add_upload_subcommand,
+    _add_ls_subcommand,
+    _add_whoami_subcommand,
+]
+
+cli = hfutilcli
+for deco in _DECORATORS:
+    cli = deco(cli)

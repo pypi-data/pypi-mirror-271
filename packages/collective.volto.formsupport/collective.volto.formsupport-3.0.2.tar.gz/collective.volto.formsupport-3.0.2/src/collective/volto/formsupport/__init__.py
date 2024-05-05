ca43370aa31bf63804b818ab7311c7d0e85342fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 14:05:26 +0000

    Add package collective.volto.formsupport/3.0.2/collective.volto.formsupport-3.0.2.tar.gz

---
+++ packages/collective.volto.formsupport/collective.volto.formsupport-3.0.2.tar.gz/collective.volto.formsupport-3.0.2/src/collective/volto/formsupport/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Init and utils."""
+import logging
+
+from zope.i18nmessageid import MessageFactory
+
+logger = logging.getLogger(__name__)
+_ = MessageFactory("collective.volto.formsupport")

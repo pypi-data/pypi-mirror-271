1e4ca574523f99276c7827bc106e573c022fa702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 04:04:46 +0000

    Add package gr1336-toolbox/0.0.7/gr1336_toolbox-0.0.7.tar.gz

---
+++ packages/gr1336-toolbox/gr1336_toolbox-0.0.7.tar.gz/gr1336_toolbox-0.0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    version="0.0.7",
+    name="gr1336_toolbox",
+    description="Personal collection of reusable tools in python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gr1336/gr1336_toolbox/",
+    install_requires=["numpy", "pyperclip", "textblob", "pyyaml", "pyarrow"],
+    author="gr1336",
+    license=" Apache Software License",
+    classifiers=[
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Pre-processors",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)

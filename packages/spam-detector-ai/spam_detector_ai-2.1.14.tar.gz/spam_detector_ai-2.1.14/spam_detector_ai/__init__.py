cd88db9f8da54d81c5f767594340adc4c4cc0f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 03:04:51 +0000

    Add package spam-detector-ai/2.1.14/spam_detector_ai-2.1.14.tar.gz

---
+++ packages/spam-detector-ai/spam_detector_ai-2.1.14.tar.gz/spam_detector_ai-2.1.14/spam_detector_ai/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Adams Pierre David"
+__author_email__ = "adamspd.developer@gmail.com"
+__author_website__ = "https://adamspierredavid.com/"
+__description__ = "A package for detecting and filtering spam messages using Machine Learning models."
+__package_name__ = "spam-detector-ai"
+__url__ = "https://github.com/adamspd/spam-detection-project"
+__version__ = "2.1.14"
+__test_version__ = False

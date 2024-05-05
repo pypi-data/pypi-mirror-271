9c347f82cd5704c00cec69d267d374887e251989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 03:04:34 +0000

    Add package scgame/0.0.1/scgame-0.0.1.tar.gz

---
+++ packages/scgame/scgame-0.0.1.tar.gz/scgame-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scgame',
+    version='0.0.1',
+    packages=find_packages(),
+    license='Apache 2.0',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Bernie Huang',
+    author_email='BernieHuang2008@163.com',
+    url='https://github.com/BernieHuang2008/scgame',
+    install_requires=[],
+)

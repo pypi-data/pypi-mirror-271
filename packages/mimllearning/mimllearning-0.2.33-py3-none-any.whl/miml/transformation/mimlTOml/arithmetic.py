30f51941c1e3ace6e5d25f1cc5a331adedfbca8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 04:04:33 +0000

    Add package mimllearning/0.2.33/mimllearning-0.2.33-py3-none-any.whl

---
+++ packages/mimllearning/mimllearning-0.2.33-py3-none-any.whl/miml/transformation/mimlTOml/arithmetic.py
@@ -0,0 +1,60 @@
+
+import numpy as np
+
+from .miml_to_ml_transformation import MIMLtoMLTransformation
+from ...data import Instance
+from ...data import Bag
+from ...data import MIMLDataset
+
+
+class ArithmeticTransformation(MIMLtoMLTransformation):
+    """
+    Class that performs an arithmetic transformation to convert a MIMLDataset class to numpy ndarrays.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def transform_dataset(self, dataset: MIMLDataset) -> MIMLDataset:
+        """
+        Transform the dataset to multilabel dataset converting each bag into a single instance being the value of each
+        attribute the mean value of the instances in the bag.
+
+        Parameters
+
+        Returns
+        -------
+        transformed_dataset : MIMLDataset
+            Transformed dataset
+
+        """
+        self.dataset = dataset
+        transformed_dataset = MIMLDataset()
+        transformed_dataset.set_name(dataset.get_name())
+        transformed_dataset.set_features_name(dataset.get_features_name())
+        transformed_dataset.set_labels_name(dataset.get_labels_name())
+        for bag_index, key in enumerate(self.dataset.data.keys()):
+            transformed_bag = self.transform_bag(self.dataset.get_bag(key))
+            transformed_dataset.add_bag(transformed_bag)
+
+        return transformed_dataset
+
+    def transform_bag(self, bag: Bag):
+        """
+        Transform a bag to a multilabel instance
+
+        Parameters
+        ----------
+        bag : Bag
+            Bag to be transformed
+
+        Returns
+        -------
+        """
+        features = bag.get_features()
+        labels = bag.get_labels()[0]
+        features = np.mean(features, axis=0)
+
+        transformed_bag = Bag(bag.key)
+        transformed_bag.add_instance(Instance(list(np.hstack((features, labels)))))
+        return transformed_bag

01d6263fa18f13026a64b1eacad86d804ed10e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 14:05:22 +0000

    Add package monisha/0.0.66/monisha-0.0.66.tar.gz

---
+++ packages/monisha/monisha-0.0.66.tar.gz/monisha-0.0.66/Monisha/scripts/__init__.py
@@ -0,0 +1,7 @@
+from .en import Scripted
+from .es import Folder
+from .es import Strine
+from .es import Humon
+from .es import Numeo
+from .eu import Links
+from .eo import Smbo
---
+++ packages/monisha/monisha-0.0.66.tar.gz/monisha-0.0.66/Monisha/scripts/eu.py
@@ -0,0 +1,4 @@
+
+class Links(object):
+
+    DATA01 = "https://www.x-rates.com/calculator/?from={}&to={}&amount=1"

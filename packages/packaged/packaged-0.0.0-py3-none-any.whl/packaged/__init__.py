9f1dc2667b35ef93bd955343a2d38a4b904b27fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 04:04:35 +0000

    Add package packaged/0.0.0/packaged-0.0.0-py3-none-any.whl

---
+++ packages/packaged/packaged-0.0.0-py3-none-any.whl/packaged/__init__.py
@@ -0,0 +1,7 @@
+"""packaged - The easiest way to ship python applications."""
+from __future__ import annotations
+
+
+def greet(name: str) -> str:
+    """Returns a greeting."""
+    return f"Hello {name}!"

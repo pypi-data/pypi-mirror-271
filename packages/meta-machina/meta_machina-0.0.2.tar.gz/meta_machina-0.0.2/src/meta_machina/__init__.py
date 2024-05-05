4caab1ceaf0401c6888df7e51afcd5538a85d266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 03:04:52 +0000

    Add package meta-machina/0.0.2/meta_machina-0.0.2.tar.gz

---
+++ packages/meta-machina/meta_machina-0.0.2.tar.gz/meta_machina-0.0.2/src/meta_machina/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Python
+
+"""Copyright (c) Alexander Fedotov.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+import githf
+import yaml
+import ruamel.yaml as ryaml
+
+
+MACHINE_ORGANIZATION_NAME = 'meta-machina'  # or other organization
+PRIVATE_REPO_WITH_TEXT = 'machina'
+
+try:
+    gh = githf.connectto_repo(organization=MACHINE_ORGANIZATION_NAME,
+                              repository_name=PRIVATE_REPO_WITH_TEXT,
+                              private=True)
+    MACHINE_YAML = githf.read_file(repository=gh, file_path='machina.yaml')
+    META_MACHINA = yaml.load(MACHINE_YAML, Loader=yaml.FullLoader)
+except Exception as e:
+    with open('machina.yaml', 'r') as f:
+        META_MACHINA= ryaml.load(f)
+
+
+if __name__ == '__main__':
+    print('You have launched main')
+

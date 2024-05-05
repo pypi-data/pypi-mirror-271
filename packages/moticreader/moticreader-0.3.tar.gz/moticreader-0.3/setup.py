29896fc883b479ea637e7ef0a271d97254d9c29c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 15:05:13 +0000

    Add package moticreader/0.3/moticreader-0.3.tar.gz

---
+++ packages/moticreader/moticreader-0.3.tar.gz/moticreader-0.3/setup.py
@@ -0,0 +1,15 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='moticreader',
+    version='0.3',
+    packages=find_packages(),
+    description='A package for managing Motic image',
+    author='yyb',
+    author_email='yyb2020123@163.com',
+    install_requires=[
+    ],
+    python_requires='>=3.6',
+)

215f907d3aec0ee9872d3967a6aeca2894eeac32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 03:04:41 +0000

    Add package transaction-middleware/0.1.6/transaction_middleware-0.1.6-py3-none-any.whl

---
+++ packages/transaction-middleware/transaction_middleware-0.1.6-py3-none-any.whl/transaction_middleware/settings.py
@@ -0,0 +1,26 @@
+from starlette.config import Config
+
+config = Config()
+
+
+class Settings:
+    """Settings for the module"""
+
+    TRANSACTION_MIDDLEWARE_LOG_LEVEL: str = config(
+        "TRANSACTION_MIDDLEWARE_LOG_LEVEL", cast=str, default="INFO"
+    ).upper()
+    TRANSACTION_MIDDLEWARE_LOG_FORMAT: str = config(
+        "TRANSACTION_MIDDLEWARE_LOG_FORMAT",
+        cast=str,
+        default="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    )
+
+    # Disable transaction for the whole application
+    TRANSACTION_MIDDLEWARE_HEADER = config(
+        "TRANSACTION_MIDDLEWARE_HEADER",
+        cast=str,
+        default="X-Transaction-ID",
+    )
+
+
+settings = Settings()

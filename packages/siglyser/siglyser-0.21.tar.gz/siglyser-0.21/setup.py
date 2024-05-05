f0396ce1f5e1df9d62ba119bfea8550bf15bb90d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 15:04:40 +0000

    Add package siglyser/0.21/siglyser-0.21.tar.gz

---
+++ packages/siglyser/siglyser-0.21.tar.gz/siglyser-0.21/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+
+setup(
+    name='siglyser',
+    version='0.21',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+
+        
+    ],
+
+    long_description=description,
+    long_description_content_type="text/markdown",
+)

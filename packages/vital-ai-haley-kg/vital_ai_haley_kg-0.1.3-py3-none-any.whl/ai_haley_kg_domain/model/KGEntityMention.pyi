df47d65caba4472f01afb8041dbc61c930c4ea09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 14:04:19 +0000

    Add package vital-ai-haley-kg/0.1.3/vital_ai_haley_kg-0.1.3-py3-none-any.whl

---
+++ packages/vital-ai-haley-kg/vital_ai_haley_kg-0.1.3-py3-none-any.whl/ai_haley_kg_domain/model/KGEntityMention.pyi
@@ -0,0 +1,12 @@
+
+import datetime
+from ai_haley_kg_domain.model.KGNode import KGNode
+
+
+class KGEntityMention(KGNode):
+        kGEntityAccountURI: str
+        kGEntityLoginURI: str
+        kGEntityType: str
+        kGEntityTypeDescription: str
+        kGExtractTaskURI: str
+

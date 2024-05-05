4479c6a6c37ac906aa9fdce62fe2bbd27705805a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 15:04:44 +0000

    Add package prodsys/0.6.1/prodsys-0.6.1.tar.gz

---
+++ packages/prodsys/prodsys-0.6.1.tar.gz/prodsys-0.6.1/prodsys/models/time_model_data.py
@@ -0,0 +1,183 @@
+"""
+The `time_model` module contains classes to specify time models in the simulation for the arrival
+of products, performance of processes and transports and the duration of states.
+
+The following time models are possible:
+- `SequentialTimeModelData`: A time model that is based on a sequence of values.
+- `FunctionTimeModelData`: A time model that is based on a distribution function which gets sampled.
+- `ManhattanDistanceTimeModelData`: A time model that is based on the manhattan distance between two nodes and a constant velocity.
+"""
+
+from __future__ import annotations
+from hashlib import md5
+from typing import List, Union
+from enum import Enum
+
+from pydantic import Field
+
+from prodsys.models.core_asset import CoreAsset
+from prodsys.util.statistical_functions import FunctionTimeModelEnum
+
+
+class TimeModelEnum(str, Enum):
+    """
+    Enum that represents the different kind time models.
+
+    - HistoryTimeModel: A time model that is based on a sequence of values.
+    - FunctionTimeModel: A time model that is based on a distribution function which gets sampled.
+    - ManhattanDistanceTimeModel: A time model that is based on the manhattan distance between two nodes and a constant velocity.
+    """
+
+    HistoryTimeModel = "HistoryTimeModel"
+    FunctionTimeModel = "FunctionTimeModel"
+    ManhattanDistanceTimeModel = "ManhattanDistanceTimeModel"
+
+
+class SequentialTimeModelData(CoreAsset):
+    """
+    Class that represents a time model that is based on a sequence of values.
+
+    Args:
+        ID (str): ID of the time model.
+        description (str): Description of the time model.
+        sequence (List[float]): Sequence of time values.
+
+    Examples:
+        Sequential time model with 7 time values:
+        ```	py
+        import prodsys
+        prodsys.time_model_data.SequentialTimeModelData(
+            ID="sequence_time_model_1",
+            description="Examplary sequence time model",
+            sequence=[25.0, 13.0, 15.0, 16.0, 17.0, 20.0, 21.0],
+        )
+        ```
+    """
+
+    sequence: List[float]
+
+    def hash(self) -> str:
+        """
+        Returns a unique hash for the time model considering its sequence. Can be used to compare time models for equal functionality.
+
+        Returns:
+            str: Hash of the time model.
+        """
+        return md5(("".join([*map(str, self.sequence)])).encode("utf-8")).hexdigest()
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "summary": "Sequential time model",
+                "value": {
+                    "ID": "sequence_time_model_1",
+                    "description": "Examplary sequence time model",
+                    "sequence": [25.0, 13.0, 15.0, 16.0, 17.0, 20.0, 21.0],
+                },
+            }
+        }
+
+
+class FunctionTimeModelData(CoreAsset):
+    """
+    Class that represents a time model that is based on a function and represents the timely values by their distribution function.
+
+    Args:
+        ID (str): ID of the time model.
+        description (str): Description of the time model.
+        distribution_function (FunctionTimeModelEnum): Zype of the distribution function of the time model.
+        location (float): Location parameter of the distribution function.
+        scale (float): Scale parameter of the distribution function.
+
+    Examples:
+        Normal distribution time model with 20 minutes:
+        ``` py
+        import prodsys
+        prodsys.time_model_data.FunctionTimeModelData(
+            ID="function_time_model_1",
+            description="normal distribution time model with 20 minutes",
+            distribution_function=prodsys.FunctionTimeModelEnum.normal,
+            location=20.0,
+            scale=5.0,
+        )
+        ```
+    """
+
+    distribution_function: FunctionTimeModelEnum
+    location: float
+    scale: float
+    batch_size: int = Field(default=100, init=False)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "summary": "Function time model",
+                "value": {
+                    "ID": "function_time_model_1",
+                    "description": "normal distribution time model with 20 minutes",
+                    "distribution_function": "normal",
+                    "location": 20.0,
+                    "scale": 5.0,
+                },
+            }
+        }
+    def hash(self) -> str:
+        """
+        Returns a unique hash for the time model considering its distribution function, location and scale. Can be used to compare time models for equal functionality.
+
+        Returns:
+            str: Hash of the time model.
+        """
+        return md5(("".join([*map(str, [self.distribution_function, self.location, self.scale])])).encode("utf-8")).hexdigest()
+    
+class ManhattanDistanceTimeModelData(CoreAsset):
+    """
+    Class that represents a time model that is based on the manhattan distance between two nodes and a constant velocity.
+
+    Args:
+        ID (str): ID of the time model.
+        description (str): Description of the time model.
+        speed (float): Speed of the transport.
+        reaction_time (float): Reaction time of the transport.
+
+    Examples:
+        Manhattan time model with speed 180 m/min = 3 m/s and reaction time 0.15 minutes:
+        ``` py
+        import prodsys
+        time_model_data.ManhattanDistanceTimeModelData(
+            ID="manhattan_time_model_1",
+            description="manhattan time model with speed 180 m/min = 3 m/s",
+            speed=180.0,
+            reaction_time=0.15,
+        )
+    """
+
+    speed: float
+    reaction_time: float
+
+    def hash(self) -> str:
+        """
+        Returns a unique hash for the time model considering its speed and reaction time. Can be used to compare time models for equal functionality.
+
+        Returns:
+            str: Hash of the time model.
+        """
+        return md5(("".join([*map(str, [self.speed, self.reaction_time])])).encode("utf-8")).hexdigest()
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "summary": "Manhattan time model",
+                "value": {
+                    "ID": "manhattan_time_model_1",
+                    "description": "manhattan time model with speed 180 m/min = 3 m/s",
+                    "speed": 30.0,
+                    "reaction_time": 0.15,
+                },
+            }
+        }
+
+
+TIME_MODEL_DATA = Union[
+    SequentialTimeModelData, ManhattanDistanceTimeModelData, FunctionTimeModelData
+]
---
+++ packages/prodsys/prodsys-0.6.1.tar.gz/prodsys-0.6.1/prodsys/simulation/time_model.py
@@ -0,0 +1,200 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Callable, List, Optional, Tuple, Union
+
+import numpy as np
+from pydantic import BaseModel, validator
+
+from prodsys.models.time_model_data import (
+    FunctionTimeModelData,
+    SequentialTimeModelData,
+    ManhattanDistanceTimeModelData,
+)
+from prodsys.util.statistical_functions import FUNCTION_DICT, FunctionTimeModelEnum
+
+
+class TimeModel(ABC, BaseModel):
+    """
+    Abstract base class for time models.
+    """
+    @abstractmethod
+    def get_next_time(
+        self,
+        origin: Optional[List[float]] = None, # TOOO: rework this with kwargs
+        target: Optional[List[float]] = None,
+    ) -> float:
+        """
+        Returns the next time of the time model.
+
+        Args:
+            origin (Optional[List[float]], optional): The origin of the product for a transport. Defaults to None.
+            target (Optional[List[float]], optional): The target of the product for a transport. Defaults to None.
+
+        Returns:
+            float: The next time of the time model.
+        """
+        pass
+
+    @abstractmethod
+    def get_expected_time(
+        self,
+        origin: Optional[List[float]] = None,
+        target: Optional[List[float]] = None,
+    ) -> float:
+        """
+        Returns the expected time of the time model.
+
+        Args:
+            origin (Optional[List[float]], optional): The origin of the product for a transport. Defaults to None.
+            target (Optional[List[float]], optional): The target of the product for a transport. Defaults to None.
+
+        Returns:
+            float: The expected time of the time model.
+        """
+        pass
+
+
+class FunctionTimeModel(TimeModel):
+    """
+    Class for time models that are based on a function.
+
+    Args:
+        time_model_data (FunctionTimeModelData): The time model data object.
+        statistics_buffer (List[float], optional): A buffer for the statistics. Defaults to [].
+        distribution_function_object (Callable[[FunctionTimeModelData], List[float]], optional): The distribution function object. Defaults to FUNCTION_DICT[FunctionTimeModelEnum.Constant].
+    """
+    time_model_data: FunctionTimeModelData
+    statistics_buffer: List[float] = []
+    distribution_function_object: Callable[
+        [FunctionTimeModelData], List[float]
+    ] = FUNCTION_DICT[FunctionTimeModelEnum.Constant]
+
+    @validator("distribution_function_object", always=True)
+    def initialize_distribution_function(cls, v, values):
+        return FUNCTION_DICT[values["time_model_data"].distribution_function]
+
+    def get_next_time(
+        self,
+        origin: Optional[List[float]] = None,
+        target: Optional[List[float]] = None,
+    ) -> float:
+        """
+        Returns the next time for a time model based on a sample value of the distribution function.
+
+        Returns:
+            float: The next time of the time model.
+        """
+        try:
+            value = self.statistics_buffer.pop()
+            if value < 0:
+                return 0.1
+            return value
+        except IndexError:
+            self._fill_buffer()
+            return self.get_next_time()
+
+    def _fill_buffer(self):
+        self.statistics_buffer = self.distribution_function_object(
+            self.time_model_data
+        )
+
+    def get_expected_time(
+        self,
+        origin: Optional[List[float]] = None,
+        target: Optional[List[float]] = None,
+    ) -> float:
+        """
+        Returns the expected time for a time model based on the expected value of the distribution function.
+
+        Returns:
+            float: The expected time of the time model.
+        """
+        return self.time_model_data.location
+
+
+class SequentialTimeModel(TimeModel):
+    """
+    Class for time models that are based on a sequence of values. A random value from the sequence is returned.
+
+    Args:
+        time_model_data (SequentialTimeModelData): The time model data object.
+    """
+    time_model_data: SequentialTimeModelData
+
+    def get_next_time(
+        self,
+        origin: Optional[List[float]] = None,
+        target: Optional[List[float]] = None,
+    ) -> float:
+        """
+        Returns the next time for a time model based on a sample value of the sequence.
+
+        Returns:
+            float: The next time of the time model.
+        """
+        return np.random.choice(self.time_model_data.sequence, 1)[0]
+
+    def get_expected_time(
+        self,
+        origin: Optional[List[float]] = None,
+        target: Optional[List[float]] = None,
+    ) -> float:
+        return sum(self.time_model_data.sequence) / len(self.time_model_data.sequence)
+
+
+class ManhattanDistanceTimeModel(TimeModel):
+    """
+    Class for time models that are based on the manhattan distance between two points and time calculation based on reaction time and speed.
+
+    Args:
+        time_model_data (ManhattanDistanceTimeModelData): The time model data object.
+    """
+    time_model_data: ManhattanDistanceTimeModelData
+    def get_next_time(
+        self,
+        origin: Optional[List[float]] = None,
+        target: Optional[List[float]] = None,
+    ) -> float:
+        """
+        Returns the next time for a time model based on the manhattan distance between two points and time calculation based on reaction time and speed.
+
+        Args:
+            origin (Optional[List[float]], optional): The origin of the product for a transport. Defaults to None.
+            target (Optional[List[float]], optional): The target of the product for a transport. Defaults to None.
+
+        Returns:
+            float: The next time of the time model.
+        """
+        if origin is None or target is None:
+            raise ("Origin and target must be defined for ManhattanDistanceTimeModel")  # type: ignore
+        x_distance = abs(origin[0] - target[0])
+        y_distance = abs(origin[1] - target[1])
+        return (
+            x_distance + y_distance
+        ) / self.time_model_data.speed + self.time_model_data.reaction_time
+
+    def get_expected_time(
+        self,
+        origin: Optional[List[float]] = None,
+        target: Optional[List[float]] = None,
+    ) -> float:
+        """
+        Returns the expected time for a time model based on the manhattan distance between two points and time calculation based on reaction time and speed.
+
+        Args:
+            origin (Optional[List[float]], optional): The origin of the product for a transport. Defaults to None.
+            target (Optional[List[float]], optional): The target of the product for a transport. Defaults to None.
+
+        Returns:
+            float: The expected time of the time model.
+        """
+        if origin is None or target is None:
+            raise ("Origin and target must be defined for ManhattanDistanceTimeModel")  # type: ignore
+        return self.get_next_time(origin, target)
+
+
+TIME_MODEL = Union[SequentialTimeModel, ManhattanDistanceTimeModel, FunctionTimeModel]
+"""
+Union type for all time models.
+"""

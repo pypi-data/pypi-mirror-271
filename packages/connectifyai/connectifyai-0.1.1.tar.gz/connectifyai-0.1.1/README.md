daf4526b19a096651ec52235526612266b359501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 14:04:30 +0000

    Add package connectifyai/0.1.1/connectifyai-0.1.1.tar.gz

---
+++ packages/connectifyai/connectifyai-0.1.1.tar.gz/connectifyai-0.1.1/README.md
@@ -0,0 +1,17 @@
+# ConnectifyCLI
+
+## Running commands
+1. Install package
+```shell
+pip install -e .
+```
+2. Run command download (dl)
+```shell
+connect dl sample_name
+```
+
+## Tests
+Use pytest to run tests:
+```python
+pytest
+```
---
+++ packages/connectifyai/connectifyai-0.1.1.tar.gz/connectifyai-0.1.1/connectifycli/cli.py
@@ -0,0 +1,23 @@
+import click
+import requests
+from connectifycli.graph.graph import Graph
+from connectifycli.utils.json import convert_to_graph
+
+@click.group()
+def main():
+    pass
+
+@main.command()
+@click.argument("hash")
+def run(hash):
+    # response = requests.post("", data={})
+    # if(response.status_code != 200):
+    #     exit("Error retrieving graph")
+    click.echo("Running graph {hash}...")
+        
+    # graph: Graph = convert_to_graph(response.json())
+    # run code to for graphs
+    
+
+
+    

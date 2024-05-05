ffa652903ac9d1ebcc36f59713fb249d802c10a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 04:04:39 +0000

    Add package faststream/0.5.4/faststream-0.5.4-py3-none-any.whl

---
+++ packages/faststream/faststream-0.5.4-py3-none-any.whl/faststream/kafka/__init__.py
@@ -0,0 +1,15 @@
+from faststream.kafka.annotations import KafkaMessage
+from faststream.kafka.broker import KafkaBroker
+from faststream.kafka.router import KafkaPublisher, KafkaRoute, KafkaRouter
+from faststream.kafka.testing import TestKafkaBroker
+from faststream.testing.app import TestApp
+
+__all__ = (
+    "KafkaBroker",
+    "KafkaMessage",
+    "KafkaRouter",
+    "KafkaRoute",
+    "KafkaPublisher",
+    "TestKafkaBroker",
+    "TestApp",
+)

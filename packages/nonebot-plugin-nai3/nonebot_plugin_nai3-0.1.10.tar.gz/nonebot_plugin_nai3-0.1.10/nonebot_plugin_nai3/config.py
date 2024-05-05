908e379e536ace779349f7002d30bfe5ed67ff4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 14:04:48 +0000

    Add package nonebot-plugin-nai3/0.1.10/nonebot_plugin_nai3-0.1.10.tar.gz

---
+++ packages/nonebot-plugin-nai3/nonebot_plugin_nai3-0.1.10.tar.gz/nonebot_plugin_nai3-0.1.10/nonebot_plugin_nai3/config.py
@@ -0,0 +1,22 @@
+from nonebot.plugin import get_plugin_config
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    nai3_token: str = "xxx"
+    nai3_negative: str = (
+        "nsfw, lowres, {bad}, error, fewer, extra, missing, worst quality, jpeg artifacts, bad quality, watermark, unfinished, displeasing, chromatic aberration, signature, extra digits, artistic error, username, scan, [abstract]"  # noqa: E501
+    )
+    nai3_limit: int = 10
+    nai3_cooltime_group: int = 30
+    nai3_cooltime_user: int = 300
+    nai3_proxy: str = None
+    nai3_r18: bool = False
+    nai3_send_to_group: bool = True
+    nai3_save: bool = False
+    nai3_save_path: str = "./data/nai3/img"
+    smms_api_url: str = "https://sm.ms/api/v2"
+    smms_token: str = "xxx"
+
+
+nai3_config = get_plugin_config(Config)

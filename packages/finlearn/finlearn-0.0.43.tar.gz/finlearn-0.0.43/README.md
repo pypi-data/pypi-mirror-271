118812f9ce42621e8a6fefc3a158969888453368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 04:04:29 +0000

    Add package finlearn/0.0.43/finlearn-0.0.43.tar.gz

---
+++ packages/finlearn/finlearn-0.0.43.tar.gz/finlearn-0.0.43/README.md
@@ -0,0 +1,6 @@
+This is an all in one complete python package to solve your entire financial analysis and market prediction demands. This library enables you to work not on the financial analysis but also complements you by giving its inputs on the market prediction.
+
+Updates will get released regularly. Do let us know about your other demands from this library at the email address provided.
+
+Launch - 19th March, 2024
+1st update release - 5th May, 2024
---
+++ packages/finlearn/finlearn-0.0.43.tar.gz/finlearn-0.0.43/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+# Package metadata
+NAME = 'finlearn'
+DESCRIPTION = 'A one-stop package for entire financial analysis and market prediction using Deep Learning'
+VERSION = '0.0.43'
+AUTHOR = 'Ankit Dutta'
+AUTHOR_EMAIL = 'ankitduttaiitkgp@gmail.com'
+#URL = 'https://github.com/your_username/your_repository'
+LICENSE = 'Apache 2.0'
+
+# Read long description from README file
+with open('README.md', 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
+# Define dependencies
+INSTALL_REQUIRES = [
+    'plotly',
+    'yfinance',
+    'matplotlib'
+    # Add more dependencies as needed
+]
+
+# Define additional classifiers
+CLASSIFIERS = [
+    'Programming Language :: Python :: 3',
+    # Add more classifiers as needed
+]
+
+# Call setup() function to define the package
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license=LICENSE,
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    classifiers=CLASSIFIERS,
+)

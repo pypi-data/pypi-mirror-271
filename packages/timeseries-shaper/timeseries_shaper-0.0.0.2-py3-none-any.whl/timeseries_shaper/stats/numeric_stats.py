1193455d4d8de797b1e3a120630f545d26058efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 04:04:09 +0000

    Add package timeseries-shaper/0.0.0.2/timeseries_shaper-0.0.0.2-py3-none-any.whl

---
+++ packages/timeseries-shaper/timeseries_shaper-0.0.0.2-py3-none-any.whl/timeseries_shaper/stats/numeric_stats.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from ..base import Base
+
+class NumericStatistics(Base):
+    def __init__(self, dataframe: pd.DataFrame, column_name: str) -> None:
+        """
+        Initialize the NumericStatistics class by using the Base class initialization.
+        """
+        super().__init__(dataframe)
+        self.column_name = column_name
+
+    def column_mean(self):
+        """Calculate the mean of a specified column."""
+        return self.dataframe[self.column_name].mean()
+
+    def column_median(self):
+        """Calculate the median of a specified column."""
+        return self.dataframe[self.column_name].median()
+
+    def column_std(self):
+        """Calculate the standard deviation of a specified column."""
+        return self.dataframe[self.column_name].std()
+
+    def column_min(self):
+        """Calculate the minimum value of a specified column."""
+        return self.dataframe[self.column_name].min()
+
+    def column_max(self):
+        """Calculate the maximum value of a specified column."""
+        return self.dataframe[self.column_name].max()
+
+    def column_sum(self):
+        """Calculate the sum of a specified column."""
+        return self.dataframe[self.column_name].sum()
+
+    def describe(self) -> pd.DataFrame:
+        """Provide a statistical summary for numeric columns in the DataFrame."""
+        return self.dataframe.describe()

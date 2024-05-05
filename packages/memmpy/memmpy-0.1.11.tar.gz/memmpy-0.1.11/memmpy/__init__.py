6cdd8b483aa5824975ee8f7d2a065dd6dc53f27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 03:04:46 +0000

    Add package memmpy/0.1.11/memmpy-0.1.11.tar.gz

---
+++ packages/memmpy/memmpy-0.1.11.tar.gz/memmpy-0.1.11/memmpy/__init__.py
@@ -0,0 +1,21 @@
+from beartype.claw import beartype_this_package
+
+beartype_this_package()
+
+
+from ._vector import Vector
+from ._shuffle import shuffle_fast, shuffle_blocks, shuffle_bands
+from ._index import batch_slices, batch_indicies, batch_indicies_split
+
+__version__ = "0.1.11"
+
+__all__ = [
+    "__version__",
+    "Vector",
+    "shuffle_fast",
+    "shuffle_blocks",
+    "shuffle_bands",
+    "batch_slices",
+    "batch_indicies",
+    "batch_indicies_split",
+]

4734b2cf315a42feb9d9c9da9d6f359a54a7ca9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 04:04:46 +0000

    Add package python-bvk/0.3.1/python-bvk-0.3.1.tar.gz

---
+++ packages/python-bvk/python-bvk-0.3.1.tar.gz/python-bvk-0.3.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requires = ["scrapy", "python-dateutil"]
+
+setuptools.setup(
+    name="python-bvk",
+    version="0.3.1",
+    author="Dan Keder",
+    author_email="dan.keder@protonmail.com",
+    description="Python library for tracking water consumption from BVK (Brnenske vodarny a kanalizace, bvk.cz)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dankeder/python-bvk",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)

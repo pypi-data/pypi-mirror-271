511c6d68f589c923e0ea44f2619452c328e09348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 03:05:04 +0000

    Add package easy-llama/0.1.18/easy_llama-0.1.18.tar.gz

---
+++ packages/easy-llama/easy_llama-0.1.18.tar.gz/easy_llama-0.1.18/easy_llama/__init__.py
@@ -0,0 +1,17 @@
+# __init__.py
+# https://github.com/ddh0/easy-llama/
+__version__ = '0.1.18'
+
+"""
+Text generation in Python, made easy
+
+For documentation, see:
+https://github.com/ddh0/easy-llama/blob/main/DOCS.md
+"""
+
+from . import formats
+from . import samplers
+from . import utils
+
+from .model  import Model
+from .thread import Thread

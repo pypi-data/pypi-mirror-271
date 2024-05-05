cba1ff93650fc656cb515a6b6dcbd25e8b82bd0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 15:05:11 +0000

    Add package potatoscript/1.0.0/potatoscript-1.0.0.tar.gz

---
+++ packages/potatoscript/potatoscript-1.0.0.tar.gz/potatoscript-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='potatoscript',
+    version='1.0.0',
+    description='A simple Template for create python project',
+    author='potatoscript',
+    author_email='potatoscript.com@gmail.com',
+    packages=['python'],
+)

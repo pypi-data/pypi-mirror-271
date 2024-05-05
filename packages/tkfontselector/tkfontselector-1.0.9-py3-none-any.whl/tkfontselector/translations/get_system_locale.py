ca4b737c08cba51993d0b04e3cd30f62f7e78820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 04:04:10 +0000

    Add package tkfontselector/1.0.9/tkfontselector-1.0.9-py3-none-any.whl

---
+++ packages/tkfontselector/tkfontselector-1.0.9-py3-none-any.whl/tkfontselector/translations/get_system_locale.py
@@ -0,0 +1,17 @@
+import ctypes
+import locale
+import os
+import platform
+from typing import Union
+
+
+def system_locale() -> Union[str, None]:
+    lang = None
+    if platform.system() == "Windows":
+        windll = ctypes.windll.kernel32
+        language_str = locale.windows_locale[windll.GetUserDefaultUILanguage()]
+        lang = language_str.split("_")[0]
+    else:
+        language_str = os.environ.get("LANG")
+        lang = language_str.split("_")[0]
+    return lang

fdf0985c16e8b74a6520411eacd22843da128790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 04:04:19 +0000

    Add package got-cli/0.0.11/got-cli-0.0.11.tar.gz

---
+++ packages/got-cli/got-cli-0.0.11.tar.gz/got-cli-0.0.11/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="got-cli",
+    version="0.0.11",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=[
+        "openai==1.25.1",
+        "groq==0.5.0",
+        "click==8.1.3",
+        "python-dotenv==1.0.1",
+        "inquirerpy==0.3.4",
+        "prompt-toolkit==3.0.43"
+    ],
+    include_package_data=True,
+    package_data={
+        "got": [
+            "prompts/*"
+        ],
+    },
+    author="Guilherme Alves Pimenta",
+    author_email="guialvespimenta27@gmail.com",
+    description="Got is a library to help you make consistent commit messages with help of AI",
+    entry_points={"console_scripts": ["got=got.cli:got"]},
+    url="https://github.com/GuiPimenta-Dev/got"
+)

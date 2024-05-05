9fe724108655bb1599fb08963638f5e0e6b20d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 14:05:00 +0000

    Add package pyje/0.0.1/pyje-0.0.1.tar.gz

---
+++ packages/pyje/pyje-0.0.1.tar.gz/pyje-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyje",
+    version="0.0.1",
+    author="jameskim",
+    author_email="kibum1991@naver.com",
+    description="Journal Entry with python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jamesKim7/pyje",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

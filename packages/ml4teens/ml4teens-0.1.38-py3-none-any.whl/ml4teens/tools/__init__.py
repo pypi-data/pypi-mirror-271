66c99ec854524fc60ac50cd2801ab64f6263d310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 03:05:03 +0000

    Add package ml4teens/0.1.38/ml4teens-0.1.38-py3-none-any.whl

---
+++ packages/ml4teens/ml4teens-0.1.38-py3-none-any.whl/ml4teens/tools/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from .embeddings import ImgEmbeddings;
+from .embeddings import TxtEmbeddings;
+
+from .tools      import image_from_url; 
+from .tools      import imageFromUrl;
+from .tools      import runnigInGoogleColab;
+from .tools      import runnigInJupyterLike;
+from .tools      import runningInNotebook;
+from .tools      import uploadFiles;
+from .tools      import UrlDownload;
+from .tools      import searchFilename;
+from .tools      import searchPattern;
+from .tools      import CocoDataset;

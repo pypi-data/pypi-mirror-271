2c352b1f24b33a8fba7f0effdaee288b44d98082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 04:04:05 +0000

    Add package blankslate-python/1.1.2/blankslate_python-1.1.2-py3-none-any.whl

---
+++ packages/blankslate-python/blankslate_python-1.1.2-py3-none-any.whl/blankslate/main.py
@@ -0,0 +1,29 @@
+"""main.py: Called when the package is run as a script."""
+
+from .generation.project_generation import generate_project
+from .interface.command_line import input_project_details
+from .logs.setup_logging import setup_logging
+
+main_logger = setup_logging()
+
+
+def main() -> None:
+    """
+    Main function for the application.
+
+    Notes
+    -----
+    This function is the entry point for the application.
+    """
+
+    try:
+        main_logger.info("Application started.")
+        project_details = input_project_details()
+        generate_project(project_details)
+    except KeyboardInterrupt:
+        print("\n")
+        main_logger.info("Exiting application due to user interrupt...")
+
+
+if __name__ == "__main__":
+    main()

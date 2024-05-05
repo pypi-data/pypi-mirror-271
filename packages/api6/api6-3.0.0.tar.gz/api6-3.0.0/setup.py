fea8f07d8e4ae4f45c1626fa00aa9a8d64b4d6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 15:04:49 +0000

    Add package api6/3.0.0/api6-3.0.0.tar.gz

---
+++ packages/api6/api6-3.0.0.tar.gz/api6-3.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='api6',
+    version='3.0.0',
+    packages= ["api6"],
+    url='https://guides.github.com/features/mastering-markdown/',
+    license='MIT',
+    author='Luis',
+    author_email='mm12mok18@gmail.com',
+    description='Luis Rubika Bot to Rubika api 6',
+    long_description= open("README.md",'r').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+)

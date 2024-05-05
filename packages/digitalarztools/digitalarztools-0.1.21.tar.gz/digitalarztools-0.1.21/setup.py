6a1fd1a958bc02198fac9b53d7dfef3af18b56bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 03:04:58 +0000

    Add package digitalarztools/0.1.21/digitalarztools-0.1.21.tar.gz

---
+++ packages/digitalarztools/digitalarztools-0.1.21.tar.gz/digitalarztools-0.1.21/setup.py
@@ -0,0 +1,46 @@
+import setuptools
+
+"""
+to create pip package
+install twine
+pip install twine 
+python setup.py sdist bdist_wheel
+twine upload dist/* 
+"""
+import os
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = os.path.join(lib_folder, 'digitalarztools/requirements.txt') #lib_folder + '/digitalarztools/requirements.txt'
+install_requires = []  # Here we'll get: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+VERSION = '0.1.21'
+DESCRIPTION = 'Digital Arz tools for applications'
+LONG_DESCRIPTION = 'Package provides tool for developing digitalarz application using rasterio, gdal, geopandas, and pandas'
+
+# Setting up
+setuptools.setup(
+    name="digitalarztools",
+    version=VERSION,
+    author="Ather Ashraf",
+    author_email="atherashraf@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    python_requires='>=3',
+    install_requires=install_requires,  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+    setup_requires=install_requires,
+    packages=setuptools.find_packages(),
+    keywords=['raster', 'vector', 'digitalarz'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
+print(install_requires)
+

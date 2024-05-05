c8d0cb8ff7d641288fcfd5b57a46b9e8c29e0072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-05 15:04:47 +0000

    Add package xcloudClients/0.1.0/xcloudClients-0.1.0-py3-none-any.whl

---
+++ packages/xcloudClients/xcloudClients-0.1.0-py3-none-any.whl/xcloudClients/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '0.1'
+__author__ = 'bailu'
+__com__ = 'China telecom Shannxi'
+__mail__ = 'yabailu@chinatelecom.cn'
+__tel__ = '15399117834'
+
+
+import pkg_resources
+
+# 全局变量存储 JAR 文件路径
+JAR_FILES = [pkg_resources.resource_filename('xcloud-clients', f'resources/{jar_name}') for jar_name in [
+    'XCloudJDBC-2.10.6.7.jar', 'slf4j-api-1.7.5.jar', 'slf4j-log4j12-1.7.5.jar', 
+    'slf4j-simple-1.7.5.jar', 'log4j-1.2.17.jar', 'libthrift-0.9.2.jar', 
+    'XCloudJDBC_SP_Procedure_Parser-0.1.3.jar', 'lz4-1.3.0.jar'
+]]

91ba55bcfec5b0a50f6f37d93602aa2b290a7886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 16:04:51 +0000

    Add package SQLConn/0.0.2/SQLConn-0.0.2.tar.gz

---
+++ packages/SQLConn/SQLConn-0.0.2.tar.gz/SQLConn-0.0.2/SQLConn/oracle.py
@@ -0,0 +1,15 @@
+import cx_Oracle
+from SQLConn import SQLConn
+class OracleConn(SQLConn):
+    def __init__(self,password:str,host:str='127.0.0.1',user:str="system",database:str="xe",port:str|int=1521) -> None:
+        super.__init__()
+        self.__host=host
+        self.__user=user
+        self.__password=password
+        self.__database=database
+        self.__port=int(port)
+        self._conn=cx_Oracle.connect(self.__user,self.__password,f'{self.__host}:{self.__port}/{self.__database}')
+        self._engine=self._makeEngine()
+    @property
+    def URL(self):
+        return f'oracle+cx_oracle://{self.__user}:{self.__password}@{self.__host}:{self.__port}/?service_name={self.__database}'

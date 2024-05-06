bc4610112710dfd3b21b64e0e5fb6c621c59ea54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 03:05:21 +0000

    Add package yamllm/0.0.1/yamllm-0.0.1.tar.gz

---
+++ packages/yamllm/yamllm-0.0.1.tar.gz/yamllm-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='yamllm',
+    version='0.0.1',
+    packages=['yamllm'],
+    description='Placeholder package for yamllm',
+    long_description='This is a placeholder package for yamllm.',
+    author='Brad Magnetta',
+    author_email='bmagnett@gmail.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)

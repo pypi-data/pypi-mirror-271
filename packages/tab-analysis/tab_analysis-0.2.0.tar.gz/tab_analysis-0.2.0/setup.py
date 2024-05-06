61dce470fea1b975b5d99c2e3ada5f0915a94233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 03:05:05 +0000

    Add package tab-analysis/0.2.0/tab_analysis-0.2.0.tar.gz

---
+++ packages/tab-analysis/tab_analysis-0.2.0.tar.gz/tab_analysis-0.2.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+`tab_analysis` setup
+"""
+
+import pathlib
+from setuptools import setup, find_packages
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="tab_analysis",
+    version="0.2.0",
+    description="TAB-analysis : A tool to analyse tabular and multi-dimensionnal structures",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/loco-philippe/tab-analysis/blob/main/README.md",
+    author="Philippe Thomy",
+    author_email="philippe@loco-labs.io",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"],
+    keywords="tabular data, open data, environmental data",
+    packages=find_packages(include=['tab_analysis', 'tab_analysis.*']),
+    #package_data={'tab_analysis': ['*.ini']},
+    python_requires=">=3.9, <4",
+    install_requires=[]
+)
---
+++ packages/tab-analysis/tab_analysis-0.2.0.tar.gz/tab_analysis-0.2.0/tab_analysis/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+***TAB-analysis Package***
+
+This package contains the following classes and functions:
+
+- module `tab-analysis.tab_analysis.analysis` :
+
+    - `tab-analysis.tab_analysis.analysis.AnaField`
+    - `tab-analysis.tab_analysis.analysis.AnaRelation`
+    - `tab-analysis.tab_analysis.analysis.AnaDfield`
+    - `tab-analysis.tab_analysis.analysis.AnaDataset`
+    - `tab-analysis.tab_analysis.analysis.Util`
+    - `tab-analysis.tab_analysis.analysis.AnaError`
+    
+For more information, see the
+[user guide](https://loco-philippe.github.io/tab-analysis/docs/user_guide.html)
+or the [github repository](https://github.com/loco-philippe/tab-analysis).   
+"""
+from tab_analysis.analysis import AnaField, AnaRelation, AnaDataset, AnaDfield, Util
+from tab_analysis.analysis import ROOT, ROOTED, DERIVED, COUPLED
+from tab_analysis.analysis import NULL, UNIQUE, COMPLETE, FULL, DEFAULT, MIXED
+
+#print('package :', __package__)

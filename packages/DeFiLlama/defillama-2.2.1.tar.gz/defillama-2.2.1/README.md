7974e5dc42644bfda8c26c84538a5affb709eb15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 16:05:07 +0000

    Add package DeFiLlama/2.2.1/defillama-2.2.1.tar.gz

---
+++ packages/DeFiLlama/defillama-2.2.1.tar.gz/defillama-2.2.1/README.md
@@ -0,0 +1,54 @@
+# DeFiLlama
+
+[![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg)](https://www.python.org/downloads/release/python-360/)
+[![Python 3.7](https://img.shields.io/badge/python-3.7-blue.svg)](https://www.python.org/downloads/release/python-370/)
+[![Python 3.8](https://img.shields.io/badge/python-3.8-blue.svg)](https://www.python.org/downloads/release/python-380/)
+[![Python 3.9](https://img.shields.io/badge/python-3.9-blue.svg)](https://www.python.org/downloads/release/python-390/)
+[![Python 3.10](https://img.shields.io/badge/python-3.10-blue.svg)](https://www.python.org/downloads/release/python-310/)
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+[![Build](https://github.com/itzmestar/DeFiLlama/actions/workflows/python-package.yml/badge.svg)](https://github.com/itzmestar/DeFiLlama/actions/workflows/python-package.yml)
+
+
+### Unofficial [DeFi Llama API](https://defillama.com/home) client in python
+
+For detailed information about the API endpoints, see [DeFi Llama API Documentation](https://defillama.com/docs/api)
+
+### Installation:
+
+use pip to install:
+
+``` 
+pip install DeFiLlama
+```
+
+-----------
+
+### Authentication:
+
+Endpoints are accessible without an API key.
+
+-----------
+
+### Example usage:
+
+```
+from defillama import DefiLlama
+
+# initialize api client
+llama = DefiLlama()
+
+# Get all protocols data
+response = llama.get_all_protocols()
+
+# Get a protocol data
+response = llama.get_protocol(name='uniswap')
+
+```
+-------
+#### Donate & Help maintain the library
+
+[![Paypal](qrcode.png)](https://www.paypal.com/ncp/payment/KLFNJN7SH39EN)
+
+-------
---
+++ packages/DeFiLlama/defillama-2.2.1.tar.gz/defillama-2.2.1/tests/test_defillama.py
@@ -0,0 +1,116 @@
+import pytest
+
+
+@pytest.mark.usefixtures('llama')
+class TestDefiLlama:
+    """
+    Test class for DefiLlama
+    """
+
+    @pytest.mark.skip(reason='TBD')
+    def test__send_message(self):
+        assert False
+
+    @pytest.mark.skip(reason='TBD')
+    def test__get(self):
+        assert False
+
+    def test_get_all_protocols(self, llama):
+        response = llama.get_all_protocols()
+        assert type(response) is list
+
+    # @pytest.mark.skip(reason='TBD')
+    def test_get_protocol(self, llama, protocol_json):
+        response = llama.get_protocol(name='aave')
+        assert type(response) is dict
+        for k in protocol_json.keys():
+            assert k in response
+
+    # @pytest.mark.skip(reason='TBD')
+    def test_get_historical_tvl(self, llama):
+        response = llama.get_historical_tvl()
+        assert type(response) is list
+        for data in response:
+            assert 'date' in data
+            assert 'tvl' in data
+            break
+
+    def test_get_historical_tvl_chain(self, llama):
+        response = llama.get_historical_tvl_chain('Ethereum')
+        assert type(response) is list
+        for data in response:
+            assert 'date' in data
+            assert 'tvl' in data
+            break
+
+    def test_get_protocol_current_tvl(self, llama):
+        response = llama.get_protocol_current_tvl('uniswap')
+        assert type(response) is float
+
+    def test_get_chains_current_tvl(self, llama):
+        response = llama.get_chains_current_tvl()
+        assert type(response) is list
+        for data in response:
+            assert 'gecko_id' in data
+            assert 'tvl' in data
+            assert 'tokenSymbol' in data
+            assert 'cmcId' in data
+            assert 'name' in data
+            assert 'chainId' in data
+            break
+
+    def test_get_pools(self, llama):
+        response = llama.get_pools()
+        assert type(response) is dict
+        assert 'status' in response
+        assert 'data' in response
+
+    def test_get_pool(self, llama):
+        response = llama.get_pool('747c1d2a-c668-4682-b9f9-296708a3dd90')
+        assert type(response) is dict
+        assert 'status' in response
+        assert 'data' in response
+
+    def test_get_dexs(self, llama):
+        response = llama.get_dexs()
+        assert type(response) is dict
+        assert 'totalDataChart' in response
+        assert 'totalDataChartBreakdown' in response
+        assert 'protocols' in response
+
+    def test_get_chain_dexs(self, llama):
+        response = llama.get_dexs('ethereum')
+        assert type(response) is dict
+        assert 'totalDataChart' in response
+        assert 'totalDataChartBreakdown' in response
+        assert 'protocols' in response
+
+    def test_get_dex_summary(self, llama):
+        response = llama.get_dex_summary('uniswap')
+        assert type(response) is dict
+        assert 'id' in response
+        assert 'name' in response
+        assert 'url' in response
+        assert 'totalDataChart' in response
+
+    def test_get_options_dexs(self, llama):
+        response = llama.get_options_dexs()
+        assert type(response) is dict
+        assert 'totalDataChart' in response
+        assert 'totalDataChartBreakdown' in response
+        assert 'protocols' in response
+
+    def test_get_chain_options_dexs(self, llama):
+        response = llama.get_chain_options_dexs('ethereum')
+        assert type(response) is dict
+        assert 'totalDataChart' in response
+        assert 'totalDataChartBreakdown' in response
+        assert 'protocols' in response
+
+    def test_get_options_dex_summary(self, llama):
+        response = llama.get_options_dex_summary('lyra')
+        assert type(response) is dict
+        assert 'id' in response
+        assert 'name' in response
+        assert 'url' in response
+        assert 'totalDataChart' in response

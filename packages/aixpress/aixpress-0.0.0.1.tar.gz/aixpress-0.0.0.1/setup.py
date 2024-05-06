3fce122a4d18b8175b7da474241a43d63f61b958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 15:05:46 +0000

    Add package aixpress/0.0.0.1/aixpress-0.0.0.1.tar.gz

---
+++ packages/aixpress/aixpress-0.0.0.1.tar.gz/aixpress-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aixpress',  
+    version='0.0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.8',
+    author='Neil Dias',
+    author_email='neildias.ai@gmail.com',
+    description='A package for rapid prototyping',
+    url='https://github.com/neildias/aixpress',
+)

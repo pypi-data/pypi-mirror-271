7f2a263a5583541caa91f6d46034365d58385f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 03:05:30 +0000

    Add package poetry-stabs-package-sample/0.1.3/poetry_stabs_package_sample-0.1.3-py3-none-any.whl

---
+++ packages/poetry-stabs-package-sample/poetry_stabs_package_sample-0.1.3-py3-none-any.whl/poetry_stabs_package_sample/sample.py
@@ -0,0 +1,12 @@
+# lambdaに型を付けるテスト
+from typing import Callable
+
+
+def main() -> None:
+    # def test_func(func: Callable[[int, int], int]) -> None:
+    #     print(func(1, 2))
+
+    # def add(a: int, b: int) -> int:
+    #     return a + b
+    lambda
+    test_func(add)

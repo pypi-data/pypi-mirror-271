926ca171aa6dc78912379567aecdd8771537f3f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 04:05:52 +0000

    Add package leads/0.8.24/leads-0.8.24.tar.gz

---
+++ packages/leads/leads-0.8.24.tar.gz/leads-0.8.24/leads_vec/_bootloader/systemd.py
@@ -0,0 +1,22 @@
+from os import chmod as _chmod, getuid as _getuid
+from os.path import abspath as _abspath
+from pwd import getpwuid as _getpwuid
+
+
+def create_service() -> None:
+    _chmod(f"{(script := _abspath(__file__)[:-10])}leads_vec.service.sh", 777)
+    with open("/etc/systemd/system/leads_vec.service", "w") as f:
+        f.write(
+            "[Unit]\n"
+            "Description=LEADS VeC\n"
+            "After=display-manager.service\n"
+            "Requires=display-manager.service\n"
+            "[Service]\n"
+            "Type=simple\n"
+            f"User={(user := _getpwuid(_getuid()).pw_name)}\n"
+            "Environment=\"DISPLAY=:0\"\n"
+            f"Environment=\"XAUTHORITY=/home/{user}/.Xauthority\"\n"
+            f"ExecStart=/bin/sh {script}\n"
+            "[Install]\n"
+            "WantedBy=graphical.target"
+        )

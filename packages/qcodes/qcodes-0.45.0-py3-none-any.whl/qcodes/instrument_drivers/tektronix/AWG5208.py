b1f2e17892f8d15e711722ea4e62a95b2c376763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 15:06:19 +0000

    Add package qcodes/0.45.0/qcodes-0.45.0-py3-none-any.whl

---
+++ packages/qcodes/qcodes-0.45.0-py3-none-any.whl/qcodes/instrument_drivers/tektronix/AWG5208.py
@@ -0,0 +1,30 @@
+from typing import Any
+
+from .AWG70000A import AWG70000A
+
+
+class TektronixAWG5208(AWG70000A):
+    """
+    The QCoDeS driver for Tektronix AWG5208
+    """
+
+    def __init__(self, name: str, address: str,
+                 timeout: float = 10, **kwargs: Any) -> None:
+        """
+        Args:
+            name: The name used internally by QCoDeS in the DataSet
+            address: The VISA resource name of the instrument
+            timeout: The VISA timeout time (in seconds).
+            **kwargs: kwargs are forwarded to base class.
+        """
+
+        super().__init__(name, address, num_channels=8,
+                         timeout=timeout, **kwargs)
+
+
+class AWG5208(TektronixAWG5208):
+    """
+    Alias with non-conformant name left for backwards compatibility
+    """
+
+    pass

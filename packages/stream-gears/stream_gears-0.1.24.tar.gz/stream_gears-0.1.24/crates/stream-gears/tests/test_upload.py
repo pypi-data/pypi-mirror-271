31b8c641a7d8911abe53537c598d4c52df0f0de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 03:06:28 +0000

    Add package stream-gears/0.1.24/stream_gears-0.1.24.tar.gz

---
+++ packages/stream-gears/stream_gears-0.1.24.tar.gz/stream_gears-0.1.24/crates/stream-gears/tests/test_upload.py
@@ -0,0 +1,23 @@
+import stream_gears
+
+if __name__ == '__main__':
+    stream_gears.upload(
+        ["examples/test.mp4"],
+        "cookies.json",
+        "title",
+        171,
+        "tag",
+        1,
+        "source",
+        "desc",
+        "dynamic",
+        "",
+        0,
+        0,
+        0,
+        0,
+        3,
+        [],
+        None,
+        stream_gears.UploadLine.Bda2,
+    )

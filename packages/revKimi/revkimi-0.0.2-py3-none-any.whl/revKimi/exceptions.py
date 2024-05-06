7c8f06bd6083c19db1136131a747cc914cb487f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 03:05:16 +0000

    Add package revKimi/0.0.2/revkimi-0.0.2-py3-none-any.whl

---
+++ packages/revKimi/revkimi-0.0.2-py3-none-any.whl/revKimi/exceptions.py
@@ -0,0 +1,13 @@
+class CreateFile(Exception):
+    """创建配置文件"""
+    pass
+
+
+class ConfigMissing(Exception):
+    """缺少配置项"""
+    pass
+
+
+class UnexpectedResponse(Exception):
+    """不正常响应"""
+    pass

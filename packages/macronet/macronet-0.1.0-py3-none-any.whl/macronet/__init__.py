2c780d0445a7fc29b8c568f1ae782eae99d07732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 03:05:00 +0000

    Add package macronet/0.1.0/macronet-0.1.0-py3-none-any.whl

---
+++ packages/macronet/macronet-0.1.0-py3-none-any.whl/macronet/__init__.py
@@ -0,0 +1,13 @@
+from monet.base import MoNetInitial
+
+try:
+    import torch.nn as nn
+    from monet.torch_ddf import torch_dict
+    m = MoNetInitial(nn)
+    m.ddf(torch_dict)
+except Exception:
+    m = MoNetInitial()
+    print("macronet loaded without torch")
+finally:
+    m.nn = m.__funcspace__
+    nn = m.nn

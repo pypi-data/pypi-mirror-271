5297d07dcc7d98ec4af88f6d2fe1e2124f0b8b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 03:04:36 +0000

    Add package desktop-notifier/3.5.7.dev0/desktop_notifier-3.5.7.dev0-py3-none-any.whl

---
+++ packages/desktop-notifier/desktop_notifier-3.5.7.dev0-py3-none-any.whl/desktop_notifier/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from .main import DesktopNotifier, Button, ReplyField, Notification, Urgency
+
+
+__version__ = "3.5.7.dev0"
+__author__ = "Sam Schott"
+__url__ = "https://github.com/samschott/desktop-notifier"
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__url__",
+    "Notification",
+    "Button",
+    "ReplyField",
+    "Urgency",
+    "DesktopNotifier",
+]

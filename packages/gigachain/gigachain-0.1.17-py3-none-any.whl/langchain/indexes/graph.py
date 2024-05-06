892dbc16a9179de71429d2f52bc326bf45b207a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 15:05:40 +0000

    Add package gigachain/0.1.17/gigachain-0.1.17-py3-none-any.whl

---
+++ packages/gigachain/gigachain-0.1.17-py3-none-any.whl/langchain/indexes/graph.py
@@ -0,0 +1,5 @@
+"""Graph Index Creator."""
+from langchain_community.graphs.index_creator import GraphIndexCreator
+from langchain_community.graphs.networkx_graph import NetworkxEntityGraph
+
+__all__ = ["GraphIndexCreator", "NetworkxEntityGraph"]

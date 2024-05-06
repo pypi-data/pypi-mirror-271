b78b6389c621365cc819e26befa96e53089c258d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 14:04:41 +0000

    Add package parediffusers/0.3.2/parediffusers-0.3.2.tar.gz

---
+++ packages/parediffusers/parediffusers-0.3.2.tar.gz/parediffusers-0.3.2/src/parediffusers/__init__.py
@@ -0,0 +1,8 @@
+from .pipeline import PareDiffusionPipeline
+from .scheduler import PareDDIMScheduler
+from .unet import PareUNet2DConditionModel
+from .vae import PareAutoencoderKL
+
+from .models import *
+
+__version__ = "0.3.1"

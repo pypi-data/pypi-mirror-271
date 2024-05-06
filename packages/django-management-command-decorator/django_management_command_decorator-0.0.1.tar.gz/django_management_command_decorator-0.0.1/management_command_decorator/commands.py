d651b5b25fd63d68606b2aba2340d1bbec763c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 03:06:18 +0000

    Add package django-management-command-decorator/0.0.1/django_management_command_decorator-0.0.1.tar.gz

---
+++ packages/django-management-command-decorator/django_management_command_decorator-0.0.1.tar.gz/django_management_command_decorator-0.0.1/management_command_decorator/commands.py
@@ -0,0 +1,16 @@
+from management_command_decorator.decorator import django_management_command
+
+
+@django_management_command
+def print_command():
+    print("Hello World!")  # noqa: T201
+
+
+@django_management_command(name="other_command")
+def print_command_2():
+    print("Hello other World!")  # noqa: T201
+
+
+@django_management_command
+def print_command_with_args(name):
+    print(f"Hello {name}")  # noqa: T201
---
+++ packages/django-management-command-decorator/django_management_command_decorator-0.0.1.tar.gz/django_management_command_decorator-0.0.1/management_command_decorator/tests.py
@@ -0,0 +1,21 @@
+from django.core.management import call_command
+
+from management_command_decorator import print_command, print_command_2
+
+
+def test_call_command():
+    call_command("print_command")
+
+
+def test_call_other_command():
+    call_command("other_command")
+
+
+def test_print_command_as_func():
+    # test we don't crash
+    print_command()
+    print_command_2()
+
+
+def test_command_with_args():
+    call_command("print_command_with_args", "name")

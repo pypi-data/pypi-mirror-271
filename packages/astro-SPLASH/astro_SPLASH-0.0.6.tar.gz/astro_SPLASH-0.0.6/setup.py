d5069324e86e005b82149f7bcee2997e88361db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 14:05:09 +0000

    Add package astro-SPLASH/0.0.6/astro_SPLASH-0.0.6.tar.gz

---
+++ packages/astro-SPLASH/astro_SPLASH-0.0.6.tar.gz/astro_SPLASH-0.0.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='astro_SPLASH',
+    version='0.0.6',
+    author='Adam Boesky',
+    author_email='apboesky@gmail.com',
+    description='SPLASH (Supernova classification Pipeline Leveraging Attributes of Supernova Hosts)',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+    install_requires=['torch',
+                      'numpy',
+                      'scikit-learn'],
+)
+
+# To republish:
+# 1. bump version nuber
+# 2. python setup.py sdist
+# 3. twine upload dist/*

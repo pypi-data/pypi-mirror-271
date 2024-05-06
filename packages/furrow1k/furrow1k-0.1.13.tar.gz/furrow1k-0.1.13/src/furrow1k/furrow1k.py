920a04ce41019365b9e2f1e202bb64e3728ac8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 14:04:34 +0000

    Add package furrow1k/0.1.13/furrow1k-0.1.13.tar.gz

---
+++ packages/furrow1k/furrow1k-0.1.13.tar.gz/furrow1k-0.1.13/src/furrow1k/furrow1k.py
@@ -0,0 +1,35 @@
+import datetime as dt
+from textwrap import dedent
+
+
+
+
+def main():
+    while True:
+        valid_options = [1, 2]
+        error_message = f"\nPlease enter a valid option: {valid_options=}"
+        try:
+            next_paycheck = input(
+                dedent(
+                    """\
+                    Is your paycheck this Friday or the next Friday? 
+                        1. This Friday
+                        2. Next Friday
+
+                    Response: [1]\
+                    """
+                )
+            )
+            if next_paycheck is None:
+                next_paycheck = 1
+            next_paycheck = int(next_paycheck)
+            if next_paycheck in valid_options:
+                break
+        except ValueError:
+            if next_paycheck is None:
+                next_paycheck = 1
+                break
+            print(error_message)
+
+    readable = "This Friday" if next_paycheck == 1 else "Next Friday"
+    print(f"Your next paycheck is {readable}.")

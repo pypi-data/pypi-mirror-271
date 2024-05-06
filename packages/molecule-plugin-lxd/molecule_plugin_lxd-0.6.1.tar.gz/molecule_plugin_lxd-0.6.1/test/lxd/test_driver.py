9159684499301f22527a6c4591a967a0c4bd463f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 16:04:56 +0000

    Add package molecule-plugin-lxd/0.6.1/molecule_plugin_lxd-0.6.1.tar.gz

---
+++ packages/molecule-plugin-lxd/molecule_plugin_lxd-0.6.1.tar.gz/molecule_plugin_lxd-0.6.1/test/lxd/test_driver.py
@@ -0,0 +1,6 @@
+"""Unit tests"""
+from molecule import api
+
+
+def test_driver_is_detected():
+    assert "lxd" in [str(d) for d in api.drivers()]
---
+++ packages/molecule-plugin-lxd/molecule_plugin_lxd-0.6.1.tar.gz/molecule_plugin_lxd-0.6.1/test/lxd/test_func.py
@@ -0,0 +1,57 @@
+#  Copyright (c) 2015-2018 Cisco Systems, Inc.
+#  Copyright (c) 2018 Red Hat, Inc.
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to
+#  deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
+"""Functional tests"""
+import pathlib
+
+from molecule.test.conftest import change_dir_to
+from molecule.util import run_command
+
+
+def test_command_init_scenario(tmp_path: pathlib.Path):
+    """Verify that init scenario works."""
+    scenario_name = "default"
+
+    with change_dir_to(tmp_path):
+        scenario_directory = tmp_path / "molecule" / scenario_name
+        cmd = [
+            "molecule",
+            "init",
+            "scenario",
+            scenario_name,
+            "--driver-name",
+            "lxd",
+        ]
+        result = run_command(cmd)
+        assert result.returncode == 0
+
+        assert scenario_directory.exists()
+
+        # run molecule reset as this may clean some leftovers from other
+        # test runs and also ensure that reset works.
+        result = run_command(["molecule", "reset"])  # default scenario
+        assert result.returncode == 0
+
+        result = run_command(["molecule", "reset", "-s", scenario_name])
+        assert result.returncode == 0
+
+        cmd = ["molecule", "--debug", "test", "-s", scenario_name]
+        result = run_command(cmd)
+        assert result.returncode == 0

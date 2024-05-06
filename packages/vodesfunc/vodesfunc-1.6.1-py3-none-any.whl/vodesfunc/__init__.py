cbd5632995ed2ea010b464cb432636512d12ccc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 04:05:20 +0000

    Add package vodesfunc/1.6.1/vodesfunc-1.6.1-py3-none-any.whl

---
+++ packages/vodesfunc/vodesfunc-1.6.1-py3-none-any.whl/vodesfunc/__init__.py
@@ -0,0 +1,16 @@
+"""
+    Oh god no
+"""
+
+# flake8: noqa
+
+from . import aa, misc, noise, scale, denoise, descale, rescale, util, spikefinder
+from .aa import *
+from .misc import *
+from .noise import *
+from .scale import *
+from .rescale import *
+from .descale import *
+from .denoise import *
+from .util import *
+from .spikefinder import *

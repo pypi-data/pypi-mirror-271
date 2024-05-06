58ce515cccfd2b553f38cd8787048a4d3750e40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 16:05:06 +0000

    Add package communication-services/0.1.1/communication-services-0.1.1.tar.gz

---
+++ packages/communication-services/communication-services-0.1.1.tar.gz/communication-services-0.1.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='communication-services',
+    version='0.1.1',
+    author='genius studio',
+    author_email='info@geniusai.com',
+    description='A short description of your project',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/geniusai1/partialtruckload-communication-serivecs.git',  # Link to your project's GitHub repo
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        # Add your project dependencies here
+        # 'requests',
+        # 'numpy',
+        'celery',
+        'twilio',
+        'firebase-admin',
+
+    ],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.6',
+)

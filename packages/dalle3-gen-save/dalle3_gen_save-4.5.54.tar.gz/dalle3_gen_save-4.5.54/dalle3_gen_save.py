dea2cc4dfd7735c79d8f33c3e1a8c26dda70bf65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 03:05:55 +0000

    Add package dalle3-gen-save/4.5.54/dalle3_gen_save-4.5.54.tar.gz

---
+++ packages/dalle3-gen-save/dalle3_gen_save-4.5.54.tar.gz/dalle3_gen_save-4.5.54/dalle3_gen_save.py
@@ -0,0 +1,29 @@
+import os
+from uuid import uuid4
+
+import dotenv
+import requests
+from openai import OpenAI
+
+dotenv.load_dotenv()
+
+
+def generate_and_save_image(prompt, folder_path='./users/BOTRUN_FOLDER/img/'):
+    client = OpenAI()
+    image_response = client.images.generate(model="dall-e-3", prompt=prompt, size="1024x1024", quality="hd", n=1)
+    image_url = image_response.data[0].url
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    image_data = requests.get(image_url).content
+    file_name = f'{folder_path}2024-05-05_{uuid4()}.png'
+    with open(file_name, 'wb') as file:
+        file.write(image_data)
+    print(f'Image saved as {file_name}')
+
+
+def main():
+    generate_and_save_image('一隻可愛貓咪與絢麗場景')
+
+
+if __name__ == '__main__':
+    main()

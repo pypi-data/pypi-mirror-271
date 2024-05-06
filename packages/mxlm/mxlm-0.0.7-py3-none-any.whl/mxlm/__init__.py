f39bd796da8341caae370c92ce897c45da515510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 14:04:59 +0000

    Add package mxlm/0.0.7/mxlm-0.0.7-py3-none-any.whl

---
+++ packages/mxlm/mxlm-0.0.7-py3-none-any.whl/mxlm/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from .__info__ import __version__, __description__
+from .chat_api import ChatAPI
+from .chatmd_utils import messages_to_chatmd, chatmd_to_messages
+from .mxlm_utils import df_to_html

2e1f16d47a0bb3b440a69a38122846b16ce6db49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 03:04:53 +0000

    Add package llama-kg/0.0.2/llama-kg-0.0.2.tar.gz

---
+++ packages/llama-kg/llama-kg-0.0.2.tar.gz/llama-kg-0.0.2/llama_kg/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Utilities for indexing a corpus with llama-index'
+LONG_DESCRIPTION = 'Utilities for indexing a corpus with llama-index'
+
+setup(
+    name='llama-kg',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+)

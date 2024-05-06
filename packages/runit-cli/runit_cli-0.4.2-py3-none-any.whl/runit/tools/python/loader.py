9059146853ae4a2d37283dbbe97aeccb7e246d79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 03:04:40 +0000

    Add package runit-cli/0.4.2/runit_cli-0.4.2-py3-none-any.whl

---
+++ packages/runit-cli/runit_cli-0.4.2-py3-none-any.whl/runit/tools/python/loader.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import inspect
+
+try:
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        filepath = os.path.split(filename)[0]
+
+        sys.path.append(filepath)
+        module = __import__(str(inspect.getmodulename(filename)))
+        functions = [f[0] for f in inspect.getmembers(module, inspect.isfunction)]
+        print(functions)
+    
+except Exception as e:
+    print(str(e))
---
+++ packages/runit-cli/runit_cli-0.4.2-py3-none-any.whl/runit/tools/python/runner.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import inspect
+
+args = sys.argv
+functionArguments = None
+
+try:
+    if len(args) >= 3:
+        filename = args[1]
+        functionname = args[2]
+        filepath = os.path.split(filename)[0]
+
+        sys.path.append(filepath)
+        module = __import__(str(inspect.getmodulename(filename)))
+        method = [f[1] for f in inspect.getmembers(module, inspect.isfunction) if f[0] == functionname][0]
+
+        if len(args) > 3:
+            functionArguments = args[3]
+
+        if functionArguments is not None:
+            print(method(functionArguments))
+        else:
+            print(method())
+    
+except Exception as e:
+    print(str(e))

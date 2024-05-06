f14513c50c7c6819405caab45dd5422bb1cb9815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 16:04:56 +0000

    Add package anicli_api/0.6.5/anicli_api-0.6.5.tar.gz

---
+++ packages/anicli_api/anicli_api-0.6.5.tar.gz/anicli_api-0.6.5/anicli_api/player/aniboom.py
@@ -0,0 +1,107 @@
+import re
+import warnings
+from typing import List
+
+from httpx import Response
+from parsel import Selector
+
+from anicli_api.player.base import BaseVideoExtractor, Video, url_validator
+
+__all__ = ["Aniboom"]
+
+_URL_EQ = re.compile(r"https://(www.)?aniboom\.one/")
+player_validator = url_validator(_URL_EQ)
+
+
+class Aniboom(BaseVideoExtractor):
+    URL_RULE = _URL_EQ
+    DEFAULT_HTTP_CONFIG = {"headers": {"referer": "https://animego.org/"}}
+    VIDEO_HEADERS = {
+        # KEYS SHOULD BE STARTED IN Title case, else hls/mpd links return 403 error
+        "Referer": "https://aniboom.one/",
+        # INCREASE DOWNLOAD SPEED with this static value. Its works only ru-RU value lol
+        "Accept-Language": "ru-RU",
+        "Origin": "https://aniboom.one",
+    }
+
+    @player_validator
+    def parse(self, url: str, **kwargs) -> List[Video]:
+        response = self.http.get(url)
+        return self._extract(response)
+
+    @player_validator
+    async def a_parse(self, url: str, **kwargs) -> List[Video]:
+        async with self.a_http as client:
+            response = await client.get(url)
+            return self._extract(response)
+
+    @staticmethod
+    def _is_not_found(resp: Response):
+        if resp.status_code == 404:
+            sel = Selector(resp.text)
+            msg = (
+                f"Aniboom returns 404 code with `{sel.css('title ::text').get()}` error. "
+                f"Maybe you missing `episode` and `translation` GET params or your IP not from CIS countries?"
+            )
+            warnings.warn(msg, stacklevel=1, category=RuntimeWarning)
+            return True
+        return False
+
+    @staticmethod
+    def _is_failed_dash_key(dash_url: str) -> bool:
+        # https://github.com/vypivshiy/ani-cli-ru/issues/29
+        #
+        # actual json signature (look dash.src and hls.src keys):
+        # { ...
+        # "dash":"{\"src\":\"https:...master_device.m3u8\",\  <<<
+        # "type\":\"application\\\/dash+xml\"}",
+        # "hls":"{\"src\":\"https:...master_device.m3u8\",    <<<
+        # \"type\":\"application\\\/x-mpegURL\"}"
+        # ... }
+        if dash_url.endswith(".m3u8"):
+            warnings.warn("Missing mpd link. This aniboom issue, not anicli-api", stacklevel=1, category=RuntimeWarning)
+            return True
+        return False
+
+    def _extract(self, response: Response) -> List[Video]:
+        if self._is_not_found(response):
+            return []
+
+        # if pre unescape response - parsel selector incorrect get data-parameters attr
+        sel = Selector(response.text)
+        # expected json signature:
+        # { ...
+        # "dash":"{\"src\":\"https:.../abcdef.mpd\",\
+        # "type\":\"application\\\/dash+xml\"}",
+        # "hls":"{\"src\":\"https:...\\\/master_device.m3u8\",
+        # \"type\":\"application\\\/x-mpegURL\"}"
+        # ... }
+        jsn = sel.xpath('//*[@id="video"]/@data-parameters')
+        videos: List[Video] = []
+
+        if dash := jsn.jmespath("dash"):
+            video_url = dash.re(r"(https:.*\.(?:mpd|m3u8))")[0].replace("\\", "")
+            # backend sometimes return m3u8 link in src.dash key
+            if self._is_failed_dash_key(video_url):
+                # in this case, hls.src == dash.src - return one Video object
+                return [Video(type="m3u8", quality=1080, url=video_url, headers=self.VIDEO_HEADERS)]
+
+            videos.append(
+                Video(
+                    type="mpd",
+                    quality=1080,
+                    url=video_url,
+                    headers=self.VIDEO_HEADERS,
+                )
+            )
+
+        if hls := jsn.jmespath("hls"):
+            videos.append(
+                Video(
+                    type="m3u8",
+                    quality=1080,
+                    url=hls.re(r"https:.*\.m3u8")[0].replace("\\", ""),
+                    headers=self.VIDEO_HEADERS,
+                )
+            )
+        return videos
---
+++ packages/anicli_api/anicli_api-0.6.5.tar.gz/anicli_api-0.6.5/anicli_api/source/jutsu.py
@@ -0,0 +1,169 @@
+import re
+import warnings
+from typing import TYPE_CHECKING, Any, Dict, List
+
+from attr import field
+from attrs import define
+
+from anicli_api._http import HTTPAsync, HTTPSync
+from anicli_api.base import BaseAnime, BaseEpisode, BaseExtractor, BaseOngoing, BaseSearch, BaseSource
+from anicli_api.player.base import Video
+from anicli_api.source.parsers.jutsu_parser import AnimeView, EpisodeView, OngoingView, SearchView, SourceView
+
+if TYPE_CHECKING:
+    from httpx import AsyncClient, Client
+
+
+class Extractor(BaseExtractor):
+    """NOTE: For playing video usage user-agent from http or http_async instance
+
+    """
+    BASE_URL = "https://jut.su"
+
+    def __init__(self, http_client: "Client" = HTTPSync(), http_async_client: "AsyncClient" = HTTPAsync()):
+        super().__init__(http_client=http_client, http_async_client=http_async_client)
+
+    @staticmethod
+    def _extract_search(resp: str) -> List["Search"]:
+        data = SearchView(resp).parse().view()
+        return [Search(**kw) for kw in data]
+
+    @staticmethod
+    def _extract_ongoing(resp: str) -> List["Ongoing"]:
+        data = OngoingView(resp).parse().view()
+        return [Ongoing(**kw) for kw in data]
+
+    def search(self, query: str):
+        resp = self.http.post(
+            self.BASE_URL + "/anime", data={"ajax_load": "yes", "start_from_page": 1, "show_search": query}
+        )
+        return self._extract_search(resp.text)
+
+    async def a_search(self, query: str):
+        resp = await self.http_async.post(
+            self.BASE_URL + "/anime", data={"ajax_load": "yes", "start_from_page": 1, "show_search": query}
+        )
+        return self._extract_search(resp.text)
+
+    def ongoing(self):
+        resp = self.http.post(self.BASE_URL + "/anime", data={"ajax_load": "yes", "start_from_page": 1})
+        return self._extract_ongoing(resp.text)
+
+    async def a_ongoing(self):
+        resp = await self.http_async.post(self.BASE_URL + "/anime", data={"ajax_load": "yes", "start_from_page": 1})
+        return self._extract_ongoing(resp.text)
+
+
+class _SearchOrOngoing:
+    url: str
+    http: "Client"
+    http_async: "AsyncClient"
+
+    @staticmethod
+    def _extract(resp: str) -> "Anime":
+        data = AnimeView(resp).parse().view()
+        # num attribute API required
+        raw_episodes = [{"num": f"{i + 1}", **kw} for i, kw in enumerate(EpisodeView(resp).parse().view())]
+        return Anime(**data, raw_episodes=raw_episodes)
+
+    def get_anime(self):
+        resp = self.http.get(self.url)
+        return self._extract(resp.text)
+
+    async def a_get_anime(self):
+        resp = await self.http_async.get(self.url)
+        return self._extract(resp.text)
+
+
+@define(kw_only=True)
+class Search(_SearchOrOngoing, BaseSearch):
+    counts: str = field(repr=False)
+
+
+@define(kw_only=True)
+class Ongoing(_SearchOrOngoing, BaseOngoing):
+    counts: str = field(repr=False)
+
+
+@define(kw_only=True)
+class Anime(BaseAnime):
+    # stub attribute
+    _raw_episodes: List[Dict[str, Any]] = field(repr=False)
+
+    def get_episodes(self):
+        return [Episode(**kw) for kw in self._raw_episodes]
+
+    async def a_get_episodes(self):
+        return self.get_episodes()
+
+
+@define(kw_only=True)
+class Episode(BaseEpisode):
+    _url: str = field(repr=False)
+
+    @staticmethod
+    def _is_blocked(raw_videos, response: str) -> bool:
+        if all(i["url"] is None for i in raw_videos):
+            # block jquery signature:
+            # var block_video_text_str = 'К сожалению, в России это видео недоступно.';
+            # var block_video_text_str_everywhere = 'К сожалению, это видео недоступно.';
+            if re.search(r"block_video_text_str_everywhere\+", response):
+                block_video = "К сожалению, это видео недоступно."
+            elif re.search(r"block_video_text_str\+", response):
+                block_video = "К сожалению, в России это видео недоступно."
+            else:
+                block_video = ""
+
+            reason = re.search(r"var block_[^>]+\s*=\s*['\"](?:<br>)?(.*?)['\"];", response)[1]
+
+            msg = f"Block issue. message: '{block_video} {reason}'."
+            warnings.warn(msg, stacklevel=1, category=RuntimeWarning)
+            return True
+        return False
+
+    def _extract(self, resp: str) -> List["Source"]:
+        data = SourceView(resp).parse().view()
+        raw_videos = [{"url": v, "quality": int(k.strip("url_"))} for k, v in data.items()]
+        # RKN blocks (RU region)
+        # eg: https://jut.su/shingekii-no-kyojin/season-1/episode-1.html
+        if self._is_blocked(raw_videos, resp):
+            return []
+        # STUB
+        # -----------------------------------v
+        return [Source(title="jut.su", url=self._url, raw_videos=raw_videos)]
+
+    def get_sources(self):
+        resp = self.http.get(self._url)
+        return self._extract(resp.text)
+
+    async def a_get_sources(self):
+        resp = await self.http_async.get(self._url)
+        return self._extract(resp.text)
+
+
+@define(kw_only=True)
+class Source(BaseSource):
+    _raw_videos: List[Dict[str, Any]] = field(repr=False)
+
+    def get_videos(self, **_) -> List["Video"]:
+        # For video playing, the user-agent MUST BE equal
+        # as a client user-agent in the extractor API
+        # ELSE VIDEO HOSTING RETURNS 403 CODE
+        return [
+            Video(**kw, headers={"User-Agent": self.http.headers["user-agent"]}, type="mp4")
+            for kw in self._raw_videos
+            if kw["url"]
+        ]
+
+    async def a_get_videos(self, **httpx_kwargs) -> List["Video"]:
+        return [
+            Video(**kw, headers={"User-Agent": self.http_async.headers["user-agent"]}, type="mp4")
+            for kw in self._raw_videos
+            if kw["url"]
+        ]
+
+
+if __name__ == "__main__":
+    from anicli_api.tools import cli
+
+    cli(Extractor())

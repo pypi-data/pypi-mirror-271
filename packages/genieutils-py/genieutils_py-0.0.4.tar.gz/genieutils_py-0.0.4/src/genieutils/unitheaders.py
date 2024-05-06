208574e652f58f44481b56b85cd07a7b399f0b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 03:05:34 +0000

    Add package genieutils-py/0.0.4/genieutils_py-0.0.4.tar.gz

---
+++ packages/genieutils-py/genieutils_py-0.0.4.tar.gz/genieutils_py-0.0.4/src/genieutils/unitheaders.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+from genieutils.common import ByteHandler, GenieClass
+from genieutils.task import Task
+from genieutils.versions import Version
+
+
+@dataclass
+class UnitHeaders(GenieClass):
+    exists: int
+    task_list: list[Task] | None = None
+
+    @classmethod
+    def from_bytes(cls, content: ByteHandler) -> 'UnitHeaders':
+        super().__init__(content)
+        exists = content.read_int_8()
+        task_list = None
+        if exists:
+            task_count = content.read_int_16()
+            task_list = content.read_class_array(Task, task_count)
+        return cls(
+            exists=exists,
+            task_list=task_list,
+        )
+
+    def to_bytes(self, version: Version) -> bytes:
+        return b''.join([
+            self.write_int_8(self.exists),
+            self.write_int_16(len(self.task_list)) if self.exists else b'',
+            self.write_class_array(self.task_list, version) if self.exists else b'',
+        ])

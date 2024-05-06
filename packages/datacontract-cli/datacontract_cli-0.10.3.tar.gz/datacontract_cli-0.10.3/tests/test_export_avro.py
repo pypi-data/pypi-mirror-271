c12ba58d625d676d5d609413d42d8c5535f1be92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 04:04:50 +0000

    Add package datacontract-cli/0.10.3/datacontract_cli-0.10.3.tar.gz

---
+++ packages/datacontract-cli/datacontract_cli-0.10.3.tar.gz/datacontract_cli-0.10.3/tests/test_export_avro.py
@@ -0,0 +1,73 @@
+import json
+import logging
+
+from typer.testing import CliRunner
+
+from datacontract.cli import app
+from datacontract.export.avro_converter import to_avro_schema_json
+from datacontract.model.data_contract_specification import DataContractSpecification
+
+logging.basicConfig(level=logging.DEBUG, force=True)
+
+
+def test_cli():
+    runner = CliRunner()
+    result = runner.invoke(app, ["export", "./fixtures/kafka-avro-remote/datacontract.yaml", "--format", "avro"])
+    assert result.exit_code == 0
+
+
+def test_to_avro_schema():
+    data_contract = DataContractSpecification.from_file("fixtures/kafka-avro-remote/datacontract.yaml")
+    expected_avro_schema = """
+    {
+  "fields": [
+    {
+      "name": "ordertime",
+      "doc": "My Field",
+      "type": "long"
+    },
+    {
+      "name": "orderid",
+      "type": "int"
+    },
+    {
+      "name": "itemid",
+      "type": "string"
+    },
+    {
+      "name": "orderunits",
+      "type": "double"
+    },
+    {
+      "name": "address",
+      "type": {
+        "fields": [
+          {
+            "name": "city",
+            "type": "string"
+          },
+          {
+            "name": "state",
+            "type": "string"
+          },
+          {
+            "name": "zipcode",
+            "type": "long"
+          }
+        ],
+        "name": "address",
+        "type": "record"
+      }
+    }
+  ],
+  "name": "orders",
+  "namespace": "com.example.checkout",
+  "doc": "My Model",
+  "type": "record"
+}
+"""
+
+    model_name, model = next(iter(data_contract.models.items()))
+    result = to_avro_schema_json(model_name, model)
+
+    assert json.loads(result) == json.loads(expected_avro_schema)
---
+++ packages/datacontract-cli/datacontract_cli-0.10.3.tar.gz/datacontract_cli-0.10.3/tests/test_import_avro.py
@@ -0,0 +1,157 @@
+import logging
+
+import yaml
+from typer.testing import CliRunner
+
+from datacontract.cli import app
+from datacontract.data_contract import DataContract
+
+logging.basicConfig(level=logging.DEBUG, force=True)
+
+
+def test_cli():
+    runner = CliRunner()
+    result = runner.invoke(
+        app,
+        [
+            "import",
+            "--format",
+            "avro",
+            "--source",
+            "fixtures/avro/data/orders.avsc",
+        ],
+    )
+    assert result.exit_code == 0
+
+
+def test_import_avro_schema():
+    result = DataContract().import_from_source("avro", "fixtures/avro/data/orders.avsc")
+
+    expected = """
+dataContractSpecification: 0.9.3
+id: my-data-contract-id
+info:
+  title: My Data Contract
+  version: 0.0.1
+models:
+  orders:
+    description: My Model
+    namespace: com.sample.schema
+    fields:
+      ordertime:
+        type: long
+        description: My Field
+        required: true
+      orderid:
+        type: int
+        required: true
+      itemid:
+        type: string
+        required: true
+      material:
+        type: string
+        required: false
+        description: An optional field
+      orderunits:
+        type: double
+        required: true
+      emailadresses:
+        type: array
+        description: Different email adresses of a customer
+        items:
+           type: string
+           format: email
+           pattern: ^.*@.*$
+        required: true
+      address:
+        type: object
+        required: true
+        fields:
+          city:
+            type: string
+            required: true
+          state:
+            type: string
+            required: true
+          zipcode:
+            type: long
+            required: true
+    """
+    print("Result:\n", result.to_yaml())
+    assert yaml.safe_load(result.to_yaml()) == yaml.safe_load(expected)
+    assert DataContract(data_contract_str=expected).lint(enabled_linters="none").has_passed()
+
+
+def test_import_avro_arrays_of_records_and_nested_arrays():
+    result = DataContract().import_from_source("avro", "fixtures/avro/data/arrays.avsc")
+
+    expected = """
+dataContractSpecification: 0.9.3
+id: my-data-contract-id
+info:
+  title: My Data Contract
+  version: 0.0.1
+models:
+  orders:
+    description: My Model
+    fields:
+      orderid:
+        type: int
+        required: true
+      adresses:
+        type: array
+        required: true
+        description: Adresses of a customer
+        items:
+          type: object
+          fields:
+            city:
+              type: string
+              required: true
+            state:
+              type: string
+              required: true
+            zipcode:
+              type: long
+              required: true
+      nestedArrays:
+        type: array
+        required: true
+        description: Example schema for an array of arrays
+        items:
+          type: array
+          items:
+            type: int
+"""
+    print("Result:\n", result.to_yaml())
+    assert yaml.safe_load(result.to_yaml()) == yaml.safe_load(expected)
+    assert DataContract(data_contract_str=expected).lint(enabled_linters="none").has_passed()
+
+
+def test_import_avro_nested_records():
+    result = DataContract().import_from_source("avro", "fixtures/avro/data/nested.avsc")
+
+    expected = """
+dataContractSpecification: 0.9.3
+id: my-data-contract-id
+info:
+  title: My Data Contract
+  version: 0.0.1
+models:
+  Doc:
+    namespace: com.xxx
+    fields:
+      fieldA:
+        type: long
+        required: false
+      fieldB:
+        type: record
+        required: false
+        fields:
+          fieldC:
+            type: string
+            required: false
+"""
+    print("Result:\n", result.to_yaml())
+    assert yaml.safe_load(result.to_yaml()) == yaml.safe_load(expected)
+    assert DataContract(data_contract_str=expected).lint(enabled_linters="none").has_passed()

06989d7292ff7451f6ae888bb3a3d05bbe481f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 16:05:00 +0000

    Add package mlessence/0.0.0.1/mlessence-0.0.0.1.tar.gz

---
+++ packages/mlessence/mlessence-0.0.0.1.tar.gz/mlessence-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mlessence',  
+    version='0.0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.8',
+    author='Neil Dias',
+    author_email='neildias.ai@gmail.com',
+    description='A fundamentals first approach to ML prototyping. The emphasis of this package is not just to prototype faster models but to do so in the most basic coherent way keeping in my basic ML practices',
+    url='https://github.com/neildias/mlessence',
+)

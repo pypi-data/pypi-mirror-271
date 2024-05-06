e1415d1c4aa6b863d5d7c88745fc450c0575f78b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 14:04:45 +0000

    Add package PyCourrier/0.1.8/PyCourrier-0.1.8.tar.gz

---
+++ packages/PyCourrier/PyCourrier-0.1.8.tar.gz/PyCourrier-0.1.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    description = f.read()
+
+setup(
+    name='PyCourrier',
+    version='0.1.8',
+    packages=find_packages(),
+    description='A simple email sender utility',
+    author='Abdelmajid Habouch',
+    author_email='Habush1610@gmail.com',
+    url='https://github.com/mjiid/PyCourrier',
+    license='MIT',
+    install_requires=[
+        'asyncio'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: Email',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    long_description=description,
+    long_description_content_type="text/markdown",
+)

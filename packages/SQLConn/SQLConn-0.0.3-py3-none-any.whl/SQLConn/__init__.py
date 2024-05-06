a3915e0193ae73ecae7bf2d087f9a17064625661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 16:05:05 +0000

    Add package SQLConn/0.0.3/SQLConn-0.0.3-py3-none-any.whl

---
+++ packages/SQLConn/SQLConn-0.0.3-py3-none-any.whl/SQLConn/__init__.py
@@ -0,0 +1,8 @@
+from .SQLConn import MSSQLConn
+from .SQLConn import MySQLConn
+from .SQLConn import OracleConn
+from .SQLConn import PostgreSQLConn
+from .SQLConn import SQLiteConn
+from .SQLConn import SQLConn
+
+__all__ = ["MSSQLConn", "MySQLConn", "OracleConn", "PostgreSQLConn", "SQLiteConn","SQLConn"]

259cd89b51167884eff8da6aed7ac9292996712c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 03:05:32 +0000

    Add package 2020239019/0.1.0/2020239019-0.1.0.tar.gz

---
+++ packages/2020239019/2020239019-0.1.0.tar.gz/2020239019-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='2020239019',
+version='0.1.0',
+author='Your Name',
+author_email='your.email@example.com',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=2.0',   # u have changed here
+)

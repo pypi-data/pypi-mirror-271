6e2251bdbe5a44c1f51b1e1c70ffd3c91f10bc84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 15:05:57 +0000

    Add package first-principles/0.0.0.1/first_principles-0.0.0.1.tar.gz

---
+++ packages/first-principles/first_principles-0.0.0.1.tar.gz/first_principles-0.0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='first_principles',  
+    version='0.0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.8',
+    author='Neil Dias',
+    author_email='neildias.ai@gmail.com',
+    description='A package that applies fundamental approach to solving ML problem',
+    url='https://github.com/neildias/first_principles',
+)

0ac4d22941390677a1f736d470e6635a1cc53e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 04:05:33 +0000

    Add package que_sdk/0.1.5/que_sdk-0.1.5-py3-none-any.whl

---
+++ packages/que_sdk/que_sdk-0.1.5-py3-none-any.whl/que_sdk/__init__.py
@@ -0,0 +1,11 @@
+from .client import (
+    QueClient,
+)
+from . import schemas
+from . import types
+
+__all__ = (
+    "QueClient",
+    "types",
+    "schemas",
+)
---
+++ packages/que_sdk/que_sdk-0.1.5-py3-none-any.whl/que_sdk/schemas.py
@@ -0,0 +1,52 @@
+import dataclasses
+from typing import (
+    Any,
+)
+
+
+@dataclasses.dataclass
+class BaseSchema:
+    def model_dump(self, exclude_none: bool = False) -> dict[Any, Any]:
+        if exclude_none:
+            return {k: v for k, v in dataclasses.asdict(self).items() if v is not None}
+        return dataclasses.asdict(self)
+
+
+@dataclasses.dataclass
+class SignUpSchema(BaseSchema):
+    username: str
+    telegram_id: int | None = None
+    password: str | None = None
+
+
+@dataclasses.dataclass
+class LoginSchema(BaseSchema):
+    username: str
+    password: str
+    telegram_id: int | None = None
+
+
+@dataclasses.dataclass
+class TMELoginSchema(BaseSchema):
+    telegram_id: int
+    signature: str
+    nonce: int
+    timestamp: int
+
+
+@dataclasses.dataclass
+class RoleSchema(BaseSchema):
+    title: str
+
+
+@dataclasses.dataclass
+class UserSchema(BaseSchema):
+    username: str | None = None
+    language: str | None = None
+
+
+@dataclasses.dataclass
+class ResetPasswordSchema(BaseSchema):
+    old_password: str
+    new_password: str
+    repeat_password: str

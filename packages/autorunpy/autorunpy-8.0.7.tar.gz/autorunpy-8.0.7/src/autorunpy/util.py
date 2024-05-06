831683a5af7f93e8b6dd3f85e51e7b5e4689045e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 14:04:54 +0000

    Add package autorunpy/8.0.7/autorunpy-8.0.7.tar.gz

---
+++ packages/autorunpy/autorunpy-8.0.7.tar.gz/autorunpy-8.0.7/src/autorunpy/util.py
@@ -0,0 +1,22 @@
+import json
+from pathlib import Path
+
+class Conf :
+    pkg = 'pip_pkg'
+    # python version to use
+    py_ver = 'py_ver'
+    # module name to run
+    module = "module"
+    # whether to remove venv after running
+    rm_venv = 'rm_venv'
+
+class Const :
+    rc = Path('/home/m/auto_run_configs')
+
+c = Const()
+
+def read_json(conf_stem) :
+    fp = c.rc / conf_stem
+    fp = fp.with_suffix('.json')
+    with open(fp , 'r') as _f :
+        return fp , json.load(_f)

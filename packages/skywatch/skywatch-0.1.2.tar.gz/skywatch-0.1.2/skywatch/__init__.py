7965da7a5791745314390cda419e35c9bcf15e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 14:04:35 +0000

    Add package skywatch/0.1.2/skywatch-0.1.2.tar.gz

---
+++ packages/skywatch/skywatch-0.1.2.tar.gz/skywatch-0.1.2/skywatch/__init__.py
@@ -0,0 +1,2 @@
+from . import access, attitude, look_angles, skypath, utils
+from .skypath import SkyPath
---
+++ packages/skywatch/skywatch-0.1.2.tar.gz/skywatch-0.1.2/skywatch/access/__init__.py
@@ -0,0 +1,3 @@
+from . import base_constraint, constraints
+from .access import Access
+from .time_interval import TimeInterval

1aa20a0731e97a12da0b24b43c67ea7fa05ed51c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 15:06:29 +0000

    Add package AstecManager/0.2.20/astecmanager-0.2.20.tar.gz

---
+++ packages/AstecManager/astecmanager-0.2.20.tar.gz/astecmanager-0.2.20/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+requirements = []
+with open('/Users/bgallean/Projects/GitHub/astecmanagerelease/requirements.txt', 'r') as fh:
+    for line in fh:
+        requirements.append(line.strip())
+
+
+setup(
+    name = "AstecManager",
+    version = "0.2.20",
+    author = "Benjamin GALLEAN",
+    author_email = "benjamin.gallean@crbm.cnrs.fr",
+    description = "This package creates a management system to run the ASTEC algorithms for developmental biology live "
+                  "imaging. ",
+    long_description = open('README.md').read(),
+    long_description_content_type = "text/markdown",
+    url = "https://gite.lirmm.fr/bgallean/astecmanagerelease",
+    project_urls = {
+        "Bug Tracker": "https://gite.lirmm.fr/bgallean/astecmanagerelease/-/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["AstecManager","AstecManager.libs","AstecManager.atlas","AstecManager.libs.ImageHandling",],
+    include_package_data = True,
+    install_requires=requirements
+)

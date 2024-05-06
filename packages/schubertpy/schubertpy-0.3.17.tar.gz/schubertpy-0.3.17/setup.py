104eadfb453e16bb07afaad90880ba42ef6cf7bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 15:05:52 +0000

    Add package schubertpy/0.3.17/schubertpy-0.3.17.tar.gz

---
+++ packages/schubertpy/schubertpy-0.3.17.tar.gz/schubertpy-0.3.17/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# Function to read the contents of the requirements.txt file
+def read_requirements():
+    with open('requirements.txt') as f:
+        return [line.strip() for line in f if line.strip()]
+    
+
+# Read the contents of your README file
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='schubertpy',
+    version='0.3.17',
+    packages=find_packages(),
+    description='This Python module facilitates operations such as quantum Pieri rules, quantum Giambelli formulae, action and multiplication of Schubert classes, and conversion between different representations of Schubert classes',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    author='Trần Duy Thanh',
+    author_email='fbtranduythanh@gmail.com',
+    url='https://github.com/tranduythanh/schubertpy',
+    install_requires=read_requirements(),
+)

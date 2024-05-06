c0c0f47f893287ec20051ddb8223cecba029872d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 16:04:53 +0000

    Add package python-telegram-broadcast/0.2/python-telegram-broadcast-0.2.tar.gz

---
+++ packages/python-telegram-broadcast/python-telegram-broadcast-0.2.tar.gz/python-telegram-broadcast-0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.2'
+DESCRIPTION = 'Package that wraps the python-telegram-bot library to make broadcasting easier.'
+
+setup(
+    name='python-telegram-broadcast',
+    version=VERSION,
+    packages=find_packages(),
+    description=DESCRIPTION,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='jonah_whaler_2348',
+    author_email='jk_saga@proton.me',
+    license='MIT',
+    install_requires=[
+        "python-telegram-bot==21.1.1",
+        "mypy"
+    ],
+    keywords=[
+        'python', 'telegram', 'broadcast', 'bot'
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+    ]
+)

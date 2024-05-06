31c09758cff1f17273186e3e410bef9c14f27383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 15:05:37 +0000

    Add package dbgpt/0.5.6rc0/dbgpt-0.5.6rc0-py3-none-any.whl

---
+++ packages/dbgpt/dbgpt-0.5.6rc0-py3-none-any.whl/dbgpt/storage/vector_store/chroma_store.py
@@ -0,0 +1,232 @@
+"""Chroma vector store."""
+import logging
+import os
+from typing import List, Optional
+
+from chromadb import PersistentClient
+from chromadb.config import Settings
+
+from dbgpt._private.pydantic import ConfigDict, Field
+from dbgpt.configs.model_config import PILOT_PATH
+from dbgpt.core import Chunk
+from dbgpt.core.awel.flow import Parameter, ResourceCategory, register_resource
+from dbgpt.util.i18n_utils import _
+
+from .base import _COMMON_PARAMETERS, VectorStoreBase, VectorStoreConfig
+from .filters import FilterOperator, MetadataFilters
+
+logger = logging.getLogger(__name__)
+
+
+@register_resource(
+    _("Chroma Vector Store"),
+    "chroma_vector_store",
+    category=ResourceCategory.VECTOR_STORE,
+    description=_("Chroma vector store."),
+    parameters=[
+        *_COMMON_PARAMETERS,
+        Parameter.build_from(
+            _("Persist Path"),
+            "persist_path",
+            str,
+            description=_("the persist path of vector store."),
+            optional=True,
+            default=None,
+        ),
+    ],
+)
+class ChromaVectorConfig(VectorStoreConfig):
+    """Chroma vector store config."""
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    persist_path: Optional[str] = Field(
+        default=os.getenv("CHROMA_PERSIST_PATH", None),
+        description="the persist path of vector store.",
+    )
+    collection_metadata: Optional[dict] = Field(
+        default=None,
+        description="the index metadata of vector store, if not set, will use the "
+        "default metadata.",
+    )
+
+
+class ChromaStore(VectorStoreBase):
+    """Chroma vector store."""
+
+    def __init__(self, vector_store_config: ChromaVectorConfig) -> None:
+        """Create a ChromaStore instance."""
+        from langchain.vectorstores import Chroma
+
+        chroma_vector_config = vector_store_config.to_dict(exclude_none=True)
+        chroma_path = chroma_vector_config.get(
+            "persist_path", os.path.join(PILOT_PATH, "data")
+        )
+        self.persist_dir = os.path.join(
+            chroma_path, vector_store_config.name + ".vectordb"
+        )
+        self.embeddings = vector_store_config.embedding_fn
+        chroma_settings = Settings(
+            # chroma_db_impl="duckdb+parquet", => deprecated configuration of Chroma
+            persist_directory=self.persist_dir,
+            anonymized_telemetry=False,
+        )
+        client = PersistentClient(path=self.persist_dir, settings=chroma_settings)
+
+        collection_metadata = chroma_vector_config.get("collection_metadata") or {
+            "hnsw:space": "cosine"
+        }
+        self.vector_store_client = Chroma(
+            persist_directory=self.persist_dir,
+            embedding_function=self.embeddings,
+            # client_settings=chroma_settings,
+            client=client,
+            collection_metadata=collection_metadata,
+        )
+
+    def similar_search(
+        self, text, topk, filters: Optional[MetadataFilters] = None
+    ) -> List[Chunk]:
+        """Search similar documents."""
+        logger.info("ChromaStore similar search")
+        where_filters = self.convert_metadata_filters(filters) if filters else None
+        lc_documents = self.vector_store_client.similarity_search(
+            text, topk, filter=where_filters
+        )
+        return [
+            Chunk(content=doc.page_content, metadata=doc.metadata)
+            for doc in lc_documents
+        ]
+
+    def similar_search_with_scores(
+        self, text, topk, score_threshold, filters: Optional[MetadataFilters] = None
+    ) -> List[Chunk]:
+        """Search similar documents with scores.
+
+        Chroma similar_search_with_score.
+        Return docs and relevance scores in the range [0, 1].
+        Args:
+            text(str): query text
+            topk(int): return docs nums. Defaults to 4.
+            score_threshold(float): score_threshold: Optional, a floating point value
+                between 0 to 1 to filter the resulting set of retrieved docs,0 is
+                dissimilar, 1 is most similar.
+            filters(MetadataFilters): metadata filters, defaults to None
+        """
+        logger.info("ChromaStore similar search with scores")
+        where_filters = self.convert_metadata_filters(filters) if filters else None
+        docs_and_scores = (
+            self.vector_store_client.similarity_search_with_relevance_scores(
+                query=text,
+                k=topk,
+                score_threshold=score_threshold,
+                filter=where_filters,
+            )
+        )
+        return [
+            Chunk(content=doc.page_content, metadata=doc.metadata, score=score)
+            for doc, score in docs_and_scores
+        ]
+
+    def vector_name_exists(self) -> bool:
+        """Whether vector name exists."""
+        logger.info(f"Check persist_dir: {self.persist_dir}")
+        if not os.path.exists(self.persist_dir):
+            return False
+        files = os.listdir(self.persist_dir)
+        # Skip default file: chroma.sqlite3
+        files = list(filter(lambda f: f != "chroma.sqlite3", files))
+        return len(files) > 0
+
+    def load_document(self, chunks: List[Chunk]) -> List[str]:
+        """Load document to vector store."""
+        logger.info("ChromaStore load document")
+        texts = [chunk.content for chunk in chunks]
+        metadatas = [chunk.metadata for chunk in chunks]
+        ids = [chunk.chunk_id for chunk in chunks]
+        self.vector_store_client.add_texts(texts=texts, metadatas=metadatas, ids=ids)
+        return ids
+
+    def delete_vector_name(self, vector_name: str):
+        """Delete vector name."""
+        logger.info(f"chroma vector_name:{vector_name} begin delete...")
+        self.vector_store_client.delete_collection()
+        self._clean_persist_folder()
+        return True
+
+    def delete_by_ids(self, ids):
+        """Delete vector by ids."""
+        logger.info(f"begin delete chroma ids: {ids}")
+        ids = ids.split(",")
+        if len(ids) > 0:
+            collection = self.vector_store_client._collection
+            collection.delete(ids=ids)
+
+    def convert_metadata_filters(
+        self,
+        filters: MetadataFilters,
+    ) -> dict:
+        """Convert metadata filters to Chroma filters.
+
+        Args:
+            filters(MetadataFilters): metadata filters.
+        Returns:
+            dict: Chroma filters.
+        """
+        where_filters = {}
+        filters_list = []
+        condition = filters.condition
+        chroma_condition = f"${condition}"
+        if filters.filters:
+            for filter in filters.filters:
+                if filter.operator:
+                    filters_list.append(
+                        {
+                            filter.key: {
+                                _convert_chroma_filter_operator(
+                                    filter.operator
+                                ): filter.value
+                            }
+                        }
+                    )
+                else:
+                    filters_list.append({filter.key: filter.value})  # type: ignore
+
+        if len(filters_list) == 1:
+            return filters_list[0]
+        elif len(filters_list) > 1:
+            where_filters[chroma_condition] = filters_list
+        return where_filters
+
+    def _clean_persist_folder(self):
+        """Clean persist folder."""
+        for root, dirs, files in os.walk(self.persist_dir, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+        os.rmdir(self.persist_dir)
+
+
+def _convert_chroma_filter_operator(operator: str) -> str:
+    """Convert operator to Chroma where operator.
+
+    Args:
+        operator(str): operator.
+    Returns:
+        str: Chroma where operator.
+    """
+    if operator == FilterOperator.EQ:
+        return "$eq"
+    elif operator == FilterOperator.NE:
+        return "$ne"
+    elif operator == FilterOperator.GT:
+        return "$gt"
+    elif operator == FilterOperator.LT:
+        return "$lt"
+    elif operator == FilterOperator.GTE:
+        return "$gte"
+    elif operator == FilterOperator.LTE:
+        return "$lte"
+    else:
+        raise ValueError(f"Chroma Where operator {operator} not supported")
---
+++ packages/dbgpt/dbgpt-0.5.6rc0-py3-none-any.whl/dbgpt/storage/vector_store/connector.py
@@ -0,0 +1,231 @@
+"""Connector for vector store."""
+
+import copy
+import os
+from typing import Any, Dict, List, Optional, Type, cast
+
+from dbgpt.core import Chunk, Embeddings
+from dbgpt.core.awel.flow import (
+    FunctionDynamicOptions,
+    OptionValue,
+    Parameter,
+    ResourceCategory,
+    register_resource,
+)
+from dbgpt.storage.vector_store.base import VectorStoreBase, VectorStoreConfig
+from dbgpt.storage.vector_store.filters import MetadataFilters
+from dbgpt.util.i18n_utils import _
+
+connector: Dict[str, Type] = {}
+
+
+def _load_vector_options() -> List[OptionValue]:
+    from dbgpt.storage import vector_store
+
+    return [
+        OptionValue(label=cls, name=cls, value=cls)
+        for cls in vector_store.__all__
+        if issubclass(getattr(vector_store, cls), VectorStoreBase)
+    ]
+
+
+@register_resource(
+    _("Vector Store Connector"),
+    "vector_store_connector",
+    category=ResourceCategory.VECTOR_STORE,
+    parameters=[
+        Parameter.build_from(
+            _("Vector Store Type"),
+            "vector_store_type",
+            str,
+            description=_("The type of vector store."),
+            options=FunctionDynamicOptions(func=_load_vector_options),
+        ),
+        Parameter.build_from(
+            _("Vector Store Implementation"),
+            "vector_store_config",
+            VectorStoreConfig,
+            description=_("The vector store implementation."),
+            optional=True,
+            default=None,
+        ),
+    ],
+)
+class VectorStoreConnector:
+    """The connector for vector store.
+
+    VectorStoreConnector, can connect different vector db provided load document api_v1
+    and similar search api_v1.
+
+    1.load_document:knowledge document source into vector store.(Chroma, Milvus,
+    Weaviate).
+    2.similar_search: similarity search from vector_store.
+    3.similar_search_with_scores: similarity search with similarity score from
+    vector_store
+
+    code example:
+    >>> from dbgpt.storage.vector_store.connector import VectorStoreConnector
+
+    >>> vector_store_config = VectorStoreConfig
+    >>> vector_store_connector = VectorStoreConnector(vector_store_type="Chroma")
+    """
+
+    def __init__(
+        self,
+        vector_store_type: str,
+        vector_store_config: Optional[VectorStoreConfig] = None,
+    ) -> None:
+        """Create a VectorStoreConnector instance.
+
+        Args:
+            - vector_store_type: vector store type Milvus, Chroma, Weaviate
+            - ctx: vector store config params.
+        """
+        self._vector_store_config = vector_store_config
+        self._register()
+
+        if self._match(vector_store_type):
+            self.connector_class = connector[vector_store_type]
+        else:
+            raise Exception(f"Vector Store Type Not support. {0}", vector_store_type)
+
+        print(self.connector_class)
+        self._vector_store_type = vector_store_type
+        self._embeddings = (
+            vector_store_config.embedding_fn if vector_store_config else None
+        )
+        self.client = self.connector_class(vector_store_config)
+
+    @classmethod
+    def from_default(
+        cls,
+        vector_store_type: Optional[str] = None,
+        embedding_fn: Optional[Any] = None,
+        vector_store_config: Optional[VectorStoreConfig] = None,
+    ) -> "VectorStoreConnector":
+        """Initialize default vector store connector."""
+        vector_store_type = vector_store_type or os.getenv(
+            "VECTOR_STORE_TYPE", "Chroma"
+        )
+        from dbgpt.storage.vector_store.chroma_store import ChromaVectorConfig
+
+        vector_store_config = vector_store_config or ChromaVectorConfig()
+        vector_store_config.embedding_fn = embedding_fn
+        real_vector_store_type = cast(str, vector_store_type)
+        return cls(real_vector_store_type, vector_store_config)
+
+    def load_document(self, chunks: List[Chunk]) -> List[str]:
+        """Load document in vector database.
+
+        Args:
+            - chunks: document chunks.
+        Return chunk ids.
+        """
+        max_chunks_once_load = (
+            self._vector_store_config.max_chunks_once_load
+            if self._vector_store_config
+            else 10
+        )
+        max_threads = (
+            self._vector_store_config.max_threads if self._vector_store_config else 1
+        )
+        return self.client.load_document_with_limit(
+            chunks,
+            max_chunks_once_load,
+            max_threads,
+        )
+
+    def similar_search(
+        self, doc: str, topk: int, filters: Optional[MetadataFilters] = None
+    ) -> List[Chunk]:
+        """Similar search in vector database.
+
+        Args:
+           - doc: query text
+           - topk: topk
+           - filters: metadata filters.
+        Return:
+            - chunks: chunks.
+        """
+        return self.client.similar_search(doc, topk, filters)
+
+    def similar_search_with_scores(
+        self,
+        doc: str,
+        topk: int,
+        score_threshold: float,
+        filters: Optional[MetadataFilters] = None,
+    ) -> List[Chunk]:
+        """Similar_search_with_score in vector database.
+
+        Return docs and relevance scores in the range [0, 1].
+
+        Args:
+            doc(str): query text
+            topk(int): return docs nums. Defaults to 4.
+            score_threshold(float): score_threshold: Optional, a floating point value
+                between 0 to 1 to filter the resulting set of retrieved docs,0 is
+                dissimilar, 1 is most similar.
+            filters: metadata filters.
+        Return:
+            - chunks: Return docs and relevance scores in the range [0, 1].
+        """
+        return self.client.similar_search_with_scores(
+            doc, topk, score_threshold, filters
+        )
+
+    @property
+    def vector_store_config(self) -> VectorStoreConfig:
+        """Return the vector store config."""
+        if not self._vector_store_config:
+            raise ValueError("vector store config not set.")
+        return self._vector_store_config
+
+    def vector_name_exists(self):
+        """Whether vector name exists."""
+        return self.client.vector_name_exists()
+
+    def delete_vector_name(self, vector_name: str):
+        """Delete vector name.
+
+        Args:
+            - vector_name: vector store name
+        """
+        return self.client.delete_vector_name(vector_name)
+
+    def delete_by_ids(self, ids):
+        """Delete vector by ids.
+
+        Args:
+            - ids: vector ids
+        """
+        return self.client.delete_by_ids(ids=ids)
+
+    @property
+    def current_embeddings(self) -> Optional[Embeddings]:
+        """Return the current embeddings."""
+        return self._embeddings
+
+    def new_connector(self, name: str, **kwargs) -> "VectorStoreConnector":
+        """Create a new connector.
+
+        New connector based on the current connector.
+        """
+        config = copy.copy(self.vector_store_config)
+        for k, v in kwargs.items():
+            if v is not None:
+                setattr(config, k, v)
+        config.name = name
+
+        return self.__class__(self._vector_store_type, config)
+
+    def _match(self, vector_store_type) -> bool:
+        return bool(connector.get(vector_store_type))
+
+    def _register(self):
+        from dbgpt.storage import vector_store
+
+        for cls in vector_store.__all__:
+            if issubclass(getattr(vector_store, cls), VectorStoreBase):
+                _k, _v = cls, getattr(vector_store, cls)
+                connector.update({_k: _v})

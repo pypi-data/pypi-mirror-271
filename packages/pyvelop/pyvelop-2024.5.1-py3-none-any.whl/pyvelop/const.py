6f8bb6e3fafbc123566f2c79840588886629c06b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 04:04:49 +0000

    Add package pyvelop/2024.5.1/pyvelop-2024.5.1-py3-none-any.whl

---
+++ packages/pyvelop/pyvelop-2024.5.1-py3-none-any.whl/pyvelop/const.py
@@ -0,0 +1,9 @@
+"""Constants for the pyvelop module."""
+
+# region #-- generic details --#
+_PACKAGE_NAME: str = "pyvelop"
+_PACKAGE_AUTHOR: str = "uvjim"
+_PACKAGE_VERSION: str = "2024.5.1"
+# endregion
+
+DEF_REDACT: str = "**REDACTED**"

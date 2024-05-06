4b27fdec7d36661009bdc0533e4bd4f7404db48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 14:04:42 +0000

    Add package my-decimal/0.1/my_decimal-0.1.tar.gz

---
+++ packages/my-decimal/my_decimal-0.1.tar.gz/my_decimal-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='my_decimal',
+    version='0.1',
+    description='A small library for formatting decimals',
+    py_modules=['my_decimal'],
+    author='k0ng999',
+    author_email='baydar_14@mail.ru',
+    url='https://github.com/yourusername/my_decimal',
+    license='MIT',
+)

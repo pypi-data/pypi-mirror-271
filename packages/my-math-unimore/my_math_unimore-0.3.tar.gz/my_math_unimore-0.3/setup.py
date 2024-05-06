3371872d0a13f2c3b3bcff0d08cda93ed891930b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 15:06:31 +0000

    Add package my-math-unimore/0.3/my_math_unimore-0.3.tar.gz

---
+++ packages/my-math-unimore/my_math_unimore-0.3.tar.gz/my_math_unimore-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup 
+  
+setup( 
+    name='my_math_unimore', 
+    version='0.3', 
+    description='A sample Python package to test CI/CD', 
+    long_description='A sample Python package to test CI/CD',
+    author='Angelo Ferrando', 
+    author_email='angelo.ferrando42@gmail.com', 
+    packages=['my_math_unimore'], 
+    install_requires=['pytest'], 
+) 

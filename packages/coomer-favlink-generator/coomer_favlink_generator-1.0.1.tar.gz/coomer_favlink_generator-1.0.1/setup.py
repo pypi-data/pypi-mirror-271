70596f808a4a75357820545f43b100193de49d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 04:05:06 +0000

    Add package coomer-favlink-generator/1.0.1/coomer_favlink_generator-1.0.1.tar.gz

---
+++ packages/coomer-favlink-generator/coomer_favlink_generator-1.0.1.tar.gz/coomer_favlink_generator-1.0.1/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='coomer_favlink_generator',
+    version='1.0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'coomer_favlink_generator=coomer_favlink_generator.link_generator:main',
+        ],
+    },
+    install_requires=[],
+)

28b91ad5d52dfaa6bc429f52ff8d6378f417f695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 15:06:16 +0000

    Add package lyywxgui/1.1/lyywxgui-1.1.tar.gz

---
+++ packages/lyywxgui/lyywxgui-1.1.tar.gz/lyywxgui-1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lyywxgui',
+    version='1.1',
+    author='lyy',
+    author_email='',
+    description='yyjdgui for lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+        "win10toast ",
+
+    ],
+)

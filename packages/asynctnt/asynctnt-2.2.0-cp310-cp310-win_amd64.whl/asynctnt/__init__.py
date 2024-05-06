9af05daa09b26d4162cae822d6e256dff62a4317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 04:05:11 +0000

    Add package asynctnt/2.2.0/asynctnt-2.2.0-cp310-cp310-win_amd64.whl

---
+++ packages/asynctnt/asynctnt-2.2.0-cp310-cp310-win_amd64.whl/asynctnt/__init__.py
@@ -0,0 +1,19 @@
+# ruff: noqa: F401
+
+from .connection import Connection, connect
+from .iproto.protocol import (
+    Db,
+    Field,
+    IProtoError,
+    IProtoErrorStackFrame,
+    Iterator,
+    Metadata,
+    PushIterator,
+    Response,
+    Schema,
+    SchemaIndex,
+    SchemaSpace,
+    TarantoolTuple,
+)
+
+__version__ = "2.2.0"

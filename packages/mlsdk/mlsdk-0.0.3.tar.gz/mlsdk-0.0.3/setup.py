111c21bca17d8021955ca6bad048acf9a5559b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-06 16:05:07 +0000

    Add package mlsdk/0.0.3/mlsdk-0.0.3.tar.gz

---
+++ packages/mlsdk/mlsdk-0.0.3.tar.gz/mlsdk-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mlsdk',
+    version='0.0.3',
+    packages=find_packages(),
+    description='Model Zoo SDK',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'boto3',
+        'python-dotenv',
+        # 'silabs-mltk[full]'
+    ],
+    author='EUR',
+    author_email='eur@gmail.com',
+    license='MIT',
+    entry_points={
+        "console_scripts": [
+            "mlsdk = mlsdk.__main__:main"
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)

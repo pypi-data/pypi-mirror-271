b8654086f775f0525f2e0d07d4aef72434ca9b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 05:05:08 +0000

    Add package galileo-core/0.13.0/galileo_core-0.13.0-py3-none-any.whl

---
+++ packages/galileo-core/galileo_core-0.13.0-py3-none-any.whl/galileo_core/schemas/protect/payload.py
@@ -0,0 +1,26 @@
+from functools import cached_property
+from typing import Optional
+
+from pydantic import BaseModel, Field, computed_field, model_validator
+
+
+class Payload(BaseModel):
+    input: Optional[str] = Field(default=None, description="Input text to be processed.")
+    output: Optional[str] = Field(default=None, description="Output text to be processed.")
+
+    @model_validator(mode="after")
+    def ensure_input_or_output(self) -> "Payload":
+        if not self.input and not self.output:
+            raise ValueError("Either input or output must be set.")
+        return self
+
+    # https://github.com/python/mypy/issues/1362
+    @computed_field  # type: ignore[misc]
+    @cached_property
+    def text(self) -> str:
+        if self.output:
+            return self.output
+        elif self.input:
+            return self.input
+        else:
+            raise ValueError("Either input or output must be set.")

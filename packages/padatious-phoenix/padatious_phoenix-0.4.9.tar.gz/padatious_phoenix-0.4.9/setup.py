d44cbde991f62532dfccb2b6553be3d07dd94e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 14:05:47 +0000

    Add package padatious-phoenix/0.4.9/padatious_phoenix-0.4.9.tar.gz

---
+++ packages/padatious-phoenix/padatious_phoenix-0.4.9.tar.gz/padatious_phoenix-0.4.9/setup.py
@@ -0,0 +1,52 @@
+from os.path import join, abspath, dirname
+from setuptools import setup
+
+with open(join(dirname(abspath(__file__)), 'requirements.txt')) as f:
+    requirements = f.readlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='padatious_phoenix',
+    version='0.4.9',  # Assurez-vous que cette version est supérieure à la version actuelle de padatious
+    description='A neural network intent parser',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/MycroftPhoenix/padatious_phoenix',
+    author='Matthew Scholefield',
+    author_email='matthew331199@gmail.com',
+    maintainer='Steve Chrétien',  # Remplacez "moder" par "maintainer"
+    maintainer_email='stevechretien111@protonmail.com',  # Remplacez "moder_email" par "maintainer_email"
+    license='Apache-2.0',
+    packages=[
+        'padatious'
+    ],
+    install_requires=requirements,
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    entry_points={
+        'console_scripts': [
+            'padatious=padatious.__main__:main'  # Corrected entry point
+        ]
+    },
+    keywords='intent-parser parser text text-processing',
+)

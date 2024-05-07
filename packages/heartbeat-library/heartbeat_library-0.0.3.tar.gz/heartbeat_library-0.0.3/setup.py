c2ad61d9e66a5ada9e5ec29e500514843aaa62cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 04:06:16 +0000

    Add package heartbeat-library/0.0.3/heartbeat_library-0.0.3.tar.gz

---
+++ packages/heartbeat-library/heartbeat_library-0.0.3.tar.gz/heartbeat_library-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='heartbeat-library',
+    version='0.0.3',
+    license='MIT License',
+    author='Pedro Ferreira Braz',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='pbraz.pedrof@gmail.com',
+    keywords='heartbeat, heartbeat-library, heartbeat-api, heartbeat-wrapper, heartbeat-python, heartbeat-python-wrapper, heartbeat-python-api, heartbeat-python-wrapper-api, heartbeat-python-api-wrapper, multithreading, threading, requests, python-requests, python-threading, python-multithreading, python-heartbeat, python-heartbeat-api, python-heartbeat-wrapper, python-heartbeat-python, python-heartbeat-python-wrapper, python-heartbeat-python-api, python-heartbeat-python-wrapper-api, python-heartbeat-python-api-wrapper',
+    description=u'Library for sending pulses to a process monitoring server',
+    packages=['heartbeat-library'],
+    install_requires=[])
+
+
+
+
+

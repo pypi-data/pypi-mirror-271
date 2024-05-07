0cf066f3dccc9aa702a6a10282278a4ec788128c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 04:06:27 +0000

    Add package model-providers/0.3.0.20240506/model_providers-0.3.0.20240506.tar.gz

---
+++ packages/model-providers/model_providers-0.3.0.20240506.tar.gz/model_providers-0.3.0.20240506/model_providers/core/utils/json_dumps.py
@@ -0,0 +1,13 @@
+import os
+
+import orjson
+from pydantic import BaseModel
+
+
+def json_dumps(o):
+    def _default(obj):
+        if isinstance(obj, BaseModel):
+            return obj.dict()
+        raise TypeError
+
+    return orjson.dumps(o, default=_default)

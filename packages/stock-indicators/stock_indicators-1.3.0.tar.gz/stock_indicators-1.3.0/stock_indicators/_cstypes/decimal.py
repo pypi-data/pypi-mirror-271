ecf2ae227bead14cb0822cbaf8cd91e3a248a4fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 04:06:15 +0000

    Add package stock-indicators/1.3.0/stock_indicators-1.3.0.tar.gz

---
+++ packages/stock-indicators/stock_indicators-1.3.0.tar.gz/stock_indicators-1.3.0/stock_indicators/_cstypes/decimal.py
@@ -0,0 +1,36 @@
+from decimal import Decimal as PyDecimal
+
+from stock_indicators._cslib import CsDecimal
+from stock_indicators._cslib import CsFormatException
+
+class Decimal:
+    """
+    Class for converting a number into C#'s `System.Decimal` class.
+
+    Parameters:
+        decimal : `int`, `float` or any `object` that can be represented as a number.
+
+    Example:
+        Constructing `System.Decimal` from `float` of Python.
+
+        >>> cs_decimal = Decimal(2.5)
+        >>> cs_decimal
+        2.5
+    """
+    def __new__(cls, decimal) -> CsDecimal:
+        try:
+            return CsDecimal.Parse(str(decimal))
+        except CsFormatException as e:
+            raise ValueError("You may be using numeric data that is incompatible with your locale environment settings.\n"
+                             "For example, you may be using decimal points instead of commas.") from e
+
+
+def to_pydecimal(cs_decimal):
+    """
+    Converts an object to a native Python decimal object.
+
+    Parameter:
+        cs_decimal : `System.Decimal` of C# or any `object` that can be represented as a number.
+    """
+    if cs_decimal is not None:
+        return PyDecimal(str(cs_decimal))

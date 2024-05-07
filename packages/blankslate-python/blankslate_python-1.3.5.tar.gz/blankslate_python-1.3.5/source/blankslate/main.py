199b9e22926112dc50758079f01000c1ff693ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 04:06:41 +0000

    Add package blankslate-python/1.3.5/blankslate_python-1.3.5.tar.gz

---
+++ packages/blankslate-python/blankslate_python-1.3.5.tar.gz/blankslate_python-1.3.5/source/blankslate/main.py
@@ -0,0 +1,32 @@
+"""main.py: Called when the package is run as a script."""
+
+from .generation.project_generation import generate_project
+from .interface.command_line import input_project_details
+from .logs.setup_logging import setup_logging
+
+main_logger = setup_logging()
+
+# TODO: fix badge generation in templates
+
+
+def main() -> None:
+    """
+    Main function for the application.
+
+    Notes
+    -----
+    This function is the entry point for the application.
+    """
+    try:
+        main_logger.info("Application started.")
+        project_details = input_project_details()
+        generate_project(
+            project_details
+        )  # TODO: add auto GitHub repo publish and other general first time user stuff
+    except KeyboardInterrupt:
+        print("\n")
+        main_logger.info("Exiting application due to user interrupt...")
+
+
+if __name__ == "__main__":
+    main()

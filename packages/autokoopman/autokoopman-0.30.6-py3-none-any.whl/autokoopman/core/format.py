afb31a9a180ea7d94ae3f1e70e57b38d615a65ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 05:04:57 +0000

    Add package autokoopman/0.30.6/autokoopman-0.30.6-py3-none-any.whl

---
+++ packages/autokoopman/autokoopman-0.30.6-py3-none-any.whl/autokoopman/core/format.py
@@ -0,0 +1,61 @@
+"""
+Utilties to format strings
+"""
+import sys
+import os
+import warnings
+
+from typing import Sequence, Optional
+
+
+def _clip_str(s: Optional[str], nlength=20) -> str:
+    """
+    if a string is too long, clip it to a fixed length and add ...
+    :param s: string
+    :param nlength: length to clip
+    :return: clipped string
+    """
+    if s is None:
+        return "None"
+    if len(s) <= nlength:
+        return s
+    else:
+        return f"{s[:nlength]}..."
+
+
+def _clip_list(s: Optional[Sequence], nlength=20) -> str:
+    """
+    if a list is too long as a string, clip it to a fixed length
+    :param s: string
+    :param nlength: length to clip
+    :return: clipped string
+    """
+    if s is None:
+        return "None"
+    return f"[{', '.join([str(si) for si in s])}]"
+
+
+class hide_prints:
+    """context manager to suppress python print output
+
+    this was created to hide "ugly stuff"
+    (e.g., GPyOpt prints)
+    """
+
+    def __enter__(self, hide_warnings=True):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, "w")
+        self.hw = hide_warnings
+
+        # TODO: it might make more sense to capture this
+        # as a log
+        if self.hw:
+            self.cw = warnings.catch_warnings()
+            self.cw.__enter__()
+            warnings.filterwarnings("ignore")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
+        if self.hw:
+            self.cw.__exit__()
---
+++ packages/autokoopman/autokoopman-0.30.6-py3-none-any.whl/test/unit_test/test_koopman.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pytest
+
+koop_config = ((True,), (False,))
+
+
+@pytest.mark.parametrize(
+    "normalize",
+    koop_config,
+)
+def test_discrete_koopman(normalize):
+    """tests the discrete time Koopman estimator"""
+    from autokoopman.benchmark.pendulum import PendulumWithInput
+    from autokoopman.core.trajectory import TrajectoriesData
+    from autokoopman.observable.observables import IdentityObservable
+    from autokoopman.observable.rff import RFFObservable
+    from autokoopman.estimator.koopman import KoopmanDiscEstimator
+    import random
+
+    pend = PendulumWithInput(l=0.6, beta=0.1)
+    n_trajs = 40
+    training_ivs = np.random.random((n_trajs, 2))
+    trajs = {}
+    teval = np.linspace(0.0, 5.0, 100)
+    for i in range(n_trajs):
+        inputs = (
+            random.random() * 2.0 * np.cos(random.random() * teval + random.random())
+        )
+        sol = pend.solve_ivp(training_ivs[i], teval=teval, inputs=inputs)
+        trajs[i] = sol
+    sols = TrajectoriesData(trajs)
+    sols = sols.interp_uniform_time(1 / 20.0)
+    disc = KoopmanDiscEstimator(
+        IdentityObservable() | RFFObservable(2, 100, 0.001),
+        0.1,
+        2,
+        100,
+        normalize=normalize,
+    )
+    disc.fit(sols)
+    new_inputs = inputs.copy()
+    new_inputs[-len(inputs) // 2 :] = -10.0
+    new_inputs[: len(inputs) // 2] = 0.0
+    preds = disc.model.solve_ivps(
+        training_ivs,
+        teval=teval,
+        inputs=[new_inputs for _ in range(len(training_ivs))],
+        sampling_period=1 / 20.0,
+    )
+    new_sols = pend.solve_ivps(
+        training_ivs,
+        teval=teval,
+        inputs=[new_inputs for _ in range(len(training_ivs))],
+        sampling_period=1 / 20.0,
+    )
+
+
+@pytest.mark.parametrize(
+    "normalize",
+    koop_config,
+)
+def test_cont_koopman(normalize):
+    """tests the continuous time Koopman estimator"""
+    from autokoopman.benchmark.pendulum import PendulumWithInput
+    from autokoopman.core.trajectory import TrajectoriesData
+    from autokoopman.observable.observables import IdentityObservable
+    from autokoopman.observable.rff import RFFObservable
+    from autokoopman.estimator.koopman import KoopmanContinuousEstimator
+    import random
+
+    pend = PendulumWithInput(l=0.6, beta=0.1)
+    n_trajs = 40
+    training_ivs = np.random.random((n_trajs, 2))
+    trajs = {}
+    teval = np.linspace(0.0, 5.0, 100)
+    for i in range(n_trajs):
+        inputs = (
+            random.random() * 2.0 * np.cos(random.random() * teval + random.random())
+        )
+        sol = pend.solve_ivp(training_ivs[i], teval=teval, inputs=inputs)
+        trajs[i] = sol
+    sols = TrajectoriesData(trajs)
+    sols = sols.interp_uniform_time(1 / 20.0)
+    cont = KoopmanContinuousEstimator(
+        IdentityObservable() | RFFObservable(2, 100, 0.001), 2, 100, normalize=normalize
+    )
+    cont.fit(sols)
+    new_inputs = inputs.copy()
+    new_inputs[-len(inputs) // 2 :] = -10.0
+    new_inputs[: len(inputs) // 2] = 0.0
+    preds = cont.model.solve_ivps(
+        training_ivs,
+        teval=teval,
+        inputs=[new_inputs for _ in range(len(training_ivs))],
+        sampling_period=1 / 20.0,
+    )
+    new_sols = pend.solve_ivps(
+        training_ivs,
+        teval=teval,
+        inputs=[new_inputs for _ in range(len(training_ivs))],
+        sampling_period=1 / 20.0,
+    )

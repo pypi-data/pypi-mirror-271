f856d445a97b4a8ec9f591b2d9d739cc8597410b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 04:06:39 +0000

    Add package django-oscar-bluelight/5.5.11/django_oscar_bluelight-5.5.11-py3-none-any.whl

---
+++ packages/django-oscar-bluelight/django_oscar_bluelight-5.5.11-py3-none-any.whl/oscarbluelight/dashboard/ranges/apps.py
@@ -0,0 +1,29 @@
+from django.urls import re_path
+from oscar.apps.dashboard.ranges import apps
+from oscar.core.loading import get_class
+
+
+class RangesDashboardConfig(apps.RangesDashboardConfig):
+    name = "oscarbluelight.dashboard.ranges"
+
+    def ready(self):
+        super().ready()
+        self.list_view = get_class("ranges_dashboard.views", "RangeListView")
+        self.products_view = get_class("ranges_dashboard.views", "RangeProductListView")
+
+    def get_urls(self):
+        price_list_view = get_class("ranges_dashboard.views", "RangePriceListView")
+        excluded_products_view = get_class(
+            "ranges_dashboard.views", "RangeExcludedProductsView"
+        )
+        urlpatterns = [
+            re_path(
+                r"^(?P<pk>\d+)/prices/$", price_list_view.as_view(), name="range-prices"
+            ),
+            re_path(
+                r"^(?P<pk>\d+)/excluded-products/$",
+                excluded_products_view.as_view(),
+                name="range-excluded-products",
+            ),
+        ]
+        return super().get_urls() + self.post_process_urls(urlpatterns)

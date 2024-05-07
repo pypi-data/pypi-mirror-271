fb55564aab98b87a8b390c67babf1de6cd54b342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 05:04:50 +0000

    Add package w2rpy/0.1.6/w2rpy-0.1.6.tar.gz

---
+++ packages/w2rpy/w2rpy-0.1.6.tar.gz/w2rpy-0.1.6/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  1 11:58:32 2024
+
+@author: lrussell
+"""
+
+from distutils.core import setup
+
+setup(
+  name = 'w2rpy',    
+  packages=['w2rpy'],     
+  py_modules=['w2rpy'],
+  version = '0.1.6',     
+  license='MIT',        
+  description = 'Water Resource Functions For Fluvial Systems',   
+  author = 'Luke Russell',                   
+  author_email = 'lrussell@wolfwaterresources.com',      
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   
+  install_requires=['pandas',
+                    'numpy',
+                    'shapely',
+                    'geopandas',
+                    'rasterio',
+                    'pysheds',
+                    'scipy'],
+  )

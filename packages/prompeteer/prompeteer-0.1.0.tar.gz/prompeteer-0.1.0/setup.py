30ab6855efe549007c017d0c978e0b340921836c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 04:04:40 +0000

    Add package prompeteer/0.1.0/prompeteer-0.1.0.tar.gz

---
+++ packages/prompeteer/prompeteer-0.1.0.tar.gz/prompeteer-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='prompeteer',
+    version='0.1.0',
+    author='Yoaz Menda',
+    author_email='yoazmenda@gmail.com',
+    description='Prompt Development and evaluation tool',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+

e28af67470ebce017db9abe72cfc7f224a7f00d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 04:05:54 +0000

    Add package tasksflow/0.3.0/tasksflow-0.3.0.tar.gz

---
+++ packages/tasksflow/tasksflow-0.3.0.tar.gz/tasksflow-0.3.0/src/tasksflow/task.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+from .cache import CacheProvider
+from .common import Payload
+
+
+def _is_payload_valid(payload: Payload) -> bool:
+    if payload is None:
+        return False
+    if not isinstance(payload, dict):
+        return False
+    if not all(isinstance(k, str) for k in payload.keys()):
+        return False
+    return True
+
+
+class Task(ABC):
+    def __init__(self, enable_cache: bool = True):
+        """
+        Task is the base class for all tasks
+
+        :param enable_cache: whether to enable cache for the task
+        """
+        self.enable_cache = enable_cache
+        self.cache_provider: Optional[CacheProvider] = None
+
+    @abstractmethod
+    def run(self, *args, **kwargs) -> Optional[Payload]:
+        """
+        user-defined task logic, should return a dict[str, Any] or None
+        """
+        raise NotImplementedError
+
+    def _execute(self, *args, **kwargs) -> Payload:
+        """
+        execute the task
+        """
+        result = self.run(*args, **kwargs)
+        if result is None:
+            result = {}
+        else:
+            if not _is_payload_valid(result):
+                raise ValueError(
+                    f"Task result must be a dict[str, Any] or None, but get {result} for task {self}"
+                )
+        return result

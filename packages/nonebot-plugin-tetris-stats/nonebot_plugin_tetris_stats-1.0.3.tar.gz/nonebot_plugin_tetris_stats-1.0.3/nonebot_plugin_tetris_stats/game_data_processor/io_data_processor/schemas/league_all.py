f63dbe6d3d269e0f122256e805fef7549d632c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 14:05:49 +0000

    Add package nonebot-plugin-tetris-stats/1.0.3/nonebot_plugin_tetris_stats-1.0.3.tar.gz

---
+++ packages/nonebot-plugin-tetris-stats/nonebot_plugin_tetris_stats-1.0.3.tar.gz/nonebot_plugin_tetris_stats-1.0.3/nonebot_plugin_tetris_stats/game_data_processor/io_data_processor/schemas/league_all.py
@@ -0,0 +1,66 @@
+from nonebot.compat import PYDANTIC_V2
+from pydantic import BaseModel, ConfigDict, Field
+
+from ..typing import Rank
+from .base import FailedModel
+from .base import SuccessModel as BaseSuccessModel
+
+
+class _User(BaseModel):
+    id: str = Field(..., alias='_id')
+    username: str
+    role: str
+    xp: float
+    supporter: bool
+    verified: bool
+    country: str | None = None
+    if PYDANTIC_V2:
+        model_config = ConfigDict(populate_by_name=True)
+    else:
+
+        class Config:
+            allow_population_by_field_name = True
+
+
+class SuccessModel(BaseSuccessModel):
+    class Data(BaseModel):
+        class ValidUser(_User):
+            class League(BaseModel):
+                gamesplayed: int
+                gameswon: int
+                rating: float
+                glicko: float
+                rd: float
+                rank: Rank
+                bestrank: Rank
+                apm: float
+                pps: float
+                vs: float
+                decaying: bool
+
+            league: League
+
+        class InvalidUser(_User):
+            class League(BaseModel):
+                gamesplayed: int
+                gameswon: int
+                rating: float
+                glicko: float | None = None
+                rd: float | None = None
+                rank: Rank
+                bestrank: Rank
+                apm: float | None = None
+                pps: float | None = None
+                vs: float | None = None
+                decaying: bool
+
+            league: League
+
+        users: list[ValidUser | InvalidUser]
+
+    data: Data
+
+
+LeagueAll = SuccessModel | FailedModel
+ValidUser = SuccessModel.Data.ValidUser
+InvalidUser = SuccessModel.Data.InvalidUser

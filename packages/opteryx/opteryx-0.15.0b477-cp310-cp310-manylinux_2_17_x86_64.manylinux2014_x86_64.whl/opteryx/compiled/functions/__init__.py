995bf1f578c3b63933fe64d2a62e340ee8615e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 03:05:17 +0000

    Add package opteryx/0.15.0b477/opteryx-0.15.0b477-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/opteryx/opteryx-0.15.0b477-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/opteryx/compiled/functions/__init__.py
@@ -0,0 +1,6 @@
+from .cython_functions import numpy_array_get_element
+from .ip_address import ip_in_cidr
+from .vectors import possible_match
+from .vectors import possible_match_indices
+from .vectors import tokenize_and_remove_punctuation
+from .vectors import vectorize

37d3bd7e96df48b6761d3d778a0757b217588203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 03:05:11 +0000

    Add package marvel-logging/0.0.1/marvel_logging-0.0.1.tar.gz

---
+++ packages/marvel-logging/marvel_logging-0.0.1.tar.gz/marvel_logging-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+setuptools.setup(
+	name='marvel_logging',
+	version='0.0.1',
+	author='',
+	author_email='',
+	description='',
+	url='https://github.azc.ext.hp.com/co-mlops-platform/marvel',
+	license='HP Internal Use Only',
+    include_package_data=True
+)

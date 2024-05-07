41122aace08c9e41496c0d51de69383f006ff21e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 03:05:28 +0000

    Add package sqlalchemy-multilingual/0.0.10/sqlalchemy_multilingual-0.0.10.tar.gz

---
+++ packages/sqlalchemy-multilingual/sqlalchemy_multilingual-0.0.10.tar.gz/sqlalchemy_multilingual-0.0.10/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    packages=find_packages(
+        where=[
+            "src"
+        ],
+        exclude=[".venv"]
+    ),
+    package_dir={"": "src"}
+)

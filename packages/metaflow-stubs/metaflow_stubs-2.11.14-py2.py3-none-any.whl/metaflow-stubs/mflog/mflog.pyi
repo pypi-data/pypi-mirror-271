7946fde97199c0111a48e57358d915b6dde73b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 14:04:55 +0000

    Add package metaflow-stubs/2.11.14/metaflow_stubs-2.11.14-py2.py3-none-any.whl

---
+++ packages/metaflow-stubs/metaflow_stubs-2.11.14-py2.py3-none-any.whl/metaflow-stubs/mflog/mflog.pyi
@@ -0,0 +1,22 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.11.14                                                            #
+# Generated on 2024-05-06T23:56:19.403971                                        #
+##################################################################################
+
+from __future__ import annotations
+
+import typing
+if typing.TYPE_CHECKING:
+    import datetime
+
+VERSION: bytes
+
+RE: bytes
+
+ISOFORMAT: str
+
+MISSING_TIMESTAMP: datetime.datetime
+
+MISSING_TIMESTAMP_STR: str
+
---
+++ packages/metaflow-stubs/metaflow_stubs-2.11.14-py2.py3-none-any.whl/metaflow-stubs/plugins/pypi/__init__.pyi
@@ -0,0 +1,18 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.11.14                                                            #
+# Generated on 2024-05-06T23:56:19.387088                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+class MetaflowException(Exception, metaclass=type):
+    def __init__(self, msg = "", lineno = None):
+        ...
+    def __str__(self):
+        ...
+    ...
+
+MAGIC_FILE: str
+

03be7cbd6531af25dcc4a821fce50face3dc9d42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 06:04:49 +0000

    Add package lasertools-pulsedispersiondata/0.0.1/lasertools_pulsedispersiondata-0.0.1.tar.gz

---
+++ packages/lasertools-pulsedispersiondata/lasertools_pulsedispersiondata-0.0.1.tar.gz/lasertools_pulsedispersiondata-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="lasertools_pulsedispersiondata",
+    version="0.0.1",
+    description="A module to store data for laser pulse dispersion",
+    license="GPLv3",
+    long_description=long_description,
+    author="brittonm",
+    author_email="68578865+brittonm@users.noreply.github.com",
+    url="https://github.com/lasertools/lasertools_pulsedispersiondata",
+    download_url = 'https://github.com/lasertools/lasertools_pulsedispersiondata/archive/refs/tags/v_0_0_1.tar.gz',
+    install_requires=[
+        "numpy",
+    ],
+    classifiers=[
+      'Programming Language :: Python :: 3.9',
+    ],
+    package_data={
+        "pulsedispersiondatadev": [
+            "*/*.json"
+        ],
+    },
+)

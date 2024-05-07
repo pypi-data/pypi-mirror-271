9c6022523183bd77144e6508a56a4adfc0b7e6a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 04:06:21 +0000

    Add package olist-loafer/6.0.0a3/olist_loafer-6.0.0a3.tar.gz

---
+++ packages/olist-loafer/olist_loafer-6.0.0a3.tar.gz/olist_loafer-6.0.0a3/src/loafer/managers.py
@@ -0,0 +1,58 @@
+import asyncio
+import logging
+import os
+
+from .dispatchers import LoaferDispatcher
+from .runners import LoaferRunner
+
+logger = logging.getLogger(__name__)
+
+
+class LoaferManager:
+    def __init__(self, routes, runner=None, max_concurrency=None):
+        if runner is None:
+            self.runner = LoaferRunner(on_stop_callback=self.on_loop__stop)
+        else:
+            self.runner = runner
+
+        self.routes = routes
+        self.dispatcher = LoaferDispatcher(self.routes, max_concurrency)
+
+    def run(self, forever=True, debug=False):  # noqa: FBT002
+        loop = self.runner.loop
+        self._future = asyncio.ensure_future(
+            self.dispatcher.dispatch_providers(forever=forever),
+            loop=loop,
+        )
+
+        self._future.add_done_callback(self.on_future__errors)
+        if not forever:
+            self._future.add_done_callback(self.runner.prepare_stop)
+
+        start = "starting loafer, pid={}, forever={}"
+        logger.info(start.format(os.getpid(), forever))
+        self.runner.start(debug=debug)
+
+    #
+    # Callbacks
+    #
+
+    def on_future__errors(self, future):
+        if future.cancelled():
+            return self.runner.prepare_stop()
+
+        exc = future.exception()
+        # Unhandled errors crashes the event loop execution
+        if isinstance(exc, BaseException):
+            logger.critical("fatal error caught: %r", exc)
+            self.runner.prepare_stop()
+            return None
+        return None
+
+    def on_loop__stop(self):
+        logger.info("cancel dispatcher operations ...")
+
+        if hasattr(self, "_future"):
+            self._future.cancel()
+
+        self.dispatcher.stop()

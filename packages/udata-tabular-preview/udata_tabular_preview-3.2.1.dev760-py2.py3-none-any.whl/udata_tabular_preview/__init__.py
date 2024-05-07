99e20de21082728343a4be162d74ff7c124e9421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 03:05:42 +0000

    Add package udata-tabular-preview/3.2.1.dev760/udata_tabular_preview-3.2.1.dev760-py2.py3-none-any.whl

---
+++ packages/udata-tabular-preview/udata_tabular_preview-3.2.1.dev760-py2.py3-none-any.whl/udata_tabular_preview/__init__.py
@@ -0,0 +1,8 @@
+'''
+Tabular Preview
+
+A plugin to preview tabular files (CSV, Excel, ...)
+'''
+
+__version__ = '3.2.1.dev'
+__description__ = 'A plugin to preview tabular files (CSV, Excel, ...) using tabular api'

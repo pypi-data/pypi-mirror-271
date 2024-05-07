62f0ebb79bb8db19a83155dbe58fcf270c4a98fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 05:04:29 +0000

    Add package halper/1.0.3/halper-1.0.3.tar.gz

---
+++ packages/halper/halper-1.0.3.tar.gz/halper-1.0.3/src/halper/constants.py
@@ -0,0 +1,48 @@
+"""Constants for Halp."""
+
+from enum import Enum
+from pathlib import Path
+
+import typer
+from peewee import SqliteDatabase
+
+
+class CommentPlacement(str, Enum):
+    """Comment placement for halp."""
+
+    ABOVE = "above"
+    INLINE = "inline"
+    BEST = "best"
+
+
+class CommandType(str, Enum):
+    """Command types for halp."""
+
+    ALIAS = "alias"
+    FUNCTION = "function"
+    SCRIPT = "script"
+    EXPORT = "export"
+
+
+class ShellType(str, Enum):
+    """Shell types for halp."""
+
+    BASH = "bash"
+    ZSH = "zsh"
+    FISH = "fish"
+    POWERSHELL = "powershell"
+    SH = "sh"
+
+
+class SearchType(str, Enum):
+    """Search types used for the search command."""
+
+    CODE = "code"
+    NAME = "name"
+
+
+APP_DIR = Path(typer.get_app_dir("halp"))
+CONFIG_PATH = APP_DIR / "config.toml"
+DB_PATH = APP_DIR / "halp.sqlite"
+DB = SqliteDatabase(DB_PATH)
+VERSION = "0.1.0"

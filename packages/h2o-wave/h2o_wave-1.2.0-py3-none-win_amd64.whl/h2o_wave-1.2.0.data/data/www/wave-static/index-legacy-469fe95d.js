03bdc18a3496c0c75c194d8046d2a80ae6434274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 03:05:32 +0000

    Add package h2o-wave/1.2.0/h2o_wave-1.2.0-py3-none-win_amd64.whl

---
+++ packages/h2o-wave/h2o_wave-1.2.0-py3-none-win_amd64.whl/h2o_wave-1.2.0.data/data/www/wave-static/index-legacy-469fe95d.js
@@ -0,0 +1,1 @@
+System.register(["./LocalizationProvider-legacy-084a4758.js","./index-legacy-c67d628b.js","./useThemeProps-legacy-87fe924b.js"],(function(e,t){"use strict";return{setters:[function(t){e({LocalizationProvider:t.L,MuiPickersAdapterContext:t.M})},null,null],execute:function(){}}}));

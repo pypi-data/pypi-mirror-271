04210102e4021f4791041d7af3286a0004bd7c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 05:05:01 +0000

    Add package globalnoc-alertmon-agent/1.0.3/globalnoc-alertmon-agent-1.0.3.tar.gz

---
+++ packages/globalnoc-alertmon-agent/globalnoc-alertmon-agent-1.0.3.tar.gz/globalnoc-alertmon-agent-1.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+  name="globalnoc-alertmon-agent",
+  version="1.0.3",
+  author="GlobalNOC Systems Engineering",
+  author_email="syseng@globalnoc.iu.edu",
+  python_requires='>=3.8.11, <4.0.0',
+  description="Facilitates the process of submitting alerts to the GlobalNOC AlertMon system.",
+  url="https://github.com/GlobalNOC/globalnoc-alertmon-agent",
+  license="Apache License, Version 2.0 ",
+  classifiers=[
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 3"
+  ],
+  install_requires=[
+    "globalnoc_wsc==1.0.2",
+    "pyyaml==6.0.1",
+  ]
+)

22aa154305cf88a3f8e0bd14ce498da2b35e8448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 04:06:39 +0000

    Add package extradecorators/0.4/extradecorators-0.4.tar.gz

---
+++ packages/extradecorators/extradecorators-0.4.tar.gz/extradecorators-0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='extradecorators',
+    version='0.4',
+    packages=find_packages(),
+    description='A package that contains some useful decorators for any case.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='RandomTimeTV',
+    author_email='dergepanzerte1@gmail.com',
+    license='MIT with required credit to the author.',
+    url='',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='Decorators',
+    install_requires=[],
+)

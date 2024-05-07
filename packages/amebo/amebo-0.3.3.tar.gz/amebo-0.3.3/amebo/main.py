fb029ee4f3bc2b23b1fdabcff44cf15c24d3000d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 03:05:16 +0000

    Add package amebo/0.3.3/amebo-0.3.3.tar.gz

---
+++ packages/amebo/amebo-0.3.3.tar.gz/amebo-0.3.3/amebo/main.py
@@ -0,0 +1,11 @@
+from subprocess import call
+from sys import argv
+
+
+def execute():
+    options = argv[1:]
+    call(['sh', 'amebo/amebo.sh', *options])
+
+
+if __name__ == '__main__':
+    execute()

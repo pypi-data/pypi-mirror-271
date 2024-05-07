5b530d18c7dc7a9f827bdc94737c3eb8289c1ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 06:04:46 +0000

    Add package indico_toolkit/6.1.0/indico_toolkit-6.1.0-py2.py3-none-any.whl

---
+++ packages/indico_toolkit/indico_toolkit-6.1.0-py2.py3-none-any.whl/indico_toolkit/auto_review/__init__.py
@@ -0,0 +1,1 @@
+from .auto_reviewer import AutoReviewer, AutoReviewFunction
---
+++ packages/indico_toolkit/indico_toolkit-6.1.0-py2.py3-none-any.whl/indico_toolkit/client.py
@@ -0,0 +1,31 @@
+from indico import IndicoClient, IndicoConfig
+from indico.errors import IndicoAuthenticationFailed, IndicoRequestError
+from indico_toolkit.errors import ToolkitAuthError
+from indico_toolkit.retry import retry
+
+
+@retry((IndicoRequestError, ConnectionError))
+def create_client(
+    host: str,
+    api_token_path: str = None,
+    api_token_string: str = None,
+    verify_ssl: bool = True,
+    **kwargs,
+) -> IndicoClient:
+    """
+    Instantiate your Indico API client. 
+    Specify either the path to your token or the token string itself.
+    """
+    config = IndicoConfig(
+        host=host,
+        api_token_path=api_token_path,
+        api_token=api_token_string,
+        verify_ssl=verify_ssl,
+        **kwargs,
+    )
+    try:
+        return IndicoClient(config)
+    except IndicoAuthenticationFailed as e:
+        raise ToolkitAuthError(
+            f"{e}\n\n Ensure that you are using your most recently downloaded token with the correct host URL"
+        )

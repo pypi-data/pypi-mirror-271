80ec1e04f1dde03234d7628e28373b631ad661b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:04:54 +0000

    Add package devinci-pyutils/0.1/devinci_pyutils-0.1.tar.gz

---
+++ packages/devinci-pyutils/devinci_pyutils-0.1.tar.gz/devinci_pyutils-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='devinci-pyutils',
+    version='0.1',
+    author='devinci-it',
+    description='Collection of Python utility modules for developers.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://gist.github.com/devinci-it/d432708e796d6e6160efe13ee6cc7bbc/',
+    license='MIT',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    setup_requires=['setuptools', 'wheel'],
+
+)

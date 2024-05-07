7ba0d5a637f9d95f0b4e29f3f6a7d9eaf60495bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 14:04:52 +0000

    Add package nbproject/0.10.2/nbproject-0.10.2-py2.py3-none-any.whl

---
+++ packages/nbproject/nbproject-0.10.2-py2.py3-none-any.whl/nbproject/__init__.py
@@ -0,0 +1,31 @@
+"""nbproject: Manage Jupyter notebooks.
+
+.. currentmodule:: nbproject
+
+Most users will only need these two functions:
+
+.. autosummary::
+   :toctree:
+
+   header
+   publish
+
+Use them with default arguments after importing them like this::
+
+   from nbproject import header, publish
+
+For more fine-grained access, use:
+
+.. autosummary::
+   :toctree:
+
+   meta
+   dev
+
+"""
+__version__ = "0.10.2"
+
+from . import dev
+from ._header import header
+from ._meta import meta
+from ._publish import publish

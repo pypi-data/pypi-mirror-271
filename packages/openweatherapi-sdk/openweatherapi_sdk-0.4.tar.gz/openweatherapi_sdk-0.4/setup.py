d8cfbcf461367a1af26c97878e4bb43763d49955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 04:05:25 +0000

    Add package openweatherapi-sdk/0.4/openweatherapi_sdk-0.4.tar.gz

---
+++ packages/openweatherapi-sdk/openweatherapi_sdk-0.4.tar.gz/openweatherapi_sdk-0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="openweatherapi-sdk",
+    version="0.4",
+    packages=find_packages('src'),
+    package_data={'weatherapi-sdk': ['*.py']},  
+    install_requires=["requests"],
+    author="Snehal Palve",
+    description="A simple SDK for interacting with the OpenWeatherMap API",
+    long_description=long_description,
+    long_description_content_type= "text/markdown",
+    classifiers =[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent"],
+    package_dir = {'':'src'},
+    python_requires = ">=3.6",
+)

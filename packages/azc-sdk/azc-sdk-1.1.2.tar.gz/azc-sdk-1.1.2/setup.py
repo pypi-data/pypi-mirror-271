403cddf3720f5961a91085d685a8795a97c4d925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 03:05:04 +0000

    Add package azc-sdk/1.1.2/azc-sdk-1.1.2.tar.gz

---
+++ packages/azc-sdk/azc-sdk-1.1.2.tar.gz/azc-sdk-1.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", mode="r", encoding="utf-8") as f:
+    readme = f.read()
+
+VERSION = "1.1.2"
+
+setup(
+    name="azc-sdk",
+    version=VERSION[:],
+    description="AZC SDK for Python",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="ai",
+    author_email="azc@tencent.com",
+    url="https://git.code.tencent.com/open_sdk/azc-sdk-python",
+    packages=find_packages(),
+    install_requires=["requests"],
+    python_requires=">=3.7",
+    keywords=["AZC", "SDK"],
+    include_package_data=True,
+    project_urls={
+        "Source": "https://git.code.tencent.com/open_sdk/azc-sdk-python",
+    },
+)

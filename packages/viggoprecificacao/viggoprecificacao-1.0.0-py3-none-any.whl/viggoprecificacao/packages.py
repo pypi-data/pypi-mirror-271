7872261ee0eeb3b41e88caa84a539ba46677fa6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 05:05:27 +0000

    Add package viggoprecificacao/1.0.0/viggoprecificacao-1.0.0-py3-none-any.whl

---
+++ packages/viggoprecificacao/viggoprecificacao-1.0.0-py3-none-any.whl/viggoprecificacao/packages.py
@@ -0,0 +1,10 @@
+from viggoprecificacao.subsystem.parametrizacao import (
+    produto_viggo,
+    servico_viggo,
+    termo_aceite_viggo)
+
+packages = [
+    produto_viggo.subsystem,
+    servico_viggo.subsystem,
+    termo_aceite_viggo.subsystem
+]

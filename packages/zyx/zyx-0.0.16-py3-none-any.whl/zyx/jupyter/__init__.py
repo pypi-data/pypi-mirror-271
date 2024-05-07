bb9b332727ef1580d2a0bf32ba662f7ec388d0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 03:05:26 +0000

    Add package zyx/0.0.16/zyx-0.0.16-py3-none-any.whl

---
+++ packages/zyx/zyx-0.0.16-py3-none-any.whl/zyx/jupyter/__init__.py
@@ -0,0 +1,1 @@
+from .tailwind import Tailwind
---
+++ packages/zyx/zyx-0.0.16-py3-none-any.whl/zyx/text/__init__.py
@@ -0,0 +1,4 @@
+from .__chunker__ import chunk_text
+from .__read_doc__ import read_file
+from ollama import embeddings as embed
+from litellm import completion as generate

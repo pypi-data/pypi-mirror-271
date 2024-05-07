3cd849f2b291a7cd3c544c85511cd4df2c5acc35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 04:04:40 +0000

    Add package remotemanager/0.11.10/remotemanager-0.11.10.tar.gz

---
+++ packages/remotemanager/remotemanager-0.11.10.tar.gz/remotemanager-0.11.10/remotemanager/connection/computers/__init__.py
@@ -0,0 +1,12 @@
+from remotemanager.connection.computers.utils import format_time
+from remotemanager.connection.computers.base import BaseComputer
+from remotemanager.connection.computers.resource import Resource, Resources
+from remotemanager.connection.computers.substitution import Substitution
+
+__all__ = [
+    "format_time",
+    "BaseComputer",
+    "Resource",
+    "Resources",
+    "Substitution",
+]

00e37dd8f421843468b6bd685b73a202e58b62cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 04:06:41 +0000

    Add package smscallbomber/1.1/SMSCallBomber-1.1-py3-none-any.whl

---
+++ packages/smscallbomber/SMSCallBomber-1.1-py3-none-any.whl/SMSСallBomber/config.py
@@ -0,0 +1,32 @@
+# set default value of default_country_code
+# only with numbers (without + sign)
+# Example:
+# default_country_code = 7
+default_country_code = None
+
+# Random data for request
+emails = [
+    "@gmail.com",
+    "@mail.com",
+    "@yandex.ru"
+]
+names = [
+    "Gabriel",
+    "Hamilton",
+    "Harrison",
+    "Jamison",
+    "Jonathan",
+    "Julian",
+    "Kingsley",
+    "Laurent",
+    "Lawrence",
+    "Leopold",
+    "Malachi",
+    "Maximus",
+    "Napoleon",
+    "Nathaniel",
+    "Nikolai",
+    "Prescott",
+    "Quentin",
+    "Sebastian"
+]

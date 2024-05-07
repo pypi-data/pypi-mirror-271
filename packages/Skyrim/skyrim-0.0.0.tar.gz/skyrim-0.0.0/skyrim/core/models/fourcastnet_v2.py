40c48a4216cc40f6749358869ec04b77b0837092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 06:04:33 +0000

    Add package Skyrim/0.0.0/skyrim-0.0.0.tar.gz

---
+++ packages/Skyrim/skyrim-0.0.0.tar.gz/skyrim-0.0.0/skyrim/core/models/fourcastnet_v2.py
@@ -0,0 +1,60 @@
+import datetime
+import xarray as xr
+import earth2mip.networks.fcnv2_sm as fcnv2_sm
+from pathlib import Path
+from earth2mip import registry, schema
+from .base import GlobalModel, GlobalPrediction
+from .utils import run_basic_inference
+
+
+# fmt: off
+    # https://github.com/NVIDIA/earth2mip/blob/86b11fe4ba2f19641802112e8b0ba6b962123130/earth2mip/networks/fcnv2_sm.py#L37-L111
+CHANNELS = [ "u10m", "v10m", "u100m", "v100m", "t2m", "sp", "msl", "tcwv", "u50", "u100",
+            "u150","u200", "u250", "u300", "u400", "u500", "u600", "u700", "u850", "u925",
+            "u1000", "v50", "v100", "v150", "v200", "v250", "v300", "v400", "v500", "v600",
+            "v700", "v850", "v925", "v1000", "z50", "z100", "z150", "z200", "z250", "z300",
+            "z400", "z500", "z600", "z700", "z850", "z925", "z1000", "t50", "t100", "t150",
+            "t200", "t250", "t300", "t400", "t500", "t600", "t700", "t850", "t925", "t1000",
+            "r50", "r100", "r150", "r200", "r250", "r300", "r400", "r500", "r600", "r700",
+            "r850", "r925","r1000",
+    ]
+# fmt: on
+class FourcastnetV2Model(GlobalModel):
+    model_name = "fourcastnet_v2"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self.model_name, *args, **kwargs)
+
+    def build_model(self):
+        return fcnv2_sm.load(registry.get_model("e2mip://fcnv2_sm"))
+
+    @property
+    def time_step(self):
+        return self.model.time_step
+
+    @property
+    def in_channel_names(self):
+        return self.model.in_channel_names
+
+    @property
+    def out_channel_names(self):
+        return self.model.out_channel_names
+
+    def predict_one_step(
+        self,
+        start_time: datetime.datetime,
+        initial_condition: str | Path | None = None,
+    ) -> xr.DataArray | xr.Dataset:
+        return run_basic_inference(
+            model=self.model,
+            n=1,
+            data_source=self.data_source,
+            time=start_time,
+            x=initial_condition,
+        )
+
+
+class FourcastnetV2Prediction(GlobalPrediction):
+    def __init__(self, source):
+        super().__init__(source)
+        self.model_name = "fourcastnet_v2"
---
+++ packages/Skyrim/skyrim-0.0.0.tar.gz/skyrim-0.0.0/skyrim/forecast.py
@@ -0,0 +1,146 @@
+import click
+import subprocess
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+from pathlib import Path
+from skyrim.common import AVAILABLE_MODELS
+
+yesterday = (datetime.now() - timedelta(days=1)).date().isoformat().replace("-", "")
+
+load_dotenv()
+
+
+# NOTE:
+# ERA5 variables are mean values for previous hour,
+# i.e. 13:01 to 14:00 are labelled as "14:00"
+
+
+def run_forecast(
+    model_name: str,
+    date: str,
+    time: str,
+    lead_time: int,
+    list_models: bool,
+    initial_conditions: str,
+    output_dir: str,
+    filter_vars: str,
+):
+    # for the local inference setup you need core packages imported here:
+    from skyrim.core import Skyrim
+    from skyrim.core.utils import ensure_cds_loaded
+
+    if list_models:
+        available_models = Skyrim.list_available_models()
+        print("Available models:", available_models)
+        exit()
+    # initialize the model
+    ensure_cds_loaded()
+    model = Skyrim(model_name, ic_source=initial_conditions)
+    pred, output_paths = model.predict(
+        date=date,
+        time=time,
+        lead_time=lead_time,
+        save=True,
+        save_config={
+            "output_dir": output_dir or str(Path.cwd() / "outputs"),
+            "filter_vars": (
+                filter_vars.split(",") if bool(filter_vars) else []
+            ),  # TODO: sanitize vars
+        },
+    )
+    return output_paths
+
+
+@click.command()
+@click.option(
+    "--model_name",
+    "-m",
+    type=click.Choice(AVAILABLE_MODELS, case_sensitive=False),
+    default="pangu",
+    help="Select model",
+)
+@click.option("--date", "-d", type=str, default=yesterday, help="YYYYMMDD")
+@click.option("--time", "-t", type=str, default="0000", help="HHMM")
+@click.option(
+    "--lead_time", "-l", type=int, default=6, help="Lead time in hours, int 0-24"
+)
+@click.option(
+    "--list_models", "-lm", is_flag=True, help="List all available models and exit"
+)
+@click.option(
+    "--initial_conditions",
+    "-ic",
+    type=click.Choice(["cds", "ifs", "gfs"], case_sensitive=False),
+    default="gfs",
+    help="Initial conditions provider.",
+)
+@click.option(
+    "--output_dir",
+    "-o",
+    type=str,
+    default="",
+    help="Output directory, can be local or s3 path (e.g. s3://my-path/)",
+)
+@click.option(
+    "--filter_vars",
+    "-f",
+    type=str,
+    default="",
+    help="Filter variables such as t2m (temperature) before saving forecasts.",
+)
+@click.option(
+    "--backend",
+    "-b",
+    type=click.Choice(["local", "modal"], case_sensitive=False),
+    default="local",
+    help="Where to run the forecasts.",
+)
+def main(
+    model_name: str,
+    date: str,
+    time: str,
+    lead_time: int,
+    list_models: bool,
+    initial_conditions: str,
+    output_dir: str,
+    filter_vars: str,
+    backend: str,
+):
+    if backend == "modal":
+        return subprocess.run(
+            [
+                "modal",
+                "run",
+                "remote/forecast.py",
+                "--model-name",
+                model_name,
+                "--date",
+                date,
+                "--time",
+                time,
+                "--lead-time",
+                str(lead_time),
+                "--initial-conditions",
+                initial_conditions,
+                "--output-dir",
+                output_dir,
+                "--filter-vars",
+                filter_vars,
+            ],
+            check=True,
+        )
+
+    return run_forecast(
+        model_name,
+        date,
+        time,
+        lead_time,
+        list_models,
+        initial_conditions,
+        output_dir,
+        filter_vars,
+    )
+
+
+if __name__ == "__main__":
+    main()

06656017db931a302284d642ce85e4fe91979bf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:04:47 +0000

    Add package bugprint/0.3.3/bugprint-0.3.3-py3-none-any.whl

---
+++ packages/bugprint/bugprint-0.3.3-py3-none-any.whl/bugprint/funcs.py
@@ -0,0 +1,22 @@
+import os
+import inspect
+
+v = False
+
+def bp_setup(verbose=False):
+    global v
+    v = verbose
+
+def bp(message=""):
+    caller_frame = inspect.currentframe().f_back
+    caller_info = inspect.getframeinfo(caller_frame)
+    relative_filename = os.path.relpath(caller_info.filename)
+    resp = f"DEBUG: {relative_filename}:{caller_info.lineno} "
+
+    if message:
+            resp = f"{resp}{message} "
+
+    if v:
+        resp = resp + "-" * (80 - len(relative_filename) - len(str(caller_info.lineno)) - len("DEBUG: "))
+
+    print(resp)

d36af30e53101fbea705e815b225863037e4a805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 14:04:39 +0000

    Add package jupyterlab-theme-KULeuven/0.1.5/jupyterlab_theme_kuleuven-0.1.5.tar.gz

---
+++ packages/jupyterlab-theme-KULeuven/jupyterlab_theme_kuleuven-0.1.5.tar.gz/jupyterlab_theme_kuleuven-0.1.5/.github/README.md
@@ -0,0 +1,25 @@
+# jupyterlab-theme-KULeuven
+
+
+A jupyterlab theme for KULeuven.
+It provides also styles jupyterlab-deck slides, but it should also work without jupyterlab-deck.
+
+## Requirements
+
+- JupyterLab >= 4.0
+
+## Install
+
+To install the extension, execute:
+
+```bash
+pip install jupyterlab-theme-KULeuven
+```
+
+## Uninstall
+
+To remove the extension, execute:
+
+```bash
+pip uninstall jupyterlab-theme-KULeuven
+```
---
+++ packages/jupyterlab-theme-KULeuven/jupyterlab_theme_kuleuven-0.1.5.tar.gz/jupyterlab_theme_kuleuven-0.1.5/src/index.ts
@@ -0,0 +1,40 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the jupyterlab-theme-KULeuven extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-theme-KULeuven',
+  autoStart: true,
+  requires: [IThemeManager],
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    console.log('JupyterLab extension jupyterlab-theme-KULeuven is activated!');
+    const style = 'jupyterlab-theme-KULeuven/index.css';
+
+    manager.register({
+      name: 'jupyterlab-theme-KULeuven',
+      isLight: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  }
+};
+
+
+/**
+ * Initialization of a simple metadata form plugin.
+ */
+const slides: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-theme-KULeuven:slides',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('jupyterlab-theme-KULeuven:slides metadata-form example activated');
+  }
+};
+
+export default [slides,extension];

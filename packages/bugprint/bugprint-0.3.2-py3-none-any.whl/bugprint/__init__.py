27d5ee991fb3964bf78c0e7f43ab15698d1451c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 15:04:43 +0000

    Add package bugprint/0.3.2/bugprint-0.3.2-py3-none-any.whl

---
+++ packages/bugprint/bugprint-0.3.2-py3-none-any.whl/bugprint/__init__.py
@@ -0,0 +1,1 @@
+from .funcs import bp, bp_setup
---
+++ packages/bugprint/bugprint-0.3.2-py3-none-any.whl/bugprint/example_usage.py
@@ -0,0 +1,14 @@
+def example_function():
+    print("Inside example_function")
+    bp()
+
+def another_function():
+    print("Inside another_function")
+    bp()
+
+# Call bp() at specific lines in your code
+bp()  # Example 1
+example_function()
+bp()  # Example 2
+another_function()
+bp()  # Example 3

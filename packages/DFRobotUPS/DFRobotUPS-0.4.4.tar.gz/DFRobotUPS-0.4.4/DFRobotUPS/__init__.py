2cd8b0d4d7dbd93cded4eb96d16524e197d07021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 03:05:08 +0000

    Add package DFRobotUPS/0.4.4/DFRobotUPS-0.4.4.tar.gz

---
+++ packages/DFRobotUPS/DFRobotUPS-0.4.4.tar.gz/DFRobotUPS-0.4.4/DFRobotUPS/__init__.py
@@ -0,0 +1,22 @@
+# DFRobotUPS.__init__
+
+
+
+__version__ = "0.4.4"
+
+
+
+from .ups import (DFRobotUPS, DEFAULT_ADDR, DEFAULT_BUS, PID, DETECT_OK,
+                  DETECT_NODEVICE, DETECT_INVALIDPID)
+
+
+
+__all__ = [
+    "DFRobotUPS",
+    "DEFAULT_ADDR",
+    "DEFAULT_BUS",
+    "PID",
+    "DETECT_OK",
+    "DETECT_NODEVICE",
+    "DETECT_INVALIDPID",
+]

c6228d2a6c6b24635601317953fdc3705748b221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-07 04:05:32 +0000

    Add package haystack-ai/2.1.0/haystack_ai-2.1.0-py3-none-any.whl

---
+++ packages/haystack-ai/haystack_ai-2.1.0-py3-none-any.whl/haystack/dataclasses/sparse_embedding.py
@@ -0,0 +1,45 @@
+from typing import List
+
+
+class SparseEmbedding:
+    """
+    Class representing a sparse embedding.
+    """
+
+    def __init__(self, indices: List[int], values: List[float]):
+        """
+        Initialize a SparseEmbedding object.
+
+        :param indices: List of indices of non-zero elements in the embedding.
+        :param values: List of values of non-zero elements in the embedding.
+
+        :raises ValueError: If the indices and values lists are not of the same length.
+        """
+        if len(indices) != len(values):
+            raise ValueError("Length of indices and values must be the same.")
+        self.indices = indices
+        self.values = values
+
+    def __eq__(self, other):
+        return self.indices == other.indices and self.values == other.values
+
+    def to_dict(self):
+        """
+        Convert the SparseEmbedding object to a dictionary.
+
+        :returns:
+            Serialized sparse embedding.
+        """
+        return {"indices": self.indices, "values": self.values}
+
+    @classmethod
+    def from_dict(cls, sparse_embedding_dict):
+        """
+        Deserializes the sparse embedding from a dictionary.
+
+        :param sparse_embedding_dict:
+            Dictionary to deserialize from.
+        :returns:
+            Deserialized sparse embedding.
+        """
+        return cls(indices=sparse_embedding_dict["indices"], values=sparse_embedding_dict["values"])
